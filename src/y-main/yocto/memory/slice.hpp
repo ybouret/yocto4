082fd8366c058b8ab6ff28c03541f1e386f376a4
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2011-06-09 07:52:51 +0000

    initial slices

--- src/y-main/yocto/memory/slice.hpp
+++ src/y-main/yocto/memory/slice.hpp
@@ -8,15 +8,48 @@ namespace yocto
 	namespace memory
 	{
 
+		//! handle a slice of memory
 		class slice
 		{
 		public:
-
+			
+			//! internal slice formating
+			struct block_t
+			{
+				union 
+				{
+					block_t *block;
+					size_t   _nil_;
+				} next, prev;
+				
+				union
+				{
+					size_t value;
+					void  *_nil_;
+				} size;
+				
+				union 
+				{
+					slice *owner;
+					size_t _nil_;
+				} info;
+			};
+			
+			static size_t blocks_for( size_t bytes ) throw();
+			slice( size_t data_size, size_t mini_size );
+			~slice() throw();
+			
+			bool is_empty() const throw();
+			
 		private:
+			size_t   count_;
+			block_t *entry_;
+			block_t *watch_;
 			YOCTO_DISABLE_COPY_AND_ASSIGN(slice);
 		};
 
 	}
+	
 }
 
 
