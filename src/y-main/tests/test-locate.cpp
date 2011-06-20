b2fe9e1c6d05b6b87953a4bbc4136a5376b5acfe
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2011-06-20 16:02:30 +0000

    initial shared_ptr

--- src/y-main/tests/test-locate.cpp
+++ src/y-main/tests/test-locate.cpp
@@ -1,5 +1,7 @@
 #include "yocto/utest/run.hpp"
 #include "yocto/core/locate.hpp"
+#include "yocto/memory/buffers.hpp"
+#include "yocto/code/rand.hpp"
 
 using namespace yocto;
 
@@ -8,61 +10,96 @@ static inline int compare( int a, int b ) throw()
 	return a-b;
 }
 
+using namespace memory;
+
 YOCTO_UNIT_TEST_IMPL(locate)
 {
-	int          tab[] = { 1, 2, 4, 7, 8 , 9 , 11, 20 };
-	const size_t num   = sizeof(tab)/sizeof(tab[0]); 
-	
-	for( size_t i=0; i < num; ++i )
 	{
-		size_t indx = 0;
-		const int target = tab[i];
-		std::cerr << "locate " << target << "..." << std::endl;
-		if( !core::locate( &target, tab, num, indx, compare ) )
+		int          tab[] = { 1, 2, 4, 7, 8 , 9 , 11, 20 };
+		const size_t num   = sizeof(tab)/sizeof(tab[0]); 
+		
+		for( size_t i=0; i < num; ++i )
 		{
-			std::cerr << "Can't find " << target << " !!!" << std::endl;
-		}
-		else
-		{
-			if( indx != i )
-				std::cerr << "Not the right position!" << std::endl;
-			else 
+			size_t indx = 0;
+			const int target = tab[i];
+			std::cerr << "locate " << target << "..." << std::endl;
+			if( !core::locate( &target, tab, num, indx, compare ) )
 			{
-				std::cerr << "@" << indx << std::endl;
+				std::cerr << "Can't find " << target << " !!!" << std::endl;
+			}
+			else
+			{
+				if( indx != i )
+					std::cerr << "Not the right position!" << std::endl;
+				else 
+				{
+					std::cerr << "@" << indx << std::endl;
+				}
+				
+			}		
+		}
+		
+		{
+			size_t indx = 0;
+			int target = -1;
+			if( !core::locate( &target, tab, num, indx, compare ) )
+			{
+				std::cerr << "should insert " << target << "@" << indx << std::endl;
 			}
 			
-		}		
+			target = 3;
+			if( !core::locate( &target, tab, num, indx, compare ) )
+			{
+				std::cerr << "should insert " << target << "@" << indx << std::endl;
+			}
+			
+			
+			target = 10;
+			if( !core::locate( &target, tab, num, indx, compare ) )
+			{
+				std::cerr << "should insert " << target << "@" << indx << std::endl;
+			}
+			
+			target = 99;
+			if( !core::locate( &target, tab, num, indx, compare ) )
+			{
+				std::cerr << "should insert " << target << "@" << indx << std::endl;
+			}
+			
+		}
+		
 	}
 	
+
 	{
-		size_t indx = 0;
-		int target = -1;
-		if( !core::locate( &target, tab, num, indx, compare ) )
+		size_t n = 20;
+		size_t j = 0;
+		buffer_of<int,global> tab( n );
+		std::cerr << "raw=";
+		for( size_t i=0; i < n; ++i )
 		{
-			std::cerr << "should insert " << target << "@" << indx << std::endl;
+			const int x    = alea_less_than<unsigned>(1000);
+			size_t    indx = 0;
+			if( !core::locate( &x, tab(), j, indx, compare ) )
+			{
+				core::insert( &x, tab(), j, indx );
+				std::cerr << " " << x;
+			}
 		}
+		std::cerr << std::endl;
 		
-		target = 3;
-		if( !core::locate( &target, tab, num, indx, compare ) )
+		std::cerr << "ord=";
+		for( size_t i=0; i < j; ++i )
 		{
-			std::cerr << "should insert " << target << "@" << indx << std::endl;
+			std::cerr << " " << *tab(i);
 		}
-		
-		
-		target = 10;
-		if( !core::locate( &target, tab, num, indx, compare ) )
-		{
-			std::cerr << "should insert " << target << "@" << indx << std::endl;
-		}
-		
-		target = 99;
-		if( !core::locate( &target, tab, num, indx, compare ) )
-		{
-			std::cerr << "should insert " << target << "@" << indx << std::endl;
-		}
+		std::cerr << std::endl;
 		
 	}
 	
 	
 }
 YOCTO_UNIT_TEST_DONE()
+
+
+
