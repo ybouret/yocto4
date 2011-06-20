33b4b8a2875f36041bb87e86c5db1596cf7fff77
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2011-06-20 08:19:13 +0000

    finished map+iterators

--- src/y-main/tests/test-map.cpp
+++ src/y-main/tests/test-map.cpp
@@ -16,13 +16,15 @@ static inline void test_map()
 {
 	typedef  map<KEY,T,KEY_HASHER,ALLOCATOR> map_t;
 	std::cerr << "map<" << typeid(KEY).name() << "," << typeid(T).name() << ">" << std::endl;
+	std::cerr << "sizeof(map)=" << sizeof(map_t) << std::endl;
 	{ map_t M; }
 	{ map_t M(1,as_capacity); }
 	
 	{
+		std::cerr << "---- generating" << std::endl;
 		map_t M;
 		vector<KEY> keys;
-		for( size_t i=0; i < 1024; ++i )
+		for( size_t i=0; i < 2048; ++i )
 		{
 			KEY key  = gen< typename map_t::mutable_key  >::get();
 			T   data = gen< typename map_t::mutable_type >::get();
@@ -38,14 +40,25 @@ static inline void test_map()
 				}
 			}
 		}
-		
+		std::cerr << "---- size=" << M.size() << "/" << M.capacity() << std::endl;
+		std::cerr << "---- copy" << std::endl;
+		map_t M2(M);
 		for( size_t i=1; i <= keys.size(); ++i )
 		{
-			if( ! M.remove( keys[i] ) )
+			if( ! M2.remove( keys[i] ) )
 			{
 				std::cerr << "Unable to remove '" << keys[i] << "' !" << std::endl;
 			}
 		}
+		
+		for( typename map_t::iterator i = M2.begin(); i != M2.end(); ++i )
+		{
+			if( !M.remove( i->key ) )
+			{
+				std::cerr << "Unable to remove '" << i->key << "' !" << std::endl;
+			}
+		}
+		
 		
 		
 	}
@@ -53,7 +66,6 @@ static inline void test_map()
 
 YOCTO_UNIT_TEST_IMPL(map)
 {
-	
 	test_map<int,int,key_hasher<int>,memory::global::allocator>();
 	test_map<const int,string,key_hasher<int,hashing::sfh>,memory::global::allocator>();
 	test_map<const string,const string,key_hasher<string,hashing::sha1>,memory::pooled::allocator>();
