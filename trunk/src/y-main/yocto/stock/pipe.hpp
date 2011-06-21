5e815d2ae1eb1581d9576b647a31e30207a38135
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2011-06-21 08:33:02 +0000

    updated stock

---
+++ trunk/src/y-main/yocto/stock/pipe.hpp
@@ -0,0 +1,105 @@
+#ifndef YOCTO_PIPE_INCLUDED
+#define YOCTO_PIPE_INCLUDED 1
+
+#include "yocto/container/stock.hpp"
+
+namespace yocto {
+	
+	enum pipe_type
+	{
+		lifo_type,
+		fifo_type
+	};
+	
+	template <
+	typename  T,
+	typename  SEQUENCE,
+	pipe_type TYPE
+	>
+	class pipe : public stock<T>
+	{
+	public:
+		YOCTO_ARGUMENTS_DECL_T;
+		
+		explicit pipe() throw() : sequence_() {}
+		virtual ~pipe() throw() {}
+		explicit pipe( size_t n, const as_capacity_t &x ) : sequence_(n,x) {}
+		
+		//--------------------------------------------------------------
+		//-- container
+		//--------------------------------------------------------------
+		
+		virtual size_t size() const throw()     { return sequence_.size();      }
+		virtual size_t capacity() const throw() { return sequence_.available(); }
+		
+		virtual void free() throw() { sequence_.free(); }
+		virtual void release() throw() { sequence_.release(); }
+		
+		virtual void reserve( size_t n ) { sequence_.reserve(n); }
+		
+		//--------------------------------------------------------------
+		//-- adaptor
+		//--------------------------------------------------------------
+		virtual void        push( param_type obj )  {   push( obj, int2type<TYPE>() ); }
+		virtual void        pop() throw()           {         pop( int2type<TYPE>() ); }
+		virtual const_type &peek() const throw()    { return peek( int2type<TYPE>() ); }
+		virtual type       &peek() throw()          { return peek( int2type<TYPE>() ); }
+	private:
+		YOCTO_DISABLE_COPY_AND_ASSIGN(pipe);
+		
+	protected:
+		SEQUENCE sequence_;
+		
+	private:
+		
+		//--------------------------------------------------------------
+		// LIFO -> think vector/stack
+		//--------------------------------------------------------------
+		inline void push( param_type obj ,int2type<lifo_type> )
+		{
+			sequence_.push_back(obj);
+		}
+		
+		inline void pop( int2type<lifo_type> ) throw()
+		{
+			sequence_.pop_back();
+		}
+		
+		inline const_type & peek( int2type<lifo_type> ) const throw()
+		{
+			return sequence_.back();
+		}
+		
+		inline type & peek( int2type<lifo_type> )  throw()
+		{
+			return sequence_.back();
+		}
+		
+		//--------------------------------------------------------------
+		// FIFO -> think list/queue
+		//--------------------------------------------------------------
+		inline void push( param_type obj ,int2type<fifo_type> )
+		{
+			sequence_.push_back(obj);
+		}
+		
+		inline void pop( int2type<fifo_type> ) throw()
+		{
+			sequence_.pop_front();
+		}
+		
+		inline const_type & peek( int2type<fifo_type> ) const throw()
+		{
+			return sequence_.front();
+		}
+		
+		inline type & peek( int2type<fifo_type> ) throw()
+		{
+			return sequence_.front();
+		}
+		
+	};
+	
+	
+}
+#endif
---
+++ trunk/src/y-main/yocto/stock/stack.hpp
@@ -0,0 +1,64 @@
+#ifndef YOCTO_STACK_INCLUDED
+#define YOCTO_STACK_INCLUDED 1
+
+
+#include "yocto/stock/pipe.hpp"
+#include "yocto/sequence/vector.hpp"
+
+namespace yocto {
+	
+	namespace hidden
+	{
+		extern const char stack_name[];
+	}
+	
+	template<
+	typename T,
+	typename SEQUENCE = vector<T>
+	>
+	class stack : public pipe<T,SEQUENCE,lifo_type>
+	{
+public:
+	YOCTO_ARGUMENTS_DECL_T;
+	
+	typedef pipe<T,SEQUENCE,lifo_type> self;
+	
+	explicit stack() throw() {}
+	virtual ~stack() throw() {}
+	explicit stack( size_t n, const as_capacity_t &x) : self(n,x)  {}
+	
+	virtual const char *name() const throw() { return hidden::stack_name; }
+	
+	type & operator[]( ptrdiff_t n ) throw()
+	{
+		return self::sequence_[ get_stack_offset(n) ];
+	}
+	
+	const_type & operator[]( ptrdiff_t n ) const throw()
+	{
+		return self::sequence_[ get_stack_offset(n) ];
+	}
+	
+private:
+	YOCTO_DISABLE_COPY_AND_ASSIGN(stack);
+	size_t get_stack_offset( ptrdiff_t n ) const throw()
+	{
+		assert( static_cast<size_t>( n < 0 ? -n : n ) <= this->size() );
+		if( n > 0 )
+		{
+			return static_cast<size_t>(n);
+		}
+		else
+		{
+			size_t   m = self::sequence_.size();
+			return ++m - static_cast<size_t>(-n);
+		}
+	}
+	
+};
+
+
+}
+
+
+#endif
