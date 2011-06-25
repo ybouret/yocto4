331d7bfa8c18f80677ce9725bd44e1e0f59779f6
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2011-06-25 12:10:38 +0000

    dictionary with iterators

---
+++ trunk/src/y-main/yocto/container/iter-dict.hpp
@@ -0,0 +1,63 @@
+#ifndef YOCTO_CONTAINER_DICT_ITER_INCLUDED
+#define YOCTO_CONTAINER_DICT_ITER_INCLUDED 1
+
+#include "yocto/container/iterator.hpp"
+#include "yocto/type-traits.hpp"
+
+namespace yocto
+{
+	
+	namespace iterating
+	{
+		
+		template <typename T,typename SLOT,direction D>
+		class dict
+		{
+		public:
+			YOCTO_ARGUMENTS_DECL_T;
+			
+			inline dict() throw()       : item_(NULL) {}
+			inline dict( SLOT **p ) throw() : item_( p )  {}
+			inline dict( const dict &other) throw() : item_(other.item_) {}
+			inline dict & operator=( const dict &other ) throw() { item_ = other.item_; return *this; }
+			
+			inline dict & operator++() throw() { this->increase( int2type<D>() );  return *this; }
+			inline dict & operator--() throw() { this->decrease( int2type<D>() );  return *this; }
+			
+			inline dict & operator+=( ptrdiff_t n ) throw()  { this->add( n, int2type<D>() ); return *this; }
+			inline dict   operator+( ptrdiff_t n ) const throw() { dict tmp( *this ); return tmp += n; }
+			
+			inline dict & operator-=( ptrdiff_t n ) throw()  { this->sub( n, int2type<D>() ); return *this; }
+			inline dict   operator-( ptrdiff_t n ) const throw() { dict tmp( *this ); return tmp -= n; }
+			
+			inline type       & operator*() throw()       { assert(item_); assert(*item_); return (*item_)->data; }
+			inline const_type & operator*() const throw() { assert(item_); assert(*item_); return (*item_)->data; }
+			
+			inline friend bool operator==( const dict &lhs, const dict &rhs ) throw() { return lhs.item_ == rhs.item_; }
+			inline friend bool operator!=( const dict &lhs, const dict &rhs ) throw() { return lhs.item_ != rhs.item_; }
+			
+
+			SLOT * operator->() throw() { assert(item_); assert(*item_); return (*item_); }
+
+		private:
+			SLOT **item_;
+			//-- forward
+			inline void increase( int2type<forward> ) throw() { ++item_; }
+			inline void decrease( int2type<forward> ) throw() { --item_; }
+			inline void add( ptrdiff_t n, int2type<forward> ) throw() { item_ += n; }
+			inline void sub( ptrdiff_t n, int2type<forward> ) throw() { item_ -= n; }
+			
+			//-- reverse
+			inline void increase( int2type<reverse> ) throw() { --item_; }
+			inline void decrease( int2type<reverse> ) throw() { ++item_; }
+			inline void add( ptrdiff_t n, int2type<reverse> ) throw() { item_ -= n; }
+			inline void sub( ptrdiff_t n, int2type<reverse> ) throw() { item_ += n; }
+			
+			
+		};
+	}
+	
+}
+
+
+#endif
---
+++ trunk/src/y-main/yocto/container/iter-handle.hpp
@@ -0,0 +1,60 @@
+#ifndef YOCTO_CONTAINER_HANDLE_ITER_INCLUDED
+#define YOCTO_CONTAINER_HANDLE_ITER_INCLUDED 1
+
+#include "yocto/container/iterator.hpp"
+#include "yocto/type-traits.hpp"
+
+namespace yocto
+{
+	
+	namespace iterating
+	{
+		
+		template <typename T,direction D>
+		class handle
+		{
+		public:
+			YOCTO_ARGUMENTS_DECL_T;
+			
+			inline handle() throw()       : item_(NULL) {}
+			inline handle( T **p ) throw() : item_( (mutable_type **)p )  {}
+			inline handle( const handle &other) throw() : item_(other.item_) {}
+			inline handle & operator=( const handle &other ) throw() { item_ = other.item_; return *this; }
+			
+			inline handle & operator++() throw() { this->increase( int2type<D>() );  return *this; }
+			inline handle & operator--() throw() { this->decrease( int2type<D>() );  return *this; }
+			
+			inline handle & operator+=( ptrdiff_t n ) throw()  { this->add( n, int2type<D>() ); return *this; }
+			inline handle   operator+( ptrdiff_t n ) const throw() { handle tmp( *this ); return tmp += n; }
+			
+			inline handle & operator-=( ptrdiff_t n ) throw()  { this->sub( n, int2type<D>() ); return *this; }
+			inline handle   operator-( ptrdiff_t n ) const throw() { handle tmp( *this ); return tmp -= n; }
+			
+			inline type       & operator*() throw()       { assert(item_); assert(*item_); return **item_; }
+			inline const_type & operator*() const throw() { assert(item_); assert(*item_); return **item_; }
+			
+			inline friend bool operator==( const handle &lhs, const handle &rhs ) throw() { return lhs.item_ == rhs.item_; }
+			inline friend bool operator!=( const handle &lhs, const handle &rhs ) throw() { return lhs.item_ != rhs.item_; }
+			
+		private:
+			mutable_type **item_;
+			//-- forward
+			inline void increase( int2type<forward> ) throw() { ++item_; }
+			inline void decrease( int2type<forward> ) throw() { --item_; }
+			inline void add( ptrdiff_t n, int2type<forward> ) throw() { item_ += n; }
+			inline void sub( ptrdiff_t n, int2type<forward> ) throw() { item_ -= n; }
+			
+			//-- reverse
+			inline void increase( int2type<reverse> ) throw() { --item_; }
+			inline void decrease( int2type<reverse> ) throw() { ++item_; }
+			inline void add( ptrdiff_t n, int2type<reverse> ) throw() { item_ -= n; }
+			inline void sub( ptrdiff_t n, int2type<reverse> ) throw() { item_ += n; }
+			
+			
+		};
+	}
+	
+}
+
+
+#endif
