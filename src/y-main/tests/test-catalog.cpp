6e42f057f98e6793a2deb94d0f839a70f002fbb6
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2011-06-28 12:54:05 +0000

    catalog+test

--- src/y-main/tests/test-catalog.cpp
+++ src/y-main/tests/test-catalog.cpp
@@ -1,11 +1,66 @@
 #include "yocto/utest/run.hpp"
 #include "yocto/ordered/catalog.hpp"
+#include "yocto/sequence/vector.hpp"
+#include "support.hpp"
+
+#include <typeinfo>
 
 using namespace yocto;
 
 
+template <typename T,typename ALLOCATOR>
+void test_catalog()
+{
+	YOCTO_ARGUMENTS_DECL_T;
+	(void) type_size;
+
+	typedef catalog<T,comparator<mutable_type>,ALLOCATOR> cat_t;
+	std::cerr << "catalog<" << typeid(T).name() << ">" << std::endl;
+	
+	cat_t C;
+	vector<const_type> objs;
+	
+	for( size_t i=0; i < 4096; ++i )
+	{
+		T tmp = gen<mutable_type>::get();
+		if( C.insert( tmp ) )
+		{
+			if( alea<float>() > 0.6f ) 
+			{
+				objs.push_back(tmp);
+			}
+		}
+	}
+	
+	cat_t C2( C );
+	for( size_t i=1; i <= objs.size(); ++i )
+	{
+		if( ! C.search(objs[i]) )
+			std::cerr << "search error level-1" << std::endl;
+		
+		if( ! C2.search( objs[i] ) )
+			std::cerr << "search error level-2" << std::endl;
+		
+		if( !C2.remove( objs[i] ) )
+			std::cerr << "remove error level-3" << std::endl;
+	}
+	
+	for( typename cat_t::iterator i = C2.begin(); i != C2.end() ; ++i )
+	{
+		if( ! C.remove( *i ) )
+		{
+			std::cerr << "remove error level-4" << std::endl;
+		}
+	}
+	
+	
+}
+
 YOCTO_UNIT_TEST_IMPL(catalog)
 {
-	catalog<int> C;
+
+	test_catalog<int,memory::global::allocator>();
+	test_catalog<string,memory::global::allocator>();
+
 }
 YOCTO_UNIT_TEST_DONE()
