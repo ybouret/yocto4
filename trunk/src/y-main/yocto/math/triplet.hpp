6fe1212d5770970b4f1a79f59d2210b801657dd5
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2011-06-28 15:59:35 +0000

    added triplet.hpp

---
+++ trunk/src/y-main/yocto/math/triplet.hpp
@@ -0,0 +1,33 @@
+/** \file triplet.hpp
+*/
+
+#ifndef _YOCTO_TRIPLET_HPP_
+#define _YOCTO_TRIPLET_HPP_
+
+#include "yocto/math/types.hpp"
+#include <iosfwd>
+
+namespace yocto {
+
+	namespace math {
+
+		template <class T>
+		struct triplet {
+			T a,b,c;
+
+			void to_scilab( std::ostream &os ) const;
+
+			friend
+			inline std::ostream & operator<<( std::ostream &os, const triplet &x)
+			{
+				x.to_scilab(os);
+				return os;
+			}
+			
+			T       &operator[]( size_t index ) throw();
+			const T &operator[]( size_t index ) const throw();
+		};
+
+	}
+}
+#endif
