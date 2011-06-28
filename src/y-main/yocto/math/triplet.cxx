7ca0106b59434901911276a702b15a01d1e8cf4e
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2011-06-28 15:59:35 +0000

    added triplet.hpp

---
+++ src/y-main/yocto/math/triplet.cxx
@@ -0,0 +1,37 @@
+#include "yocto/math/triplet.hpp"
+#include "yocto/math/ztype.hpp"
+
+#include <iostream>
+
+namespace yocto {
+
+	namespace math
+	{
+
+
+		template <>
+		real_t & triplet<real_t>:: operator[]( size_t index ) throw()
+		{
+			assert( index >= 1 );
+			assert( index <= 3 );
+			return *(((&a)-1)+index);
+		}
+
+		template <>
+		const real_t & triplet<real_t>:: operator[]( size_t index ) const throw()
+		{
+			assert( index >= 1 );
+			assert( index <= 3 );
+			return *(((&a)-1)+index);
+		}
+
+
+		template <>
+		void triplet<real_t>::to_scilab( std::ostream &os ) const
+		{
+			os << "[ " << a << " " << b << " " << c << " ]'";
+		}
+
+	}
+
+}
