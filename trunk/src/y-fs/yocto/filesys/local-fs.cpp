d37144ac6aa51f209de743c14989ab6e4f5de3fe
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2011-07-17 10:57:28 +0000

    fs+math tests

--- trunk/src/y-fs/yocto/filesys/local-fs.cpp
+++ trunk/src/y-fs/yocto/filesys/local-fs.cpp
@@ -86,6 +86,7 @@ namespace yocto
 		void local_fs:: create_dir(  const string &dirname, const bool allow_already_exists )
 		{
 #if defined(YOCTO_BSD)
+			YOCTO_GIANT_LOCK();
 			if( mkdir( &dirname[0],  (S_IRUSR | S_IWUSR | S_IXUSR) | (S_IRGRP | S_IXGRP ) ) < 0 ) {
 				
 				const int err = errno;
@@ -100,12 +101,13 @@ namespace yocto
 #endif
 			
 #if defined(YOCTO_WIN)
+			YOCTO_GIANT_LOCK();
 			const DWORD attr = ::GetFileAttributes( &dirname[0] );
 			
 			if( (attr != INVALID_FILE_ATTRIBUTES) && ( (attr & FILE_ATTRIBUTE_DIRECTORY) != 0 ) ) {
 				if( !allow_already_exists  )
 				{
-					throw windows::exception( ERROR_ALREADY_EXISTS, "create_dir(%s)", &dirname[0] );
+					throw win32::exception( ERROR_ALREADY_EXISTS, "create_dir(%s)", &dirname[0] );
 				}
 				else
 					return;
@@ -113,7 +115,7 @@ namespace yocto
 			
 			if( ! ::CreateDirectory( &dirname[0], NULL ) )
 			{
-				throw windows::exception( ::GetLastError(), "create_dir(%s)", &dirname[0] );
+				throw win32::exception( ::GetLastError(), "create_dir(%s)", &dirname[0] );
 			}
 #endif
 			
@@ -121,6 +123,7 @@ namespace yocto
 		
 		void local_fs:: remove_dir(  const string &dirname   )
 		{
+			YOCTO_GIANT_LOCK();
 #if defined(YOCTO_BSD)
 			if( rmdir( &dirname[0] ) != 0 ) {
 				throw libc::exception( errno, "remove_dir(%s)", &dirname[0] );
@@ -129,13 +132,14 @@ namespace yocto
 			
 #if defined(YOCTO_WIN)
 			if(! ::RemoveDirectory( &dirname[0] ) ) {
-				throw windows::exception( ::GetLastError(), "remove_dir(%s)", &dirname[0] );
+				throw win32::exception( ::GetLastError(), "remove_dir(%s)", &dirname[0] );
 			}
 #endif
 		}
 		
 		void local_fs:: remove_file( const string &filename )
 		{
+			YOCTO_GIANT_LOCK();
 #if defined(YOCTO_BSD)
 			if( unlink( &filename[0] ) != 0 )
 			{
@@ -146,7 +150,7 @@ namespace yocto
 #if defined(YOCTO_WIN)
 			if( ! ::DeleteFile( &filename[0] ) )
 			{
-				throw windows::exception( ::GetLastError(), "::DeleteFile(%s)", &filename[0] );
+				throw win32::exception( ::GetLastError(), "::DeleteFile(%s)", &filename[0] );
 			}
 #endif
 		}
---
+++ trunk/src/y-mk/tests/test-drvs.cpp
@@ -0,0 +1,68 @@
+#include "yocto/utest/run.hpp"
+#include "yocto/math/fcn/integrate.hpp"
+#include "yocto/math/fcn/derivative.hpp"
+#include "yocto/ios/ocstream.hpp"
+#include <typeinfo>
+
+using namespace yocto;
+using namespace math;
+
+namespace {
+	
+	template <typename T>
+	static inline T F(T x )
+	{
+		return Sin(x);
+	}
+	
+	template <typename T>
+	class dummy
+	{
+	public:
+		dummy()   : drvs(), func( cfunctor( F<T> ) ) {}
+		~dummy() throw() {}
+		
+		T compute( T x )
+		{
+			const T ans = drvs( func, x, 1e-4 );
+			return ans;
+		}
+		
+	private:
+		YOCTO_DISABLE_COPY_AND_ASSIGN(dummy);
+		derivative<T>                 drvs;
+		typename numeric<T>::function func;
+	};
+	
+	template <typename T>
+	static inline void test_drvs()
+	{
+		std::cerr << "drvs/intg" << std::endl;
+		const T      xmax = numeric<T>::two_pi;
+		const size_t N = 128;
+		integrate<T> intg = { 2*numeric<T>::sqrteps };
+		dummy<T>     dum;
+		typename numeric<T>::function dF( &dum, & dummy<T>::compute );
+		const string out = string("drvs-") + typeid(T).name() + ".txt";
+		ios::ocstream fp( out, false );
+		T z = 0;
+		fp("%g %g\n", 0.0, z );
+		for( size_t i=1; i <= N; ++i )
+		{
+			const T x1 = (i*xmax)/N;			
+			const T x0 = ( (i-1) * xmax)/N; 
+			std::cerr << x0 << " -> " << x1 << std::endl;
+			const T y = intg( x0, x1, dF );
+			z += y;
+			std::cerr << "\t" << z << " / " << F<T>(x1) << std::endl;
+			fp("%g %g\n", x1, z );
+		}
+	}
+}
+
+YOCTO_UNIT_TEST_IMPL(drvs)
+{
+	//test_drvs<float>();
+	test_drvs<double>();
+}
+YOCTO_UNIT_TEST_DONE()
