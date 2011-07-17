ee6121cae7340c1aeddc0fa50d4d40022c33ef39
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2011-07-17 20:53:23 +0000

    tcp_server test

---
+++ src/y-main/yocto/ios/iflux.cpp
@@ -0,0 +1,60 @@
+#include "yocto/ios/iflux.hpp"
+#include "yocto/exceptions.hpp"
+
+#include <cerrno>
+
+
+
+namespace yocto
+{
+	namespace ios
+	{
+		iflux:: ~iflux() throw()
+		{}
+		
+		iflux:: iflux( ios::ichannel &ch, memory::rw_buffer &buff )  :
+		in_( ch ),
+		init_( static_cast<char *>( buff.rw() ) ),
+		curr_( init_ ),
+		size_( buff.length() ),
+		last_( init_  )
+		{
+			if( size_ <= 0 ) throw libc:: exception( ENOMEM, "iflux(empty buffer)" );
+		}
+		
+		bool iflux:: query( char &C )
+		{
+			if( curr_ >= last_ && ! fetch() )
+				return false;
+			else 
+			{
+				C = *(curr_++);
+				return true;
+			}
+			
+		}
+		
+		bool iflux:: fetch()
+		{
+			last_ = curr_ = init_;
+			size_t  done  = 0;
+			in_.get( init_, size_, done );
+			if( done > 0 )
+			{
+				last_ = init_ + done;
+				return true;
+			}
+			else
+				return false;
+		}
+		
+		void iflux:: store( char C )
+		{
+			if( curr_ <= init_ )
+				throw exception( "iflux.store(IMPOSSIBLE)" );
+			*(--curr_) = C;
+		}
+		
+	}
+	
+}
---
+++ src/y-main/yocto/ios/iflux.hpp
@@ -0,0 +1,32 @@
+#ifndef YOCTO_IFLUX_INCLUDED
+#define YOCTO_IFLUX_INCLUDED 1
+
+#include "yocto/ios/istream.hpp"
+#include "yocto/memory/buffer.hpp"
+
+namespace yocto
+{
+	
+	namespace ios
+	{
+		class iflux : public istream
+		{
+		public:
+			virtual ~iflux() throw();
+			explicit iflux( ios::ichannel &ch, memory::rw_buffer &buff );
+			
+			virtual bool query( char &C );
+			virtual void store( char C );
+			
+		private:
+			ios::ichannel  &in_;
+			char           *init_;
+			char           *curr_;
+			const size_t    size_;
+			const char     *last_;
+			bool            fetch();
+			YOCTO_DISABLE_COPY_AND_ASSIGN(iflux);
+		};
+	}
+}
+#endif
