d1ed84da6b1952fa09fef05416e4818f04d1da18
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2011-07-17 14:41:21 +0000

    net resolve

---
+++ trunk/src/y-net/tests/test-resolve.cpp
@@ -0,0 +1,45 @@
+#include "yocto/utest/run.hpp"
+#include "yocto/net/ip-address.hpp"
+#include "yocto/net/network.hpp"
+
+using namespace yocto;
+using namespace network;
+
+YOCTO_UNIT_TEST_IMPL(resolve)
+{
+	net &nt  = net::instance();
+	const string name = argc > 1 ? argv[1] : "localhost";
+	std::cerr << "resolving '" << name << "'" << std::endl;
+	
+	try {
+		IPv4address addr4;
+		nt.resolve( addr4, name);
+		std::cerr << "addr4=" << addr4 << std::endl;
+	}
+	catch( const exception &e )
+	{
+		std::cerr << e.what() << std::endl;
+		std::cerr << e.when() << std::endl;
+	}
+	catch (...) {
+		std::cerr << "unhandled exception!" << std::endl;
+	}
+	
+	try {
+		IPv6address addr6;
+		nt.resolve( addr6, name);
+		std::cerr << "addr6=" << addr6 << std::endl;
+	}
+	catch( const exception &e )
+	{
+		std::cerr << e.what() << std::endl;
+		std::cerr << e.when() << std::endl;
+	}
+	catch (...) {
+		std::cerr << "unhandled exception!" << std::endl;
+	}
+	
+	
+	
+}
+YOCTO_UNIT_TEST_DONE()
