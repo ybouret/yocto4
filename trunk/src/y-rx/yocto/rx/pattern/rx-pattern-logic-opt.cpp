3e2251f6d3b7ee03a1f6dd4b763652fb848cc60d
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2011-07-24 06:15:57 +0000

    added rx/optimize

---
+++ trunk/src/y-rx/yocto/rx/pattern/rx-pattern-logic-opt.cpp
@@ -0,0 +1,74 @@
+#include "yocto/rx/pattern/logic.hpp"
+#include "yocto/rx/source.hpp"
+
+namespace yocto
+{
+
+	namespace regex
+	{
+
+		namespace logical
+		{
+
+			void AND:: optimize() throw()
+			{
+				p_list tmp;
+				while( operands.size )
+				{
+					pattern *p = operands.pop_front();
+					switch( p->type )
+					{
+					case AND::id:
+						assert(p->data);
+						static_cast<AND *>(p->data)->optimize();
+						tmp.merge_back( static_cast<AND *>(p->data)->operands );
+						delete p;
+						break;
+
+					case OR::id:
+						assert(p->data);
+						static_cast<OR *>(p->data)->optimize();
+						tmp.push_back( p );
+						break;
+
+					default:
+						tmp.push_back( p );
+					}
+				}
+				mswap(tmp,operands);
+			}
+
+
+			void OR:: optimize() throw()
+			{
+				p_list tmp;
+				while( operands.size )
+				{
+					pattern *p = operands.pop_front();
+					switch( p->type )
+					{
+					case AND::id:
+						assert(p->data);
+						static_cast<AND *>(p->data)->optimize();
+						tmp.push_back( p );
+						break;
+
+					case OR::id:
+						assert(p->data);
+						static_cast<OR *>(p->data)->optimize();
+						tmp.merge_back( static_cast<AND *>(p->data)->operands );
+						delete p;
+						break;
+
+					default:
+						tmp.push_back( p );
+					}
+				}
+				mswap(tmp,operands);
+			}
+
+		}
+
+	}
+
+}
