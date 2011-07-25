128c5b9b3fb934b4a462e359d7c733953d3d092e
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2011-07-25 07:47:03 +0000

    code cleaning, much more to do...

--- src/share/rules.cmake
+++ src/share/rules.cmake
@@ -62,11 +62,11 @@ ENDIF()
 IF( "${CC_NAME}" MATCHES "icc.*" )
   MESSAGE( STATUS "Using Intel compilers" )
   
-  SET(COMMON_C_FLAGS        "-Wall -pipe ${MY_CFLAGS}" )
+  SET(COMMON_C_FLAGS        "-Wall -pipe -wd981 ${MY_CFLAGS}" )
   SET(CMAKE_C_FLAGS_DEBUG   "${COMMON_C_FLAGS} -g" )
   SET(CMAKE_C_FLAGS_RELEASE "${COMMON_C_FLAGS} -O2 -DNDEBUG=1")
   
-  SET(COMMON_CXX_FLAGS        "-Wall -pipe -fexceptions${MY_CXXFLAGS}" )
+  SET(COMMON_CXX_FLAGS        "-Wall -pipe -wd981 -fexceptions${MY_CXXFLAGS}" )
   SET(CMAKE_CXX_FLAGS_DEBUG   "${COMMON_CXX_FLAGS} -g" )
   SET(CMAKE_CXX_FLAGS_RELEASE "${COMMON_CXX_FLAGS} -O2 -DNDEBUG=1")
 ENDIF()
