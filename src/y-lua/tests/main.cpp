afad8120e7ef46c4a916ff5a9797bd632b43f7d6
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2011-07-27 08:54:14 +0000

    standalone lua

---
+++ src/y-lua/tests/main.cpp
@@ -0,0 +1,38 @@
+#include "yocto/lua/lua.hpp"
+#include "yocto/exceptions.hpp"
+
+#include <iostream>
+#include <cerrno>
+
+using namespace yocto;
+
+int main( int argc, char *argv[] )
+{
+	lua_State *L = NULL;
+	try 
+	{
+		L = lua_open();
+		if( !L )
+			throw libc::exception( ENOMEM, "lua_open");
+		luaL_openlibs(L);
+		
+		for( int i=1; i < argc; ++i )
+		{
+			const char *filename = argv[i];
+			if( luaL_dofile( L, filename ) )
+			{
+				throw imported::exception( lua_tostring(L,-1), "lua_dofile(%s)", filename );
+			}
+		}
+		
+		lua_close(L);
+	}
+	catch( const exception &e )
+	{
+		std::cerr << e.what() << std::endl;
+		std::cerr << e.when() << std::endl;
+		lua_close(L);
+		return -1;
+	}
+	return 0;
+}
