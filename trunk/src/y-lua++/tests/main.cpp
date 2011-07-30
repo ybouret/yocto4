8a12b8bc3d9d6b719559679ba8cf487899f396dd
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2011-07-30 07:12:12 +0000

    lua++/tests

--- trunk/src/y-lua++/tests/main.cpp
+++ trunk/src/y-lua++/tests/main.cpp
@@ -1,7 +1,41 @@
-#include "yocto/utest/driver.hpp"
+#include "yocto/lua/lua-state.hpp"
+#include "yocto/lua/lua-config.hpp"
 
-YOCTO_UNIT_TEST_INIT(16)
+#include "yocto/lua/x/lua-array.hpp"
+#include "yocto/lua/x/lua-vec3.hpp"
+
+#include "yocto/lua/lua-bind.hpp"
+
+#include "yocto/exceptions.hpp"
+
+#include <iostream>
+#include <cerrno>
+
+using namespace yocto;
+
+int main( int argc, char *argv[] )
 {
-	//YOCTO_UNIT_TEST_DECL(compiler);
+	
+	
+	try 
+	{
+		Lua::State vm;
+		lua_State *L = vm();
+		Lua::Require< Lua::array >( L );
+		Lua::Require< Lua::vec3  >( L );
+
+		for( int i=1; i < argc; ++i )
+		{
+			const char *filename = argv[i];
+			Lua::Config::DoFile( L, filename );
+		}
+		
+	}
+	catch( const exception &e )
+	{
+		std::cerr << e.what() << std::endl;
+		std::cerr << e.when() << std::endl;
+		return -1;
+	}
+	return 0;
 }
-YOCTO_UNIT_TEST_EXEC()
