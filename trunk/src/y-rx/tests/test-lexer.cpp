6aa99d32f0766727df0d9b7830eac3a325e52c15
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2011-08-25 07:19:30 +0000

    removed lexer

--- trunk/src/y-rx/tests/test-lexer.cpp
+++ trunk/src/y-rx/tests/test-lexer.cpp
@@ -1,11 +1,9 @@
 #include "yocto/utest/run.hpp"
-#include "yocto/rx/lexer.hpp"
 
 using namespace yocto;
 
 
 YOCTO_UNIT_TEST_IMPL(lexer)
 {
-	std::cerr << "sizeof(regex::lexical::engine)= " << sizeof(regex::lexical::engine) << std::endl;
 }
 YOCTO_UNIT_TEST_DONE()
