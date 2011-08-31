9a31381fd44d8321a21a5c4c95394d98c222f1ad
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2011-08-31 07:27:29 +0000

    updated CMake...

---
+++ src/share/fltk.cmake
@@ -0,0 +1,129 @@
+########################################################################
+##
+## Simplified FLTK configuration, based on fltk-config
+##
+########################################################################
+
+MESSAGE( STATUS "<FLTK>" )
+
+########################################################################
+## fltk-config and fluid
+########################################################################
+SET(FLTK_BIN $ENV{FLTK_BIN} CACHE STRING "Overide fltk-config location" )
+IF( "" STREQUAL "${FLTK_BIN}" )
+	
+	FIND_PROGRAM( FLTK-CONFIG fltk-config )
+	IF( FLTK-CONFIG-NOTFOUND )
+		MESSAGE( FATAL_ERROR "Can't find fltk-config" )
+	ENDIF(FLTK-CONFIG-NOTFOUND)
+	
+	FIND_PROGRAM( FLUID fluid )
+	IF( FLUID-NOTFOUND )
+		MESSAGE( FATAL_ERROR "Can't find fluid" )
+	ENDIF(FLUID-NOTFOUND)
+
+ELSE()
+
+	SET(FLTK-CONFIG "${FLTK_BIN}/fltk-config" )
+	SET(FLUID       "${FLTK_BIN}/fluid"       )
+	
+ENDIF()
+
+MESSAGE( STATUS "  @FLTK fltk-config='${FLTK-CONFIG}'" )
+MESSAGE( STATUS "  @FLTK fluid='${FLUID}" )
+
+########################################################################
+## Compilation
+########################################################################
+
+#-----------------------------------------------------------------------
+# get cxxflags 
+#-----------------------------------------------------------------------
+MESSAGE( STATUS "  @FLTK query cxxflags..." )
+#EXECUTE_PROCESS( COMMAND "bash ${FLTK-CONFIG} --cxxflags" OUTPUT_VARIABLE FLTK-CXXFLAGS ERROR_VARIABLE FLTK-BUG OUTPUT_STRIP_TRAILING_WHITESPACE )
+EXEC_PROGRAM( bash ARGS ${FLTK-CONFIG} --cxxflags OUTPUT_VARIABLE FLTK-CXXFLAGS)
+MESSAGE( STATUS "  @FLTK-CXXFLAGS='${FLTK-CXXFLAGS}'" )
+
+#-----------------------------------------------------------------------
+# extract include directories
+#-----------------------------------------------------------------------
+STRING( REGEX MATCHALL "[-][I]([^ ;])+" FLTK-INCLUDES ${FLTK-CXXFLAGS} )
+STRING( REPLACE  "-I" "" FLTK-INCLUDES "${FLTK-INCLUDES}")
+MESSAGE( STATUS "  @FLTK-INCLUDES='${FLTK-INCLUDES}'" )
+INCLUDE_DIRECTORIES(${FLTK-INCLUDES})
+
+#-----------------------------------------------------------------------
+# extract definitions
+#-----------------------------------------------------------------------
+STRING( REGEX MATCHALL "[-][D]([^ ;])+" FLTK-DEFINES ${FLTK-CXXFLAGS} )
+MESSAGE( STATUS "  @FLTK-DEFINES='${FLTK-DEFINES}'")
+ADD_DEFINITIONS( ${FLTK-DEFINES} )
+
+#-----------------------------------------------------------------------
+# extract FLTK link directory
+#-----------------------------------------------------------------------
+EXEC_PROGRAM( bash ARGS ${FLTK-CONFIG} --ldflags OUTPUT_VARIABLE FLTK-LDFLAGS)
+MESSAGE( STATUS "  @FLTK-LDFLAGS='${FLTK-LDFLAGS}'")
+STRING( REGEX MATCHALL "[-][L]([^ ;])+" FLTK-LINK-DIR ${FLTK-LDFLAGS} )
+STRING( REPLACE  "-L" "" FLTK-LINK-DIR "${FLTK-LINK-DIR}")
+MESSAGE( STATUS "  @FLTK-LINK-DIR='${FLTK-LINK-DIR}'" )
+LINK_DIRECTORIES( ${FLTK-LINK-DIR} )
+
+########################################################################
+## Linking
+########################################################################
+## Link with optional FLTK libraries
+MACRO(FLTK_LINK_TO THE_TARGET)
+
+	#initialize arguments for fltk-config
+	MESSAGE( STATUS "${THE_TARGET} will use FLTK libraries" )
+	SET(_fltk_ldflags "--ldflags" )
+
+	#collect extra arguments (images,gl,...)
+	FOREACH( extra ${ARGN} )
+		SET(_fltk_ldflags "${_fltk_ldflags} --use-${extra}" )
+	ENDFOREACH( extra ${ARGN} )
+
+	#get all args
+	MESSAGE( STATUS "(with 'fltk-config ${_fltk_ldflags}')" )
+	EXEC_PROGRAM( bash ARGS ${FLTK-CONFIG} ${_fltk_ldflags} OUTPUT_VARIABLE FLTK-LDFLAGS)
+	#MESSAGE( STATUS "FLTK-LDFLAGS='${FLTK-LDFLAGS}'")
+
+	#extract libraries, link directories is already set
+	STRING( REGEX MATCHALL "([-][l]([^ ;])+)|(-framework ([^ ;])+)" FLTK-LIBS ${FLTK-LDFLAGS})
+	STRING( REPLACE  "-l" "" FLTK-LIBS "${FLTK-LIBS}")
+	MESSAGE( STATUS "${THE_TARGET} needs '${FLTK-LIBS}'")
+	
+	#and declare libraries to be linked !
+	#SET_TARGET_PROPERTIES( ${THE_TARGET} PROPERTIES LINK_FLAGS "${FLTK-LDFLAGS}" )
+	TARGET_LINK_LIBRARIES(${THE_TARGET} ${FLTK-LIBS})
+ENDMACRO(FLTK_LINK_TO)
+
+########################################################################
+## fluid compiler API
+## FLUID_UIC(UI [PATH]) will produce UI_SOURCE UI_HEADER UI_SOURCES
+########################################################################
+MACRO(FLUID_UIC THE_UI)
+	SET(_UI_FL     "${CMAKE_CURRENT_SOURCE_DIR}/${THE_UI}.fl" )
+	SET(_UI_SOURCE "${CMAKE_CURRENT_BINARY_DIR}/${THE_UI}.cxx")
+	SET(_UI_HEADER "${CMAKE_CURRENT_BINARY_DIR}/${THE_UI}.h")
+	MESSAGE( STATUS "${THE_UI} will compile"   )
+	MESSAGE( STATUS "fluid : ${_UI_FL}"     )
+	MESSAGE( STATUS "source: ${_UI_SOURCE}" )
+	MESSAGE( STATUS "header: ${_UI_HEADER}" )
+	
+	#create the command
+	ADD_CUSTOM_COMMAND( OUTPUT  ${_UI_SOURCE} ${_UI_HEADER}
+	                    COMMAND ${FLUID} -c -o ${_UI_SOURCE} -h ${_UI_HEADER} ${_UI_FL} 
+						DEPENDS ${_UI_FL} )
+	
+	#register the output directory	
+	INCLUDE_DIRECTORIES( ${CMAKE_CURRENT_BINARY_DIR} )
+
+	#build the global variables
+	SET( ${THE_UI}_SOURCE  ${_UI_SOURCE} )
+	SET( ${THE_UI}_HEADER  ${_UI_HEADER} )
+	SET( ${THE_UI}_SOURCES ${_UI_SOURCE} ${_UI_HEADER})
+ENDMACRO(FLUID_UIC)
+
+MESSAGE( STATUS "</FLTK>" )
---
+++ src/share/opencl.cmake
@@ -0,0 +1,135 @@
+########################################################################
+#
+# add OpenCL include path and peprare the OPENCL_LINK_TO macro
+#
+########################################################################
+
+SET( OPENCL_HAS_SDK OFF )
+SET( OPENCL_ON_DARWIN OFF )
+
+IF(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+	SET(OPENCL_ON_DARWIN ON)
+ENDIF(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+
+SET( OPENCL_ON_LINUX  OFF )
+IF(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+	SET(OPENCL_ON_LINUX ON)
+ENDIF(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+
+SET( OPENCL_ON_WIN32 WIN32 )
+
+IF(OPENCL_ON_DARWIN)
+	MESSAGE( STATUS "[OpenCL] Using Apple System")
+	SET( OPENCL_HAS_SDK ON )
+	####################################################################
+	##
+	## Mac OS X >= 10.6 Settings
+	##
+	####################################################################
+
+	#-------------------------------------------------------------------
+	# CL/OpenCL.h is in the path
+	#-------------------------------------------------------------------
+	
+	#-------------------------------------------------------------------
+	# Use OpenCL framework
+	#-------------------------------------------------------------------
+	MACRO( OPENCL_LINK_TO tgt )
+		MESSAGE( STATUS "${tgt} will use OpenCL (Apple)" )
+		TARGET_LINK_LIBRARIES( ${tgt} "-framework OpenCL")
+	ENDMACRO(OPENCL_LINK_TO)
+	
+ENDIF(OPENCL_ON_DARWIN)
+	
+IF( NOT OPENCL_HAS_SDK )
+	####################################################################
+	#
+	# Look for ATI Stream SDK, any other platform
+	#
+	####################################################################
+	SET( ATISTREAMSDKROOT $ENV{ATISTREAMSDKROOT} )
+	SET( OPENCL_HAS_ATI ON )
+	IF( "${ATISTREAMSDKROOT}" STREQUAL "" )
+	SET( OPENCL_HAS_ATI OFF )
+	ENDIF( "${ATISTREAMSDKROOT}" STREQUAL "" )
+	
+	IF( OPENCL_HAS_ATI )
+		MESSAGE(STATUS "Found ATI" )
+		SET( OPENCL_HAS_SDK ON )
+		#---------------------------------------------------------------
+		# register the include path
+		#---------------------------------------------------------------
+		INCLUDE_DIRECTORIES( "${ATISTREAMSDKROOT}/include" )
+		IF( FORCE64 )
+			MESSAGE( STATUS "OpenCL 64 bits" )
+			LINK_DIRECTORIES( "${ATISTREAMSDKROOT}/lib/x86_64" )
+		ELSE( FORCE64 )
+			MESSAGE( STATUS "OpenCL 32 bits" )
+			LINK_DIRECTORIES( "${ATISTREAMSDKROOT}/lib/x86" )
+		ENDIF( FORCE64 )
+		
+		#---------------------------------------------------------------
+		# Use OpenCL 
+		#---------------------------------------------------------------
+		MACRO( OPENCL_LINK_TO tgt )
+			MESSAGE( STATUS "${tgt} will use OpenCL (ATI Stream SDK)" )
+			TARGET_LINK_LIBRARIES( ${tgt} "OpenCL")
+		ENDMACRO(OPENCL_LINK_TO)
+	ENDIF( OPENCL_HAS_ATI )
+ENDIF( NOT OPENCL_HAS_SDK )
+
+IF( NOT OPENCL_HAS_SDK AND WIN32 )
+	#####################################################################
+	##
+	## Try to Detect Windows CUDA OpenCL, since ATI is not detected
+	##
+	#####################################################################
+	SET(OPENCL_INCLUDE_PATH "$ENV{CUDA_INC_PATH}" )
+	SET(OPENCL_LIBRARY_PATH "$ENV{CUDA_LIB_PATH}" )
+	SET(OPENCL_HAS_NVIDIA ON)
+	IF( ${CMAKE_SYSTEM_NAME} MATCHES "Linux"  OR "${OPENCL_INCLUDE_PATH}" STREQUAL "" )
+	SET(OPENCL_HAS_NVIDIA OFF)
+	ENDIF( ${CMAKE_SYSTEM_NAME} MATCHES "Linux"  OR "${OPENCL_INCLUDE_PATH}" STREQUAL "" )
+	
+	IF( OPENCL_HAS_NVIDIA )
+		SET(OPENCL_HAS_SDK ON)
+		INCLUDE_DIRECTORIES( "${OPENCL_INCLUDE_PATH}" )
+		LINK_DIRECTORIES(    "${OPENCL_LIBRARY_PATH}" )
+		MACRO( OPENCL_LINK_TO tgt )
+			MESSAGE( STATUS "${tgt} will use OpenCL (nVidia CUDA/Windows)" )
+			TARGET_LINK_LIBRARIES( ${tgt} "OpenCL")
+		ENDMACRO(OPENCL_LINK_TO)
+	ENDIF( OPENCL_HAS_NVIDIA ) 
+		
+		
+ENDIF( NOT OPENCL_HAS_SDK AND WIN32 )
+
+
+IF( NOT OPENCL_HAS_SDK AND OPENCL_ON_LINUX )
+	#####################################################################
+	##
+	##Looking for system OpenCL	
+	##
+	#####################################################################
+	MESSAGE( STATUS "Looking for system OpenCL" )
+	FIND_FILE(CL-H cl.h PATHS /usr/include/CL /usr/local/cuda/include/CL)
+	MESSAGE( STATUS "cl.h=${CL-H}" )
+	IF( NOT "${CL-H}" STREQUAL "CL-H-NOTFOUND"  )
+		MESSAGE( STATUS "found system cl.h"  )
+		GET_FILENAME_COMPONENT(OPENCL_INCLUDE_CL ${CL-H} PATH )
+		GET_FILENAME_COMPONENT(OPENCL_INCLUDE    ${OPENCL_INCLUDE_CL} PATH )
+		MESSAGE( STATUS "in ${OPENCL_INCLUDE}" )	
+		SET(OPENCL_HAS_SDK ON )
+		INCLUDE_DIRECTORIES( ${OPENCL_INCLUDE} )
+		MACRO( OPENCL_LINK_TO tgt )
+			MESSAGE( STATUS "${tgt} will use OpenCL (Linux/System)" )
+			TARGET_LINK_LIBRARIES( ${tgt} "OpenCL")
+		ENDMACRO(OPENCL_LINK_TO)
+	ENDIF(  NOT "${CL-H}" STREQUAL "CL-H-NOTFOUND" )
+		
+ENDIF( NOT OPENCL_HAS_SDK AND OPENCL_ON_LINUX )
+
+
+IF( NOT OPENCL_HAS_SDK )
+  MESSAGE( FATAL_ERROR "Unknown OpenCL SDK" )
+ENDIF( NOT OPENCL_HAS_SDK )
