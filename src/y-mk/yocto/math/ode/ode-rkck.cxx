3f9dce079ce255cde9d668f475f3df7af409309d
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2011-09-20 15:43:21 +0000

    typo updated

--- src/y-mk/yocto/math/ode/ode-rkck.cxx
+++ src/y-mk/yocto/math/ode/ode-rkck.cxx
@@ -1,8 +1,6 @@
 #include "yocto/math/ode/rkck.hpp"
 #include "yocto/math/ztype.hpp"
 
-#include <iostream>
-
 namespace yocto
 {
 	namespace math
@@ -38,19 +36,17 @@ namespace yocto
 										   const array<real_t>  &dydx 
 										   )
 			{
-				static const real_t a2=0.2,a3=0.3,a4=0.6,a5=1.0,a6=0.875,b21=0.2,
-				b31=3.0/40.0,b32=9.0/40.0,b41=0.3,b42 = -0.9,b43=1.2,
-				b51 = -11.0/54.0, b52=2.5,b53 = -70.0/27.0,b54=35.0/27.0,
-				b61=1631.0/55296.0,b62=175.0/512.0,b63=575.0/13824.0,
-				b64=44275.0/110592.0,b65=253.0/4096.0,c1=37.0/378.0,
-				c3=250.0/621.0,c4=125.0/594.0,c6=512.0/1771.0,
-				dc5 = -277.00/14336.0;
-				static const real_t dc1=c1-2825.0/27648.0,dc3=c3-18575.0/48384.0,
-				dc4=c4-13525.0/55296.0,dc6=c6-0.25;
+				static const real_t a2=0.2, a3=0.3, a4=0.6, a5=1.0, a6=0.875;
+				static const real_t b21 = 0.2;
+				static const real_t b31 = 3.0/40.0,        b32=9.0/40.0;
+				static const real_t b41 = 0.3,             b42 = -0.9,      b43=1.2;
+				static const real_t b51 = -11.0/54.0,      b52=2.5,         b53 = -70.0/27.0,  b54=35.0/27.0;
+				static const real_t b61 = 1631.0/55296.0,  b62=175.0/512.0, b63=575.0/13824.0, b64=44275.0/110592.0, b65=253.0/4096.0;
+				static const real_t c1=37.0/378.0, c3=250.0/621.0, c4=125.0/594.0, c6=512.0/1771.0;
+				static const real_t dc1=c1-2825.0/27648.0, dc3=c3-18575.0/48384.0, dc4=c4-13525.0/55296.0, dc5 = -277.00/14336.0, dc6=c6-0.25;
 				
 				const size_t n = y.size();
 				
-				//std::cerr << "rkck@" << x << "+(" << h << ")" << std::endl;
 				
 				for (size_t i=n;i>0;--i) 
 					ytmp[i]=y[i]+b21*h*dydx[i];
--- src/y-rx/yocto/rx/syntactic/syn-rule.cpp
+++ src/y-rx/yocto/rx/syntactic/syn-rule.cpp
@@ -1,2 +1,25 @@
 #include "yocto/rx/syntactic/rule.hpp"
 
+namespace yocto
+{
+	
+	namespace regex
+	{
+		namespace syntactic
+		{
+
+			rule:: ~rule() throw()
+			{
+			}
+			
+			rule:: rule() throw()
+			{
+			}
+			
+			
+			
+		}
+
+	}
+
+}
