6f641b02bf7f53ffc56f9430a7bd892de51528f9
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2011-09-23 12:29:14 +0000

    parser: initial...

--- src/y-rx/yocto/rx/parser.hpp
+++ src/y-rx/yocto/rx/parser.hpp
@@ -2,6 +2,28 @@
 #define YOCTO_RX_PARSER_INCLUDED 1
 
 #include "yocto/rx/syntactic/rule.hpp"
+#include "yocto/auto-ptr.hpp"
 
+namespace yocto
+{
+	
+	namespace regex
+	{
+	
+		
+		class parser
+		{
+		public:
+			explicit parser( syntactic::rule *root ) throw();
+			virtual ~parser() throw();
+			
+		private:
+			auto_ptr<syntactic::rule> root_;
+			YOCTO_DISABLE_COPY_AND_ASSIGN(parser);
+		};
+		
+	}
+	
+}
 
 #endif
--- src/y-rx/yocto/rx/syntactic/rule.hpp
+++ src/y-rx/yocto/rx/syntactic/rule.hpp
@@ -2,22 +2,37 @@
 #define YOCTO_RX_SYN_RULE_INCLUDED 1
 
 #include "yocto/rx/lexer.hpp"
+#include "yocto/rx/source.hpp"
 
 namespace yocto
 {
 	
 	namespace regex
 	{
+		enum syntax_result
+		{
+			syntax_success, //!< found the right lexeme
+			syntax_nothing, //!< found nothing: End of Source
+			syntax_error,   //!< found unregistered token
+			syntax_invalid, //!< found unexpected   token
+		};
+		
 		namespace syntactic
 		{
+			
+			
 			
 			class rule : public object
 			{
 			public:
+				
 				rule        *parent;
 				const string name;
+				rule        *next;
+				rule        *prev;
+				
 				virtual ~rule() throw();
-				virtual  bool match( lexer &, source &) = 0;
+				virtual  syntax_result match( lexer &, source &) = 0;
 				
 			protected:
 				explicit rule( rule *p, const string &id );
@@ -26,7 +41,7 @@ namespace yocto
 				YOCTO_DISABLE_COPY_AND_ASSIGN(rule);
 			};
 			
-			
+#if 1
 			class terminal : public rule
 			{
 			public:
@@ -34,29 +49,30 @@ namespace yocto
 				
 				explicit terminal( rule *p, const string &id ) : rule(p,id) {}
 				
-				virtual bool match( lexer &L, source &src)
+				virtual syntax_result match( lexer &L, source &src)
 				{
 					lexeme *lx = L.lookahead( src );
 					if( !lx )
-						return false;
+						return src.is_active() ? syntax_error : syntax_error;
 					else
 					{
 						if( lx->label == name )
 						{
 							
-							return true;
+							return syntax_success;
 						}
 						else {
-							return false;
+							return syntax_invalid;
 						}
-
+						
 					}
 				}
 				
 			private:
 				YOCTO_DISABLE_COPY_AND_ASSIGN(terminal);
-										
+				
 			};
+#endif
 			
 		}
 	}
