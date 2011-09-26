a334ca79e8f9fbbb6ebfd76e3301355551fb8e56
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2011-09-26 15:39:49 +0000

    goin' on

---
+++ src/y-rx/yocto/rx/syntactic/at-least.hpp
@@ -0,0 +1,12 @@
+#ifndef YOCTO_RX_SYN_JOKER_INCLUDED
+#define YOCTO_RX_SYN_JOKER_INCLUDED 1
+
+#include "yocto/rx/syntactic/rule.hpp"
+
+namespace yocto
+{
+	namespace regex
+	{
+		
+		namespace syntactic
+		{
