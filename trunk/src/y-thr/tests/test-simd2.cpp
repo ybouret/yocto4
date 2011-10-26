b2af6dc7a88b25b0d4e93d36ef70d88848066d51
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2011-10-26 09:47:14 +0000

    SIMD new version

---
+++ trunk/src/y-thr/tests/test-simd2.cpp
@@ -0,0 +1,49 @@
+#include "yocto/utest/run.hpp"
+#include "yocto/threading/simd.hpp"
+#include "yocto/ios/icstream.hpp"
+
+using namespace yocto;
+using namespace threading;
+
+#include <cstdlib>
+
+YOCTO_UNIT_TEST_IMPL(SIMD2)
+{
+	size_t np = 2;
+	size_t nc = 2;
+	if( argc > 1 ) np = atol( argv[1] );
+	if( argc > 2 ) nc = atol( argv[2] );
+	std::cerr << ">>>> SIMD " << np << " " << nc << std::endl;
+	
+	std::cerr << "[Testing Alone]" << std::endl;
+	{
+		SIMD local_simd(np);
+	}
+	
+	SIMD simd(np);
+	ios::icstream in( ios::cstdin );
+	string        line;
+	
+	while( line.clear(), fprintf(stderr,">"), fflush(stderr), in.read_line(line) >= 0 )
+	{
+		if( line == 'c' )
+		{
+			for( size_t i=0; i < nc; ++i )
+			{
+				//std::cerr << "[SIMD.enter cyle]" << std::endl;
+				//simd.do_work = alea<double>() > 0.5;
+				simd.cycle();
+				//std::cerr << "[SIMD.leave cyle]" << std::endl;
+			}
+			continue;
+		}
+		
+		if( line == 'q' )
+		{
+			break;
+		}
+	}
+	
+	
+}
+YOCTO_UNIT_TEST_DONE()
