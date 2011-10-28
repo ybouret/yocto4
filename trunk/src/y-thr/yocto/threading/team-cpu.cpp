fde93d43865ac2d6e5725cd1e5953d175c488249
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2011-10-28 06:57:39 +0000

    win32/linux thread affinity+nproc

--- trunk/src/y-thr/yocto/threading/team-cpu.cpp
+++ trunk/src/y-thr/yocto/threading/team-cpu.cpp
@@ -5,22 +5,32 @@
 #else
 #	if defined(__GNUC__)
 #		if defined(__linux__)
+#			include <sys/sysinfo.h>
 #			define YOCTO_CPU_SET_PTHREAD 1
 #		endif
 #	endif
+#endif
+
+#if defined(_WIN32) || defined(YOCTO_CPU_SET_PTHREAD)
+#	define YOCTO_THREAD_AFFINITY 1
 #endif
 
 #include "yocto/threading/team.hpp"
 #include "yocto/exceptions.hpp"
 #include "yocto/threading/thread.hpp"
 
+#include <iostream>
+
 namespace yocto
 {
 	namespace threading
 	{
 		
+		//----------------------------------------------------------------------
+		// Win32 API wrapper
+		//----------------------------------------------------------------------
 #if defined(_WIN32)
-		static inline DWORD __numCPU()
+		static inline size_t __numCPU()
 		{
 			SYSTEM_INFO sysinfo;
 			::GetSystemInfo( &sysinfo );
@@ -28,56 +38,82 @@ namespace yocto
 			if( n <= 0 ) throw exception("sysinfo.dwNumberOfProcessors=0");
 			return n;
 		}
+		
+		static inline void __assign( thread::handle_t h, const size_t j )
+		{
+			const DWORD_PTR mask = DWORD_PTR(1) << j;
+			if( ! ::SetThreadAffinityMask( h, mask ) )
+			{
+				const DWORD err = ::GetLastError();
+				throw win32::exception( err, "::SetThreadAffinityMask" );
+			}
+		}
 #endif
+		
+		//----------------------------------------------------------------------
+		// pthread wrapper
+		//----------------------------------------------------------------------
+#if defined(YOCTO_CPU_SET_PTHREAD)
+#	if defined (__linux__)
+		static inline size_t __numCPU() 
+		{
+			const int n = get_nprocs();
+			if( n <= 0 ) 
+				throw exception("No CPU Online!");
+			return n;
+		}
+#endif
+		
+		static  void __assign( thread::handle_t h, const size_t j )
+		{
+			cpu_set_t  cpu_set;
+			CPU_ZERO(  &cpu_set );
+			CPU_SET(j, &cpu_set );
+			const int err = pthread_setaffinity_np( h, sizeof(cpu_set_t), &cpu_set );
+			if( err != 0 )
+				throw libc::exception( err, "pthread_setaffinity_np" );
+		}
+#endif
+		
+		
 #include "./team-member.hxx"
+		
+		static inline size_t cpu_index(const size_t cpu_idx, 
+									   const size_t cpu_count, 
+									   const size_t cpu_setsize ) throw()
+		{
+			const size_t j = ( cpu_idx % cpu_count ) % cpu_setsize;
+			std::cerr << "-- assign on CPU #" << j << "/" << cpu_setsize << std::endl;
+			return j;
+		}
 		
 		void team:: place( size_t cpu_start, size_t cpu_count )
 		{
 			if(cpu_count<=0)
 				throw exception("team.place(cpu_count=0)");
-#if defined(YOCTO_CPU_SET_PTHREAD)
-			const size_t cpu_setsize = CPU_SETSIZE;
-			if( cpu_setsize <= 0 )
-				throw exception("CPU_SETSIZE=0");
-			cpu_set_t  cpu_set;
-			size_t     cpu_idx = cpu_start;
-			member    *m       = static_cast<member *>(wksp_);
+			
+#if defined(YOCTO_THREAD_AFFINITY)
+			const size_t cpu_setsize = __numCPU();
+			member       *m          = static_cast<member *>(wksp_);
+			size_t        cpu_idx    = cpu_start;
+			//------------------------------------------------------------------
+			// assign main thread
+			//------------------------------------------------------------------
+			__assign( thread::get_current_handle(), cpu_index( cpu_idx, cpu_count, cpu_setsize ) );
+			
+			//------------------------------------------------------------------
+			// assign team thread
+			//------------------------------------------------------------------
 			for( size_t i=0; i < size; ++i )
-			{
-				const size_t j = (cpu_idx++ % cpu_count) % cpu_setsize;
-				CPU_ZERO(  &cpu_set );
-				CPU_SET(j, &cpu_set );
-				const int err = pthread_setaffinity_np( m[i].thr.get_handle(), sizeof(cpu_set_t), &cpu_set );
-				if( err != 0 )
-					throw libc::exception( err, "pthread_setaffinity_np" );
-			}
+				__assign(  m[i].thr.get_handle(), cpu_index( cpu_idx++, cpu_count, cpu_setsize ) );
 #endif
 			
-#if defined(_WIN32)
-			const DWORD cpu_setsize = __numCPU(); // TODO: <= 32 ?
-			size_t      cpu_idx = cpu_start;
-			member    *m       = static_cast<member *>(wksp_);
-			for( size_t i=0; i < size; ++i )
-			{
-				const size_t j = (cpu_idx++ % cpu_count) % cpu_setsize;
-				const DWORD_PTR mask = DWORD_PTR(1) << j;
-				if( ! ::SetThreadAffinityMask( m[i].thr.get_handle(), mask ) )
-				{
-					const DWORD err = ::GetLastError();
-					throw win32::exception( err, "::SetThreadAffinityMask" );
-				}
-			}
-#endif
 			
 		}
 		
 		void team:: flat()
 		{
-#if defined(YOCTO_CPU_SET_PTHREAD)
-			place(0,CPU_SETSIZE);
-#endif
-			
-#if defined(_WIN32)
+#if defined(YOCTO_THREAD_AFFINITY)
 			place(0,__numCPU());
 #endif
 		}
