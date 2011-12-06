a00a88288c781d20460414cc478b348da88e5dfe
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2011-12-06 12:33:54 +0000

    TODO: algebra tests

---
+++ src/y-mk/tests/test-algebra.cpp
@@ -0,0 +1,20 @@
+#include "yocto/utest/run.hpp"
+#include "yocto/math/kernel/algebra.hpp"
+#include "yocto/sequence/vector.hpp"
+#include "yocto/wtime.hpp"
+
+using namespace yocto;
+using namespace math;
+
+
+
+YOCTO_UNIT_TEST_IMPL(algebra)
+{
+	
+	wtime chrono;
+	
+		
+}
+YOCTO_UNIT_TEST_DONE()
+
+
