703d4814a79f34e7e23b0d5859f5d25d83e5ec53
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2011-12-09 13:19:08 +0000

    initial algebra tests

--- trunk/src/y-mk/tests/test-algebra.cpp
+++ trunk/src/y-mk/tests/test-algebra.cpp
@@ -1,17 +1,45 @@
 #include "yocto/utest/run.hpp"
 #include "yocto/math/kernel/algebra.hpp"
 #include "yocto/sequence/vector.hpp"
+#include "yocto/code/rand.hpp"
+#include "yocto/math/complex.hpp"
 #include "yocto/wtime.hpp"
 
 using namespace yocto;
 using namespace math;
 
+template <typename T>
+static inline double test_copy( size_t N )
+{
+	const size_t num_iter = 100;
+	wtime  chrono;
+	double t = 0;
+	vector<T> X(N,0);
+	vector<T> Y(N,0);
+	for( size_t iter=0; iter < num_iter; ++iter )
+	{
+		for( size_t i=N;i>0; --i ) 
+			X[i] = alea<float>();
+		chrono.start();
+		algebra<T>::copy(Y,X);
+		t += chrono.query();
+	}
+	return t / num_iter;
+}
 
+
+#define _TEST(NAME,TYPE) fprintf(stderr,"%-8s | %-20s | N=%6d | t=%.4g ms\n", #NAME, #TYPE, int(N), 1000.0 * test_##NAME< TYPE >(N) )
+
+#define _TEST_ALL(NAME) _TEST(NAME,float); _TEST(NAME,double); _TEST(NAME,complex<float>)
 
 YOCTO_UNIT_TEST_IMPL(algebra)
 {
 	
-	wtime chrono;
+	for( size_t p=0; p < 18; ++p )
+	{
+		const size_t N = 1 << p;
+		_TEST_ALL(copy);
+	}
 	
 		
 }
