440020e215c8e33430d73694796def9bc785661c
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2011-12-13 14:17:53 +0000

    added popen sample

---
+++ trunk/prj/src/driver/slave1.cpp
@@ -0,0 +1,29 @@
+#include "yocto/wtime.hpp"
+
+#include <cstdio>
+
+using namespace yocto;
+
+int main()
+{
+		
+	wtime chrono;
+	
+	chrono.start();
+	size_t index = 0;
+	fprintf( stderr, "Running for 10 seconds...\n");
+	while( chrono.query() <= 10.0 )
+	{
+		putc( char( 'A' + index++ ), stdout );
+		if( index >= 26 ) 
+		{
+			putc('\n',stdout);
+			index = 0;
+		}
+		fflush( stdout );
+		wtime::sleep( 0.1 );
+		
+	}
+	putc('\n',stdout);
+	return 0;
+}
