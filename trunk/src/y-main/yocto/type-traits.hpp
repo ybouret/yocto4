93a77eb1677a9eab1ba95592892e900d81af5dc9
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2011-12-16 09:45:37 +0000

    corrected test-map bug

--- trunk/src/y-main/yocto/type-traits.hpp
+++ trunk/src/y-main/yocto/type-traits.hpp
@@ -225,23 +225,23 @@ namespace yocto
 		 * streams, containers,..., can't contain references,
 		 * so these are the aliases used for in/out parameters passing
 		 */
-#define	YOCTO_ARGUMENTS_DECL_T                                        \
-typedef T                                        type;                \
-typedef typename type_traits<T>::mutable_type    mutable_type;        \
-typedef const mutable_type                       const_type;          \
-typedef typename                                                               \
-yocto::select<                                                                 \
+#define	YOCTO_ARGUMENTS_DECL_T                                              \
+typedef T                                        type;                      \
+typedef typename type_traits<T>::mutable_type    mutable_type;              \
+typedef const mutable_type                       const_type;                \
+typedef typename                                                            \
+yocto::select<                                                              \
 type_traits<mutable_type>::is_primitive, /* no references in containers */  \
 const_type  , /* pass primitive mutable types by const value            */  \
 const_type &  /* pass non primitive mutable types by const reference    */  \
 >::result param_type
 		
-#define	YOCTO_ARGUMENTS_DECL_KEY                                      \
-typedef KEY                                      key_type;            \
-typedef typename type_traits<KEY>::mutable_type  mutable_key;        \
-typedef const mutable_key                        const_key;          \
-typedef typename                                                               \
-yocto::select<                                                                 \
+#define	YOCTO_ARGUMENTS_DECL_KEY                                           \
+typedef KEY                                      key_type;                 \
+typedef typename type_traits<KEY>::mutable_type  mutable_key;              \
+typedef const mutable_key                        const_key;                \
+typedef typename                                                           \
+yocto::select<                                                             \
 type_traits<mutable_key>::is_primitive, /* no references in containers */  \
 const_key  , /* pass primitive mutable types by const value            */  \
 const_key &  /* pass non primitive mutable types by const reference    */  \
