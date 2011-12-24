ad722566a2ccb4882dae8f9bb1138a0c2df435a9
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2011-12-24 10:34:58 +0000

    swig with namespaces

--- trunk/prj/src/wrapper/sample.hpp
+++ trunk/prj/src/wrapper/sample.hpp
@@ -14,5 +14,18 @@ private:
 	YOCTO_DISABLE_COPY_AND_ASSIGN(Foo);
 };
 
+namespace Toto
+{
+	class Bar
+	{
+	public:
+		int b;
+		explicit Bar() throw();
+		virtual ~Bar() throw();
+		
+	private:
+		YOCTO_DISABLE_COPY_AND_ASSIGN(Bar);
+	};
+}
 
 #endif
