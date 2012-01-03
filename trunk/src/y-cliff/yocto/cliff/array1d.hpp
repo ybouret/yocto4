5a3ee57e853f1f54dafe6a273216a3b8ade58318
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-01-03 16:41:32 +0000

    updated cliff

---
+++ trunk/src/y-cliff/yocto/cliff/array1d.hpp
@@ -0,0 +1,37 @@
+#ifndef YOCTO_CLIFF_ARRAY1D_INCLUDED
+#define YOCTO_CLIFF_ARRAY1D_INCLUDED 1
+
+#include "yocto/cliff/linear.hpp"
+#include "yocto/cliff/in1d.hpp"
+
+
+namespace yocto
+{
+	
+	namespace cliff
+	{	
+		
+		template <typename T>
+		class array1D : public linear<T,layout1D>
+		{
+		public:
+			YOCTO_ARGUMENTS_DECL_T;
+			typedef linear<T,layout1D> linear_type;
+			typedef layout1D           layout_type;
+			
+			explicit array1D( const layout_type &L ) throw();
+			virtual ~array1D() throw();
+			
+			type       & operator[]( unit_t x ) throw();
+			const_type & operator[]( unit_t x ) const throw();
+			
+			
+			
+			
+		private:
+			YOCTO_DISABLE_COPY_AND_ASSIGN(array1D);
+		};
+	}
+}
+
+#endif
