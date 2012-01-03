d7f2a4c3af8bb558ee11f05f8ca31312e4119d85
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-01-03 07:50:50 +0000

    SWIG: updated for C++

--- src/share/swig.cmake
+++ src/share/swig.cmake
@@ -4,7 +4,10 @@
 ##
 ########################################################################
 
-## find the swig program
+
+########################################################################
+# find the swig program
+########################################################################
 SET(SWIG_FOUND OFF)
 FIND_PROGRAM(SWIG swig)
 
@@ -32,7 +35,9 @@ ELSE()
 	MACRO(SWIG_WRAPPER name kind)
 		MESSAGE( STATUS "[SWIG]: generating wrapper for [${name}] in [${kind}]")
 		
+		####################################################################
 		## common variables
+		####################################################################
 		SET(swig_output_dir   "${CMAKE_CURRENT_BINARY_DIR}/${kind}")
 		SET(swig_wrapper_name "${name}_wrapper_${kind}.cpp" )
 		SET(swig_wrapper_code "${swig_output_dir}/${swig_wrapper_name}")
@@ -40,7 +45,9 @@ ELSE()
 		SET(swig_user_code    "${CMAKE_CURRENT_SOURCE_DIR}/${name}.cpp")
 		SET(swig_target       "${name}_${kind}")
 		
+		####################################################################
 		## dependencies
+		####################################################################
 		EXECUTE_PROCESS(
 			COMMAND ${SWIG} -c++ -${kind} -MM "${swig_interface}"
 			OUTPUT_VARIABLE SWIG_DEPS
@@ -57,7 +64,9 @@ ELSE()
 			LIST( APPEND SWIG_DEPENDS "${D2}"  )
 		ENDFOREACH()
 		
+		####################################################################
 		## generation
+		####################################################################
 		ADD_CUSTOM_COMMAND(
 			OUTPUT  "${swig_output_dir}/${swig_wrapper_name}"
 			COMMAND ${CMAKE_COMMAND} -E make_directory "${swig_output_dir}"
@@ -65,14 +74,22 @@ ELSE()
 			DEPENDS ${SWIG_DEPENDS}
 			)
 		
+		####################################################################
 		## default module to build
+		####################################################################
 		ADD_LIBRARY(${swig_target} MODULE "${swig_user_code}" "${swig_wrapper_code}")
 		
 
+		####################################################################
 		## and its exact location
+		####################################################################
 		GET_TARGET_PROPERTY(swig_target_name ${swig_target} LOCATION)
 		
+		####################################################################
+		##
 		## python tuning
+		##
+		####################################################################
 		IF( "${kind}" STREQUAL "python" )
 			IF( NOT SWIG_PYTHON )
 				MESSAGE( FATAL_ERROR "[SWIG]: No python was detected!" )
@@ -92,11 +109,21 @@ ELSE()
 			)
 		ENDIF()
 	
+		####################################################################
+		##
 		## system tuning
-		IF(YOCTO_SUNOS)
-			MESSAGE( STATUS "[SWIG]: explicit stdc++" )
-			TARGET_LINK_LIBRARIES(${swig_target} stdc++)
+		##
+		####################################################################
+		SET(swig_cxx OFF)
+		IF(YOCTO_SUNOS OR YOCTO_CLANG)
+			SET(swig_cxx ON)
 		ENDIF()	
+		
+		
+		IF(swig_cxx)
+			MESSAGE( STATUS "[SWIG]: explicit stdc++ linking" )
+			TARGET_LINK_LIBRARIES(${swig_target} stdc++)
+		ENDIF()
 	ENDMACRO(SWIG_WRAPPER)
 	
 ENDIF()
