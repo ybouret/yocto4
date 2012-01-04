72ea1ef52d89ab1fc30f15e828c5e94329dd1644
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-01-04 10:16:35 +0000

    added colors

---
+++ trunk/src/y-main/yocto/geom/ramp/rgba32-16.hpp
@@ -0,0 +1,11 @@
+#ifndef YOCTO_RGBA32_16_HPP
+#define YOCTO_RGBA32_16_HPP 1
+#include "yocto/geom/color.hpp"
+
+namespace yocto { namespace color {
+
+	 extern const rgba32 rgba32_16[256];
+
+} }
+
+#endif
---
+++ trunk/src/y-main/yocto/geom/ramp/rgba32-19.hpp
@@ -0,0 +1,11 @@
+#ifndef YOCTO_RGBA32_19_HPP
+#define YOCTO_RGBA32_19_HPP 1
+#include "yocto/geom/color.hpp"
+
+namespace yocto { namespace color {
+
+	 extern const rgba32 rgba32_19[256];
+
+} }
+
+#endif
