12863ef7651ca05e7200cc8eea6a832774ba6353
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-01-05 11:13:16 +0000

    CLIFF: updated workspaces

--- src/y-cliff/yocto/cliff/workspace.hpp
+++ src/y-cliff/yocto/cliff/workspace.hpp
@@ -3,8 +3,11 @@
 
 #include "yocto/cliff/types.hpp"
 #include "yocto/cliff/components.hpp"
+
 #include "yocto/type-traits.hpp"
 #include "yocto/code/static-check.hpp"
+#include "yocto/shared-ptr.hpp"
+#include "yocto/memory/global.hpp"
 
 namespace yocto
 {
@@ -18,32 +21,63 @@ namespace yocto
 			
 		};
 		
+			
 		template <
 		typename                  T,        // real/complex...
-		typename                  LAYOUT,   // layout[1|2|3]D
+		template <typename> class ARRAY,   // layout[1|2|3]D
 		typename                  U,        // float/double for real coordinates
 		template <typename> class REGION    // region with associated vertex
 		>
-		class workspace : public LAYOUT, public components
+		class workspace : public  ARRAY<T>::layout_type, public components
 		{
 		public:
 			//==================================================================
 			// compute types
 			//==================================================================
 			YOCTO_ARGUMENTS_DECL_T; //!< for data handling
-			typedef LAYOUT                             layout_type;
-			typedef typename LAYOUT::coord_t           coord_t;
-			typedef typename LAYOUT::const_coord       const_coord;
-			typedef typename LAYOUT::param_coord       param_coord;
+			typedef ARRAY<T>                           array_type;
+			typedef typename array_type::layout_type   layout_type;
+			typedef typename layout_type::coord_t      coord_t;
+			typedef typename layout_type::const_coord  const_coord;
+			typedef typename layout_type::param_coord  param_coord;
 			typedef U                                  real_t;
 			typedef typename REGION<U>::type           region_type;
 			typedef typename region_type::vertex_t     vertex_t;
 			typedef typename region_type::param_vertex param_vertex;
-			static  const    size_t                    DIMENSIONS = LAYOUT::DIMENSIONS;
+			typedef memory::global                     memory_kind;
+			
+			static  const    size_t                    DIMENSIONS = layout_type::DIMENSIONS;
+			
+			//==================================================================
+			// blocks for memory handling per species
+			//==================================================================
+			class block : public object, public array_type
+			{
+			public:
+				explicit block( const layout_type &L ) :
+				array_type(L),
+				n( this->items ),
+				p( memory::kind<memory_kind>::acquire_as<type>(n) )
+				{
+					this->link(p);
+				}
+				
+				virtual ~block() throw()
+				{
+					memory::kind<memory_kind>::release_as<type>(p,n);
+				}
+				
+				
+			private:
+				size_t     n;      //!< this->items
+				type      *p;      //!< for linking
+				YOCTO_DISABLE_COPY_AND_ASSIGN(block);
+			};
+			typedef shared_ptr<block> block_ptr;
 			
 			
-			const layout_type outline; //!< LAYOUT+ghosts
-			const region_type region;
+			const layout_type outline; //!< original layout+ghosts
+			const region_type region;  //!< real space associated to layout (NOT outline)
 			
 			//! construct a workspace
 			/**
@@ -59,12 +93,21 @@ namespace yocto
 							   size_t       b,
 							   const char  *names_list[]
 							   ) :
-			LAYOUT( lo, hi ),
+			layout_type( lo, hi ),
 			components(a,b,names_list),
 			outline( compute_outline( *this, ghosts_lo, ghosts_up) ),
-			region(vmin,vmax)
+			region(vmin,vmax),
+			blocks( this->size, as_capacity )
 			{
 				YOCTO_STATIC_CHECK(DIMENSIONS==region_type::DIMENSIONS,cliff_workspace);
+				//--------------------------------------------------------------
+				// allocate memory for each component
+				//--------------------------------------------------------------
+				for( size_t i=1; i <= size; ++i )
+				{
+					const block_ptr blk( new block( outline ) );
+					blocks.push_back( blk );
+				}
 			}
 			
 			
@@ -76,13 +119,15 @@ namespace yocto
 			
 		private:
 			YOCTO_DISABLE_COPY_AND_ASSIGN(workspace);
-			static inline LAYOUT compute_outline( const LAYOUT &L, param_coord ghosts_lo, param_coord ghosts_up )
+			vector<block_ptr> blocks;
+			
+			static inline layout_type compute_outline( const layout_type &L, param_coord ghosts_lo, param_coord ghosts_up )
 			{
 				workspace_base::check( &ghosts_lo, DIMENSIONS, "lower ghosts" );
 				workspace_base::check( &ghosts_up, DIMENSIONS, "upper ghosts" );
 				const_coord out_lo = L.lower - ghosts_lo;
 				const_coord out_up = L.upper + ghosts_up;
-				return LAYOUT(out_lo,out_up);
+				return layout_type(out_lo,out_up);
 			}
 		};
 	}
