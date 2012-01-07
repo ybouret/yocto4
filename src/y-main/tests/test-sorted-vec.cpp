d82d9066ef28394079eafb41c1f1beecd4784eb2
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-01-07 17:03:04 +0000

    functional sorted-vector

--- src/y-main/tests/test-sorted-vec.cpp
+++ src/y-main/tests/test-sorted-vec.cpp
@@ -1,7 +1,9 @@
 #include "yocto/utest/run.hpp"
 #include "yocto/ordered/sorted-vector.hpp"
 #include "yocto/sequence/vector.hpp"
+#include "yocto/code/rand.hpp"
 #include "support.hpp"
+#include "yocto/code/utils.hpp"
 
 #include <typeinfo>
 
@@ -12,6 +14,44 @@ YOCTO_UNIT_TEST_IMPL(sorted_vec)
 	
 	{
 		sorted_vector<size_t> sv;
+		size_t count = 0;
+		const size_t nmax = 1000;
+		for( size_t i=0; i < nmax; ++i)
+		{
+			if( sv.insert( _rand.less_than(nmax) ) )
+			{
+				++count;
+			}
+		}
+		std::cerr << "inserted " << count << "/" << nmax << std::endl;
+		assert( sv.size() == count );
+		const size_t step = max_of<size_t>(1,count/10);
+		for( size_t i=1; i <= count; i += step )
+		{
+			std::cerr << sv[i] << std::endl;
+		}
+	}
+	
+	{
+		sorted_vector<string> sv;
+		size_t count = 0;
+		const size_t nmax = 1000;
+		for( size_t i=0; i < nmax; ++i)
+		{
+			const string tmp = gen<string>::get();
+			if( sv.insert( tmp ) )
+			{
+				++count;
+			}
+		}
+		std::cerr << "inserted " << count << "/" << nmax << std::endl;
+		assert( sv.size() == count );
+		const size_t step = max_of<size_t>(1,count/10);
+		for( size_t i=1; i <= count; i += step )
+		{
+			std::cerr << sv[i] << std::endl;
+		}
+		
 	}
 }
 YOCTO_UNIT_TEST_DONE()
