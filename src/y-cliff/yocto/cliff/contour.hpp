c4bfeb2d7af7e10d6381e7284a03282221b37c1c
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-01-08 16:45:02 +0000

    CLIFF: functional contour

--- src/y-cliff/yocto/cliff/contour.hpp
+++ src/y-cliff/yocto/cliff/contour.hpp
@@ -12,14 +12,13 @@ namespace yocto
 		struct contour
 		{
 			typedef typename vertex2D<T>::type vertex_t;
-			typedef void   (*callback_t)( const vertex_t &, const vertex_t &, T, void *);
-			static inline void compute(const array2D<T> &d,
-									   const array1D<T> &x, 
-									   const array1D<T> &y,
-									   const layout2D   &sub,
-									   const array<T>   &z,
-									   callback_t       proc,
-									   void            *args);
+			typedef functor<void,TL3(const vertex_t &, const vertex_t &,T)> callback;
+			static void compute(const array2D<T> &d,
+								const array1D<T> &x, 
+								const array1D<T> &y,
+								const layout2D   &sub,
+								const array<T>   &z,
+								callback          proc);
 		};
 		
 	}
