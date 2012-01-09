44c254ab22db4ca64a9467a2e20bd41ab42af612
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-01-09 19:12:11 +0000

    updated contour2D

--- src/y-cliff/tests/test-contour.cpp
+++ src/y-cliff/tests/test-contour.cpp
@@ -13,11 +13,10 @@ namespace
 {
 	static inline double f2( double x, double y )
 	{
-		//return sin( sqrt( x*x + 1.5 * y*y ) );
+		const double dx = (x-1);
+		return sin( sqrt( x*x + 1.5 * y*y ) ) + 1/(dx*dx+1);
 		
-		const double den1 = x*x + (y-0.842)*(y+0.842);
-		const double den2 = x*(y-0.842)*2;
-		return 1/(den1*den1 + den2*den2);
+	
 	}
 	
 	
@@ -34,8 +33,8 @@ namespace
 		
 		void process( const vtx_t &a, const vtx_t &b, const level_set<double>::level &l )
 		{
-			const int key = l.key;
-			const bool append = l.flags != 0;
+			const int  key    = l.key;
+			const bool append = (l.flags != 0);
 			l.flags = 1;
 			ios::ocstream fp( vformat("iso%d.dat",key), append );
 			fp("%g %g\n", a.x, a.y);
@@ -64,23 +63,27 @@ YOCTO_UNIT_TEST_IMPL(contour)
 	typedef wksp_type::vertex_t   vertex_t;
 	wksp_type w(coord2D(1,1), coord2D(200,200),
 				coord2D(0,0), coord2D(0,0),
-				vertex_t(-1.5,-1.5),
-				vertex_t(1.5,1.5),
+				vertex_t(-3.5,-3.5),
+				vertex_t(3.5,3.5),
 				1,1,NULL);
 	
 	array2D<double>      &d = w[1];
 	fill_type::function2  f( cfunctor2(f2) );
 	fill_type::with( f, d, d, w.X, w.Y );
-	d.ppm("iso.ppm", "iso", d, vproc, NULL);
+	double vmin=0,vmax=0;
+	d.get_min_max(vmin,NULL,vmax,NULL);
+	std::cerr << "vmin= " << vmin << ", vmax=" << vmax << std::endl;
+	d.ppm("iso.ppm", "iso", d, vproc, NULL, vmin, vmax);
 	
 	level_set<double> levels;
-	levels.add(1, 0.5 );
-	levels.add(2, 1.0 );
-	levels.add(3, 2.0 );
-	levels.add(4, 3.0 );
+	levels.add(1, -0.5 );
+	levels.add(2,  0.5 );
+	levels.add(3,  1.0 );
+	levels.add(4,  1.5 );
 	
 	for( size_t i=1; i <= levels.size(); ++i )
 	{
+		assert( 0 == levels[i].flags );
 		std::cerr << "level #" << i << " key=" << levels[i].key << ", value=" << levels[i].value << std::endl;
 	}
 		
