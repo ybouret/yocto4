48f0fb700459929698a123615641c51eb5e60f57
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-01-16 08:05:22 +0000

    string conversion

--- src/y-main/yocto/string/conv.hpp
+++ src/y-main/yocto/string/conv.hpp
@@ -9,6 +9,7 @@ namespace yocto
 	struct strconv
 	{
 		static size_t to_size( const string &txt, const char *ctx = NULL );
+		static int    to_int( const string &txt, const char *ctx = NULL );
 		template <typename T> 
 		static T to_real( const string &txt, const char *ctx = NULL );
 	};
--- src/y-main/yocto/string/str-conv.cpp
+++ src/y-main/yocto/string/str-conv.cpp
@@ -7,9 +7,9 @@
 namespace yocto 
 {
 	
-
 	
-
+	
+	
 	size_t strconv:: to_size( const string &txt, const char *ctx  )
 	{
 		static const char fmt[] = "strconv::to_size(invalid char '%c') for <%s>";
@@ -33,21 +33,61 @@ namespace yocto
 				case '8': ans += 8; break;
 				case '9': ans += 9; break;
 					
-				default:
-					if( ctx )
-					{
-						throw imported::exception( ctx, fmt, C, ctx );
-					}
-					else
-					{
-						throw libc::exception( EINVAL, fmt, C, ctx );
-					}
-
+				default:					
+					throw libc::exception( EINVAL, fmt, C, ctx );
+					
 			}
 		}
 		return ans;
 	}
-
+	
+	int strconv:: to_int( const string &txt, const char *ctx  )
+	{
+		static const char fmt[] = "strconv::to_int(invalid char '%c') for <%s>";
+		if( !ctx )        ctx   = "";
+		assert( txt.size() > 0 );
+		const size_t n   =  txt.size();
+		size_t       i   = 0;
+		bool         neg = false;
+		
+		switch( txt[i] )
+		{
+			case '-':
+				neg = true;
+			case '+':
+				if( ++i >= n ) throw libc::exception( EINVAL, "strconv::to_int(no char after '%c') for <%s>", txt[i], ctx);
+			default:
+				break;
+		}
+		assert( i < n );
+		int ans = 0;
+		for( ; i < n; ++i )
+		{
+			const char C = txt[i];
+			ans *= 10;
+			switch( C )
+			{
+				case '0': break;
+				case '1': ++ans;    break;
+				case '2': ans += 2; break;
+				case '3': ans += 3; break;
+				case '4': ans += 4; break;
+				case '5': ans += 5; break;
+				case '6': ans += 6; break;
+				case '7': ans += 7; break;
+				case '8': ans += 8; break;
+				case '9': ans += 9; break;
+					
+				default:					
+					throw libc::exception( EINVAL, fmt, C, ctx );
+					
+			}
+		}
+		return ans;
+	}
+	
+	
+	
 #define real_t float
 #include "conv.cxx"
 	
