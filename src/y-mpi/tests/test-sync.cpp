2b3340263c3484f5a18ae4eb1f6fa2549778e883
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-01-20 13:19:59 +0000

    MPI: parallel printf

---
+++ src/y-mpi/tests/test-sync.cpp
@@ -0,0 +1,20 @@
+#include "yocto/utest/run.hpp"
+
+#include "yocto/mpi/mpi.hpp"
+
+using namespace yocto;
+
+
+
+YOCTO_UNIT_TEST_IMPL(sync)
+{
+	
+	
+	
+	mpi &MPI = mpi::init( &argc, & argv );
+	std::cerr.flush();
+	MPI.Printf( stderr, "From rank %d\n", MPI.CommWorldRank );
+	std::cerr.flush();
+	
+}
+YOCTO_UNIT_TEST_DONE()
