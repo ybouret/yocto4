ae0aef37cd20b9d09fdbeaebd8d75b3ab1598ad8
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-01-26 16:59:56 +0000

    MPI: Scatter/Gather

--- src/y-mpi/yocto/mpi/mpi-col.cpp
+++ src/y-mpi/yocto/mpi/mpi-col.cpp
@@ -20,7 +20,27 @@ namespace yocto
 		
 		const int err = MPI_Bcast( (void *)buffer, count, datatype, root, comm );
 		if( err != MPI_SUCCESS )
-			throw mpi::exception( err, "MPI_Bcast()" );
+			throw mpi::exception( err, "MPI_Bcast(root=%d)", root );
+	}
+	
+	
+	void mpi:: Scatter( const void *sendbuf, size_t sendcnt, MPI_Datatype sendtype, void *recvbuf, size_t recvcnt, MPI_Datatype recvtype, int root, MPI_Comm comm ) const
+	{
+		assert( !(NULL==sendbuf && sendcnt > 0 ) );
+		assert( !(NULL==recvbuf && recvcnt > 0 ) );
+		const int err = MPI_Scatter((void*)sendbuf,sendcnt,sendtype,recvbuf,recvcnt,recvtype,root,comm);
+		if( err != MPI_SUCCESS )
+			throw mpi::exception( err, "MPI_Scatter(root=%d)", root );
+	}
+
+	
+	void mpi:: Gather( const void *sendbuf, size_t sendcnt, MPI_Datatype sendtype, void *recvbuf, size_t recvcnt, MPI_Datatype recvtype, int root, MPI_Comm comm ) const
+	{
+		assert( !(NULL==sendbuf && sendcnt > 0 ) );
+		assert( !(NULL==recvbuf && recvcnt > 0 ) );
+		const int err = MPI_Gather((void*)sendbuf,sendcnt,sendtype,recvbuf,recvcnt,recvtype,root,comm);
+		if( err != MPI_SUCCESS )
+			throw mpi::exception( err, "MPI_Gather(root=%d)", root );
 	}
 	
 	
