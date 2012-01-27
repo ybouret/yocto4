452b7db4663aae4dee995f7aa313f704bc95bad3
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-01-27 13:21:21 +0000

    MPI: some collective routines

---
+++ trunk/src/y-mpi/tests/test-col.cpp
@@ -0,0 +1,54 @@
+#include "yocto/utest/run.hpp"
+
+#include "yocto/mpi/mpi.hpp"
+#include "yocto/sequence/vector.hpp"
+
+using namespace yocto;
+
+YOCTO_UNIT_TEST_IMPL(bcast)
+{
+	
+	mpi &MPI = mpi::init( &argc, & argv );
+	const int rank = MPI.CommWorldRank;
+	const int size = MPI.CommWorldSize;
+	MPI.Printf( stderr, "Bcast: Rank %d/%d is ready\n", rank, size ); 
+	
+	int msg = 0;
+	if( 0 == rank )
+	{
+		msg = 7;
+	}
+	MPI.Printf0( stderr, "Broadcasting %d\n", msg );
+	MPI.Bcast(&msg, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI.Printf( stderr, "Rank %d: msg=%d\n", rank, msg );
+		
+}
+YOCTO_UNIT_TEST_DONE()
+
+
+YOCTO_UNIT_TEST_IMPL(scatter)
+{
+	
+	mpi &MPI = mpi::init( &argc, & argv );
+	const int rank = MPI.CommWorldRank;
+	const int size = MPI.CommWorldSize;
+	MPI.Printf( stderr, "Scatter: Rank %d/%d is ready\n", rank, size ); 
+
+	const size_t  N = 100;
+	vector<float> src;
+	if( rank == 0 ) 
+	{
+		src.make( N * size, 1.0f );
+		for( size_t i=1; i <= src.size(); ++i ) src[i] = i;
+	}
+	vector<float> dst(N,0.0);
+	
+	float *sendbuff = rank == 0 ? &src[1] : NULL;
+	MPI.Scatter(sendbuff, N, MPI_FLOAT, &dst[1], N, MPI_FLOAT, 0, MPI_COMM_WORLD);
+	MPI.Printf( stderr, "Rank %d: dst[1]= %g\n", rank, dst[1] );
+	
+	MPI.Barrier(MPI_COMM_WORLD);
+	
+	
+}
+YOCTO_UNIT_TEST_DONE()
