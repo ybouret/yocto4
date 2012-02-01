6b7f6aa92c44ca1a4484db0757e93fabc224f67a
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-02-01 15:23:28 +0000

    NET: new io

---
+++ src/y-net/yocto/net/io-block.cpp
@@ -0,0 +1,115 @@
+#include "yocto/net/io-block.hpp"
+#include "yocto/memory/global.hpp"
+
+#include <cstring>
+
+namespace yocto
+{
+	namespace network
+	{
+		
+		io_block:: ~io_block() throw()
+		{
+			size_t n = final - start; assert(n>0);
+			memory::kind<memory::global>::release_as<uint8_t>( start, n );
+			final = NULL;
+			curr  = last = NULL;
+		}
+		
+		io_block:: io_block( size_t bs ) :
+		next(NULL),
+		prev(NULL),
+		curr(NULL),
+		last(NULL),
+		start(NULL),
+		final(NULL)
+		{
+			size_t n = bs;
+			start    = memory::kind<memory::global>::acquire_as<uint8_t>( n );
+			final    = start + bs;
+			clear();
+		}
+		
+		
+		void io_block:: clear() throw()
+		{
+			curr = last = start;
+		}
+		
+		size_t io_block:: length() const throw()
+		{
+			return last - curr;
+		}
+		
+		size_t io_block:: unused() const throw()
+		{
+			return final - last;
+		}
+		
+		size_t io_block:: offset() const throw()
+		{
+			return curr - start;
+		}
+		
+		const void * io_block:: get_address() const throw() { return curr; }
+		
+		
+		bool io_block:: recv( io_socket &sock )
+		{
+			size_t nr = sock.recv( last, unused() );
+			if( nr > 0 )
+			{
+				last += nr;
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		
+		void io_block:: defrag() throw()
+		{
+			if( curr > start ) 
+			{
+				const size_t n = length();
+				memmove(start, curr, n);
+				curr = start;
+				last = curr + n;
+			}
+		}
+		
+		bool io_block:: try_steal( io_block &blk ) throw()
+		{
+			assert( this != &blk );
+			const size_t n = blk.length();
+			if( unused() + offset() >= n )
+			{
+				defrag();
+				memcpy( last, blk.curr, n );
+				last += n;
+				blk.clear();
+				return true;
+			}
+			else
+				return false;
+		}
+		
+		bool io_block:: sent( io_socket &sock )
+		{
+			size_t ns = sock.send( curr, length() );
+			curr += ns;
+			if( curr >= last )
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		
+				
+	}
+	
+}
---
+++ src/y-net/yocto/net/io-queue.hpp
@@ -0,0 +1,67 @@
+#ifndef YOCTO_NET_IO_QUEUE_INCLUDED
+#define YOCTO_NET_IO_QUEUE_INCLUDED 1
+
+
+#include "yocto/net/io-block.hpp"
+#include "yocto/core/list.hpp"
+#include "yocto/core/pool.hpp"
+
+#include "yocto/ios/istream.hpp"
+#include "yocto/ios/ostream.hpp"
+
+namespace yocto
+{
+	namespace network
+	{
+		
+		class io_queue : public ios::istream, public ios::ostream
+		{
+		public:
+			explicit io_queue( size_t bs );
+			virtual ~io_queue() throw();
+			
+			const size_t block_size;
+			
+			//! receive data from an io_socket
+			/**
+			 data are stored in recv_blocks from
+			 a new block. If possible, compacted with the 
+			 last recv_blocks.
+			 */
+			bool recv( io_socket &sock );
+			
+			//! send data into an io_socket
+			/**
+			 data are taken from the first send_blocks.
+			 return true if send_blocks is empty afterwards
+			 */
+			bool sent( io_socket &sock );
+			
+			void reset() throw();
+
+			//------------------------------------------------------------------
+			// use recv blocks
+			//------------------------------------------------------------------
+			virtual bool query( char &C );
+			virtual void store( char  C );
+			virtual void get( void *data, size_t size, size_t &done );
+			
+			//------------------------------------------------------------------
+			// use send blocks
+			//------------------------------------------------------------------
+			virtual void write( char C );
+			virtual void flush();
+		
+			
+		private:
+			core::list_of<io_block>  send_blocks;
+			core::list_of<io_block>  recv_blocks;
+			core::pool_of<io_block>  pool_blocks;
+			io_block *fetch();
+			YOCTO_DISABLE_COPY_AND_ASSIGN(io_queue);
+		};
+		
+	}
+}
+
+#endif
