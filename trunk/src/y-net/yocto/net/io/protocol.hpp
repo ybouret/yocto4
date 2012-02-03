5fee6f549fec9cc037722519e525ec6fd7ff4dc1
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-02-03 16:13:32 +0000

    NET: initial protocol

---
+++ trunk/src/y-net/yocto/net/io/protocol.hpp
@@ -0,0 +1,48 @@
+#ifndef YOCTO_NETWORK_IO_PROTOCOL_INCLUDED
+#define YOCTO_NETWORK_IO_PROTOCOL_INCLUDED 1
+
+#include "yocto/net/io/connexion.hpp"
+#include "yocto/net/socket-set.hpp"
+#include "yocto/associative/set.hpp"
+
+namespace yocto
+{
+	namespace network
+	{
+		
+		class protocol 
+		{
+		public:
+			virtual ~protocol() throw();
+			explicit protocol(size_t bs);
+
+			
+		protected:
+			typedef set<socket_address,connexion> connDB;
+			socket_set   sock_db;
+			connDB       conn_db;
+		public:
+			delay        waiting;
+		protected:
+			io_cache     cache; 
+			
+		private:
+			YOCTO_DISABLE_COPY_AND_ASSIGN(protocol);
+		};
+		
+	
+		class server_protocol : public protocol
+		{
+		public:
+			
+			
+		private:
+			YOCTO_DISABLE_COPY_AND_ASSIGN(server_protocol);
+		};
+		
+		
+	}
+}
+
+
+#endif
