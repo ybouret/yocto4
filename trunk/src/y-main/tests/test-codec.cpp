575ea2c17bd80e8ce55387539846d5ec9e84175c
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-02-06 16:13:30 +0000

    CODEC: in main BWT and MTF

--- trunk/src/y-main/tests/test-codec.cpp
+++ trunk/src/y-main/tests/test-codec.cpp
@@ -1,5 +1,7 @@
 #include "yocto/utest/run.hpp"
 #include "yocto/code/bwt.hpp"
+#include "yocto/code/mtf.hpp"
+
 #include "yocto/string.hpp"
 #include "yocto/sequence/vector.hpp"
 
@@ -8,9 +10,13 @@
 using namespace yocto;
 
 
+
 YOCTO_UNIT_TEST_IMPL(codec)
 {
 
+	core::move_to_front mtf_encoder;
+	core::move_to_front mtf_decoder;
+	
 	string         line;
 	vector<size_t> indices;
 	ios::icstream  fp( ios::cstdin );
@@ -23,9 +29,28 @@ YOCTO_UNIT_TEST_IMPL(codec)
 		const size_t pidx = core::bwt::encode( ostr.rw(), line.ro(), size, size ? &indices[1] : NULL);
 		std::cerr << "src: [" << line << "]"   << std::endl;
 		std::cerr << "dst: [" << ostr << "] @" << pidx << std::endl; 
+		
+		string enc;
+		std::cerr << "enc: [";
+		for( size_t i=0; i < size; ++i )
+		{
+ 			const char C = mtf_encoder.encode( ostr[i] );
+			enc.append( C );
+			std::cerr << make_visible(C);
+		}
+		std::cerr << "]" << std::endl;
+		string dec;
+		for( size_t i=0; i < size; ++i )
+		{
+			dec.append( mtf_decoder.decode( enc[i] ) );
+		}
+		std::cerr << "dec: [" << dec << "]" << std::endl; 
+
+		
+		
 		string       fstr = ostr;
 		core::bwt::decode( fstr.rw(), ostr.ro(), size, size ? &indices[1] : NULL, pidx );
-		std::cerr << "dec: [" << fstr << "]"   << std::endl;
+		std::cerr << "org: [" << fstr << "]"   << std::endl;
 		line.clear();
 	}
 	
---
+++ trunk/src/y-main/yocto/code/mtf.cpp
@@ -0,0 +1,75 @@
+#include "yocto/code/mtf.hpp"
+
+#include <cstring>
+
+namespace yocto {
+	
+	namespace core {
+		
+		move_to_front:: ~move_to_front() throw()
+		{
+			list_.reset();
+		}
+		
+		
+		move_to_front:: move_to_front() throw() :
+		list_(),
+		node_()
+		{
+			build();
+		}
+	
+		
+		void move_to_front::reset() throw() 
+		{
+			list_.reset();
+			build();
+		}
+		
+		void move_to_front:: build() throw() 
+		{
+			memset( node_, 0, sizeof(node_) );
+			node_t *node = node_;
+			int i=256;
+			while( --i >= 0 ) {
+				node->data = uint8_t(i);
+				list_.push_front( node );
+				++node;
+			}
+			assert( 256 == list_.size );
+		}
+		
+		uint8_t move_to_front:: encode( uint8_t x ) throw()
+		{
+			assert(list_.size == 256 );
+			node_t *node = list_.head;
+			int     indx = 0;
+			while( node->data != x ) {
+				++indx;
+				node = node->next;
+				assert( node != NULL );
+				assert( indx < 256   );
+			}
+			list_.move_to_front(node);
+			assert(list_.size == 256 );
+			return uint8_t(indx);
+		}
+		
+		uint8_t move_to_front:: decode( uint8_t x ) throw() 
+		{
+			assert(list_.size == 256 );
+			node_t *node = list_.head;
+			for(register size_t i=x;i>0;--i) {
+				node=node->next;
+				assert(NULL!=node);
+			}
+			list_.move_to_front(node);
+			return node->data;
+		}
+		
+		
+		
+	}
+	
+}
+
