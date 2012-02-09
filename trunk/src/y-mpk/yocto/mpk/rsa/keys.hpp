8bd28ab3f1d0a1973bc22e89a7acde42cf7de0e2
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-02-09 10:21:46 +0000

    MPK: rsa keys

---
+++ trunk/src/y-mpk/yocto/mpk/rsa/keys.hpp
@@ -0,0 +1,80 @@
+#ifndef YOCTO_MPK_RSA_KEYS_INCLUDED
+#define YOCTO_MPK_RSA_KEYS_INCLUDED 1
+
+
+#include "yocto/mpk/natural.hpp"
+
+namespace yocto
+{
+	
+	namespace mpk
+	{
+		
+		class rsa_key 
+		{
+		public:
+			virtual ~rsa_key() throw();
+			rsa_key( const rsa_key & );
+			
+			const natural modulus;
+			const size_t  maxbits; //!< #bits for modulus -1
+			
+			virtual natural compute( const natural & ) const = 0;
+			
+		protected:
+			explicit rsa_key( const natural &rsa_modulus );
+			
+		private:
+			YOCTO_DISABLE_ASSIGN(rsa_key);
+		};
+		
+		
+		class rsa_public_key : public rsa_key
+		{
+		public:
+			const natural publicExponent;
+			virtual natural compute( const natural & ) const;
+			
+			virtual ~rsa_public_key() throw();
+			rsa_public_key( const rsa_public_key &);
+			explicit rsa_public_key( const natural &rsa_modulus, const natural &rsa_publicExponent );
+			
+		private:
+			YOCTO_DISABLE_ASSIGN(rsa_public_key);
+		};
+		
+		class rsa_private_key : public rsa_public_key
+		{
+		public:
+			virtual ~rsa_private_key() throw();
+			explicit rsa_private_key(const natural  &rsa_modulus,
+									 const natural  &rsa_publicExponent,
+									 const natural  &rsa_privateExponent,
+									 const natural  &rsa_prime1,
+									 const natural  &rsa_prime2,
+									 const natural  &rsa_exponent1,
+									 const natural  &rsa_exponent2,
+									 const natural  &rsa_coefficient);
+			
+			rsa_private_key( const rsa_private_key & );
+			
+			virtual natural compute( const natural &x ) const;
+			
+		private:
+			const natural privateExponent; //!< (1/publicExponent) %( (prime1-1) * (prime2-1) )
+			const natural prime1;          //!< bigger  prime
+			const natural prime2;          //!< smaller prime
+			const natural exponent1;       //!< Chinese Remainder Theoreme exponent1   = d % (prime1-1)
+			const natural exponent2;       //!< Chinese Remainder Theoreme exponent2   = d % (prime2-1)
+			const natural coefficient;     //!< Chinese Remainder Theoreme coefficient = (1/prime2)  % prime1
+			YOCTO_DISABLE_ASSIGN( rsa_private_key );
+		};
+		
+		
+		
+		
+	}
+	
+}
+
+#endif
