a9145dbdb20c6c952fa817a8d8b6d7935b949f2e
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-02-12 14:51:25 +0000

    added codec interface

---
+++ src/y-main/yocto/ios/codec.hpp
@@ -0,0 +1,26 @@
+#ifndef YOCTO_IOS_CODEC_INCLUDED
+#define YOCTO_IOS_CODEC_INCLUDED 1
+
+#include "yocto/ios/istream.hpp"
+#include "yocto/ios/ostream.hpp"
+
+namespace yocto
+{
+    namespace ios
+    {
+        class codec : public istream, public ostream
+        {
+        public:
+            virtual ~codec() throw();
+
+        protected:
+            explicit codec() throw();
+            
+        private:
+            YOCTO_DISABLE_COPY_AND_ASSIGN(codec);
+        };
+    }
+    
+}
+
+#endif
