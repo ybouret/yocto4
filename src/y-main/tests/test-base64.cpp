1968375ebe6e5ade618ddd7784e35c055f91bd79
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-02-20 08:14:27 +0000

    base64 codec OK

--- src/y-main/tests/test-base64.cpp
+++ src/y-main/tests/test-base64.cpp
@@ -23,41 +23,56 @@ YOCTO_UNIT_TEST_IMPL(base64)
     // generate reverse table
     //
     //--------------------------------------------------------------------------
-    for( size_t i=0; i < 256; ++i )
+    if( false )
     {
-        const char    C   = char(i);
-        if( C == '=' )
+        for( size_t i=0; i < 256; ++i )
         {
-            fprintf( stderr, "0x40" );
-        }
-        else
-        {
-            int           idx = search_char( C, base64::encoder::table_iso );
-            if( idx < 0 ) idx = search_char( C, base64::encoder::table_url );
-            if( idx < 0 )
-                fprintf( stderr, "0xff");
+            const char    C   = char(i);
+            if( C == '=' )
+            {
+                fprintf( stderr, "0x40" );
+            }
             else
-                fprintf( stderr, "%4d", idx );
+            {
+                int           idx = search_char( C, base64::encoder::table_iso );
+                if( idx < 0 ) idx = search_char( C, base64::encoder::table_url );
+                if( idx < 0 )
+                    fprintf( stderr, "0xff");
+                else
+                    fprintf( stderr, "%4d", idx );
+            }
+            if( i < 255 )
+                fprintf( stderr, ",");
+            if( 0 == ( (i+1) % 16 ) )
+                fprintf( stderr, "\n");
+            else
+                fprintf( stderr, " ");
         }
-        if( i < 255 )
-            fprintf( stderr, ",");
-        if( 0 == ( (i+1) % 16 ) )
-            fprintf( stderr, "\n");
-        else
-            fprintf( stderr, " ");
     }
-    
     
     base64::encoder enc;
     ios::icstream   input( ios::cstdin );
-    string line;
+    string          line;
+    base64::decoder dec;
+    std::cerr << "Ready for input:" << std::endl;
     while( input.read_line(line) > 0 )
     {
+        std::cerr << "-- encoding" << std::endl;
         enc.append( line );
         enc.flush();
         char C;
         while( enc.query(C) )
+        {
             std::cerr << C;
+            dec.write(C);
+        }
+        dec.flush();
+        std::cerr << std::endl;
+        std::cerr << "-- decoding" << std::endl;
+        while( dec.query(C) )
+        {
+            std::cerr << C;
+        }
         std::cerr << std::endl;
         
         line.clear();
