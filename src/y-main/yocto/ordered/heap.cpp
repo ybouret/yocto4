805c57d05d1fb43e4da6d50929022f04e0d102a9
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-03-06 08:23:58 +0000

    initial virtual heap

---
+++ src/y-main/yocto/ordered/heap.cpp
@@ -0,0 +1,11 @@
+#include "yocto/ordered/heap.hpp"
+#include "yocto/memory/global.hpp"
+
+namespace yocto
+{
+    namespace hidden
+    {
+        const char heap_name[] = "heap";
+    }
+    
+}
---
+++ src/y-main/yocto/ordered/heap.hpp
@@ -0,0 +1,74 @@
+#ifndef YOCTO_HEAP_INCLUDED
+#define YOCTO_HEAP_INCLUDED 1
+
+#include "yocto/comparator.hpp"
+#include "yocto/memory/global.hpp"
+#include "yocto/container/container.hpp"
+#include "yocto/type-traits.hpp"
+
+namespace yocto 
+{
+    
+    
+    namespace hidden
+    {
+        extern const char heap_name[];
+    }
+    
+    template <
+	typename T,
+	typename COMPARATOR = comparator<T>,
+	typename ALLOCATOR  = memory::global::allocator
+	>
+	class heap : public container
+    {
+    public:
+        YOCTO_ARGUMENTS_DECL_T;
+        explicit heap() throw() : hmem_(), size_(0), maxi_(0), data_(NULL), comp_() {}
+        explicit heap( size_t n, const  as_capacity_t & ) :
+        hmem_(),
+        size_(0),
+        maxi_(n),
+        data_( hmem_.template acquire_as<slot_t>( maxi_ ) ),
+        comp_()
+        {
+        }
+        
+        virtual ~heap() throw() { kill(); }
+        
+        virtual size_t size()     const throw() { return size_; }
+        virtual size_t capacity() const throw() { return maxi_; }
+        virtual const char *name() const throw() { return hidden::heap_name; }
+        
+        virtual void free()    throw() { size_ = 0; }
+        virtual void release() throw() { kill();    }
+        virtual void reserve( size_t n ) 
+        {
+            if( n > 0 )
+            {
+                size_t  new_maxi = maxi_ + n;
+                slot_t *new_data = hmem_.template acquire_as<slot_t>( new_maxi );
+                hmem_.template release_as<slot_t>( data_, maxi_ );
+                data_ = new_data;
+                maxi_ = new_maxi;
+            }
+        }
+        
+    private:
+        typedef mutable_type *slot_t;
+        ALLOCATOR      hmem_;
+        size_t         size_;
+        size_t         maxi_;
+        slot_t        *data_;
+        COMPARATOR     comp_;
+        YOCTO_DISABLE_COPY_AND_ASSIGN(heap);
+        inline void kill() throw()
+        { 
+            size_ = 0;
+            hmem_.template release_as<slot_t>( data_, maxi_ );
+        }
+    };
+    
+}
+
+#endif
