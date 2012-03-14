dd4ae74e2a38ed420dc040ad08148fab4bc946b1
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-03-14 16:29:57 +0000

    ODE: stiff solver initial

---
+++ trunk/src/y-mk/yocto/math/ode/stiff-solver.hpp
@@ -0,0 +1,27 @@
+#ifndef YOCTO_STIFF_SOLVER_INCLUDED
+#define YOCTO_STIFF_SOLVER_INCLUDED 1
+
+#include "yocto/math/ode/stiff-step.hpp"
+
+namespace yocto
+{
+    namespace math 
+    {
+        namespace ode
+        {
+            
+            template <typename T>
+            class stiff_solver : public lw_arrays<T,memory_type>
+            {
+            public:
+                
+                
+            private:
+                
+            };
+            
+        }
+    }
+}
+
+#endif
