acb1efd83b537a30560293dceae8eac2e7e52239
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-03-14 14:00:54 +0000

    ODE: start stiff

--- trunk/src/y-mk/yocto/math/ode/types.hpp
+++ trunk/src/y-mk/yocto/math/ode/types.hpp
@@ -4,6 +4,7 @@
 #include "yocto/functor.hpp"
 #include "yocto/sequence/array.hpp"
 #include "yocto/memory/global.hpp"
+#include "yocto/math/kernel/matrix.hpp"
 
 namespace yocto
 {
@@ -17,9 +18,11 @@ namespace yocto
 			
 			template <typename T> struct field
 			{
-				//! field( dydx[], x, y[] )
+				//! derivs( dydx[], x, y[] )
 				typedef functor<void,TL3(array<T>&,T,const array<T>&)> type;
-				
+                
+                //! jacobn( dfdx[], dfdy[][], x, y[] );
+				typedef functor<void,TL4(array<T> &, matrix<T> &, T, const array<T>&)> jacobian_type;
 			};
 			
 		}
