65419ca737d668e03394173c749797025bcca6e4
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-03-14 14:00:54 +0000

    ODE: start stiff

---
+++ src/y-mk/yocto/math/ode/ode-stiff-step.cxx
@@ -0,0 +1,19 @@
+#include "yocto/math/ztype.hpp"
+#include "yocto/math/ode/stiff-step.hpp"
+
+
+namespace yocto
+{
+    namespace math 
+    {
+        
+        namespace ode
+        {
+            template <> stiff_step<real_t>:: ~stiff_step() throw() {}
+            template <> stiff_step<real_t>:: stiff_step(const size_t num) :
+            lw_arrays<real_t, memory_type>(num)
+            {}
+        }
+        
+    }
+}
---
+++ src/y-mk/yocto/math/ode/stiff-step.hpp
@@ -0,0 +1,49 @@
+#ifndef YOCTO_ODE_STIFF_STEP_INCLUDED
+#define YOCTO_ODE_STIFF_STEP_INCLUDED 1
+
+#include "yocto/math/ode/types.hpp"
+#include "yocto/sequence/lw-arrays.hpp"
+#include "yocto/math/kernel/matrix.hpp"
+
+namespace yocto
+{
+    namespace math
+    {
+        
+        namespace ode
+        {
+            
+            template <typename T>
+            class stiff_step : public lw_arrays<T,memory_type>
+            {
+            public:
+                typedef typename field<T>::type          equation;
+                typedef typename field<T>::jacobian_type jacobian;
+                
+                virtual ~stiff_step() throw();
+                
+                virtual void operator()(array<T>       &y,
+                                        const array<T> &dydx,
+                                        T              &x,
+                                        T               htry,
+                                        T               eps,
+                                        array<T>       &yscal,
+                                        real_t         &hdid,
+                                        real_t         &hnext,
+                                        equation       &derivs,
+                                        jacobian       &jacobn
+                                        );
+                
+            private:
+                explicit stiff_step( const size_t num );
+                YOCTO_DISABLE_COPY_AND_ASSIGN(stiff_step);
+            };
+            
+        }
+        
+    }
+}
+
+
+
+#endif
