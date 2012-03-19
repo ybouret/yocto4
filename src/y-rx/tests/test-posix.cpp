d3905c2152ff8ac4d0ab34e7217fe5c99a1e5ca6
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-03-19 15:05:32 +0000

    RX: new implementation

---
+++ src/y-rx/tests/test-posix.cpp
@@ -0,0 +1,51 @@
+#include "yocto/utest/run.hpp"
+#include "yocto/rx/pattern/posix.hpp"
+#include "yocto/ios/ocstream.hpp"
+#include "yocto/auto-ptr.hpp"
+#include "yocto/rx/pattern/dict.hpp"
+
+#include <cstdlib>
+
+using namespace yocto;
+
+
+static inline void test_posix( const string &name, const regex::pattern &p )
+{
+	const string fdot = name + ".dot";
+	const string fpng = name + ".png";
+	
+	{
+		ios::ocstream fp( fdot, false );
+		p.graphviz( fp, "G" );
+	}
+	
+	const string cmd = "dot -Tpng " + fdot + " -o " + fpng;
+	std::cerr << cmd << std::endl;
+	system( cmd.c_str() );
+	
+}
+
+#define _TEST(NAME) \
+do { auto_ptr<regex::pattern> p( regex::posix::NAME() ); \
+ test_posix( #NAME, *p); \
+ db.record( #NAME, p.yield() ); \
+ p.reset( db.create( #NAME ) ); } while( false )
+
+YOCTO_UNIT_TEST_IMPL(posix)
+{
+	regex::pattern_dict db;
+	_TEST(lower);
+	_TEST(upper);
+	_TEST(digit);
+	_TEST(alpha);
+	_TEST(alnum);
+	_TEST(xdigit);
+	_TEST(blank);
+	_TEST(space);
+	_TEST(punct);
+	_TEST(word);
+	_TEST(dot);
+	_TEST(endl);
+	_TEST(cstring);
+}
+YOCTO_UNIT_TEST_DONE()
