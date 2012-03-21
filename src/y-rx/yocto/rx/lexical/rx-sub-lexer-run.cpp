54df395275daf8275ac939d73963055bfd4890bc
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-03-21 08:14:49 +0000

    initial sub_lexer

---
+++ src/y-rx/yocto/rx/lexical/rx-sub-lexer-run.cpp
@@ -0,0 +1,122 @@
+#include "yocto/rx/lexical/sub-lexer.hpp"
+#include "yocto/exception.hpp"
+#include "yocto/rx/source.hpp"
+
+namespace yocto
+{
+    namespace regex
+    {
+        
+        
+        bool sub_lexer:: process( source &src )
+        {
+            //==================================================================
+            //
+            // check lexer
+            //
+            //==================================================================
+            if( rules_.size <= 0 )
+                throw exception("sub_lexer['%s'] is EMPTY", &name[0] );
+            
+            //==================================================================
+            //
+            // test input
+            //
+            //==================================================================
+            if( !src.is_active() )
+            {
+                return false;
+            }
+            else 
+            {
+                pattern         *best = NULL;
+                lexical::rule   *todo = NULL;
+                lexical::rule   *r    = rules_.head; assert( r != NULL );
+                
+                //--------------------------------------------------------------
+                //
+                // find an accepting rule
+                //
+                //--------------------------------------------------------------
+                while( r != NULL )
+                {
+                    pattern *p = r->motif;
+                    if( p->accept(src) )
+                    {
+                        best = p;
+                        todo = r;
+                        break;
+                    }
+                    r=r->next;
+                }
+                
+                //--------------------------------------------------------------
+                //
+                // no matching rule
+                //
+                //--------------------------------------------------------------
+                if( !best )
+                {
+                    assert( src.peek() );
+                    const char C = src.peek()->data;
+                    if( C >= 32 && C < 127 )
+                        throw exception( "in sub_lexer['%s']: invalid char '%c'", &name[0], C );
+                    else
+                        throw exception("in sub_lexer['%s']: invalid char 0x%02x", &name[0], unsigned(C) );
+                }
+                
+                //--------------------------------------------------------------
+                //
+                // find a better rule ?
+                //
+                //--------------------------------------------------------------
+                
+                //--------------------------------------------------------------
+                //-- restore source for next probe
+                //--------------------------------------------------------------
+                src.uncpy( *best );
+                while( r != NULL )
+                {
+                    pattern *p = r->motif;
+                    if( p->accept(src) )
+                    {
+                        //------------------------------------------------------
+                        // new candidate
+                        //------------------------------------------------------
+                        if( p->size > best->size )
+                        {
+                            // new winner !
+                            best->clear();
+                            best = p;
+                            todo = r;
+                            src.uncpy( *best );
+                        }
+                        else
+                        {
+                            // too late !
+                            src.unget( *p );
+                            p->clear();
+                        }
+                    }
+                    r = r->next;
+                }
+                
+                //--------------------------------------------------------------
+                //
+                // apply the rule to do
+                //
+                //--------------------------------------------------------------
+                todo->apply();            //! the code
+                src.skip( best->size );   //! update the soruce
+                best->clear();            //! final clean up
+                
+            
+                
+                return true;
+            }
+            
+        }
+        
+    }
+    
+}
--- src/y-rx/yocto/rx/lexical/sub-lexer.hpp
+++ src/y-rx/yocto/rx/lexical/sub-lexer.hpp
@@ -8,28 +8,35 @@ namespace yocto
 {
     namespace regex
     {
-        namespace lexical
+        
+        class pattern_dict;
+       
+        class sub_lexer : public object, public counted
         {
-         
-            class sub_lexer : public object, public counted
-            {
-            public:
-                explicit sub_lexer( const string &id );
-                virtual ~sub_lexer() throw();
-                
-                const string   name;
-                const string & key() const throw();
-                
-                void make( pattern *p, const action &a );
-                
-                
-                
-            private:
-                rules rules_;
-                YOCTO_DISABLE_COPY_AND_ASSIGN(sub_lexer);
-            };
+        public:
+            explicit sub_lexer( const string &id );
+            explicit sub_lexer( const char   *id );
+            virtual ~sub_lexer() throw();
             
-        }
+            const string   name;
+            const string & key() const throw();
+            
+            void make( pattern *p, const lexical::action &a );
+            void make( const string &expr, const lexical::action &a, pattern_dict *dict = NULL );
+            void make( const char   *expr, const lexical::action &a, pattern_dict *dict = NULL );
+            
+            //! apply best rule
+            bool process( source &src );
+            
+            //! recover after a crash...
+            void reset() throw();
+            
+        private:
+            lexical::rules rules_;
+            YOCTO_DISABLE_COPY_AND_ASSIGN(sub_lexer);
+        };
+        
+        
     }
 }
 #endif
