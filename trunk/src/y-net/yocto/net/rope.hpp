4a979ab6136a9b1cec664e3fa4e84758ac89d0bb
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-04-05 15:33:20 +0000

    net.rope: in progress

---
+++ trunk/src/y-net/yocto/net/rope.hpp
@@ -0,0 +1,30 @@
+#ifndef YOCTO_NET_ROPE_INCLUDED
+#define YOCTO_NET_ROPE_INCLUDED 1
+
+#include "yocto/net/io/protocol.hpp"
+
+namespace yocto 
+{
+    namespace network
+    {
+        //! Request Oriented Protocol Element
+        namespace rope
+        {
+            
+            class server : public server_protocol
+            {
+            public:
+                
+                
+            private:
+                YOCTO_DISABLE_COPY_AND_ASSIGN(server);
+            };
+            
+        }
+        
+    }
+    
+}
+
+
+#endif
