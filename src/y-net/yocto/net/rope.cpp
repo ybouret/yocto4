fd7cf1f93d7c3019bf5f5e9e25a50069e0e7be5e
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-04-05 15:33:20 +0000

    net.rope: in progress

---
+++ src/y-net/yocto/net/rope.cpp
@@ -0,0 +1,16 @@
+#include "yocto/net/rope.hpp"
+
+namespace yocto 
+{
+    namespace network
+    {
+        //! Request Oriented Protocol Element
+        namespace rope
+        {
+            
+            
+        }
+        
+    }
+    
+}
