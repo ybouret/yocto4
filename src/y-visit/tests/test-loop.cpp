89a5eda2f9744a79fe0c577855416590f4bd2589
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-04-24 13:29:05 +0000

    VisIt: with custom interface

--- src/y-visit/tests/test-loop.cpp
+++ src/y-visit/tests/test-loop.cpp
@@ -4,13 +4,46 @@
 using namespace yocto;
 
 
-void
+static void
 ui_run_clicked(void *cbdata)
 {
     VisIt::Simulation *sim = (VisIt::Simulation *)cbdata;
     if(sim->master) fprintf(stderr,"ui_run_clicked\n");
     sim->runMode = VISIT_SIMMODE_RUNNING;
     VisItTimeStepChanged();
+}
+
+static void
+ui_halt_clicked(void *cbdata)
+{
+    VisIt::Simulation *sim = (VisIt::Simulation *)cbdata;
+    if(sim->master) fprintf(stderr,"ui_halt_clicked\n");
+    sim->runMode = VISIT_SIMMODE_STOPPED;
+    VisItTimeStepChanged();
+}
+
+static void
+ui_step_clicked(void *cbdata)
+{
+    VisIt::Simulation *sim = (VisIt::Simulation *)cbdata;
+    if(sim->master) fprintf(stderr,"ui_step_clicked\n");
+    sim->runMode = VISIT_SIMMODE_STOPPED;
+    VisIt::OneStep( *sim );
+    VisItTimeStepChanged();
+}
+
+static void
+ui_levels_changed(int value, void *cbdata)
+{
+    VisIt::Simulation *sim = (VisIt::Simulation *)cbdata;
+    if(sim->master) fprintf(stderr,"ui_levels_changed: %d\n", value);
+}
+
+static void
+ui_option_changed(int value, void *cbdata)
+{
+    VisIt::Simulation *sim = (VisIt::Simulation *)cbdata;
+    if(sim->master) fprintf(stderr,"ui_option_changed: %d\n", value);
 }
 
 YOCTO_UNIT_TEST_IMPL(loop)
@@ -27,7 +60,13 @@ YOCTO_UNIT_TEST_IMPL(loop)
     VisIt:: SetupParallel( MPI, sim_name, sim_comment, sim_path, &sim_ui);
     VisIt::Simulation sim(MPI);
     
-    VisItUI_clicked("RUN", ui_run_clicked, &sim);
+    VisItUI_clicked("RUN",         ui_run_clicked,     &sim);
+    VisItUI_clicked("HALT",        ui_halt_clicked,    &sim);
+    VisItUI_clicked("STEP",        ui_step_clicked,    &sim);
+
+    VisItUI_valueChanged("LEVELS", ui_levels_changed, &sim);
+    VisItUI_stateChanged("OPTION", ui_option_changed, &sim);
+    
     VisIt:: MainLoop( MPI, sim, true);
     
     
