0120b7db4b4d67708c280bb9a7a4056424793c5a
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-04-26 13:28:37 +0000

    Swamp: update

--- src/y-swamp/yocto/swamp/linear.cpp
+++ src/y-swamp/yocto/swamp/linear.cpp
@@ -4,6 +4,15 @@ namespace yocto
 {
     namespace swamp
     {
+        linear_base::  linear_base() throw() {}
+        linear_base:: ~linear_base() throw() {}
+
+        size_t linear_base:: compute_bytes( size_t items, size_t item_size ) throw()
+        {
+            assert(items>0);
+            assert(item_size>0);
+            return items  * item_size;
+        }
         
     }
     
