e0d384c9781d0f10d99c9f9ed3d955605a6dbf54
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-04-27 13:23:51 +0000

    Swamp: array3D

--- src/y-swamp/tests/test-array.cpp
+++ src/y-swamp/tests/test-array.cpp
@@ -1,7 +1,7 @@
 
 #include "yocto/utest/run.hpp"
 
-#include "yocto/swamp/array2d.hpp"
+#include "yocto/swamp/array3d.hpp"
 
 #include "yocto/code/rand.hpp"
 #include "yocto/swamp/data-block.hpp"
@@ -18,7 +18,7 @@ static inline void display( const LAYOUT &L )
 
 static inline coord1D rand1()
 {
-    return 50 - 100 * alea<double>();
+    return 20 - 40 * alea<double>();
 }
 
 static inline coord2D rand2()
@@ -26,7 +26,7 @@ static inline coord2D rand2()
     return coord2D( rand1(), rand1() );
 }
 
-//static inline coord3D rand3() {    return coord3D( rand1(), rand1(), rand1() ); }
+static inline coord3D rand3() {    return coord3D( rand1(), rand1(), rand1() ); }
 
 
 YOCTO_UNIT_TEST_IMPL(array)
@@ -64,8 +64,28 @@ YOCTO_UNIT_TEST_IMPL(array)
                     A[j][i] = i*j;
                 }
             }
-            
         }
+        
+        {
+            const layout3D  L( rand3(), rand3() );
+            array3D<double> A(L);
+            data_block      mb(A);
+            display(A);
+            A.ldz();
+            
+            for( unit_t k=A.lower.z; k <= A.upper.z; ++k )
+            {
+                for( unit_t j=A.lower.y; j <= A.upper.y; ++j )
+                {
+                    for( unit_t i=A.lower.x; i <= A.upper.x; ++i )
+                    {
+                        A[k][j][i] = i*j*k;
+                    }
+                }
+            }
+        }
+        
+        
     }
 }
 YOCTO_UNIT_TEST_DONE()
