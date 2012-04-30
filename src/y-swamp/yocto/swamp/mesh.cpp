427ddb3c3292105493b2c370b624866113923bf7
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-04-30 07:50:15 +0000

    initial mesh

---
+++ src/y-swamp/yocto/swamp/mesh.cpp
@@ -0,0 +1,14 @@
+#include "yocto/swamp/mesh.hpp"
+
+namespace yocto 
+{
+    
+    namespace swamp 
+    {
+        
+        mesh:: ~mesh() throw() {}
+        mesh::  mesh( array_db &db ) throw() :adb( db ) {}
+        
+    }
+    
+}
