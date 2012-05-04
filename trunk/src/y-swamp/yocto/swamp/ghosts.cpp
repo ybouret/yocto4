499fcf971ae7cb2fdc6d6d1866adad28c890e22d
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-05-04 12:16:16 +0000

    swamp: factorized ghosts

--- trunk/src/y-swamp/yocto/swamp/ghosts.cpp
+++ trunk/src/y-swamp/yocto/swamp/ghosts.cpp
@@ -98,7 +98,155 @@ namespace yocto
         
         local_ghosts_pair:: ~local_ghosts_pair() throw() {}
         
+        ////////////////////////////////////////////////////////////////////////
+        //
+        // ghosts_base
+        //
+        ////////////////////////////////////////////////////////////////////////
+        ghosts_base:: ghosts_base( size_t num_ghosts ) throw() :
+        count(num_ghosts),
+        num_offsets(0)
+        {
+        }
         
+        ghosts_base:: ~ghosts_base() throw() {}
+        
+        
+        ////////////////////////////////////////////////////////////////////////
+        //
+        // local_ghosts
+        //
+        ////////////////////////////////////////////////////////////////////////
+        local_ghosts:: local_ghosts( size_t num_ghosts, size_t dim ) :
+        ghosts_base( num_ghosts ),
+        lower( ghost::get_lower_position(dim) ),
+        upper( ghost::get_upper_position(dim) )
+        {
+        }
+        
+        local_ghosts:: ~local_ghosts() throw() {}
+        
+        void local_ghosts:: transfer( const linear_handles & handles ) const throw()
+        {
+            assert( num_offsets == lower.inside.offsets.size() );
+            assert( num_offsets == lower.mirror.offsets.size() );
+            assert( num_offsets == upper.inside.offsets.size() );
+            assert( num_offsets == upper.mirror.offsets.size() );                
+            const size_t num_handles = handles.size();
+            for( size_t i=num_offsets; i>0; --i )
+            {
+                const size_t lower_mirror = lower.mirror.offsets[i];
+                const size_t lower_inside = lower.inside.offsets[i];
+                const size_t upper_mirror = upper.mirror.offsets[i];
+                const size_t upper_inside = upper.inside.offsets[i];
+                for( size_t j=num_handles; j>0; --j )
+                {
+                    linear_base *A = handles[j]; assert(A!=NULL);
+                    A->local_copy( lower_mirror, lower_inside );
+                    A->local_copy( upper_mirror, upper_inside );
+                }
+            }
+        }
+        
+        
+        ////////////////////////////////////////////////////////////////////////
+        //
+        // async_ghost_pair
+        //
+        ////////////////////////////////////////////////////////////////////////
+        async_ghosts_pair:: async_ghosts_pair( ghost::position source ) throw() :
+        inner( source ),
+        outer( source )
+        {
+        }
+        
+        async_ghosts_pair:: ~async_ghosts_pair() throw()
+        {
+        }
+
+        
+        ////////////////////////////////////////////////////////////////////////
+        //
+        // async_ghosts
+        //
+        ////////////////////////////////////////////////////////////////////////
+        async_ghosts:: async_ghosts( size_t num_ghosts, ghost::position source, int a_peer ) :
+        ghosts_base( num_ghosts ),
+        self( source ),
+        peer( a_peer ),
+        inner_buf(NULL),
+        outer_buf(NULL),
+        length(0),
+        iolen(0)
+        {
+        }
+        
+        async_ghosts:: ~async_ghosts() throw()
+        {
+            if( inner_buf )
+            {
+                assert( outer_buf != NULL );
+                memory::kind<memory::global>::release_as<uint8_t>( inner_buf, iolen );
+                outer_buf = NULL;
+                length    = 0;
+            }
+        }
+
+        
+        //! allocate memory once offsets are computed
+        void async_ghosts:: allocate_for( linear_handles &handles )
+        {
+            assert( NULL == inner_buf );
+            assert( NULL == outer_buf );
+            assert( 0    == iolen );
+            assert( 0    == length );
+            for( size_t j=handles.size(); j>0; --j )
+            {
+                length += handles[j]->item_size();
+            }
+            length *= num_offsets;
+            iolen   = length * 2;
+            try
+            {
+                inner_buf  = memory::kind<memory::global>::acquire_as<uint8_t>( iolen );
+            }
+            catch(...) { length = 0; throw; }
+            outer_buf  = inner_buf + length;
+        }
+        
+        //! store inner data into inner_buf
+        void async_ghosts:: store_inner( const linear_handles &handles ) throw()
+        {
+            uint8_t *ptr = inner_buf;
+            for( size_t i=num_offsets; i>0; --i )
+            {
+                const size_t k = self.inner.offsets[i];
+                for( size_t j=handles.size(); j>0; --j )
+                {
+                    const linear_base &A = *handles[j];
+                    A.async_store(ptr,k);
+                    assert( ptr <= inner_buf + length );
+                }
+            }
+        }
+        
+        //! query outer data from outer_buf
+        void async_ghosts:: query_outer( const linear_handles &handles ) const throw()
+        {
+            const uint8_t *ptr = outer_buf;
+            for( size_t i=num_offsets; i>0; --i )
+            {
+                const size_t k = self.outer.offsets[i];
+                for( size_t j=handles.size(); j>0; --j )
+                {
+                    linear_base &A = *handles[j];
+                    A.async_query(ptr,k);
+                    assert( ptr <= outer_buf + length );
+                }
+            }
+            
+        }
+
         
     }
 }
