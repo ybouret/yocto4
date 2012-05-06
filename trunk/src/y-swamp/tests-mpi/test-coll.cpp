04b6747d75228873e1a0d198138aa65f63300702
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-05-06 12:49:49 +0000

    swamp: failure on collect0 2D

---
+++ trunk/src/y-swamp/tests-mpi/test-coll.cpp
@@ -0,0 +1,52 @@
+#include "yocto/utest/run.hpp"
+#include "yocto/swamp/mpi.hpp"
+#include "yocto/ios/ocstream.hpp"
+#include "yocto/auto-ptr.hpp"
+
+using namespace yocto;
+using namespace swamp;
+
+
+YOCTO_UNIT_TEST_IMPL(coll2D)
+{
+    
+    mpi & MPI = mpi::init( &argc, &argv );
+    const int rank = MPI.CommWorldRank;
+    const int size = MPI.CommWorldSize;
+    fields_setup<layout2D> F;
+    Y_SWAMP_DECL_VAR(F, "A", array2D<double> );
+    ghosts_setup<coord2D> G;
+    if( size > 1 )
+    {
+        G.lower.count.y = 2;
+        G.lower.peer.y  = MPI.CommWorldPrev();
+        G.upper.count.y = 2;
+        G.upper.peer.y  = MPI.CommWorldNext();
+    }
+    else {
+        G.local.count.y = 2;
+    }
+    
+    const layout2D full_layout( coord2D(1,1), coord2D(20,20) );
+    const layout2D L = full_layout.split(rank,size);
+    dataspace<layout2D> D( L, G, F );
+    
+    
+    array2D<double> &A = D["A"].as< array2D<double> >();
+    A.set_all( L, rank+1 );
+    auto_ptr< array2D<double> > pA0;
+    if( 0 == rank )
+        pA0.reset( new array2D<double>(full_layout) );
+    
+    D.prepare_ghosts();
+    mpi::Requests requests( D.num_requests() );
+    
+    _mpi::init_exchange(MPI, D, requests);
+    _mpi::wait_exchange(MPI, D, requests);
+    
+    _mpi::collect0(MPI, pA0.__get(), A, full_layout);
+    
+    
+    
+}
+YOCTO_UNIT_TEST_DONE()
