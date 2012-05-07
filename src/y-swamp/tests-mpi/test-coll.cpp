49958a7b0cb5d5bbc421b2ba5ffaabb8ed543dfa
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-05-07 07:14:11 +0000

    swamp: collect 3D ok

--- src/y-swamp/tests-mpi/test-coll.cpp
+++ src/y-swamp/tests-mpi/test-coll.cpp
@@ -1,5 +1,9 @@
 #include "yocto/utest/run.hpp"
+
+#include "yocto/swamp/common.hpp"
 #include "yocto/swamp/mpi.hpp"
+#include "yocto/swamp/rwops.hpp"
+
 #include "yocto/ios/ocstream.hpp"
 #include "yocto/auto-ptr.hpp"
 
@@ -57,5 +61,72 @@ YOCTO_UNIT_TEST_IMPL(coll2D)
     
 }
 YOCTO_UNIT_TEST_DONE()
+
+YOCTO_UNIT_TEST_IMPL(coll3D)
+{
+    
+    typedef workspace<layout3D,double,rmesh> W3;
+    
+    mpi & MPI = mpi::init( &argc, &argv );
+    const int rank = MPI.CommWorldRank;
+    const int size = MPI.CommWorldSize;
+    fields_setup<layout3D> F;
+    Y_SWAMP_DECL_VAR(F, "A", array3D<double> );
+    ghosts_setup<coord3D> G;
+    if( size > 1 )
+    {
+        G.lower.count.z = 2;
+        G.lower.peer.z  = MPI.CommWorldPrev();
+        G.upper.count.z = 2;
+        G.upper.peer.z  = MPI.CommWorldNext();
+    }
+    else 
+    {
+        G.local.count.z = 2;
+    }
+    
+    const layout3D full_layout( coord3D(1,1,1), coord3D(10,10,100) );
+    const layout3D L = full_layout.split(rank,size);
+    W3 D( L, G, F );
+    typedef vertex3D<double>::type V3D;
+    const   region3D<double>::type R( V3D(-1,-1,-1), V3D(1,1,1) );
+    
+    D.mesh.regular_map_to(R, full_layout);
+    
+    
+    array3D<double> &A = D["A"].as< array3D<double> >();
+    A.set_all( L, rank+1 );
+    auto_ptr< W3 > pW;
+    array3D<double> *pA0 = NULL;
+    if( 0 == rank )
+    {
+        const ghosts_setup<coord3D> no_ghosts;
+        pW.reset( new W3(full_layout,no_ghosts,F) );
+        pA0 = &  (*pW)["A"].as< array3D<double> >();
+        pW->mesh.regular_map_to(R,full_layout);
+    }
+    
+    D.prepare_ghosts();
+    mpi::Requests requests( D.num_requests() );
+    
+    _mpi::init_exchange(MPI, D, requests);
+    _mpi::wait_exchange(MPI, D, requests);
+    
+    _mpi::collect0(MPI, pA0, A, full_layout);
+    
+    vector<string> var;
+    var.push_back( "A" );
+    
+    if( 0 == rank )
+    {
+        rwops<double>::save_vtk("full.vtk", "full3D", *pW, var, full_layout);
+    }
+    
+    rwops<double>::save_vtk( vformat("part%d.vtk", rank), "part3D", D, var, L);
+    
+    
+}
+YOCTO_UNIT_TEST_DONE()
+
 
 
