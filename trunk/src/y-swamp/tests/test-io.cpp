b67a1a04324e4538512c4321be354e6246f5233a
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-05-10 15:42:05 +0000

    swamp: data query/store

--- trunk/src/y-swamp/tests/test-io.cpp
+++ trunk/src/y-swamp/tests/test-io.cpp
@@ -13,6 +13,8 @@ using namespace swamp;
 YOCTO_UNIT_TEST_IMPL(io)
 {
     typedef array1D<double> Array1D;
+    typedef array2D<double> Array2D;
+    typedef array3D<double> Array3D;
     {
         fields_setup<layout1D>      F;
         Y_SWAMP_DECL_VAR(F, "B", Array1D );
@@ -24,9 +26,21 @@ YOCTO_UNIT_TEST_IMPL(io)
         variables                   var;
         var << "A" << "B";
         dataIO::make_loader(ld, D, var);
+        vector<double>  values(2,0);
         
-        dataIO::process<double,Array1D>(ld,D);
+        Array1D &A = D["A"].as<Array1D>();
+        Array1D &B = D["B"].as<Array1D>();
+        A.set_all(A,1);
+        B.set_all(B,2);
         
+        dataIO::query<double,Array1D>(values,ld,0);
+        std::cerr << "values=" << values << std::endl;
+        for( size_t i=D.in_layout.size(); i>0;--i)
+        {
+            const size_t j = D.in_layout[i];
+            dataIO::query<double,Array1D>(values,ld,j);
+            dataIO::store<double,Array1D>(ld,values,j);
+        }
     }
     
     {
