28c9d4437f71b79db98dc321adf7c9a970134aaa
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-05-10 06:33:22 +0000

    swamp: done current vtk writer

--- trunk/src/y-swamp/yocto/swamp/vtk-writer.hpp
+++ trunk/src/y-swamp/yocto/swamp/vtk-writer.hpp
@@ -3,7 +3,8 @@
 
 #include "yocto/swamp/workspace.hpp"
 #include "yocto/swamp/rmesh.hpp"
-#include "yocto/ios/ostream.hpp"
+#include "yocto/swamp/triangle.hpp"
+
 #include "yocto/string.hpp"
 #include "yocto/associative/set.hpp"
 #include "yocto/type-spec.hpp"
@@ -87,14 +88,14 @@ namespace yocto
                 const array1D<T> &X = mesh.X(); assert(X.lower>=sub.lower.x); assert(X.upper<=sub.upper.x);
                 const array1D<T> &Y = mesh.Y(); assert(Y.lower>=sub.lower.y); assert(Y.upper<=sub.upper.y);
                 const array1D<T> &Z = mesh.Z(); assert(Z.lower>=sub.lower.z); assert(Z.upper<=sub.upper.z);
-
+                
                 write_rmesh_sub(fp,sub);
                 write_axis_sub(fp, &X[sub.lower.x], sub.width.x, sizeof(T), r, 'X');
                 write_axis_sub(fp, &Y[sub.lower.y], sub.width.y, sizeof(T), r, 'Y');
                 write_axis_sub(fp, &Z[sub.lower.z], sub.width.z, sizeof(T), r, 'Z' );
             }
-
-               
+            
+            
             //! write POINT_DATA
             void prolog( ios::ostream &fp, const string &name, size_t num, const record &r) const;
             
@@ -103,7 +104,7 @@ namespace yocto
             
             //! write a 2D array (after its mesh)
             void write_array( ios::ostream &fp, const string &name, const varray &arr, const layout2D &full, const layout2D &sub) const;
-                       
+            
             //! write a 3D array (after its mesh)
             void write_array( ios::ostream &fp, const string &name, const varray &arr, const layout3D &full, const layout3D &sub) const;
             
@@ -122,14 +123,38 @@ namespace yocto
                     write_array(fp, name, arr, wksp.outline, sub);
                 }
             }
-
+            
+            template <typename T>
+            inline void save( const string &filename, const string &title, const array< triangle3D<T> > &triangles ) const
+            {
+                const unsigned nt = triangles.size();
+                ios::ocstream fp( filename, false );
+                header(fp,title);
+                fp("DATASET POLYDATA\n");
+                fp("POINTS %u float\n", nt * 3 );
+                const record &r = (*this)[ typeid( typename vertex3D<T>::type ) ];
+                for(size_t i=1; i <= nt; ++i )
+                {
+                    const triangle3D<T> &t = triangles[i];
+                    for( size_t j=0; j <3; ++j )
+                    {
+                        write1(fp, &t[j], r);
+                    }
+                }
+                fp("POLYGONS %u %u\n", nt, 4 * nt );
+                unsigned count = 0;
+                for(size_t i=1; i <= nt; ++i, count += 3)
+                {
+                    fp("3 %u %u %u\n", count, count+1, count+2 );
+                }
+            }
             
         private:            
             vtk_format              format;
             set<type_spec,record>   out_db;
             void  write_rmesh_sub( ios::ostream &fp, const layout2D &sub ) const;
             void  write_rmesh_sub( ios::ostream &fp, const layout3D &sub ) const;
-
+            
             void  write_axis_sub( ios::ostream &fp, const void *data, size_t num, size_t item_size, const record &r, char axis_id ) const;
             
             YOCTO_DISABLE_COPY_AND_ASSIGN(vtk_writer);
