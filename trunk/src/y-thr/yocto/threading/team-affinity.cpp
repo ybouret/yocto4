e62e0dccc413a8642aba4a49c9995e0772ab1109
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-05-18 08:54:01 +0000

    upgraded team affinity

--- trunk/src/y-thr/yocto/threading/team-affinity.cpp
+++ trunk/src/y-thr/yocto/threading/team-affinity.cpp
@@ -1,27 +1,3 @@
-
-#if 0
-// thread placement is not supported on MacOSX
-#if defined(_WIN32)
-#	include "yocto/code/utils.hpp"
-#else
-#	if defined(__GNUC__)
-#		if defined(__linux__) 
-#			define YOCTO_CPU_SET_PTHREAD 1
-#			define YOCTO_CPU_SET cpu_set_t
-#		endif
-#		if defined(__FreeBSD__)
-#			include <pthread_np.h>
-#			define YOCTO_CPU_SET_PTHREAD 1
-#			define YOCTO_CPU_SET cpuset_t
-#		endif
-#	endif
-#endif
-
-#if defined(_WIN32) || defined(YOCTO_CPU_SET_PTHREAD)
-#	define YOCTO_THREAD_AFFINITY 1
-#endif
-#endif
-
 #include "yocto/threading/team.hpp"
 #include "yocto/threading/thread.hpp"
 
@@ -35,37 +11,6 @@ namespace yocto
 	namespace threading
 	{
 		
-#if 0
-		//----------------------------------------------------------------------
-		// Win32 API wrapper
-		//----------------------------------------------------------------------
-#if defined(_WIN32)
-		static inline void __assign( thread::handle_t h, const size_t j )
-		{
-			const DWORD_PTR mask = DWORD_PTR(1) << j;
-			if( ! ::SetThreadAffinityMask( h, mask ) )
-			{
-				const DWORD err = ::GetLastError();
-				throw win32::exception( err, "::SetThreadAffinityMask" );
-			}
-		}
-#endif
-		
-		//----------------------------------------------------------------------
-		// pthread wrapper
-		//----------------------------------------------------------------------
-#if defined(YOCTO_CPU_SET_PTHREAD)
-		static  void __assign( thread::handle_t h, const size_t j )
-		{
-			YOCTO_CPU_SET cpu_set;
-			CPU_ZERO(  &cpu_set );
-			CPU_SET(j, &cpu_set );
-			const int err = pthread_setaffinity_np( h, sizeof(YOCTO_CPU_SET), &cpu_set );
-			if( err != 0 )
-				throw libc::exception( err, "pthread_setaffinity_np" );
-		}
-#endif
-#endif
 		
 #include "./team-member.hxx"
 		
