0d022e8b535969a2cc87b2926ee216ffd2ba735a
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-05-28 12:12:32 +0000

    lang:syntax initial

---
+++ trunk/src/y-lang/yocto/lang/syntax/rule.hpp
@@ -0,0 +1,20 @@
+#ifndef YOCTO_LANG_SYNTAX_RULE_INCLUDED
+#define YOCTO_LANG_SYNTAX_RULE_INCLUDED 1
+
+#include "yocto/lang/lexer.hpp"
+
+namespace yocto 
+{
+    namespace lang 
+    {
+        namespace syntax 
+        {
+            
+            
+            
+        }
+    }
+}
+
+
+#endif
