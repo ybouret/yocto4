2310ea103ce0526f3722d12cc5fe8894fd1c8d7f
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-05-29 07:36:40 +0000

    syntax: rule update

--- trunk/src/y-lang/yocto/lang/syntax/rule.cpp
+++ trunk/src/y-lang/yocto/lang/syntax/rule.cpp
@@ -1,1 +1,28 @@
 #include "yocto/lang/syntax/rule.hpp"
+
+namespace yocto 
+{
+    namespace lang 
+    {
+        namespace syntax 
+        {
+            
+            rule:: ~rule() throw() {}
+            
+            
+            rule:: rule( const string &id ) : 
+            name(id) 
+            {}
+            
+            rule:: rule( const rule &other ) :
+            name( other.name )
+            {
+            }
+            
+            
+            
+        }
+
+    }
+
+}
--- trunk/src/y-lang/yocto/lang/syntax/rule.hpp
+++ trunk/src/y-lang/yocto/lang/syntax/rule.hpp
@@ -10,7 +10,24 @@ namespace yocto
         namespace syntax 
         {
             
-            
+            class rule : public object 
+            {
+            public:
+                const string name;
+                virtual ~rule() throw();
+                
+                virtual rule *clone() const = 0;
+                virtual bool  accept( lexer &lx ) = 0;
+                
+                
+                
+            protected:
+                explicit rule( const string &id );
+                rule( const rule & );
+                
+            private:
+                YOCTO_DISABLE_ASSIGN(rule);
+            };
             
         }
     }
