a61fbe9d461a9bd9b8e42371bd7ac4a8ff120b76
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-05-29 15:14:25 +0000

    start json scanner

--- src/y-json/yocto/json/json-parser.cpp
+++ src/y-json/yocto/json/json-parser.cpp
@@ -2,9 +2,12 @@
 #include "yocto/exception.hpp"
 #include "yocto/rx/pattern/basic.hpp"
 #include "yocto/rx/source.hpp"
+#include "yocto/lang/lexer.hpp"
+#include "yocto/auto-ptr.hpp"
 
 namespace yocto 
 {
+    using namespace lang;
     
     namespace JSON
     {
@@ -14,14 +17,27 @@ namespace yocto
         static const char end_array[]  = "\\]";
         static const char NumberRX[]   = "[:digit:]+";
         
-        class Parser :: Impl
+        class Parser :: Impl : public lexer
         {
         public:
-                       
-            
-            Impl() 
+            enum status_type
             {
-               
+                WaitingForArrayOrObject
+            };
+            
+            
+            lexical::scanner  &scan;
+            status_type        status;
+            
+            Impl() : lexer( "JSON::Parser" ),
+            scan( first() ),
+            status( WaitingForArrayOrObject )
+            {
+                
+                scan.make( "BLANKS", "[ \t\v\f]+", & scan.discard );
+                scan.make( "ENDL",   "[:endl:]",   & scan.newline );
+                scan.make( "NEW_ARRAY", new_array, this, & Impl::NewArray ); 
+                scan.make( "END_ARRAY", end_array, this, & Impl::NewArray ); 
             }
             
             virtual ~Impl() throw()
@@ -35,11 +51,34 @@ namespace yocto
             //==================================================================
             void call( Value &value, ios::istream &fp )
             {
-               
+                value.nullify();
+                reset();
+                regex::source src(fp);
+                
+                lexeme *lx = NULL;
+                while( 0 != (lx=next_lexeme(src)) )
+                {
+                    delete lx;
+                }
+                
             }
             
-                       
-                      
+            //==================================================================
+            // Array
+            //==================================================================
+            bool NewArray( const regex::token & )
+            {
+                std::cerr << "NewArray@line " << line << std::endl;
+                return false;
+            }
+            
+            bool EndArray( const regex::token & )
+            {
+                std::cerr << "EndArray@line" << line << std::endl;
+                return false;
+            }
+            
+            
         private:
             YOCTO_DISABLE_COPY_AND_ASSIGN(Impl);
         };
