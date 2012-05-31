bfae1e7c9e108f84079ff3edc76fcb60e57e1243
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-05-31 07:50:18 +0000

    parse-node upgrade

--- src/y-lang/yocto/lang/syntax/parse-node.hpp
+++ src/y-lang/yocto/lang/syntax/parse-node.hpp
@@ -2,6 +2,7 @@
 #define YOCTO_LANG_SYNTAX_PARSE_NODE_INCLUDED 1
 
 #include "yocto/lang/lexer.hpp"
+#include "yocto/code/round.hpp"
 
 namespace yocto 
 {
@@ -10,19 +11,25 @@ namespace yocto
         
         namespace syntax
         {
+            
             class parse_node : public object
             {
             public:
                 typedef core::list_of<parse_node> child_list;
-                const string &label; //!< reference to the corresponding rule label
+                const string &label;    //!< reference to the corresponding rule label
+                const bool    terminal; //!< true is terminal => one lexeme
                 parse_node   *prev;
                 parse_node   *next;
                 parse_node   *parent;
-                child_list    children;
                 
                 virtual ~parse_node() throw();
+                explicit parse_node( const string &label_ref, lexeme *lx ) throw();
+                
+                lexeme     * lex() throw();
+                child_list & children() throw();
                 
             private:
+                uint64_t wksp[ YOCTO_U64_FOR_ITEM(child_list) ];
                 YOCTO_DISABLE_COPY_AND_ASSIGN(parse_node);
             };
         }
