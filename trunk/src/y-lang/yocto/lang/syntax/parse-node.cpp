e785d0a20405c121687542d1c6c7cbe7cff7a910
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-05-31 07:50:18 +0000

    parse-node upgrade

--- trunk/src/y-lang/yocto/lang/syntax/parse-node.cpp
+++ trunk/src/y-lang/yocto/lang/syntax/parse-node.cpp
@@ -1,4 +1,5 @@
 #include "yocto/lang/syntax/parse-node.hpp"
+#include <cstring>
 
 namespace yocto 
 {
@@ -9,8 +10,49 @@ namespace yocto
         {
             parse_node:: ~parse_node() throw()
             {
-                
+                if( terminal )
+                {
+                    lexeme *lx = lex();
+                    if( lx ) delete lx;
+                }
+                else
+                {
+                    child_list &ch = children();
+                    while( ch.size ) delete ch.pop_back();
+                }
+                memset(wksp,0,sizeof(wksp));
             }
+            
+            
+            parse_node:: parse_node( const string &label_ref, lexeme *lx ) throw() :
+            label( label_ref ), 
+            terminal( lx != 0 ),
+            prev(0),
+            next(0), 
+            parent(0),
+            wksp()
+            {
+                memset(wksp,0,sizeof(wksp));
+                if( lx )
+                {
+                    lexeme **ppLx = (lexeme **) &wksp[0];
+                    *ppLx = lx;
+                }
+            }
+            
+            lexeme * parse_node:: lex() throw()
+            {
+                assert(terminal);
+                lexeme **ppLx = (lexeme **) &wksp[0];
+                return *ppLx;
+            }
+            
+            parse_node::child_list & parse_node::children() throw()
+            {
+                assert(!terminal);
+                return *(child_list *) &wksp[0];
+            }
+            
         }
         
     }
