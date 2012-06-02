9ef7c5ec9a4c17e818e2071fe80fcb420f5e6621
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-06-02 17:04:51 +0000

    chemsys: need to check cvg

--- src/y-aqueous/tests/test-chemsys.cpp
+++ src/y-aqueous/tests/test-chemsys.cpp
@@ -10,7 +10,7 @@ YOCTO_UNIT_TEST_IMPL(chemsys)
     
     
     library lib;
-    chemsys cs(lib);
+    chemsys cs(lib,1e-4);
     
     lib.add( "H+", 1 );
     lib.add( "HO-", -1 );
@@ -18,19 +18,19 @@ YOCTO_UNIT_TEST_IMPL(chemsys)
     lib.add( "Ac-", -1);
     
     
-        equilibrium &water = cs.create( "water", 1e-14 );    
-        water.add("H+", 1);
-        water.add("HO-",1);
-        std::cerr << water << std::endl;
+    equilibrium &water = cs.create( "water", 1e-14 );    
+    water.add("H+", 1);
+    water.add("HO-",1);
+    std::cerr << water << std::endl;
     
     
-    {
-        equilibrium &Ac = cs.create("Ac", pow(10,-4.8));
-        Ac.add("H+",1);
-        Ac.add("Ac-",1);
-        Ac.add("AcH",-1);
-        std::cerr << Ac << std::endl;
-    }
+    
+    equilibrium &Ac = cs.create("Ac", pow(10,-4.8));
+    Ac.add("H+",1);
+    Ac.add("Ac-",1);
+    Ac.add("AcH",-1);
+    std::cerr << Ac << std::endl;
+    
     
     cs.build();
     std::cerr << "nu=" << cs.nu << std::endl;
@@ -40,8 +40,15 @@ YOCTO_UNIT_TEST_IMPL(chemsys)
     solution s(lib);
     s["H+"] = 1e-6;
     s["HO-"] = 1.1 * water.K(0) / s["H+"];
-    cs.computeGamma(s,0.0);
-    std::cerr << "Gamma=" << cs.Gamma << std::endl;
+    s["AcH"] = 0.1;
+    s["Ac-"] = 0.01;
+    
+    s.put(cs.C);
+    cs.computeW(s,0.0);
     std::cerr << "water: " << water.Gamma(s, 0.0) << std::endl;
+    std::cerr << "Ac:    " << Ac.Gamma(s,0.0)     << std::endl;
+    std::cerr << "Gamma="  << cs.Gamma << std::endl;
+    std::cerr << "Phi="    << cs.Phi << std::endl;
+    std::cerr << "W="      << cs.W   << std::endl;
 }
 YOCTO_UNIT_TEST_DONE()
