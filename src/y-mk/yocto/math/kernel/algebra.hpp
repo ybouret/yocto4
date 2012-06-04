1fa68b0ab81e4334f883a40ad3a1002dbbe6af36
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-06-04 08:28:11 +0000

    aqueous init: need to find cutoff

--- src/y-mk/yocto/math/kernel/algebra.hpp
+++ src/y-mk/yocto/math/kernel/algebra.hpp
@@ -2,6 +2,7 @@
 #define YOCTO_MATH_KERNEL_ALGEBRA_INCLUDED 1
 
 #include "yocto/math/kernel/matrix.hpp"
+#include "yocto/math/types.hpp"
 
 namespace yocto
 {
@@ -14,8 +15,10 @@ namespace yocto
 			static void set(    array<T> &v, T a ) throw();                     //!< v = a
 			static void set(    array<T> &v, const array<T> &u ) throw();       //!< v = u
 			static void add(    array<T> &v, const array<T> &u ) throw();       //!< v += u
+            static void sub(    array<T> &v, const array<T> &u ) throw();       //!< v -= u
 			static void muladd( array<T> &v, T a, const array<T> &u ) throw();  //!< v += a * u
 			static void mulset( array<T> &v, T a, const array<T> &u ) throw();  //!< v  = a * u
+            
             
 			static void mul( array<T> &v, const matrix<T> &M, const array<T> &u ) throw();
 			static void muladd( array<T> &v, const matrix<T> &M, const array<T> &u ) throw();
@@ -25,8 +28,17 @@ namespace yocto
 			static void mul_ltrn( matrix<T> &M, const matrix<T> &lhs, const matrix<T> &rhs) throw();
 			static void mul_rtrn( matrix<T> &M, const matrix<T> &lhs, const matrix<T> &rhs) throw();
 			static void gram_schmidt( matrix<T> &U, const matrix<T> &V, bool normalize);
+            
+            template <typename U>
+            static inline T norm2( const array<U> &v ) throw()
+            {
+                T ans(0.0);
+                for( size_t i=v.size();i>0;--i) ans += Square( v[i] );
+                return ans;
+            }
 		};
-		
+        
+        
 	}
 	
 }
