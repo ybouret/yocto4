96a9d0ece2b06c67f389804acff82b4e31027fb9
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-06-05 05:57:09 +0000

    aqueous: lua code cleanup and upgrade

--- src/y-aqueous/yocto/aqueous/lua.cpp
+++ src/y-aqueous/yocto/aqueous/lua.cpp
@@ -8,7 +8,7 @@ namespace yocto
     namespace aqueous
     {
         
-        void _lua:: load( lua_State *L, library &lib, const string &libname)
+        void _lua:: load( lua_State *L, library &lib, const string &libname, species_ctor *cb )
         {
             //==================================================================
             // load the library table
@@ -26,10 +26,8 @@ namespace yocto
             std::cerr << "*** Parsing " << ns << " species" << std::endl;
             for( unsigned i=1; i <= ns; ++i )
             {
-                //--------------------------------------------------------------
-                // get the species data on the stack
-                //--------------------------------------------------------------
-                lua_rawgeti(L,-1,i);
+                
+                lua_rawgeti(L,-1,i); //------ get the species data -------------
                 
                 
                 if( ! lua_istable(L, -1) )
@@ -37,37 +35,38 @@ namespace yocto
                 
                 //-- get the number of fields
                 const unsigned nf = lua_objlen(L, -1);
-                if( nf < 2 )
-                    throw exception("species #%u: not enough fields");
+                if( nf != 2 && nf != 3 )
+                    throw exception("species #%u: invalid #fields=%u, must be 2 or 3",nf);
                 
-                //-- get the id
-                lua_rawgeti(L, -1, 1);
+                
+                lua_rawgeti(L, -1, 1);  //-------- get the id ------------------
                 if( !lua_isstring(L, -1) )
                     throw exception("species #%u: first field is not a string");
                 size_t       len = 0;
                 const char  *ptr = lua_tolstring(L, -1, &len);
                 const string id(ptr,len);
                 const char  *ch = id.c_str();
-                lua_pop(L,1);
+                lua_pop(L,1);           //-------- pop the id ------------------
                 
-                //-- get the algebraic charge
-                lua_rawgeti(L, -1, 2);
+                
+                lua_rawgeti(L, -1, 2);  //-------- get the algebraic charge ----
                 if( !lua_isnumber(L, -1) )
                     throw exception("%s: second field is not a number",ch);
                 const int z = int( lua_tonumber(L, -1) );
                 std::cerr << "***\t<" << id << ">, z=" << z << std::endl;
-                lua_pop(L,1);
+                lua_pop(L,1);           //-------- pop the algebraic charge ----
                 
                 //-- create the species
                 species &sp = lib.add(id, z);
                 
-                // TODO: further elements
-                (void)sp;
+                if( (0 != cb) && (3 == nf) )
+                {
+                    lua_rawgeti(L, -1, 3); //-------- get the extra field ------ 
+                    (*cb)(sp,L);           // call the ctor
+                    lua_pop(L,1);          //-------- pop the extra field ------
+                }
                 
-                //--------------------------------------------------------------
-                // remove the species data
-                //--------------------------------------------------------------
-                lua_pop(L,1);
+                lua_pop(L,1);        //------ pop the species data -------------
             }
         }
         
@@ -89,10 +88,7 @@ namespace yocto
             std::cerr << "*** Parsing " << neq << " equilibria" << std::endl;
             for( unsigned i=1; i <= neq; ++i )
             {
-                //--------------------------------------------------------------
-                // get the equilibrum data
-                //--------------------------------------------------------------
-                lua_rawgeti(L,-1,i);
+                lua_rawgeti(L,-1,i); //-------- get the equilibrum data --------
                 
                 if( !lua_istable(L, -1) )
                     throw exception( "equilibrium #%u: not a table", i);
@@ -101,19 +97,19 @@ namespace yocto
                 if( nf < 3 )
                     throw exception("equilibrium #%u: not enough fields",i);
                 
-                //-- get the name
-                lua_rawgeti(L,-1,1);
+                
+                lua_rawgeti(L,-1,1); //-------- get the name -------------------
                 if( !lua_isstring(L, -1) )
                     throw exception("equilibrium #%u: first field is not a string", i);
                 size_t       len = 0;
                 const char  *ptr = lua_tolstring(L, -1, &len);
                 const string id(ptr,len);
                 const char  *ch = id.c_str();
-                lua_pop(L,1);
+                lua_pop(L,1);        //-------- pop the name -------------------
                 
-                //-- get the constant: function or constant
+                
                 equilibrium *eq = NULL;
-                lua_rawgeti(L, -1,2);
+                lua_rawgeti(L, -1,2); //-------- get the constant --------------
                 if( lua_isnumber(L,-1) )
                 {
                     // create with a real constant
@@ -134,50 +130,50 @@ namespace yocto
                     else 
                         throw exception("[%s]: invalid constant type", ch);
                 }
-                lua_pop(L,1);
+                lua_pop(L,1);         //-------- pop the constant --------------
                 
                 assert( NULL != eq );
                 
+                //--------------------------------------------------------------
                 //-- parse the actors
+                //--------------------------------------------------------------
                 for( unsigned j=3,k=1; j <= nf; ++j, ++k )
                 {
-                    // get the actor table
-                    lua_rawgeti(L,-1,j);
+                    
+                    lua_rawgeti(L,-1,j); //-------- get the actor table --------
                     {
                         if( !lua_istable(L, -1) )
                             throw exception("[%s]: invalid actor #%u", ch, k );
                         if( lua_objlen(L, -1) != 2 )
                             throw exception("[%s]: invalid actor #%u format", ch, k );
                         
-                        // get the actor name
-                        lua_rawgeti(L, -1, 2);
+                        
+                        lua_rawgeti(L, -1, 2); //-------- get the actor name ---
                         if( !lua_isstring(L, -1) )
                             throw exception("[%s]: invalid actor #%u name, not a string", ch, k);
                         ptr = lua_tolstring(L, -1, &len);
                         const string ac(ptr,len);
                         const char  *acn = ac.c_str();
-                        lua_pop(L, 1); // remove the actor name
+                        lua_pop(L, 1);         //-------- pop the actor name ---
                         
-                        // get the actor coefficient
-                        lua_rawgeti(L, -1, 1);
+                        
+                        lua_rawgeti(L, -1, 1); //-- get the actor coefficient --
                         if( !lua_isnumber(L, -1) )
                             throw exception("[%s]: invalid {%s} coefficient",ch,acn);
                         const int nu = int( lua_tonumber(L, -1) );
-                        lua_pop(L, 1); // remove the actor coefficient
+                        lua_pop(L, 1);         //-- pop the actor coefficient --
                         if( 0 == nu )
                             throw exception("[%s]: zero coefficient for {%s}",ch,acn);
                         
-                        // Ok, create the actor
+                        // create the actor
                         eq->add( ac, nu );
                     }
-                    lua_pop(L,1); // remove the actor table
+                    lua_pop(L,1);        //-------- pop the actor table --------
                 }
                 
                 std::cerr << "***\t\t" << *eq << std::endl;
-                //--------------------------------------------------------------
-                //remove the equilibrium data
-                //--------------------------------------------------------------
-                lua_pop(L,1);
+                
+                lua_pop(L,1);        //-------- pop the equilibrum data --------
                 
             }
             
@@ -196,13 +192,13 @@ namespace yocto
                 throw exception("%s is not a table", ini_name );
             
             //==================================================================
-            // load the chemsys table
+            // parse all the constraints
             //==================================================================
             const unsigned nc = lua_objlen(L, -1);
             std::cerr << "*** Parsing " << nc << " constraints" << std::endl;
             for( unsigned i=1; i <= nc; ++i )
             {
-                lua_rawgeti(L, -1, i); // -------- load the constraint
+                lua_rawgeti(L, -1, i); //-------- get  the constraint ----------
                 if( !lua_istable(L, -1) )
                     throw exception("%s: invalid constraint #%u", ini_name, i);
                 
@@ -210,16 +206,16 @@ namespace yocto
                 if( nf < 2 )
                     throw exception("%s: not enough data for constraint #%u", ini_name, i);
                 
-                lua_rawgeti(L, -1, 1); // ------------ load constraint value
+                lua_rawgeti(L, -1, 1); //-------- get the constraint value -----
                 if( !lua_isnumber(L, -1) )
                     throw exception("%s: invalid type for constraint #%u value", ini_name, i);
                 constraint &c = ini.create( lua_tonumber(L, -1));
-                lua_pop(L, 1);         // ------------ remove contraint value
+                lua_pop(L, 1);         //-------- pop the constraint value -----
                 
                 for( unsigned j=2,k=1; j <= nf; ++j, ++k )
                 {
                     
-                    lua_rawgeti(L,-1,j); // -------- get the item table
+                    lua_rawgeti(L,-1,j); //-------- get the item table ---------
                     {
                         if( !lua_istable(L, -1) )
                             throw exception("%s: constraint #%u has invalid field #%u", ini_name, i, k );
@@ -227,30 +223,30 @@ namespace yocto
                             throw exception("%s: constraint #%u has invalid field #%u format", ini_name, i, k );
                         
                         
-                        lua_rawgeti(L, -1, 2); // -------- get the item name
+                        lua_rawgeti(L, -1, 2); //-------- get the item name ----
                         if( !lua_isstring(L, -1) )
                             throw exception("%s: constraint #%u, field #%u: invalid name", ini_name, i, k);
                         size_t       len = 0;
                         const char  *ptr = lua_tolstring(L, -1, &len);
                         const string id(ptr,len);
                         const char  *sp = id.c_str();
-                        lua_pop(L, 1); // remove the actor name
+                        lua_pop(L, 1);         //-------- pop the item name ----
                         
                         
-                        lua_rawgeti(L, -1, 1); // -------- get the item coefficient
+                        lua_rawgeti(L, -1, 1); //-------- get the coefficient --
                         if( !lua_isnumber(L, -1) )
                             throw exception("%s: constraint #%u, field #%u: invalid {%s} coefficient",ini_name,i,k,sp);
                         const double w = lua_tonumber(L, -1);
-                        lua_pop(L, 1);        // -------- remove the item coefficient
+                        lua_pop(L, 1);         //-------- pop the coefficient --
                         
                         // create the constraint
                         c.add( id, w);
                     }
-                    lua_pop(L,1);       // -------- remove the item  table
-
+                    lua_pop(L,1);        //-------- pop the item table ---------
+                    
                 }
                 
-                lua_pop(L,1);          // -------- remove the constraint
+                lua_pop(L,1);          //-------- get  the constraint ----------
             }
         }
         
