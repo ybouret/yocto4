3040a82dba88150fdee74fc87049ac0d2c29740c
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-06-05 07:10:04 +0000

    added swig/tcl

--- src/share/swig.cmake
+++ src/share/swig.cmake
@@ -14,30 +14,38 @@ FIND_PROGRAM(SWIG swig)
 IF( ${SWIG} STREQUAL "SWIG-NOTFOUND" )
 	MESSAGE( STATUS "no SWIG available")
 ELSE()
-	######################################################################
+	####################################################################
 	## we have swig
-	######################################################################
+	####################################################################
 	SET(SWIG_FOUND ON)
 	MESSAGE( STATUS "[SWIG]: is ${SWIG}" )
 	
-	######################################################################
+	####################################################################
 	# find wich kind of wrapper can be done
-	######################################################################
+	####################################################################
 	SET(SWIG_PYTHON OFF)
 	FIND_PACKAGE(PythonLibs)
 	IF(PYTHONLIBS_FOUND)
 		SET(SWIG_PYTHON ON)
+		MESSAGE( "[SWIG] has python" )
 	ENDIF()
 	
-	######################################################################
+	SET(SWIG_TCL OFF)
+	FIND_PACKAGE(TCL)
+	IF(TCL_FOUND)
+		SET(SWIG_TCL ON)
+		MESSAGE( "[SWIG] has TCL" )
+	ENDIF()
+	
+	####################################################################
 	# create the swig macro: kind=python|..., source=
-	######################################################################
+	####################################################################
 	MACRO(SWIG_WRAPPER name kind)
 		MESSAGE( STATUS "[SWIG]: generating wrapper for [${name}] in [${kind}]")
 		
-		####################################################################
+		################################################################
 		## common variables
-		####################################################################
+		################################################################
 		SET(swig_output_dir   "${CMAKE_CURRENT_BINARY_DIR}/${kind}")
 		SET(swig_wrapper_name "${name}_wrapper_${kind}.cpp" )
 		SET(swig_wrapper_code "${swig_output_dir}/${swig_wrapper_name}")
@@ -45,9 +53,9 @@ ELSE()
 		SET(swig_user_code    "${CMAKE_CURRENT_SOURCE_DIR}/${name}.cpp")
 		SET(swig_target       "${name}_${kind}")
 		
-		####################################################################
+		################################################################
 		## dependencies
-		####################################################################
+		################################################################
 		EXECUTE_PROCESS(
 			COMMAND ${SWIG} -c++ -${kind} -MM "${swig_interface}"
 			OUTPUT_VARIABLE SWIG_DEPS
@@ -62,9 +70,9 @@ ELSE()
 			MESSAGE( STATUS "[SWIG]: depends on '${D2}'" )
 			LIST( APPEND SWIG_DEPENDS "${D2}"  )
 		ENDFOREACH()
-		####################################################################
+		################################################################
 		## generation
-		####################################################################
+		################################################################
 		ADD_CUSTOM_COMMAND(
 			OUTPUT  "${swig_output_dir}/${swig_wrapper_name}"
 			COMMAND ${CMAKE_COMMAND} -E make_directory "${swig_output_dir}"
@@ -72,22 +80,22 @@ ELSE()
 			DEPENDS ${SWIG_DEPENDS}
 			)
 		
-		####################################################################
+		################################################################
 		## default module to build
-		####################################################################
+		################################################################
 		ADD_LIBRARY(${swig_target} MODULE "${swig_user_code}" "${swig_wrapper_code}")
 		
 
-		####################################################################
+		################################################################
 		## and its exact location
-		####################################################################
+		################################################################
 		GET_TARGET_PROPERTY(swig_target_name ${swig_target} LOCATION)
 		
-		####################################################################
+		################################################################
 		##
 		## python tuning
 		##
-		####################################################################
+		################################################################
 		IF( "${kind}" STREQUAL "python" )
 			IF( NOT SWIG_PYTHON )
 				MESSAGE( FATAL_ERROR "[SWIG]: No python was detected!" )
@@ -106,12 +114,37 @@ ELSE()
 				COMMAND ${CMAKE_COMMAND} -E copy "${swig_target_name}" "${swig_output_dir}/_${name}.${swig_ext}"
 			)
 		ENDIF()
+		
+		################################################################
+		##
+		## tcl tuning
+		##
+		################################################################
+		IF( "${kind}" STREQUAL "tcl" )
+			IF( NOT SWIG_TCL )
+				MESSAGE( FATAL_ERROR "[SWIG]: No tcl was detected!" )
+			ELSE()
+				MESSAGE( STATUS      "[SWIG]: tuning for [tcl]")
+			ENDIF()
+			INCLUDE_DIRECTORIES(${TCL_INCLUDE_PATH})
+			TARGET_LINK_LIBRARIES(${swig_target} ${TCL_LIBRARY})
+			SET(swig_ext "so" )
+			IF( WIN32 )
+				SET(swig_ext "dll")
+			ENDIF()
+			ADD_CUSTOM_COMMAND(
+				TARGET ${swig_target}
+				POST_BUILD
+				COMMAND ${CMAKE_COMMAND} -E copy "${swig_target_name}" "${swig_output_dir}/${name}.${swig_ext}"
+			)
+		ENDIF()
+		
 	
-		####################################################################
+		################################################################
 		##
 		## system tuning
 		##
-		####################################################################
+		################################################################
 		SET(swig_cxx OFF)
 		IF(YOCTO_SUNOS OR YOCTO_CLANG)
 			SET(swig_cxx ON)
