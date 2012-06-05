2810e71808242e18ba78caa16237683cc8ee6d8b
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-06-05 13:40:55 +0000

    swig samples: added C#

---
+++ trunk/samples/swig/cs/runme.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class runme {
+     static void Main() {
+         Console.WriteLine(example.My_variable);
+         Console.WriteLine(example.fact(5));
+         Console.WriteLine(example.get_time());
+     }
+ }
+
