227b845c23cef2200a7b514aa21870f64d4ee0f2
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-06-05 05:57:09 +0000

    aqueous: lua code cleanup and upgrade

--- trunk/src/y-aqueous/yocto/aqueous/lua.hpp
+++ trunk/src/y-aqueous/yocto/aqueous/lua.hpp
@@ -18,10 +18,12 @@ namespace yocto
         
         struct _lua
         {
+            typedef functor<void,TL2(species&,lua_State*)> species_ctor;
             
-            static void load( lua_State *L, library     &lib, const string &libname );
+            static void load( lua_State *L, library     &lib, const string &libname, species_ctor *cb = NULL);
             static void load( lua_State *L, chemsys     &cs,  const string &csname  );
             static void load( lua_State *L, initializer &ini, const string &ininame );
+            
         };
         
     }
