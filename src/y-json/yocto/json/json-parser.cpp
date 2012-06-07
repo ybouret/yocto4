c3226e8348011bad28112ca4a4fb29622b3827eb
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-06-07 14:43:12 +0000

    JSON: need to walk the AST

--- src/y-json/yocto/json/json-parser.cpp
+++ src/y-json/yocto/json/json-parser.cpp
@@ -4,6 +4,12 @@
 #include "yocto/lang/grammar.hpp"
 #include "yocto/auto-ptr.hpp"
 
+#define Y_JSON_OUTPUT
+#if defined(Y_JSON_OUTPUT)
+#include "yocto/ios/ocstream.hpp"
+#include <cstdlib>
+#endif
+
 namespace yocto 
 {
     using namespace lang;
@@ -66,6 +72,21 @@ namespace yocto
                 //--------------------------------------------------------------
                 syntax::terminal &LBRACK = term( "LBRACK", syntax::is_discardable );
                 syntax::terminal &RBRACK = term( "RBRACK", syntax::is_discardable );
+                syntax::terminal &LBRACE = term( "LBRACE", syntax::is_discardable );
+                syntax::terminal &RBRACE = term( "RBRACE", syntax::is_discardable );
+                syntax::terminal &COMMA  = term( "COMMA",  syntax::is_discardable );
+                syntax::terminal &NUMBER = term( "NUMBER" );
+                syntax::terminal &Null   = term( "null",   syntax::is_specialized );
+                syntax::terminal &True   = term( "true",   syntax::is_specialized );
+                syntax::terminal &False  = term( "false",  syntax::is_specialized );
+                syntax::terminal &STRING = term( "JSON::String" );
+                syntax::terminal &COLUMN = term( "COLUMN", syntax::is_discardable );
+                
+                //--------------------------------------------------------------
+                // initialize value
+                //--------------------------------------------------------------
+                syntax::alternate &VALUE = alt("VALUE");
+                VALUE << NUMBER << STRING << Null << True << False;
                 
                 //--------------------------------------------------------------
                 // ARRAY
@@ -73,13 +94,48 @@ namespace yocto
                 syntax::alternate &ARRAYS = alt( "ARRAYS" );
                 
                 {
-                    syntax::aggregate &EMPTY_ARRAY = agg( "EMPTY_ARRAY" );
-                    EMPTY_ARRAY << LBRACK << RBRACK;
-                    ARRAYS |= EMPTY_ARRAY;
+                    {
+                        syntax::aggregate &EMPTY_ARRAY = agg( "EMPTY_ARRAY" );
+                        EMPTY_ARRAY << LBRACK << RBRACK;
+                        ARRAYS |= EMPTY_ARRAY;
+                    }
+                    {
+                        syntax::aggregate &ARRAY       = agg( "ARRAY" );
+                        syntax::aggregate &TAIL_VALUE  = agg( "TAIL_VALUE", syntax::is_merging );
+                        TAIL_VALUE << COMMA << VALUE;
+                        
+                        ARRAY << LBRACK << VALUE << rep( "ZERO_OR_MORE_TAIL_VALUE", TAIL_VALUE, 0 ) << RBRACK;
+                        ARRAYS |= ARRAY;
+                    }
                 }
                 
-                INSTANCE |= ARRAYS;
+                VALUE |= ARRAYS;
                 
+                //--------------------------------------------------------------
+                // OBJECT
+                //--------------------------------------------------------------
+                syntax::alternate &OBJECTS = alt("OBJECTS");
+                {
+                    {
+                        syntax::aggregate &EMPTY_OBJECT = agg( "EMPTY_OBJECT" );
+                        EMPTY_OBJECT << LBRACE << RBRACE;
+                        OBJECTS |= EMPTY_OBJECT;
+                    }
+                    {
+                        syntax::aggregate &PAIR = agg("PAIR");
+                        PAIR << STRING << COLUMN << VALUE;
+                        syntax::aggregate &TAIL_PAIR = agg("TAIL_PAIR", syntax::is_merging);
+                        TAIL_PAIR << COMMA << PAIR;
+                        syntax::aggregate &OBJECT = agg("OBJECT");
+                        OBJECT << LBRACE << PAIR << rep("ZERO_OR_MORE_TAIL_PAIR", TAIL_PAIR, 0 ) << RBRACE;
+                        OBJECTS |= OBJECT;
+                    }
+                    
+                }
+                VALUE |= OBJECTS;
+                
+                INSTANCE |= ARRAYS;
+                INSTANCE |= OBJECTS;
                 
             }
             
@@ -111,15 +167,33 @@ namespace yocto
             //==================================================================
             void call( Value &value, ios::istream &fp )
             {
-                value.nullify();
-                reset();
-                regex::source src(fp);
-                lexeme *lx = NULL;
-                while( (lx=next_lexeme(src)) != NULL )
+                value.nullify(); //! make a null value 
+                reset();         //! lexer reset
+                
+                regex::source                S(fp);
+                auto_ptr<syntax::parse_node> Tree( accept(*this,S) );
+                
+                Tree->AST();
+                
+#if defined (Y_JSON_OUTPUT)
+                std::cerr << "Saving tree..." << std::endl;
                 {
-                    std::cerr << "line " << lx->line << "\t<" << lx->label << "> = '" << *lx << "'" << std::endl;
-                    delete lx;
+                    ios::ocstream os("json.dot",false);
+                    Tree->graphviz("G", os);
                 }
+                system( "dot -Tpng json.dot -o json.png" );
+#endif
+                
+                walk( value, Tree.__get() );
+            }
+            
+            
+            //==================================================================
+            // walk the tree
+            //==================================================================
+            void walk( Value &value, const syntax::parse_node *node )
+            {
+                assert(node!=NULL);
             }
             
             
