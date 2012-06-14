6e7918d9bd4cc3bdb10b54035bd0bc897a3a8a80
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-06-14 15:23:14 +0000

    FLTK::Axis autoscale

--- src/y-fltk/yocto/FLTK/Axis.cpp
+++ src/y-fltk/yocto/FLTK/Axis.cpp
@@ -31,5 +31,25 @@ namespace yocto
             (double&)vmax   = amax;
             (double&)length = vmax - vmin;
         }
+        
+        void Axis:: autoscaleY( const Curve &C, const double extra )
+        {
+            if( C.size() > 0 )
+            {
+                double cmin = C[1].y;
+                double cmax = cmin;
+                for( size_t i=2; i <= C.size(); ++i )
+                {
+                    const double tmp = C[i].y;
+                    if( tmp < cmin )
+                        cmin = tmp;
+                    if( tmp > cmax )
+                        cmax = tmp;
+                }
+                const double amplitude = cmax - cmin;
+                const double delta     = amplitude * extra;
+                set_range( cmin - delta, cmax + delta );
+            }
+        }
     }
 }
