f11c9a6c0c303cf5e5f61f61fe54261ba9c638f6
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-06-18 13:37:53 +0000

    new huffman

---
+++ src/y-pack/yocto/pack/huffman.cpp
@@ -0,0 +1,20 @@
+#include "yocto/pack/huffman.hpp"
+
+namespace yocto 
+{
+    
+    namespace packing
+    {
+        
+        Huffman:: Tree:: Tree() 
+        {
+        }
+        
+        Huffman:: Tree:: ~Tree() throw()
+        {
+        }
+        
+        
+    }
+    
+}
