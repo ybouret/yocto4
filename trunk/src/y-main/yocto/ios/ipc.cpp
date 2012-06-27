9f23a5aa14109d5372544aabbe99667d53bdb4ab
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-06-27 13:01:51 +0000

    try ipc

---
+++ trunk/src/y-main/yocto/ios/ipc.cpp
@@ -0,0 +1,26 @@
+#include "yocto/ios/ipc.hpp"
+#include "yocto/exceptions.hpp"
+
+#if defined(YOCTO_BSD)
+#include <unistd.h>
+#include <cerrno>
+#endif
+
+namespace yocto 
+{
+    
+    namespace ios 
+    {
+        
+#if defined(YOCTO_BSD)
+        namespace
+        {
+            static const size_t FD_READ  = 0;
+            static const size_t FD_WRITE = 1;
+            static const size_t FD_COUNT = 2;
+        }            
+#endif
+        
+    }
+    
+}
