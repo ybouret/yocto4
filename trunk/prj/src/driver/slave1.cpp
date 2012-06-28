d5da97a6b772069bb5771d1737667883a8f96ff8
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-06-28 08:58:53 +0000

    improved pterm

--- trunk/prj/src/driver/slave1.cpp
+++ trunk/prj/src/driver/slave1.cpp
@@ -4,15 +4,18 @@
 
 using namespace yocto;
 
-int main()
+int main(int argc, char *argv[])
 {
 		
 	wtime chrono;
-	
+    double tmax = 10.0;
+	if( argc > 1 )
+        tmax = atof(argv[1]);
+    
 	chrono.start();
 	size_t index = 0;
-	fprintf( stderr, "Running for 10 seconds...\n");
-	while( chrono.query() <= 10.0 )
+	fprintf( stderr, "Running for %g seconds...\n", tmax);
+	while( chrono.query() <= tmax )
 	{
 		putc( char( 'A' + index++ ), stdout );
 		if( index >= 26 ) 
