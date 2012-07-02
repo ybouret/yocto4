03c3bd284ab9e9ae5a3bda507424e98fbc396d20
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-07-02 07:45:42 +0000

    Win32 upgrades

---
+++ prj/src/wpipes/wpterm.cpp
@@ -0,0 +1,31 @@
+#include "yocto/string.hpp"
+#include "yocto/exceptions.hpp"
+#include <iostream>
+#include <windows.h>
+
+using namespace yocto;
+
+class Context
+{
+
+};
+
+int main( int argc, char *argv[] )
+{
+
+	try 
+    {   
+		
+		return 0;
+    }
+    catch( const exception &e )
+	{
+		std::cerr << e.when() << std::endl;
+		std::cerr << e.what() << std::endl;
+	}
+	catch (...) 
+	{
+		std::cerr << "unhandled exception!" << std::endl;
+	}
+    return 1;
+}
