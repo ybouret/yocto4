d6034f10a443ab696ec6ef3490ec5247458473e8
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-07-02 14:14:09 +0000

    compiler build init

---
+++ src/y-lang/yocto/lang/compiler-build.cpp
@@ -0,0 +1,17 @@
+#include "yocto/lang/compiler.hpp"
+
+namespace yocto 
+{
+
+    namespace lang
+    {
+        
+        grammar * compiler:: build( syntax::parse_node *ast) const
+        {
+            assert( ast != NULL );
+            return NULL;
+        }
+
+    }
+
+}
