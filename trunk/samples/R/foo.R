af4f134696a2d28eee86e447418e873195b12935
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-07-02 15:14:35 +0000

    little update

--- trunk/samples/R/foo.R
+++ trunk/samples/R/foo.R
@@ -6,4 +6,9 @@ hello2 <- function(n) {
 }
 
 hello2(3);
+m <- matrix( data=1, nrow=3, ncol=4)
+
+.C("showMatrix", as.double(m), as.integer(nrow(m)), as.integer(ncol(m)) );
+
+.Call("vecSum",rnorm(10));
 
