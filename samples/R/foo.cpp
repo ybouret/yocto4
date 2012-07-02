a8fa76d20533a2889d03b643495ea8904ed5c459
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-07-02 15:14:35 +0000

    little update

--- samples/R/foo.cpp
+++ samples/R/foo.cpp
@@ -1,9 +1,6 @@
 #include <R.h>
-
-extern "C" void foo( )
-{
-	Rprintf("Foo!\n");
-}
+#include <Rinternals.h>
+#include <Rmath.h>
 
 extern "C" void hello(int *n)
 {
@@ -11,4 +8,31 @@ extern "C" void hello(int *n)
   	for(i=0; i < *n; i++) {
     	Rprintf("Hello, world!\n");
 	} 
+}
+
+extern "C"
+void showMatrix( double *a, int *nr, int *nc )
+{
+	Rprintf("nrow=%d\n", *nr);
+	Rprintf("ncol=%d\n", *nc);
+	for( int i=0; i < *nr; ++i )
+	{
+		double *a_i = a+i* (*nc);
+		for( int j=0; j < *nc; ++j )
+		{
+			a_i[j] = j;
+		}
+	}
+}
+
+extern "C"
+SEXP vecSum(SEXP Rvec)
+{
+	int i, n;
+	double *vec, value = 0; vec = REAL(Rvec);
+	n = length(Rvec);
+	for (i = 0; i < n; i++) 
+		value += vec[i]; 
+	printf("The value of #items=%d is: %4.6f \n", n, value); 
+	return R_NilValue;
 }
--- src/y-lang/yocto/lang/compiler-build.cpp
+++ src/y-lang/yocto/lang/compiler-build.cpp
@@ -1,4 +1,5 @@
 #include "yocto/lang/compiler.hpp"
+#include "yocto/exception.hpp"
 
 namespace yocto 
 {
@@ -6,9 +7,14 @@ namespace yocto
     namespace lang
     {
         
+#define __CHECK(EXPR,ERROR) do { if( !(EXPR) ) throw exception("%s: " #ERROR,fn ); } while(false)
+        
         grammar * compiler:: build( syntax::parse_node *ast) const
         {
-            assert( ast != NULL );
+            static const char fn[] = "lang.compiler.build(ast)";
+            __CHECK(ast!=NULL,"NULL ast");
+            __CHECK(! ast->terminal,"invalid ast");
+            
             return NULL;
         }
 
