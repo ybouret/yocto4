43029d8f04216d93053d3a4546318a5cfd890383
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-07-24 12:18:15 +0000

    prepare for one field exchange

--- trunk/src/y-swamp/yocto/swamp/ghosts.hpp
+++ trunk/src/y-swamp/yocto/swamp/ghosts.hpp
@@ -126,8 +126,19 @@ namespace yocto
             //! store inner data into inner_buf
             void store_inner( const linear_handles &handles ) throw();
             
+            //! store inner data into inner_buf
+            /**
+             must have enough memory...
+             \return the number_of stored bytes : A.item_size() * num_offset
+             */
+            size_t store_inner1( const linear_base &A ) throw();
+            
             //! query outer data from outer_buf
             void query_outer( const linear_handles &handles ) const throw();
+            
+            //! query outer data from outer_buf
+            void query_outer1( linear_base &A ) const throw();
+            
             
             typedef shared_ptr<async_ghosts> ptr;
             
