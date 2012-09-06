dee0860f1db7cb71907c30825261b7578d6e7198
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-09-06 10:31:27 +0000

    ok for array db

--- trunk/src/y-spade/yocto/spade/array-db.hpp
+++ trunk/src/y-spade/yocto/spade/array-db.hpp
@@ -1,5 +1,5 @@
-#ifndef YOCTO_SPADE_ARRAYS_INCLUDED
-#define YOCTO_SPADE_ARRAYS_INCLUDED 1
+#ifndef YOCTO_SPADE_ARRAY_DB_INCLUDED
+#define YOCTO_SPADE_ARRAY_DB_INCLUDED 1
 
 #include "yocto/spade/varray.hpp"
 #include "yocto/associative/set.hpp"
@@ -9,7 +9,24 @@ namespace yocto
     namespace spade
     {
         
-        class array
+        class array_db
+        {
+        public:
+            explicit array_db() throw();
+            explicit array_db(size_t);
+            virtual ~array_db() throw();
+            
+            void insert( const varray::ptr &arr );
+            
+            varray & operator[]( const string & );
+            varray & operator[]( const char  *  );
+            const varray & operator[]( const string & ) const;
+            const varray & operator[]( const char  *  ) const;
+            
+            
+        private:
+            set<string,varray::ptr> arrays;
+        };
         
     }
 }
