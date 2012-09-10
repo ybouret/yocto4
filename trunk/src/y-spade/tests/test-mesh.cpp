22808255c9c7f2d5e3bb9b97d51ca08124dd43bc
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-09-10 13:00:56 +0000

    rmesh

---
+++ trunk/src/y-spade/tests/test-mesh.cpp
@@ -0,0 +1,40 @@
+#include "yocto/utest/run.hpp"
+#include "yocto/spade/rmesh.hpp"
+
+#include "./main.hpp"
+
+YOCTO_UNIT_TEST_IMPL(mesh)
+{
+    
+    {
+        array_db   adb;
+        layout1D   L( RandCoord1D(), RandCoord1D());
+        rmesh<double,layout1D> m1(L,adb);
+        std::cerr << "mesh1d: " << L << std::endl;
+        std::cerr << "X     : " << m1.X().as_layout() << std::endl;
+    }
+    
+    {
+        array_db   adb;
+        layout2D   L( RandCoord2D(), RandCoord2D());
+        rmesh<float,layout2D> m2(L,adb);
+        std::cerr << "mesh2d: " << L << std::endl;
+        std::cerr << "X     : " << m2.X().as_layout() << std::endl;
+        std::cerr << "Y     : " << m2.Y().as_layout() << std::endl;
+    }
+    
+    
+    {
+        array_db   adb;
+        layout3D   L( RandCoord3D(), RandCoord3D());
+        rmesh<ptrdiff_t,layout3D> m3(L,adb);
+        std::cerr << "mesh3d: " << L<< std::endl;
+        std::cerr << "X     : " << m3.X().as_layout() << std::endl;
+        std::cerr << "Y     : " << m3.Y().as_layout() << std::endl;
+        std::cerr << "Z     : " << m3.Z().as_layout() << std::endl;
+
+    }
+
+}
+YOCTO_UNIT_TEST_DONE()
+
