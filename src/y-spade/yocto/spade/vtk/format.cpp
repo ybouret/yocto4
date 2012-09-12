8706e6cd7faac2d407295cb371182f37b48b6376
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-09-12 14:01:28 +0000

    start vtk for spade

---
+++ src/y-spade/yocto/spade/vtk/format.cpp
@@ -0,0 +1,13 @@
+#include "yocto/spade/vtk/format.hpp"
+
+namespace yocto
+{
+    namespace spade
+    {
+        vtk_format:: vtk_format() : real_fmt( "%g" ), unit_fmt( "%ld" )
+		{}
+        
+		vtk_format:: ~vtk_format() throw() {}
+    }
+    
+}
