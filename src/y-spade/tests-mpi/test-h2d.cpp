6edfb0988b7e03dd14f51ad7cdaa2c44ca0c74fd
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-09-13 06:54:23 +0000

    preparing heat 2D

---
+++ src/y-spade/tests-mpi/test-h2d.cpp
@@ -0,0 +1,23 @@
+#include "yocto/utest/run.hpp"
+#include "yocto/spade/mpi/workspace.hpp"
+#include "yocto/spade/rmesh.hpp"
+#include "yocto/spade/mpi/collect0.hpp"
+#include "yocto/auto-ptr.hpp"
+#include "yocto/spade/vtk/writer.hpp"
+
+#include "../tests/main.hpp"
+#include "yocto/ios/ocstream.hpp"
+
+#define LX (1.2)
+#define NX (100)
+
+#define LY (1.4)
+#define NY (150)
+
+YOCTO_UNIT_TEST_IMPL(h2d)
+{
+    
+    
+}
+YOCTO_UNIT_TEST_DONE()
+
