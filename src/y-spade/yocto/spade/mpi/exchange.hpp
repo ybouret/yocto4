96500a3701143a7f893e4267f4da5732ff539ef4
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-09-20 14:37:51 +0000

    mpi::exchange 1 field

--- src/y-spade/yocto/spade/mpi/exchange.hpp
+++ src/y-spade/yocto/spade/mpi/exchange.hpp
@@ -86,6 +86,82 @@ namespace yocto
                 wait(MPI,ds,requests);
             }
             
+            template <typename DATASPACE> static inline
+            void init1( const mpi &MPI, linear &handle, DATASPACE &ds, mpi::Requests &requests )
+            {
+                static const int tag = 0xCA441;
+                assert( requests.count == ds.num_requests );
+                
+                //--------------------------------------------------------------
+                // transfert local ghosts
+                //--------------------------------------------------------------
+                for( size_t i=ds.local_count;i>0;--i)
+                    ds.get_local(i).transfer( handle );
+                
+                
+                //--------------------------------------------------------------
+                // load and create send requests
+                //--------------------------------------------------------------
+                size_t iRequest = 0;
+                for( size_t i=ds.async_count;i>0;--i)
+                {
+                    async_ghosts &g = ds.get_async(i);
+                    assert(g.peer>=0);
+                    assert(g.peer<MPI.CommWorldSize);
+                    assert(g.ibuffer!=0);
+                    assert(g.obuffer!=0);
+                    assert(g.iobytes>0);
+                    
+                    //----------------------------------------------------------
+                    // create ibuffer
+                    //----------------------------------------------------------
+                    const size_t ns = g.inner_store( handle );
+                    
+                    //----------------------------------------------------------
+                    // create non blocking recv
+                    //----------------------------------------------------------
+                    MPI.Irecv(g.obuffer, ns, MPI_BYTE, g.peer, tag, MPI_COMM_WORLD, requests[iRequest++]);
+                    assert(iRequest<=requests.count);
+                    
+                    //----------------------------------------------------------
+                    // create non blocking send
+                    //----------------------------------------------------------
+                    MPI.Isend(g.ibuffer, ns, MPI_BYTE, g.peer, tag, MPI_COMM_WORLD, requests[iRequest++]);
+                    assert(iRequest<=requests.count);
+                }
+                
+                //--------------------------------------------------------------
+                // process with MPI
+                //--------------------------------------------------------------
+                MPI.Startall(requests);
+            }
+            
+            template <typename DATASPACE> static inline
+            void wait1( const mpi & MPI, linear &handle, DATASPACE &ds, mpi::Requests &requests )
+            {
+                assert( requests.count == ds.num_requests );
+                //--------------------------------------------------------------
+                // wait for MPI I/O
+                //--------------------------------------------------------------
+                MPI.Waitall(requests);
+                
+                //--------------------------------------------------------------
+                // dispatch ghosts data
+                //--------------------------------------------------------------
+                for( size_t i=ds.async_count;i>0;--i)
+                {
+                    async_ghosts &g = ds.get_async(i);
+                    g.outer_query(handle);
+                }
+            }
+
+            template <typename DATASPACE> static inline
+            void sync1( const mpi & MPI, linear &handle, DATASPACE &ds, mpi::Requests &requests )
+            {
+                init1(MPI,handle,ds,requests);
+                wait1(MPI,handle,ds,requests);
+            }
+
             
             
         };
