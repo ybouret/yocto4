cf86646cea2b6814d402bade49a2df44657ce34d
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-09-25 08:06:04 +0000

    corrected vtk writer

---
+++ src/y-spade/yocto/spade/variables.cpp
@@ -0,0 +1,27 @@
+#include "yocto/spade/variables.hpp"
+
+namespace yocto
+{
+    namespace spade
+    {
+     
+        variables:: ~variables() throw() {}
+        
+        variables:: variables() throw() : variables_base() {}
+        
+        variables:: variables( const variables &other ) : variables_base(other) {}
+        
+        void variables:: append( const string &id )
+        {
+            push_back(id);
+        }
+
+        
+        void variables:: append( const char *id )
+        {
+            const string ID(id);
+            push_back(ID);
+        }
+        
+    }
+}
