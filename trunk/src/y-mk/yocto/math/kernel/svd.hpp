2d9df054c00cc9a3f1b8110d3eb8741f0604dfb3
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-09-27 09:23:08 +0000

    initial svd

---
+++ trunk/src/y-mk/yocto/math/kernel/svd.hpp
@@ -0,0 +1,15 @@
+#ifndef YOCTO_MATH_SVD_INCLUDED
+#define YOCTO_MATH_SVD_INCLUDED 1
+
+#include "yocto/math/kernel/matrix.hpp"
+
+namespace yocto
+{
+    namespace math
+    {
+        
+    }
+}
+
+
+#endif
