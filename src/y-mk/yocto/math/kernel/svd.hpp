8fdee5cf86b212dc477eb6cb93e008702995e6b2
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-10-01 07:12:54 +0000

    more SVD code...

--- src/y-mk/yocto/math/kernel/svd.hpp
+++ src/y-mk/yocto/math/kernel/svd.hpp
@@ -8,6 +8,41 @@ namespace yocto
     namespace math
     {
         
+        template <typename T>
+        struct svd
+        {
+            //! singular value decomposition
+            /**
+             Given a matrix a[1..m][1..n], this routine computes its singular value
+             decomposition, A = U.W.VT.  The matrix U replaces a on output.  The diagonal
+             matrix of singular values W is output as a vector w[1..n].  The matrix V (not
+             the transpose VT) is output as v[1..n][1..n].
+             */
+            static bool build(matrix<T> &a, array<T> &w, matrix<T> &v);
+            
+            //! solve a system after a singular value decomposition
+            /**
+             Solves A·X = B for a vector X, where A is specified
+             by the arrays u[1..m][1..n], w[1..n], v[1..n][1..n] as returned by svdcmp.
+             m and n are the dimensions of a,
+             and will be equal for square matrices.
+             b[1..m] is the input right-hand side.
+             x[1..n] is the output solution vector.
+             No input quantities are destroyed, so the routine may be called sequentially with different b’s.
+             */
+            static
+            void solve(const matrix<T> &u,
+                       const array<T>  &w,
+                       const matrix<T> &v,
+                       const array<T>  &b,
+                       array<T>        &x);
+          
+            //! truncate singular value to ftol * max|w|
+            static
+            void truncate( array<T> &w, const T ftol );
+            
+        };
+        
     }
 }
 
