9fd4cee01b440c81d42d7dd4683a81ed7b24e607
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-10-01 15:38:25 +0000

    still level set

---
+++ src/y-spade/tests/test-levels.cpp
@@ -0,0 +1,11 @@
+#include "yocto/utest/run.hpp"
+#include "yocto/spade/contour/levels.hpp"
+
+using namespace yocto;
+using namespace spade;
+
+YOCTO_UNIT_TEST_IMPL(levels)
+{
+
+}
+YOCTO_UNIT_TEST_DONE()
