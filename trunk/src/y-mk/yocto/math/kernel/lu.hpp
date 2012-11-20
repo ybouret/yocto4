cbc04a73bc5dd35eb757ae7682a0c0e86d128061
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2012-11-20 13:50:48 +0000

    initial lu commit

---
+++ trunk/src/y-mk/yocto/math/kernel/lu.hpp
@@ -0,0 +1,37 @@
+#ifndef YOCTO_MATH_KERNEL_LU_INCLUDED
+#define YOCTO_MATH_KERNEL_LU_INCLUDED 1
+
+#include "yocto/math/kernel/matrix.hpp"
+#include "yocto/sequence/array.hpp"
+#include "yocto/math/types.hpp"
+
+namespace yocto
+{
+    namespace math
+    {
+        
+        template <typename T>
+        struct lu
+        {
+            typedef typename real_of<T>::type real_type;
+        
+            static bool build(matrix<T>        &a,
+                              array<size_t>    &indx,
+                              array<real_type> &scal,
+                              bool             *dneg_p=0) throw();
+            
+            static void solve(const matrix<T>     &a,
+                              const array<size_t> &indx,
+                              array<T>            &b) throw();
+            
+            static void solve(const matrix<T>     &M,
+                              const array<size_t> &indx,
+                              matrix<T>           &Q) throw();
+        };
+        
+    }
+    
+}
+
+#endif
+
