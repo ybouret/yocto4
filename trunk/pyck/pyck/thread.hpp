4811ac00ecf3a8025bc3fb767e4d25b10066c067
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-01-07 13:54:07 +0000

    basic threads

---
+++ trunk/pyck/pyck/thread.hpp
@@ -0,0 +1,48 @@
+#ifndef PYCK_THREAD_INCLUDED
+#define PYCK_THREAD_INCLUDED 1
+
+#include "./mutex.hpp"
+
+class Thread
+{
+public:
+  
+    
+#if defined(PYCK_BSD)
+    typedef pthread_t Handle;
+    typedef pthread_t ID;
+#endif
+    
+#if defined(PYCK_WIN)
+    typedef HANDLE Handle;
+    typedef DWORD  ID;
+#endif
+    
+    typedef void (*Proc)( void *);
+
+    explicit Thread( Thread::Proc proc, void *data);
+    virtual ~Thread() throw();
+    
+    void join() throw();
+    
+    
+private:
+    Thread( const Thread & );
+    Thread &operator=( const Thread & );
+    Proc         proc_;
+    void        *data_;
+    
+#if defined(PYCK_WIN)
+     static DWORD WINAPI Launch( LPVOID ) throw();
+    ID           id32_;
+#endif
+    Handle       handle_;
+    
+#if defined(PYCK_BSD)
+    static void * Launch( void * ) throw();
+#endif
+
+    
+};
+
+#endif
