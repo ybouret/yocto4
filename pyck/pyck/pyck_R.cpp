5b3d3391885a9b9038bb1015a7587089235ca26d
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-01-09 09:27:36 +0000

    R++.hpp handling vec/mat

--- pyck/pyck/pyck_R.cpp
+++ pyck/pyck/pyck_R.cpp
@@ -4,13 +4,21 @@
 extern "C"
 SEXP dumpVec( SEXP args ) throw()
 {
-    const RVector<double> v(args);
-    Rprintf("\t#items=%u\n", unsigned(v.size));
-    for(size_t i=0; i < v.size;++i)
+    try
     {
-        Rprintf("\tv[%2u] = %g\n",unsigned(i), v[i]);
+        const RVector<double> v(args);
+        Rprintf("\t#items=%u\n", unsigned(v.size));
+        for(size_t i=0; i < v.size;++i)
+        {
+            Rprintf("\tv[%2u] = %g\n",unsigned(i), v[i]);
+        }
+        return R_NilValue;
     }
-    return R_NilValue;
+    catch(...)
+    {
+        Rprintf("an error occured!\n");
+        return R_NilValue;
+    }
 }
 
 extern "C"
@@ -32,6 +40,33 @@ SEXP sumVec( SEXP args ) throw()
     catch(...)
     {
         Rprintf("an error occured!\n");
+        return R_NilValue;
+    }
+}
+
+extern "C"
+SEXP dumpMat( SEXP args ) throw()
+{
+    try
+    {
+        const RMatrix<double> m(args);
+        Rprintf("Matrix %ux%u\n", unsigned(m.rows), unsigned(m.cols));
+        for(size_t i=0; i < m.rows; ++i )
+        {
+            for(size_t j=0; j < m.cols; ++j)
+            {
+                Rprintf(" %g", m[j][i]);
+            }
+            Rprintf("\n");
+        }
+        
+        RMatrix<double> q( m.rows, m.cols );
+        memcpy( &q[0][0], &m[0][0], sizeof(double) * q.items );
+        
+        return *q;
+    }
+    catch(...)
+    {
         return R_NilValue;
     }
 }
