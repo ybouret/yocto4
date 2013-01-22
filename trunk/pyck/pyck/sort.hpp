fbbd0c2a3be78db36592579d8f520abb7b6bebe5
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-01-22 14:34:51 +0000

    PYCK: inlined Sort

--- trunk/pyck/pyck/sort.hpp
+++ trunk/pyck/pyck/sort.hpp
@@ -2,10 +2,22 @@
 #define PYCK_SORT_INCLUDED 1
 
 #include "./os.hpp"
+#include <cstdlib>
 
-void Sort( double *x, size_t n ) throw();
-void Sort( float  *x, size_t n ) throw();
-void Sort( size_t *x, size_t n ) throw();
+template <typename T>
+inline int Compare( const void *lhs, const void *rhs ) throw()
+{
+	const T L = *(T *)lhs;
+	const T R = *(T *)rhs;
+	return L < R ? -1 : ( R < L ? 1 : 0 );
+}
+
+template <typename T>
+inline void Sort( T *x, size_t n ) throw()
+{
+    assert(!(x==NULL&&n>0));
+	qsort(x, n, sizeof(T), Compare<T>);
+}
 
 
 #endif
