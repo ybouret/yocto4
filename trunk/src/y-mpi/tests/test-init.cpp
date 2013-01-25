b175939620916663be96d162af8a3a7e0b2f53db
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-01-25 10:07:29 +0000

    upgraded mpi test-init

--- trunk/src/y-mpi/tests/test-init.cpp
+++ trunk/src/y-mpi/tests/test-init.cpp
@@ -8,21 +8,11 @@ YOCTO_UNIT_TEST_IMPL(init)
 {
 	
 	
-	std::cerr << "Start Program" << std::endl;
-	std::cerr << "\t sizeof(mpi)=" << sizeof(mpi) << std::endl;
+    mpi &MPI = mpi::init( &argc, & argv );
 	
-	mpi &MPI = mpi::init( &argc, & argv );
+    MPI.Printf(stderr, "%u.%u '%s' is ready\n", unsigned(MPI.CommWorldSize), unsigned(MPI.CommWorldRank),MPI.ProcessorName );
 	
-	std::cerr << "size=" << MPI.CommWorldSize   << std::endl;
-	std::cerr << "rank=" << MPI.CommWorldRank   << std::endl;
-	std::cerr << "proc=" << MPI.ProcessorName   << std::endl;
-	
-	MPI.Barrier(MPI_COMM_WORLD);
-	std::cerr << MPI.CommWorldRank + 1 << " / " <<  MPI.CommWorldSize << " is ready.." << std::endl;
-	
-	MPI.Barrier(MPI_COMM_WORLD);
 	MPI.Finalize();
-	std::cerr << "End Program" << std::endl;
 	
 	
 }
