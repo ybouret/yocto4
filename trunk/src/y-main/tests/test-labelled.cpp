80c241d900c9191f110b6f266018e7cf0a499248
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-01-25 11:30:50 +0000

    labelled for associative types

---
+++ trunk/src/y-main/tests/test-labelled.cpp
@@ -0,0 +1,59 @@
+#include "yocto/associative/labelled.hpp"
+#include "yocto/utest/run.hpp"
+
+#include "yocto/associative/map.hpp"
+#include "yocto/associative/dictionary.hpp"
+#include "yocto/associative/set.hpp"
+
+using namespace yocto;
+
+namespace  {
+    
+    class dummy
+    {
+    public:
+        
+        explicit dummy( const char *id ) :
+        name(id)
+        {
+        }
+        
+        const string name;
+        
+        const string & key() const throw() { return name; }
+        
+        virtual ~dummy() throw()
+        {
+        }
+        
+        dummy( const dummy &other ) : name(other.name)
+        {
+            
+        }
+        
+    private:
+        YOCTO_DISABLE_ASSIGN(dummy);
+    };
+    
+}
+
+YOCTO_UNIT_TEST_IMPL(labelled)
+{
+    
+    labelled<map,int> M;
+    M.store( "hello", 1 );
+    M.query( "hello" );
+    
+    labelled<dictionary,string,comparator<string> > D;
+    D.store("Hello","Bonjour");
+    D.query("Hello");
+    
+    labelled<set,dummy> S;
+    {
+        const dummy d("hello");
+        S.store( d );
+    }
+    S.query( "hello" );
+    
+}
+YOCTO_UNIT_TEST_DONE()
