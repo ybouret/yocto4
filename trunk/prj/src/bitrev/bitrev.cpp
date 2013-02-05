56e78be0330a8e03632a195e0da03882d425d494
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-02-05 10:30:44 +0000

    bitrev optim

--- trunk/prj/src/bitrev/bitrev.cpp
+++ trunk/prj/src/bitrev/bitrev.cpp
@@ -1,19 +1,74 @@
 #include "yocto/ios/ocstream.hpp"
 #include <iostream>
+#include <cstdio>
 #include "yocto/code/utils.hpp"
-
+#include "yocto/wtime.hpp"
+#include "yocto/sequence/vector.hpp"
+#include "yocto/math/types.hpp"
+#include "yocto/code/bswap.hpp"
 
 using namespace yocto;
+using namespace math;
 
+
+#define ITER_MAX 8192
+
+template <typename T>
+static inline
+void perf( size_t p )
+{
+    wtime chrono;
+    chrono.start();
+    const size_t size = 1 << p;
+    const size_t n    = size << 1;
+    double       ell  = 0;
+    vector<T> arr(n, numeric<T>::zero );
+    for( size_t iter=0; iter < ITER_MAX; ++iter)
+    {
+        const double stamp = chrono.query();
+        size_t j=1;
+        for (size_t i=1; i<n; i+=2)
+        {
+            if (j > i)
+            {
+#if 0
+                cswap(arr[i],arr[j]);
+                cswap(arr[i+1],arr[j+1]);
+                
+#else
+                core::bswap<2*sizeof(T)>( &arr[i], &arr[j] );
+#endif
+            }
+            size_t m = size; // m=  n / 2;
+            while (m >= 2 && j > m)
+            {
+                j -= m;
+                m >>= 1;
+            }
+            j += m;
+        }
+        ell += (chrono.query() - stamp);
+    }
+    const double fac = double(ITER_MAX) * double(size) * 1e-6;
+    std::cerr.flush();
+    fprintf( stderr, "type=%-8s | speed%-5u = %10.5f M/s\n", typeid(T).name(), unsigned(size), fac/ell);
+    fflush(stderr);
+}
 
 int main(int argc, char *argv[] )
 {
     
     try
     {
-        
+      
+
         for( size_t p=0; p < 12; ++p )
         {
+            perf<float>(p);
+            perf<double>(p);
+            std::cerr << std::endl;
+            
+#if 0
             const size_t size = 1 << p;
             std::cerr << "size=" << size << std::endl;
             size_t nops = 0;
@@ -37,6 +92,7 @@ int main(int argc, char *argv[] )
             }
             std::cerr << "\tnops = " << nops << " *2";
             std::cerr << std::endl;
+#endif
         }
         
         return 0;
