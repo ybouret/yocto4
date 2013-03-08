7fdf5778d91ae4b0bbc97cfc785cfb6864c5c3c0
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-03-08 16:30:33 +0000

    starting DBOptlist

--- trunk/src/y-visit/tests/test-silo.cpp
+++ trunk/src/y-visit/tests/test-silo.cpp
@@ -7,7 +7,12 @@ using namespace spade;
 
 YOCTO_UNIT_TEST_IMPL(silo)
 {
-    Silo::File db( "demo.silo", "Just A Demo");
+    Silo::File    db( "demo.silo", "Just A Demo");
+    Silo::OptList optlist(2);
+    
+    optlist.AddTime(0);
+    optlist.AddCycle(0);
+    
     
 }
 YOCTO_UNIT_TEST_DONE()
