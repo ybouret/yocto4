b0d654b208f1728dffd5f5f486b27db9c2f5e223
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-03-15 11:00:54 +0000

    framework for codec32

---
+++ src/y-z++/yocto/z++/codec32.cpp
@@ -0,0 +1,13 @@
+#include "yocto/z++/codec32.hpp"
+
+namespace yocto
+{
+    namespace zlib
+    {
+        codec32:: codec32() throw() {}
+        
+        codec32:: ~codec32() throw() {}
+        
+    }
+    
+}
