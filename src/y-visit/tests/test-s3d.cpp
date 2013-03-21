84541d7b2b9de328bdc554d7f38d6f34cd8331ce
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-03-21 08:10:07 +0000

    with install of test-visit

--- src/y-visit/tests/test-s3d.cpp
+++ src/y-visit/tests/test-s3d.cpp
@@ -28,294 +28,295 @@ typedef array1D<Real>                    Array1D;
 typedef region3D<Real>::type             Region;
 
 
-
-class Parameters
+namespace
 {
-public:
-    static const size_t NumGhosts = 1;
-    
-    FieldsSetup fields;
-    GhostsSetup ghosts;
-    explicit Parameters( const mpi &MPI ) :
-    fields(sizeof(Real)),
-    ghosts()
+    class Parameters
     {
-        //-- declare the fields
-        Y_SPADE_FIELD( fields, "U",  Array);
-        Y_SPADE_FIELD( fields, "dU", Array);
+    public:
+        static const size_t NumGhosts = 1;
         
-        //-- declare the ghosts
-        ghosts.set_local(on_x, NumGhosts); // PBC on x
-        ghosts.set_local(on_y, NumGhosts); // PBC on y
-        if( MPI.IsParallel )
+        FieldsSetup fields;
+        GhostsSetup ghosts;
+        explicit Parameters( const mpi &MPI ) :
+        fields(sizeof(Real)),
+        ghosts()
         {
-            ghosts.set_async(ghost::at_lower_z, NumGhosts, MPI.CommWorldPrev());
-            ghosts.set_async(ghost::at_upper_z, NumGhosts, MPI.CommWorldNext());
-        }
-        else
-            ghosts.set_local(on_z, NumGhosts); // PBC on z, anyway
-        
-    }
-    
-    virtual ~Parameters() throw()
-    {
-    }
-    
-    
-private:
-    YOCTO_DISABLE_COPY_AND_ASSIGN(Parameters);
-};
-
-
-class MySim :
-public VisIt:: Simulation,
-public Parameters,
-public Workspace,
-public VisItIO
-{
-public:
-    Array         &U;
-    Array         &dU;
-    Array1D       &X;
-    Array1D       &Y;
-    Array1D       &Z;
-    double         dt;
-    const Vertex   delta;
-    const Vertex   delsq;
-    const Vertex   inv_dsq;
-    linear_handles handles; //!< for MPI
-    Real           Du;
-    mutable double sum_steps;
-    mutable size_t num_steps;
-    mutable double sum_tcomm;
-    bool           do_sync;
-    int            num_iter;
-    size_t         counts;
-    
-    explicit MySim(const mpi    &ref,
-                   const Layout &l,
-                   const Vertex &d ) :
-    VisIt::Simulation(ref),
-    Parameters(MPI),
-    Workspace( l, fields, ghosts   ),
-    U(  (*this)["U" ].as<Array>()  ),
-    dU( (*this)["dU"].as<Array>()  ),
-    X( mesh.X() ),
-    Y( mesh.Y() ),
-    Z( mesh.Z() ),
-    dt(0),
-    delta(d),
-    delsq(delta.x*delta.x,delta.y*delta.y,delta.z*delta.z),
-    inv_dsq( 1.0/delsq.x, 1.0/delsq.y, 1.0/delsq.z),
-    handles(),
-    Du( 5.0 ),
-    sum_steps(0),
-    num_steps(0),
-    sum_tcomm(0),
-    do_sync(true),
-    num_iter(1),
-    counts(0)
-    {
-        query( handles, "U" );
-        dt = math::log_round(0.1 * min_of( delsq.x, min_of(delsq.y, delsq.z))/max_of(Du,Du));
-        num_iter = int(ceil(0.1/dt));
-        MPI.PrintfI(stderr, "Ready (dt=%g|num_iter=%d)\n", dt, num_iter);
-        
-    }
-    
-    virtual ~MySim() throw()
-    {
-    }
-    
-    void initialize()
-    {
-        U.ldz();
-        for( unit_t i=lower.x;i<=upper.x;++i)
-        {
-            for(unit_t j=lower.y;j<=upper.y;++j)
-            {
-                for(unit_t k=lower.z;k<=upper.z;++k)
-                {
-                    U[k][j][i] = 0.5*(1-2*alea<Real>());
-                }
-            }
-        }
-        sync(MPI,handles);
-        counts=0;
-    }
-    
-    
-    virtual void get_meta_data( visit_handle &md ) const
-    {
-        assert( VISIT_INVALID_HANDLE != md );
-        //! append an UI command
-        add_generic_command("raz", md);
-        
-        //! append an UI command
-        add_generic_command("quit", md);
-        
-        //! append the mesh
-        {
-            visit_handle mmd = mesh_meta_data(mesh, "mesh", par_size);
-            VisIt_SimulationMetaData_addMesh(md, mmd);
-            VisIt_MeshMetaData_setXLabel(mmd, "X");
-            VisIt_MeshMetaData_setYLabel(mmd, "Y");
-            VisIt_MeshMetaData_setZLabel(mmd, "Z");
-        }
-        
-        //! append U on mesh
-        {
-            visit_handle vmd = variable_meta_data<Real>("U", "mesh");
-            VisIt_SimulationMetaData_addVariable(md, vmd);
-        }
-        
-        
-    }
-    
-    
-    virtual visit_handle get_mesh( int domain, const string &name ) const
-    {
-        visit_handle h = VISIT_INVALID_HANDLE;
-        if( name == "mesh" )
-        {
-            if( VisIt_RectilinearMesh_alloc(&h) == VISIT_OKAY )
-            {
-                assert( h != VISIT_INVALID_HANDLE );
-                
-                visit_handle   hx = VISIT_INVALID_HANDLE;
-                visit_handle   hy = VISIT_INVALID_HANDLE;
-                visit_handle   hz = VISIT_INVALID_HANDLE;
-                
-                VisIt_VariableData_alloc(&hx);
-                VisIt_VariableData_setDataD(hx, VISIT_OWNER_SIM, 1, X.items, X.entry );
-                
-                VisIt_VariableData_alloc(&hy);
-                VisIt_VariableData_setDataD(hy, VISIT_OWNER_SIM, 1, Y.items, Y.entry );
-                
-                VisIt_VariableData_alloc(&hz);
-                VisIt_VariableData_setDataD(hz, VISIT_OWNER_SIM, 1, Z.items, Z.entry );
-                
-                VisIt_RectilinearMesh_setCoordsXYZ(h, hx, hy, hz);
-                
-#if 0
-                int minRealIndex[3];
-                int maxRealIndex[3];
-                VisIt_RectilinearMesh_getRealIndices(h,minRealIndex,maxRealIndex);
-                MPI.PrintfI( stderr,
-                            "minRealIndex= { %d, %d, %d } maxRealIndex={ %d, %d, %d } widths={ %d, %d, %d}\n",
-                            minRealIndex[0],
-                            minRealIndex[1],
-                            minRealIndex[2],
-                            maxRealIndex[0],
-                            maxRealIndex[1],
-                            maxRealIndex[2],
-                            int(X.width),
-                            int(Y.width),
-                            int(Z.width)
-                            );
-#endif
-                
-#if 0
-                int minRealIndex[3] = { 0,         0 ,        0        };
-                int maxRealIndex[3] = { X.width-1, Y.width-1, Z.width-1};
-                if( par_rank < par_last ) -- maxRealIndex[2];
-                
-                VisIt_RectilinearMesh_setRealIndices(h, minRealIndex, maxRealIndex);
-#endif
-            }
-        }
-        return h;
-    }
-    
-    virtual visit_handle get_variable( int domain, const string &name ) const
-    {
-        //MPI.Printf( stderr, "rank %d> SIM:get_variable '%s', domain=%d\n", par_rank, name.c_str(), domain);
-        visit_handle h = VISIT_INVALID_HANDLE;
-        
-        if( name == "U" )
-        {
-            const int nComponents=1;
-            const int nTuples    =U.items;
-            //MPI.Printf0( stderr, "Sending U: %dx%d\n", nComponents, nTuples);
-            assert(U.entry!=NULL);
-            if(VisIt_VariableData_alloc(&h) == VISIT_OKAY)
-            {
-                VisIt_VariableData_setDataD(h, VISIT_OWNER_SIM, nComponents, nTuples, U.entry);
-            }
-        }
-        
-        return h;
-    }
-    
-    
-    //! auxiliary commands
-    virtual void perform( const string &cmd, const array<string> & )
-    {
-        MPI.PrintfI( stderr, "cmd: %s\n", cmd.c_str() );
-        if( cmd == "raz" )
-        {
-            initialize();
-            cycle   = 0;
-            runTime = 0;
-        }
-    }
-    
-    
-    virtual void step_prolog()
-    {
-        VisIt::Simulation::step_prolog();
-    }
-    
-    virtual void step()
-    {
-        for( int iter=0; iter<num_iter;++iter)
-        {
-            ++counts;
-            runTime = counts * dt;
+            //-- declare the fields
+            Y_SPADE_FIELD( fields, "U",  Array);
+            Y_SPADE_FIELD( fields, "dU", Array);
             
-            laplacian<Real>::compute( dU, U, as_layout(), inv_dsq);
+            //-- declare the ghosts
+            ghosts.set_local(on_x, NumGhosts); // PBC on x
+            ghosts.set_local(on_y, NumGhosts); // PBC on y
+            if( MPI.IsParallel )
+            {
+                ghosts.set_async(ghost::at_lower_z, NumGhosts, MPI.CommWorldPrev());
+                ghosts.set_async(ghost::at_upper_z, NumGhosts, MPI.CommWorldNext());
+            }
+            else
+                ghosts.set_local(on_z, NumGhosts); // PBC on z, anyway
             
-            for(unit_t k=lower.z;k<=upper.z;++k)
+        }
+        
+        virtual ~Parameters() throw()
+        {
+        }
+        
+        
+    private:
+        YOCTO_DISABLE_COPY_AND_ASSIGN(Parameters);
+    };
+    
+    
+    class MySim :
+    public VisIt:: Simulation,
+    public Parameters,
+    public Workspace,
+    public VisItIO
+    {
+    public:
+        Array         &U;
+        Array         &dU;
+        Array1D       &X;
+        Array1D       &Y;
+        Array1D       &Z;
+        double         dt;
+        const Vertex   delta;
+        const Vertex   delsq;
+        const Vertex   inv_dsq;
+        linear_handles handles; //!< for MPI
+        Real           Du;
+        mutable double sum_steps;
+        mutable size_t num_steps;
+        mutable double sum_tcomm;
+        bool           do_sync;
+        int            num_iter;
+        size_t         counts;
+        
+        explicit MySim(const mpi    &ref,
+                       const Layout &l,
+                       const Vertex &d ) :
+        VisIt::Simulation(ref),
+        Parameters(MPI),
+        Workspace( l, fields, ghosts   ),
+        U(  (*this)["U" ].as<Array>()  ),
+        dU( (*this)["dU"].as<Array>()  ),
+        X( mesh.X() ),
+        Y( mesh.Y() ),
+        Z( mesh.Z() ),
+        dt(0),
+        delta(d),
+        delsq(delta.x*delta.x,delta.y*delta.y,delta.z*delta.z),
+        inv_dsq( 1.0/delsq.x, 1.0/delsq.y, 1.0/delsq.z),
+        handles(),
+        Du( 5.0 ),
+        sum_steps(0),
+        num_steps(0),
+        sum_tcomm(0),
+        do_sync(true),
+        num_iter(1),
+        counts(0)
+        {
+            query( handles, "U" );
+            dt = math::log_round(0.1 * min_of( delsq.x, min_of(delsq.y, delsq.z))/max_of(Du,Du));
+            num_iter = int(ceil(0.1/dt));
+            MPI.PrintfI(stderr, "Ready (dt=%g|num_iter=%d)\n", dt, num_iter);
+            
+        }
+        
+        virtual ~MySim() throw()
+        {
+        }
+        
+        void initialize()
+        {
+            U.ldz();
+            for( unit_t i=lower.x;i<=upper.x;++i)
             {
                 for(unit_t j=lower.y;j<=upper.y;++j)
                 {
-                    for( unit_t i=lower.x;i<=upper.x;++i)
+                    for(unit_t k=lower.z;k<=upper.z;++k)
                     {
-                        const Real u = U[k][j][i];
-                        U[k][j][i] += dt * ( (u - u*u*u) + Du * dU[k][j][i] );
+                        U[k][j][i] = 0.5*(1-2*alea<Real>());
                     }
                 }
             }
-            if(do_sync)
-                sync(MPI,handles);
+            sync(MPI,handles);
+            counts=0;
         }
-    }
-    
-    virtual void step_epilog()
-    {
-        sum_steps += stepTime;
-        sum_tcomm += double(commTime);
-        num_steps += 1;
-        MPI.Printf0(stderr,
-                    "\tsteps/s = %8.2f < %8.2f >\n"
-                    "\tloops/s = %8.2f [ %8.5fx ]\n"
-                    "\tComm/us = %8u [ %8.3f%% ] < %8.3f%% >\n"
-                    ,
-                    1.0/stepTime,
-                    num_steps/sum_steps,
-                    1.0/loopTime,
-                    stepTime/loopTime,
-                    commTime,
-                    ( 1e-4 * double(commTime) ) / stepTime,
-                    ( 1e-4 * sum_tcomm) / sum_steps
-                    );
         
-    }
+        
+        virtual void get_meta_data( visit_handle &md ) const
+        {
+            assert( VISIT_INVALID_HANDLE != md );
+            //! append an UI command
+            add_generic_command("raz", md);
+            
+            //! append an UI command
+            add_generic_command("quit", md);
+            
+            //! append the mesh
+            {
+                visit_handle mmd = mesh_meta_data(mesh, "mesh", par_size);
+                VisIt_SimulationMetaData_addMesh(md, mmd);
+                VisIt_MeshMetaData_setXLabel(mmd, "X");
+                VisIt_MeshMetaData_setYLabel(mmd, "Y");
+                VisIt_MeshMetaData_setZLabel(mmd, "Z");
+            }
+            
+            //! append U on mesh
+            {
+                visit_handle vmd = variable_meta_data<Real>("U", "mesh");
+                VisIt_SimulationMetaData_addVariable(md, vmd);
+            }
+            
+            
+        }
+        
+        
+        virtual visit_handle get_mesh( int domain, const string &name ) const
+        {
+            visit_handle h = VISIT_INVALID_HANDLE;
+            if( name == "mesh" )
+            {
+                if( VisIt_RectilinearMesh_alloc(&h) == VISIT_OKAY )
+                {
+                    assert( h != VISIT_INVALID_HANDLE );
+                    
+                    visit_handle   hx = VISIT_INVALID_HANDLE;
+                    visit_handle   hy = VISIT_INVALID_HANDLE;
+                    visit_handle   hz = VISIT_INVALID_HANDLE;
+                    
+                    VisIt_VariableData_alloc(&hx);
+                    VisIt_VariableData_setDataD(hx, VISIT_OWNER_SIM, 1, X.items, X.entry );
+                    
+                    VisIt_VariableData_alloc(&hy);
+                    VisIt_VariableData_setDataD(hy, VISIT_OWNER_SIM, 1, Y.items, Y.entry );
+                    
+                    VisIt_VariableData_alloc(&hz);
+                    VisIt_VariableData_setDataD(hz, VISIT_OWNER_SIM, 1, Z.items, Z.entry );
+                    
+                    VisIt_RectilinearMesh_setCoordsXYZ(h, hx, hy, hz);
+                    
+#if 0
+                    int minRealIndex[3];
+                    int maxRealIndex[3];
+                    VisIt_RectilinearMesh_getRealIndices(h,minRealIndex,maxRealIndex);
+                    MPI.PrintfI( stderr,
+                                "minRealIndex= { %d, %d, %d } maxRealIndex={ %d, %d, %d } widths={ %d, %d, %d}\n",
+                                minRealIndex[0],
+                                minRealIndex[1],
+                                minRealIndex[2],
+                                maxRealIndex[0],
+                                maxRealIndex[1],
+                                maxRealIndex[2],
+                                int(X.width),
+                                int(Y.width),
+                                int(Z.width)
+                                );
+#endif
+                    
+#if 0
+                    int minRealIndex[3] = { 0,         0 ,        0        };
+                    int maxRealIndex[3] = { X.width-1, Y.width-1, Z.width-1};
+                    if( par_rank < par_last ) -- maxRealIndex[2];
+                    
+                    VisIt_RectilinearMesh_setRealIndices(h, minRealIndex, maxRealIndex);
+#endif
+                }
+            }
+            return h;
+        }
+        
+        virtual visit_handle get_variable( int domain, const string &name ) const
+        {
+            visit_handle h = VISIT_INVALID_HANDLE;
+            
+            if( name == "U" )
+            {
+                const int nComponents=1;
+                const int nTuples    =U.items;
+                assert(U.entry!=NULL);
+                if(VisIt_VariableData_alloc(&h) == VISIT_OKAY)
+                {
+                    VisIt_VariableData_setDataD(h, VISIT_OWNER_SIM, nComponents, nTuples, U.entry);
+                }
+            }
+            
+            return h;
+        }
+        
+        
+        //! auxiliary commands
+        virtual void perform( const string &cmd, const array<string> & )
+        {
+            MPI.PrintfI( stderr, "cmd: %s\n", cmd.c_str() );
+            if( cmd == "raz" )
+            {
+                initialize();
+                cycle   = 0;
+                runTime = 0;
+            }
+        }
+        
+        
+        virtual void step_prolog()
+        {
+            VisIt::Simulation::step_prolog();
+        }
+        
+        virtual void step()
+        {
+            for( int iter=0; iter<num_iter;++iter)
+            {
+                ++counts;
+                runTime = counts * dt;
+                
+                laplacian<Real>::compute( dU, U, as_layout(), inv_dsq);
+                
+                for(unit_t k=lower.z;k<=upper.z;++k)
+                {
+                    for(unit_t j=lower.y;j<=upper.y;++j)
+                    {
+                        for( unit_t i=lower.x;i<=upper.x;++i)
+                        {
+                            const Real u = U[k][j][i];
+                            U[k][j][i] += dt * ( (u - u*u*u) + Du * dU[k][j][i] );
+                        }
+                    }
+                }
+                if(do_sync)
+                    sync(MPI,handles);
+            }
+        }
+        
+        virtual void step_epilog()
+        {
+            sum_steps += stepTime;
+            sum_tcomm += double(commTime);
+            num_steps += 1;
+            MPI.Printf0(stderr,
+                        "\tsteps/s = %8.2f < %8.2f >\n"
+                        "\tloops/s = %8.2f [ %8.5fx ]\n"
+                        "\tComm/us = %8u [ %8.3f%% ] < %8.3f%% >\n"
+                        ,
+                        1.0/stepTime,
+                        num_steps/sum_steps,
+                        1.0/loopTime,
+                        stepTime/loopTime,
+                        commTime,
+                        ( 1e-4 * double(commTime) ) / stepTime,
+                        ( 1e-4 * sum_tcomm) / sum_steps
+                        );
+            
+        }
+        
+        
+    private:
+        YOCTO_DISABLE_COPY_AND_ASSIGN(MySim);
+    };
     
-    
-private:
-    YOCTO_DISABLE_COPY_AND_ASSIGN(MySim);
-};
+}
 
 #include "yocto/string/conv.hpp"
 #include "yocto/string/env.hpp"
