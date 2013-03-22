8bd145ceb1c01a67d7e0b6461816187f983b9bd8
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-03-22 13:05:15 +0000

    seems ok for sides in 1D/2D/3D

--- src/y-spade/tests/test-ctrls.cpp
+++ src/y-spade/tests/test-ctrls.cpp
@@ -21,7 +21,7 @@ namespace
         std::cerr << std::endl;
     }
     
-    void ctrl1D( )
+    static void ctrl1D( )
     {
         std::cerr << std::endl << "\t\t1D" << std::endl << std::endl;
         layout1D               L( 1, 10 );
@@ -40,8 +40,38 @@ namespace
             dataspace<layout1D>  D(L,F,G);
             display(D, "1DXLO1XHI2");
         }
-        
     }
+    
+    static void ctrl2D( )
+    {
+        std::cerr << std::endl << "\t\t2D" << std::endl << std::endl;
+        layout2D               L( coord2D(1,1), coord2D(10,20) );
+        fields_setup<layout2D> F(16);
+        {
+            ghosts_setup           G;
+            G.set_async( ghost::at_lower_y, 1, 0);
+            G.set_async( ghost::at_upper_y, 2, 0);
+            dataspace<layout2D>  D(L,F,G);
+            display(D, "2DYLO1HI2");
+        }
+    }
+    
+    static void ctrl3D( )
+    {
+        std::cerr << std::endl << "\t\t3D" << std::endl << std::endl;
+        layout3D               L( coord3D(1,1,1), coord3D(10,20,30) );
+        fields_setup<layout3D> F(16);
+        {
+            ghosts_setup           G;
+            G.set_async( ghost::at_lower_z, 1, 0);
+            G.set_async( ghost::at_upper_z, 2, 0);
+            dataspace<layout3D>  D(L,F,G);
+            display(D, "3DZLO1ZHI2");
+        }
+    }
+
+    
+    
 }
 
 
@@ -49,8 +79,8 @@ YOCTO_UNIT_TEST_IMPL(ctrls)
 {
     
     ctrl1D();
-    
-    
+    ctrl2D();
+    ctrl3D();
     
 }
 YOCTO_UNIT_TEST_DONE()
