fad67f091cd5d9b6fba42ead67ff5b25513ccdc8
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-03-28 14:17:18 +0000

    initial MPI/thread

--- trunk/src/y-mpi/tests/main.cpp
+++ trunk/src/y-mpi/tests/main.cpp
@@ -1,6 +1,6 @@
 #include "yocto/utest/driver.hpp"
 
-YOCTO_UNIT_TEST_INIT(8)
+YOCTO_UNIT_TEST_INIT(16)
 {
 	YOCTO_UNIT_TEST_DECL(init);
 	YOCTO_UNIT_TEST_DECL(ping);
@@ -9,7 +9,7 @@ YOCTO_UNIT_TEST_INIT(8)
 	YOCTO_UNIT_TEST_DECL(scatter);
     YOCTO_UNIT_TEST_DECL(string);
     YOCTO_UNIT_TEST_DECL(over);
-
+    YOCTO_UNIT_TEST_DECL(comm);
 }
 
 YOCTO_UNIT_TEST_EXEC()
