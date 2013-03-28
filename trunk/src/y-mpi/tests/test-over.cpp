b1cd45236a09e6b6842978d44d7248281a0b43a7
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-03-28 10:27:42 +0000

    start testing over/Async

--- trunk/src/y-mpi/tests/test-over.cpp
+++ trunk/src/y-mpi/tests/test-over.cpp
@@ -9,7 +9,9 @@ namespace {
     
     typedef memory::buffer_of<uint8_t, memory::global> memblock;
     
-    static void perform_over( const mpi &MPI, size_t block_size )
+#define NUM_ITER 32
+    
+    static void perform_over( const mpi &MPI, size_t block_size, double &bw, double &t_ave)
     {
         MPI.Printf0(stderr, "\nTesting %u\n", unsigned(block_size) );
         const int tag = 1234;
@@ -35,10 +37,11 @@ namespace {
         //----------------------------------------------------------------------
         // statistics on sync comms
         //----------------------------------------------------------------------
+        MPI.Printf0(stderr, "[[Synchronous Exchanges]]\n");
         unsigned long bytes = 0;
         MPI.Barrier(MPI_COMM_WORLD);
-        const double  t_ini = MPI.Wtime();
-        for( size_t i=0; i < 16; ++i )
+        double  t_ini = MPI.Wtime();
+        for( size_t i=0; i < NUM_ITER; ++i )
         {
             MPI_Status status;
             
@@ -57,15 +60,37 @@ namespace {
             
         }
         
-        const double t_end = MPI.Wtime() - t_ini;
+        double        t_end = MPI.Wtime() - t_ini;
         double        t_sum = 0;
         unsigned long b_sum = 0;
         MPI.Reduce(&t_end, &t_sum, 1, MPI_DOUBLE,        MPI_SUM, 0, MPI_COMM_WORLD);
         MPI.Reduce(&bytes, &b_sum, 1, MPI_UNSIGNED_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
         //MPI.Printf0(stderr,"sum_bytes=%lu/sum_time=%g\n", b_sum, t_sum);
-        const double t_ave = t_sum/size;
-        const double bw = rank == 0 ? (GbitsFactor*b_sum)/(t_ave) : 0;
+        t_ave = t_sum/size;
+        bw    = rank == 0 ? (GbitsFactor*b_sum)/(t_ave) : 0;
         MPI.Printf0(stderr, "<Gbits/s> = %.3f\n", bw);
+        
+        
+        MPI.Printf0(stderr, "[[Asynchronous Exchanges]]\n");
+        bytes = 0;
+        t_ini = MPI.Wtime();
+        for( size_t i=0; i <NUM_ITER; ++i )
+        {
+            MPI_Request req[4];
+            MPI_Status  sta[4];
+            size_t      ir = 0;
+            MPI.Isend( send_next.ro(), block_size, MPI_BYTE, next, tag, MPI_COMM_WORLD, req[ir++]);
+            MPI.Irecv( recv_next.rw(), block_size, MPI_BYTE, next, tag, MPI_COMM_WORLD, req[ir++]);
+            MPI.Isend( send_prev.ro(), block_size, MPI_BYTE, prev, tag, MPI_COMM_WORLD, req[ir++]);
+            MPI.Irecv( recv_prev.rw(), block_size, MPI_BYTE, prev, tag, MPI_COMM_WORLD, req[ir++]);
+            
+            MPI.Waitall(ir, req, sta);
+        }
+        t_end = MPI.Wtime() - t_ini;
+        t_sum = 0;
+        MPI.Reduce(&t_end, &t_sum, 1, MPI_DOUBLE,        MPI_SUM, 0, MPI_COMM_WORLD);
+        MPI.Printf0(stderr, "t_ave=%g / %g\n", t_sum/size, t_ave);
+        
     }
     
 }
@@ -77,10 +102,19 @@ YOCTO_UNIT_TEST_IMPL(over)
     MPI.CloseStdIO();
     if( MPI.IsParallel )
     {
-        for( int kb=1; kb <= 128; kb <<= 1)
+        FILE *fp = 0;
+        if( MPI.IsFirst ) fp = fopen( "over-perf.dat", "wb");
+        for( int kb=1; kb <= 1024; kb <<= 1)
         {
-            perform_over(MPI, kb*1024);
+            double t_ave = 0;
+            double bw    = 0;
+            perform_over(MPI, kb*1024, bw, t_ave);
+            if(MPI.IsFirst)
+            {
+                fprintf(fp,"%d %g %g\n", kb, bw, t_ave);
+            }
         }
+        if(fp) fclose(fp);
     }
     
 }
