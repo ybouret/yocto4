7c63cea92a5379d612aad58174ee8f5bb619275c
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-04-02 08:20:14 +0000

    thread proxy OK

--- src/y-main/tests/test-thread.cpp
+++ src/y-main/tests/test-thread.cpp
@@ -13,9 +13,9 @@ namespace
 		mutex  *synchro;
 		double  sum;
 	};
-
-	static 
-		void thread_proc( void *args )
+    
+	static
+    void thread_proc( void *args )
 	{
 		thread_data       &d = *(thread_data *)args;
 		mutex             &m = *d.synchro;
@@ -34,11 +34,11 @@ YOCTO_UNIT_TEST_IMPL(thread)
 {
 	mutex synchro( "synchro" );
 	thread_data d = { &synchro, 0 };
-
+    
 	thread thr1( thread_proc, &d );
 	thread thr2( thread_proc, &d );
 	thread thr3( thread_proc, &d );
-
+    
 	thr1.join();
 	thr2.join();
 	thr3.join();
@@ -46,12 +46,14 @@ YOCTO_UNIT_TEST_IMPL(thread)
 }
 YOCTO_UNIT_TEST_DONE()
 
+
+
 #include <cstdio>
 #include <cstdlib>
 #include <cmath>
 
 
-	static void cpu_proc( size_t cpu_id, void * )
+static void cpu_proc( size_t cpu_id, void * )
 {
 	std::cerr << "\tOn CPU #" << cpu_id << std::endl;
 }
@@ -66,7 +68,7 @@ YOCTO_UNIT_TEST_IMPL(place)
 	thread::assign_cpu( thread::get_current_handle(), cpu_id);
 	std::cerr << "After Placement:  " << std::endl;
 	thread::foreach_cpu( thread::get_current_handle(), cpu_proc, 0 );
-
+    
 #if 0
 	std::cerr << "Press Enter after check!" << std::endl;
 	fgetc(stdin);
@@ -89,6 +91,47 @@ YOCTO_UNIT_TEST_IMPL(place)
 		if( chrono.query() >= 5.0 )
 			break;
 	}
+}
+YOCTO_UNIT_TEST_DONE()
+
+
+namespace {
+    
+    static void do_something()
+    {
+        std::cerr << "doing something in thread" << std::endl;
+    }
+    
+    struct run_something
+    {
+        void operator()(void)
+        {
+            std::cerr << "run something()" << std::endl;
+        }
+        
+        void compute()
+        {
+            std::cerr << "run compute()" << std::endl;
+        }
+    };
+}
+
+#include "yocto/functor.hpp"
+
+YOCTO_UNIT_TEST_IMPL(thr_proxy)
+{
+    thread_proxy thr;
+    thr.launch( do_something );
+    thr.finish();
+    
+    run_something run;
+    thr.launch(run);
+    thr.finish();
+    
+    functor<void,null_type> fn( &run , & run_something::compute );
+    thr.launch(fn);
+    thr.finish();
+        
 }
 YOCTO_UNIT_TEST_DONE()
 
