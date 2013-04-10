56956b2673da51f37caba17f88fa78da25bf7ae1
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-04-10 14:53:31 +0000

    init hull2D

---
+++ src/y-mk/yocto/math/algo2d/hull-s.cpp
@@ -0,0 +1,3 @@
+#define YOCTO_ZTYPE 's'
+#include "hull.cxx"
+
---
+++ src/y-mk/yocto/math/algo2d/hull.hpp
@@ -0,0 +1,30 @@
+#ifndef YOCTO_MATH_ALGO2D_HULL_INCLUDED
+#define YOCTO_MATH_ALGO2D_HULL_INCLUDED 1
+
+#include "yocto/geom/v2d.hpp"
+#include "yocto/sequence/array.hpp"
+
+namespace yocto
+{
+ 
+    namespace math
+    {
+        
+        namespace algo2d
+        {
+            
+            template <typename T>
+            struct hull
+            {
+                typedef geom::v2d<T> vertex;
+                void build( sequence<vertex> &h, const array<vertex> &points);
+            };
+            
+        }
+        
+    }
+    
+}
+
+#endif
+
