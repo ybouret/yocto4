f8bf39cd6c32446ceb1cafa86ed5921610fd6d5c
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-04-12 11:47:37 +0000

    first test on delaunay

--- src/y-mk/tests/test-hull2d.cpp
+++ src/y-mk/tests/test-hull2d.cpp
@@ -3,15 +3,34 @@
 #include "yocto/code/rand.hpp"
 
 #include "yocto/ios/ocstream.hpp"
+#include "yocto/sequence/vector.hpp"
 
 using namespace yocto;
 using namespace math;
 
+typedef geom::v2d<double> V2;
 
 YOCTO_UNIT_TEST_IMPL(tr2d)
 {
-        
-        
+    
+    const int  n=3;
+    vector<V2> vec2(n,as_capacity);
+    for(size_t i=1; i<=n; ++i )
+    {
+        const V2 tmp( 10.0 * ( 0.5 - alea<double>()), 10.0 * ( 0.5 - alea<double>()) );
+        vec2.push_back(tmp);
+    }
+    
+    {
+        ios::ocstream fp("points.dat",false);
+        for( size_t i=1; i <= vec2.size(); ++i )
+        {
+            fp("%g %g\n", vec2[i].x, vec2[i].y);
+        }
+    }
+    
+    delaunay<double>::build(vec2);
+    
 }
 YOCTO_UNIT_TEST_DONE();
 
