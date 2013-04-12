91d6390f606a75aac85416825ca66df229499c0c
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-04-12 10:20:21 +0000

    progress on delaunay

---
+++ trunk/src/y-main/yocto/auto-arr.hpp
@@ -0,0 +1,47 @@
+#ifndef YOCTO_AUTO_ARR_INCLUDED
+#define YOCTO_AUTO_ARR_INCLUDED 1
+
+#include "yocto/memory/allocator.hpp"
+#include "yocto/type-traits.hpp"
+
+namespace yocto
+{
+    
+    //! for C-style inline arrays
+    template <
+    typename T,
+    typename ALLOCATOR = memory::global
+    >
+    class auto_arr
+    {
+    public:
+        YOCTO_ARGUMENTS_DECL_T;
+        
+        const size_t size;
+        
+        explicit auto_arr( size_t n ) :
+        size(n),
+        addr( memory::kind<ALLOCATOR>::template acquire_as<mutable_type>( (size_t&)size ))
+        {
+        }
+        
+        virtual ~auto_arr() throw()
+        {
+            memory::kind<ALLOCATOR>::template release_as<mutable_type>( addr, (size_t&)size );
+        }
+        
+        type *      base() throw()       { return addr; }
+        const_type *base() const throw() { return addr; }
+        
+        inline type       & operator[](size_t i) throw()       { assert(i<size); return addr[i]; }
+        inline const_type & operator[](size_t i) const throw() { assert(i<size); return addr[i]; }
+        
+    private:
+        mutable_type *addr;
+        YOCTO_DISABLE_COPY_AND_ASSIGN(auto_arr);
+    };
+    
+}
+
+
+#endif
