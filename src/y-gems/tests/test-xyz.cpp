65b6402d7391ca7e0e99523428da8bbe840187cc
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-04-15 09:38:50 +0000

    ok for XYZ

--- src/y-gems/tests/test-xyz.cpp
+++ src/y-gems/tests/test-xyz.cpp
@@ -1,6 +1,7 @@
 #include "yocto/utest/run.hpp"
 #include "yocto/gems/io/xyz.hpp"
 #include "yocto/ios/icstream.hpp"
+#include "yocto/string/conv.hpp"
 
 using namespace yocto;
 using namespace gems;
@@ -13,10 +14,18 @@ YOCTO_UNIT_TEST_IMPL(xyz)
         const string  fn = argv[1];
         ios::icstream fp(fn);
         
-        IO::XYZ::Frames frames;
         
-        frames.load( fp, 10 );
-        std::cerr << "#frames=" << frames.size() << std::endl;
+        size_t nmax = 10;
+        if( argc > 2)
+            nmax = strconv::to_size( argv[2], "nmax");
+        
+        if(true)
+        {
+            IO::XYZ::Frames frames;
+            frames.load( fp, nmax );
+            std::cerr << "#frames=" << frames.size() << std::endl;
+        }
+        
     }
 }
 YOCTO_UNIT_TEST_DONE()
