bfd02100b77b4b7beef05a9283b58f4416abe5ad
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-04-17 12:33:40 +0000

    moving fft into mk

---
+++ src/y-mk/yocto/math/fft/fft1.cxx
@@ -0,0 +1,105 @@
+#include "yocto/math/fft/fft1.hpp"
+#include "yocto/math/ztype.hpp"
+#include "yocto/code/utils.hpp"
+#include "yocto/math/types.hpp"
+#include "yocto/code/unroll.hpp"
+#include "yocto/code/bswap.hpp"
+
+namespace yocto
+{
+	namespace math
+	{
+		
+		
+		template <>
+		void Fourier<real_t>:: Transform(real_t      *data,
+										 const size_t size, 
+										 const int    isign
+										 ) throw()
+		{
+			assert( data != NULL );
+			assert( is_a_power_of_two(size) );
+			assert( isign==1 || isign==-1);
+			--data;
+			
+			//==================================================================
+			// bit reversal algorithm
+			//==================================================================
+			const size_t n    = size << 1; 
+			const real_t coef = REAL(1.0)/ Sqrt(  real_t(size) ); 
+			{
+				size_t j=1;
+				for (size_t i=1;i<n;i+=2) 
+				{ 
+					if(j>i) 
+					{ 
+						
+#if 0
+                        real_t *d_i = data+i;
+						real_t *d_j = data+j;
+						cswap(d_j[0], d_i[0] );
+						cswap(d_j[1], d_i[1] );
+#else
+                        core::bswap<2*sizeof(real_t)>( data+i, data+j);
+#endif
+					}
+					size_t m = size; // m=  n / 2;
+					while( m>=2 && j>m )
+					{
+						j -= m;
+						m >>= 1;
+					}
+					j += m;
+				}
+			}
+			
+			//==================================================================
+			// Lanczos-Algorithm
+			//==================================================================
+			{
+				const real_t sgn_two_pi = isign * numeric<real_t>::two_pi;
+				size_t       mmax       = 2;
+				while (n > mmax) 
+				{ 
+					const size_t istep = mmax << 1;
+					const real_t theta = sgn_two_pi/mmax; 
+					real_t wtemp       = Sin(REAL(0.5)*theta);
+					real_t wpr         = -REAL(2.0)*wtemp*wtemp;
+					real_t wpi         = Sin(theta);
+					real_t wr          = REAL(1.0);
+					real_t wi          = REAL(0.0);
+					
+					for (size_t m=1;m<mmax;m+=2) 
+					{ 
+						for (size_t i=m;i<=n;i+=istep) 
+						{
+							real_t      *d_i   = data+i;
+							size_t       j     = i+mmax;
+							real_t      *d_j   = data+j;
+							const real_t tempr = wr*d_j[0]-wi*d_j[1];
+							const real_t tempi = wr*d_j[1]+wi*d_j[0];
+							
+							d_j[0]  = d_i[0]-tempr;
+							d_j[1]  = d_i[1]-tempi;
+							d_i[0] += tempr;
+							d_i[1] += tempi;
+						}
+						wr=(wtemp=wr)*wpr-wi*wpi+wr; 
+						wi=wi*wpr+wtemp*wpi+wi;
+					}
+					mmax=istep;
+				}
+			}
+			
+			//==================================================================
+			// Normalizing
+			//==================================================================
+#define _YOCTO_FFT_SCALE(INDEX) data[INDEX] *= coef
+			YOCTO_LOOP_FUNC_(n,_YOCTO_FFT_SCALE,1);
+		}
+		
+		
+		
+		
+	}
+}
---
+++ src/y-mk/yocto/math/fft/psd.cxx
@@ -0,0 +1,291 @@
+#include "yocto/math/ztype.hpp"
+#include "yocto/math/types.hpp"
+#include "yocto/math/fft/psd.hpp"
+#include "yocto/math/fft/fft1.hpp"
+
+#include "yocto/code/utils.hpp"
+#include "yocto/code/unroll.hpp"
+
+#include "yocto/exceptions.hpp"
+#include "yocto/sequence/vector.hpp"
+
+#include <cerrno>
+
+namespace yocto
+{
+	
+    namespace math
+    {
+		
+        //======================================================================
+        template <>
+        real_t PSD<real_t>:: Square( real_t X ) throw()
+        {
+            return X>=REAL(0.0) && X<=REAL(1.0) ? REAL(1.0) : REAL(0.0);
+        }
+		
+        template <>
+        real_t PSD<real_t>:: Bartlett( real_t X ) throw()
+        {
+            return X>REAL(0.0) && X<REAL(1.0) ? REAL(1.0) - Fabs( (X-REAL(0.5) / REAL(0.5) ) ): REAL(0.0);
+        }
+		
+        template <>
+        real_t PSD<real_t>:: Hann( real_t X ) throw()
+        {
+            return X>REAL(0.0) && X<REAL(1.0) ? REAL(0.5) * (REAL(1.0) - Cos( numeric<real_t>::two_pi * X)) : REAL(0.0) ;
+        }
+		
+        template <>
+        real_t PSD<real_t>:: Welch( real_t X ) throw()
+        {
+            if(  X>0 && X<1 )
+            {
+                const real_t tmp = (X-REAL(0.5) / REAL(0.5));
+                return REAL(1.0) - tmp * tmp;
+            }
+            else
+            {
+                return 0;
+            }
+			
+        }
+		
+        
+        //======================================================================
+        template<>
+        real_t PSD<real_t>:: Blackman( real_t X ) throw()
+        {
+            if( X > 0 && X < 1 )
+            {
+                const real_t tmp  = numeric<real_t>::two_pi * X;
+                return REAL(0.42) - REAL(0.50) * Cos( tmp  ) + REAL(0.08) * Cos( tmp + tmp );
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        
+        template <>
+        real_t PSD<real_t>:: Nutall( real_t X ) throw()
+        {
+            if( X > 0 && X < 1 )
+            {
+                const real_t pi2 = numeric<real_t>:: two_pi * X;
+                const real_t pi4 = pi2+pi2;
+                const real_t pi6 = pi4+pi2;
+                return REAL(0.355768) - REAL(0.487396) * Cos(pi2) + REAL(0.144232) * Cos(pi4) - REAL(0.012604) * Cos(pi6);
+            }
+            else
+                return 0;
+        }
+		
+		
+#if 0
+#define YOCTO_PSD_INITIALIZE(INDEX)       p[INDEX] = 0
+#define YOCTO_PSD_COMPUTE_WEIGHT(INDEX)   const real_t tmp = weight[INDEX] = w( INDEX * wfact ); sumw2 += tmp*tmp
+#define YOCTO_PSD_COPY_INDEPENDENT(INDEX) input[INDEX] = cplx_t(start[INDEX] * weight[INDEX])
+#define YOCTO_PSD_COPY_OVERLAPPING(INDEX) input[INDEX] = cplx_t(data[ (idx++) & mask ] * weight[INDEX])
+		
+#define YOCTO_PSD_ADD(INDEX) p[INDEX] += (input[INDEX].mod2())
+		
+#define YOCTO_PSD_NRM(INDEX) p[INDEX] *= pfact
+		
+        template<>
+        size_t PSD<real_t>::Compute(Window       &w,
+                                    real_t       *p,
+                                    const size_t  m,
+                                    const real_t *data,
+                                    const size_t  size,
+                                    const size_t  options)
+        {
+			const cplx_t cplx_zero(0);
+            assert( p    != NULL );
+            assert( data != NULL );
+            assert( is_a_power_of_two(size) );
+            assert( is_a_power_of_two(m)    );
+			
+			const bool   overlap   = (PSD_Overlap   & options) != 0;
+            const size_t M         = m << 1;
+            if( M > size )
+                throw libc::exception(EDOM,"math::PowerSpectralDensity(2*m=%u>#data=%u)", unsigned(M), unsigned(size));
+			
+            YOCTO_LOOP_FUNC_(m,YOCTO_PSD_INITIALIZE,0);
+            const size_t   shift = overlap ? m : M;
+            const size_t   nsubs = size/shift;
+            
+			vector<cplx_t> work( M, cplx_zero );
+            cplx_t        *input = work(0);
+			vector<real_t> work2(M,0);
+			real_t        *weight = work2(0);
+			real_t         sumw2  = 0;
+			{
+				const real_t   wfact = REAL(1.0)/(M-1);
+				YOCTO_LOOP_FUNC_(M,YOCTO_PSD_COMPUTE_WEIGHT,0);
+			}
+			
+			
+			
+			if( overlap )
+			{
+				const size_t mask = size - 1;
+				size_t       idx0 = 0;
+				for( size_t k=0; k < nsubs; ++k, idx0 += shift )
+				{
+					//-- initialize input :
+					//-- cyclic weighted copy
+					size_t idx = idx0;
+					YOCTO_LOOP_FUNC_(M,YOCTO_PSD_COPY_OVERLAPPING,0);
+					
+					//-- Fourier Transform
+					FFT( input, M );
+					
+					//-- gather the spectrum from [0..m-1]
+					YOCTO_LOOP_FUNC_(m,YOCTO_PSD_ADD,0);
+				}
+			}
+			else
+			{
+				const real_t  *start = data;
+				for(size_t k=0;
+					k < nsubs;
+					++k, start += shift )
+				{
+					//-- initialize input :
+					//-- copy start[0..M-1] into input, weighted
+					YOCTO_LOOP_FUNC_(M,YOCTO_PSD_COPY_INDEPENDENT,0);
+					
+					//-- Fourier Transform
+					FFT( input, M );
+					
+					//-- gather the spectrum from [0..m-1]
+					YOCTO_LOOP_FUNC_(m,YOCTO_PSD_ADD,0);
+				}
+				
+			}
+			
+			//! Partial Normalization
+			const real_t pfact = REAL(1.0) / (sumw2*nsubs);
+			YOCTO_LOOP_FUNC_(m,YOCTO_PSD_NRM,0);
+			
+            return M;
+        }
+        
+        template<>
+        size_t PSD<real_t>::Compute(Window              &w,
+                                    array<real_t>       &psd,
+                                    const array<real_t> &data,
+                                    const size_t         options)
+        {
+            
+            return PSD<real_t>::Compute(w, psd(), psd.size(), data(), data.size(), options);
+        }
+#endif
+        
+        template <>
+        void PSD<real_t>:: Compute(Window        &w,
+                                   real_t        *p,
+                                   const size_t   m,
+                                   const real_t *data,
+                                   const size_t  size)
+        {
+            
+            assert(p!=NULL);
+            assert(m>0);
+            assert(is_a_power_of_two(m));
+            assert(!(data==NULL&&size>0));
+            
+            //==================================================================
+            // initialize data
+            //==================================================================
+            const size_t M = m << 1;
+            if( M > size )
+                throw exception("PSD: %u*2 > data size=%u", unsigned(m), unsigned(size));
+            
+            
+            vector<cplx_t> work(  M, numeric<cplx_t>::zero );
+            vector<real_t> work2( M, numeric<real_t>::zero );
+            cplx_t        *input  = work(0);
+            real_t        *weight = work2(0);
+            
+            //==================================================================
+            // initialize the weights
+            //==================================================================
+            real_t         sumw2  = 0;
+            {
+                const real_t wfac = REAL(1.0) / (M-1);
+                for( size_t i=0; i < M; ++i )
+                {
+                    const real_t tmp = weight[i] = w( i * wfac ); // or (i+0.5)/M ?
+                    sumw2 += tmp*tmp;
+                }
+            }
+            
+            //==================================================================
+            // initialize the spectrum
+            //==================================================================
+            for(size_t i=0;i<m;++i)
+                p[i] = 0;
+            
+            //==================================================================
+            // loop over segments
+            //==================================================================
+            size_t nsub = 0;
+            const size_t M2 = M*M;
+            const size_t M3 = M2*M;
+            for( size_t start=0; start+M <= size; ++start)
+            {
+                real_t a0=0,a1=0;
+                //--------------------------------------------------------------
+                // harmonizing data
+                //--------------------------------------------------------------
+                real_t sig1 = 0;
+                real_t sig2 = 0;
+                for(size_t i=0;i<M;++i)
+                {
+                    const real_t f_i = data[ start+i ];
+                    sig1 += f_i;
+                    sig2 += (REAL(0.5) + real_t(i)) * f_i;
+                }
+                a0 = (6*sig2)/M2 - (4*sig1)/M;
+                a1 = (6*sig1)/M2 - (12*sig2)/M3;
+                
+                //--------------------------------------------------------------
+                //-- load the sample
+                //--------------------------------------------------------------
+                for( size_t i=0; i<M; ++i )
+                {
+                    assert(start+i<size);
+                    cplx_t &f = input[i];
+                    f.re = (data[ start+i ] + (a1*i+a0) ) * weight[i];
+                    f.im = 0;
+                }
+                
+                //--------------------------------------------------------------
+                //-- take the local FFT
+                //--------------------------------------------------------------
+                FFT(input,M);
+                for(size_t i=0;i<m;++i)
+                    p[i] += input[i].mod2();
+                ++nsub;
+            }
+            
+            //==================================================================
+            // normalize
+            //==================================================================
+            const real_t nfac = REAL(1.0)/(nsub*sumw2);
+            for(size_t i=0;i<m;++i)
+                p[i] *= nfac;
+        }
+        
+        template <>
+        void PSD<real_t>:: Compute(Window &w, array<real_t> &psd, const array<real_t> &data)
+        {
+            PSD<real_t>::Compute( w, psd(0), psd.size(), data(0), data.size() );
+        }
+        
+        
+    }
+	
+}
