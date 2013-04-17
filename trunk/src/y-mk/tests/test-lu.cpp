f5c5c52668644a3973438f748748f767611fa04d
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-04-17 12:03:46 +0000

    upgrading PSD

--- trunk/src/y-mk/tests/test-lu.cpp
+++ trunk/src/y-mk/tests/test-lu.cpp
@@ -39,7 +39,7 @@ static inline void test_lu()
 		}
 		vector<T> b(n,numeric<T>::zero);
 		vector<T> x(n,numeric<T>::zero);
-        	vector<T> R(n,numeric<T>::zero);
+        vector<T> R(n,numeric<T>::zero);
         
 		for( size_t i=1; i <= n; ++i )
 		{
@@ -61,11 +61,45 @@ static inline void test_lu()
 	
 }
 
+
+
 YOCTO_UNIT_TEST_IMPL(lu)
 {
 	test_lu<float>();
 	test_lu< complex<float> > ();
 	test_lu<double>();
 	test_lu< complex<double> >();
+}
+YOCTO_UNIT_TEST_DONE()
+
+YOCTO_UNIT_TEST_IMPL(lu2)
+{
+    const size_t   n = 1 + alea_leq(10);
+    matrix<double> mu(n,n);
+    matrix<double> mu0(n,n);
+    lu<double>     LU(n);
+    
+    do
+    {
+        for( size_t i=1; i <= n; ++i)
+        {
+            for(size_t j=1;j<=n;++j)
+            {
+                mu0[j][i] = mu[j][i] = 0.5 - alea<double>();
+            }
+        }
+    } while( ! LU.build(mu) );
+    std::cerr << "mu=" << mu0 << std::endl;
+    
+    const size_t m = 1 + alea_leq(10);
+    matrix<double> sigma(n,m);
+    for(size_t i=1; i<=n; ++i)
+    {
+        for(size_t j=1;j<=m;++j)
+            sigma[i][j] = 0.5 - alea<double>();
+    }
+    std::cerr << "sigma=" << sigma << std::endl;
+    LU.solve(mu,sigma);
+    std::cerr << "r=" << sigma << std::endl;
 }
 YOCTO_UNIT_TEST_DONE()
