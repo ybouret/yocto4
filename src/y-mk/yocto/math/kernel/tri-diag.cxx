e52324deda496474b98636dc736a884b92ff9d2d
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-04-22 13:23:27 +0000

    tridiag upgraded

--- src/y-mk/yocto/math/kernel/tri-diag.cxx
+++ src/y-mk/yocto/math/kernel/tri-diag.cxx
@@ -9,46 +9,46 @@
 #include <iostream>
 
 namespace yocto {
-
+    
 	namespace math {
-
+        
 		template <>
 		tridiag<z_type>:: tridiag() throw() :
-				size_(0),
-				wksp_( ),
-				a_( 0 ),
-				b_( 0 ),
-				c_( 0 ),
-				g_( 0 )
+        size_(0),
+        wksp_( ),
+        a_( 0 ),
+        b_( 0 ),
+        c_( 0 ),
+        g_( 0 )
 		{
-
+            
 		}
-
+        
 		template <>
 		tridiag<z_type>:: tridiag( size_t n ) :
-				size_( n ),
-				wksp_( size_ * 4, numeric<z_type>::zero),
-				a_( wksp_(-1) ),
-				b_( a_ + size_ ),
-				c_( b_ + size_ ),
-				g_( c_ + size_ )
+        size_( n ),
+        wksp_( size_ * 4, numeric<z_type>::zero),
+        a_( wksp_(-1) ),
+        b_( a_ + size_ ),
+        c_( b_ + size_ ),
+        g_( c_ + size_ )
 		{
-
+            
 		}
-
+        
 		template <>
 		tridiag<z_type>:: ~tridiag() throw() {
-
+            
 		}
-
+        
 		template <>
 		size_t tridiag<z_type>:: size() const throw() {
 			return size_;
 		}
-
-
-
-
+        
+        
+        
+        
 		template <>
 		void tridiag<z_type>:: make( size_t n ) {
 			if( n > 0 ) {
@@ -65,7 +65,7 @@ namespace yocto {
 				a_ = b_ = c_ = g_ = 0;
 			}
 		}
-
+        
 		template <>
 		z_type &tridiag<z_type>:: a( size_t n ) throw() {
 			assert(n>=1);
@@ -73,7 +73,7 @@ namespace yocto {
 			assert(a_!=NULL);
 			return a_[n];
 		}
-
+        
 		template <>
 		const z_type &tridiag<z_type>:: a( size_t n ) const throw() {
 			assert(n>=1);
@@ -81,7 +81,7 @@ namespace yocto {
 			assert(a_!=NULL);
 			return a_[n];
 		}
-
+        
 		template <>
 		z_type &tridiag<z_type>:: b( size_t n ) throw() {
 			assert(n>=1);
@@ -89,7 +89,7 @@ namespace yocto {
 			assert(b_!=NULL);
 			return b_[n];
 		}
-
+        
 		template <>
 		const z_type &tridiag<z_type>:: b( size_t n ) const throw() {
 			assert(n>=1);
@@ -97,7 +97,7 @@ namespace yocto {
 			assert(b_!=NULL);
 			return b_[n];
 		}
-
+        
 		template <>
 		z_type &tridiag<z_type>:: c( size_t n ) throw() {
 			assert(n>=1);
@@ -105,7 +105,7 @@ namespace yocto {
 			assert(c_!=NULL);
 			return c_[n];
 		}
-
+        
 		template <>
 		const z_type &tridiag<z_type>:: c( size_t n ) const throw() {
 			assert(n>=1);
@@ -113,32 +113,32 @@ namespace yocto {
 			assert(c_!=NULL);
 			return c_[n];
 		}
-
+        
 		template <>
 		z_type tridiag<z_type>:: operator()( size_t i, size_t j) const throw()
 		{
-
+            
 			assert(i>=1);
 			assert(i<=this->size());
 			assert(j>=1);
 			assert(j<=this->size());
-
+            
 			switch( int(i) - int(j) ) {
 				case 0:
 					return b_[i];
-
+                    
 				case 1:
 					return a_[i];
-
+                    
 				case -1:
 					return c_[i];
-
+                    
 				default:
 					return 0;
 			}
-
+            
 		}
-
+        
 		template <>
 		void tridiag<z_type>:: solve( array<z_type> &u, const array<z_type> &r) const
         {
@@ -146,14 +146,14 @@ namespace yocto {
 			assert(u.size() == this->size());
 			assert(r.size() == this->size());
 			const size_t n = size_;
-
+            
 			z_type piv = b_[1];
 			if( Fabs( piv ) <= REAL_MIN )
 				throw libc::exception( EDOM, "tridiag.solve(B(1)=0)" );
-
-
+            
+            
 			u[1] = r[1] / piv;
-
+            
 			for( size_t j=2, jm=1; j <= n; ++j,++jm )
             {
 				assert(j-1==jm);
@@ -163,13 +163,13 @@ namespace yocto {
 					throw libc::exception( EDOM, "tridiag.solve(pivot=0)" );
 				u[j] = (r[j] - a_[j] * u[jm])/piv;
 			}
-
+            
 			for( size_t j=n-1, jp=n;j>0;--j,--jp)
             {
 				assert(j+1==jp);
 				u[j] -= g_[jp] * u[jp];
 			}
-
+            
 		}
         
         template <>
@@ -204,16 +204,16 @@ namespace yocto {
                     assert(j+1==jp);
                     M[j][c] -= g_[jp] * M[jp][c];
                 }
-
+                
             }
             
             
         }
-
-
+        
+        
 		template <>
 		void tridiag<z_type>:: to_scilab( std::ostream &os ) const {
-
+            
 			os << "[";
 			for( size_t i=1; i <= size_; ++i ) {
 				for( size_t j=1; j <= size_; ++j ) {
@@ -222,12 +222,46 @@ namespace yocto {
 				if( i < size_ )
 					os << " ;";
 			}
-			os << "];" << std::endl;
-
+			os << "];";
+            
 		}
-
+        
+        template <>
+        void tridiag<z_type>:: apply( array<z_type> &v, const array<z_type> &u) const throw()
+        {
+            const size_t n = size_;
+            assert(v.size() == n);
+            assert(u.size() == n);
+            for(size_t i=n; i>0; --i)
+            {
+                v[i]  = b_[i] * u[i];
+                if(i<n) v[i] += c_[i] * u[i+1];
+                if(i>1) v[i] += a_[i] * u[i-1];
+            }
+            
+        }
+        
+        template <>
+        void tridiag<z_type>::  apply(matrix<z_type> &V, const matrix<z_type> &U) const throw()
+        {
+            const size_t n = size_;
+            assert(V.rows == n);
+            assert(U.rows == n);
+            assert(V.cols == U.cols );
+            for( size_t j=V.cols;j>0;--j)
+            {
+                for(size_t i=n; i>0; --i)
+                {
+                    V[i][j]  = b_[i] * U[i][j];
+                    if(i<n) V[i][j] += c_[i] * U[i+1][j];
+                    if(i>1) V[i][j] += a_[i] * U[i-1][j];
+                }
+            }
+        }
+        
+        
 	}
-
-
+    
+    
 }
 
