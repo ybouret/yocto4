a2524ce2970b30b9b421c8156819db8f8d706999
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-04-26 08:17:34 +0000

    functioning splines

--- src/y-mk/yocto/math/dat/spline.cxx
+++ src/y-mk/yocto/math/dat/spline.cxx
@@ -9,180 +9,16 @@ namespace yocto {
     
 	namespace math {
         
-#if 0
-#if defined(__GNUC__)
-		template <>
-#endif
-		spline<real_t>::boundary:: ~boundary() throw()
-		{
-		}
-        
-#if defined(__GNUC__)
-		template <>
-#endif
-		spline<real_t>::boundary:: boundary( const bool natural, const real_t slope ) throw() :
-        is_natural( natural ),
-        derivative( slope )
-		{
-		}
-        
-#if defined(__GNUC__)
-		template <>
-#endif
-		spline<real_t>::boundary:: boundary( const boundary &b ) throw() :
-        is_natural( b.is_natural ),
-        derivative( b.derivative )
-		{
-		}
-        
-		template <>
-		spline<real_t>:: ~spline() throw()
-		{
-		}
-        
-		template <>
-		spline<real_t>:: spline( const array<real_t> &x, const array<real_t> &y, const boundary &lo, const boundary &hi ) :
-        x_( NULL ),
-        y_( NULL ),
-        y2_( NULL ),
-        n_( x.size() ),
-        xlo_(0),
-        xhi_(0),
-        ylo_(0),
-        yhi_(0),
-        w_( 3*n_, 0 )
-		{
-			assert( x.size()>=2 );
-			assert( y.size()>=2 );
-			assert( x.size() == y.size() );
-            
-			//-- prepare arrays
-			x_  = w_(-1);
-			y_  = x_ + n_;
-			y2_ = y_ + n_;
-            
-			//-- temporary
-			vector<real_t> u( n_-1, 0 );
-            
-			//-- initialize arrays
-			for( size_t i=n_;i>0;--i)
-			{
-				x_[i]  = x[i];
-				y_[i]  = y[i];
-				y2_[i] = 0;
-			}
-            
-			*(real_t *)&xlo_ = x_[1];
-			*(real_t *)&ylo_ = y_[1];
-			*(real_t *)&xhi_ = x_[n_];
-			*(real_t *)&yhi_ = y_[n_];
-            
-			//------------------------------------------------------------------
-			// lo
-			//------------------------------------------------------------------
-			if( lo.is_natural )
-				y2_[1] = u[1] = 0.0;
-			else
-			{
-				y2_[1] = -REAL(0.5);
-				const real_t dx = x_[2] - x_[1];
-				u[1] = (REAL(3.0) / dx) * ( (y_[2] - y_[1]) / dx - lo.derivative );
-			}
-            
-			//------------------------------------------------------------------
-			// mid
-			//------------------------------------------------------------------
-			for( size_t i=2; i < n_; ++i )
-			{
-				const real_t sig = ( x_[i] - x_[i-1] ) / ( x_[i+1] - x_[i-1] );
-				const real_t p   = sig * y2_[i-1] + REAL(2.0);
-				const real_t tmp = (y_[i+1] - y[i])/(x_[i+1]-x_[i]) - (y_[i] - y[i-1])/(x_[i]-x_[i-1]);
-                
-				y2_[i] = ( sig - REAL(1.0) ) / p;
-				u[i]   = ( REAL(6.0) * tmp / (x_[i+1]-x_[i-1]) - sig * u[i-1]) / p;
-                
-			}
-            
-			//------------------------------------------------------------------
-			// hi
-			//------------------------------------------------------------------
-			real_t qn = 0, un = 0;
-			if( !hi.is_natural )
-			{
-				const real_t dx = x_[n_] - x_[n_-1];
-				qn = REAL(0.5);
-				un = ( REAL(3.0) / dx ) * ( hi.derivative - (y_[n_] - y_[n_-1] ) / dx );
-			}
-			y2_[n_] = ( un - qn * u[n_-1] ) / ( qn * y2_[n_-1] + REAL(1.0) );
-			for( size_t k=n_-1;k>0;--k)
-			{
-				y2_[k] = y2_[k] * y2_[k+1] + u[k];
-			}
-            
-		}
-        
-        
-		template <>
-		real_t spline<real_t>:: operator() ( real_t x ) const throw()
-		{
-			if( x <= xlo_ )
-				return ylo_;
-			else {
-                
-				if( x >= xhi_ )
-					return yhi_;
-				else
-				{
-					size_t klo = 1, khi = n_;
-					while( (khi-klo)> 1 )
-					{
-						const size_t k = (klo+khi)>>1;
-						if( x_[k] > x )
-							khi = k;
-						else
-							klo = k;
-					}
-					const real_t h = x_[khi] - x_[klo];
-					const real_t a = (x_[khi] - x)/h;
-					const real_t b = (x - x_[klo])/h;
-					return (a * y_[klo]) + (b * y_[khi]) + ( (a*a*a-a) * y2_[klo] + (b*b*b-b) * y2_[khi] ) * (h*h) / REAL(6.0);
-				}
-			}
-		}
-#endif
         template <>
         spline<real_t>:: ~spline() throw()
         {
         }
         
+        
         template <>
-        spline<real_t>:: spline( const array<real_t> &X, const array<real_t> &Y, const boundaries &b ) :
-        type(b.type),
-        arrays(3),
-        x(  arrays.next_array() ),
-        y(  arrays.next_array() ),
-        y2( arrays.next_array() ),
-        n( X.size() ),
-        xlo(0),
-        xup(0),
-        width(0)
+        void spline<real_t>:: compute(const boundaries &b)
         {
-            assert( X.size() == Y.size() );
-            assert( X.size() > 1 );
-            
-            //==================================================================
-            // prepare data
-            //==================================================================
-            arrays.prepare(n);
-            for(size_t i=1; i <= n; ++i )
-            {
-                x[i] = X[i];
-                y[i] = Y[i];
-            }
-            co_hsort(x,y);
-            (real_t&)xlo   = x[1];
-            (real_t&)xup   = x[n];
-            (real_t&)width = xup - xlo;
+            (spline_type &)type = b.type;
             
             //==================================================================
             // create the system matrix
@@ -208,9 +44,11 @@ namespace yocto {
             
             for(size_t i=2;i<n;++i)
             {
-                const real_t xm  = x[i-1];
+                const size_t im  = i-1;
+                const size_t ip  = i+1;
+                const real_t xm  = x[im];
                 const real_t xi  = x[i];
-                const real_t xp  = x[i+1];
+                const real_t xp  = x[ip];
                 
                 const real_t dxp = xp - xi;
                 const real_t dxm = xi - xm;
@@ -219,7 +57,7 @@ namespace yocto {
                 M.b[i] = (xp - xm)/REAL(3.0);
                 M.c[i] = dxp/REAL(6.0);
                 const real_t yi = y[i];
-                y2[i]  = (y[i+1]-yi)/dxp - (yi-y[i-1])/dxm;
+                y2[i]  = (y[ip]-yi)/dxp - (yi-y[im])/dxm;
             }
             
             const real_t dx1 = x[2] - x[1];
@@ -264,6 +102,42 @@ namespace yocto {
             }
             if(!M.solve(y2) )
                 throw exception("singular spline");
+            
+        }
+        
+        template <>
+        spline<real_t>:: spline( const array<real_t> &X, const array<real_t> &Y, const boundaries &b ) :
+        type(b.type),
+        arrays(3),
+        x(  arrays.next_array() ),
+        y(  arrays.next_array() ),
+        y2( arrays.next_array() ),
+        n( X.size() ),
+        xlo(0),
+        xup(0),
+        width(0)
+        {
+            assert( X.size() == Y.size() );
+            assert( X.size() > 1 );
+            
+            //==================================================================
+            // prepare data
+            //==================================================================
+            arrays.prepare(n);
+            for(size_t i=1; i <= n; ++i )
+            {
+                x[i] = X[i];
+                y[i] = Y[i];
+            }
+            co_hsort(x,y);
+            (real_t&)xlo   = x[1];
+            (real_t&)xup   = x[n];
+            (real_t&)width = xup - xlo;
+            
+            //==================================================================
+            // compute spline
+            //==================================================================
+            compute(b);
         }
         
         template<>
