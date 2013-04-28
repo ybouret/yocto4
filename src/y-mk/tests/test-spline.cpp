8a77a54c1acd968db4b9a7f52896ce44a6e85230
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-04-28 09:02:22 +0000

    spline 1D seems ok

--- src/y-mk/tests/test-spline.cpp
+++ src/y-mk/tests/test-spline.cpp
@@ -40,33 +40,22 @@ YOCTO_UNIT_TEST_IMPL(spline)
     }
     
     matrix<double> Y2(5,nc);
-   // const double width = X[nc] - X[1];
-    size_t idx=1;
+    const double width = X[nc] - X[1];
     std::cerr << "spline natural..." << std::endl;
-    spline<double>::compute(spline_natural,       X, Y, Y2[idx++]);
+    spline<double>::compute(spline_natural,       X, Y, Y2[1]);
     
     std::cerr << "spline periodic..." << std::endl;
-    spline<double>::compute(spline_periodic,      X, Y, Y2[idx++]);
+    spline<double>::compute(spline_periodic,      X, Y, Y2[2]);
     
     std::cerr << "spline tangent left..." << std::endl;
-    spline<double>::compute(spline_tangent_left,  X, Y, Y2[idx++],0,0);
+    spline<double>::compute(spline_tangent_left,  X, Y, Y2[3],0,0);
     
     std::cerr << "spline tangent right..." << std::endl;
-    spline<double>::compute(spline_tangent_right, X, Y, Y2[idx++],0,0);
+    spline<double>::compute(spline_tangent_right, X, Y, Y2[4],0,0);
     
     std::cerr << "spline tangent both..." << std::endl;
-    spline<double>::compute(spline_tangent_both,  X, Y, Y2[idx++],0,0);
+    spline<double>::compute(spline_tangent_both,  X, Y, Y2[5],0,0);
 
-    
-#if 0
-    const spline<double>::boundaries bnd1( spline_regular );
-    spline<double> S1(X,Y,bnd1);
-    
-    const spline<double>::boundaries bnd2(false,0,false,0);
-    spline<double> S2(X,Y,bnd2);
-    
-    const spline<double>::boundaries bnd3( spline_cyclic );
-    spline<double> S3(X,Y,bnd3);
     
     
     const size_t np = 200;
@@ -76,10 +65,15 @@ YOCTO_UNIT_TEST_IMPL(spline)
         for( size_t i=0; i<=np;++i)
         {
             const double x = (i*L)/np;
-            fp("%g %g %g %g\n", x, S1(x), S2(x), S3(x));
+            fp("%g", x);
+            fp(" %g", spline<double>::eval(x, X, Y, Y2[1], NULL));
+            fp(" %g", spline<double>::eval(x, X, Y, Y2[2], &width));
+            fp(" %g", spline<double>::eval(x, X, Y, Y2[3], NULL));
+            fp(" %g", spline<double>::eval(x, X, Y, Y2[4], NULL));
+            fp(" %g", spline<double>::eval(x, X, Y, Y2[5], NULL));
+            fp("\n");
         }
     }
-#endif
     
 }
 YOCTO_UNIT_TEST_DONE()
