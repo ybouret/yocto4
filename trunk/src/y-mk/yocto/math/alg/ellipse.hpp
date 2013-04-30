9ef4d4d58f9a20f6c06004dfeaf2af13b3d05b7e
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-04-30 15:31:51 +0000

    ellipse...

---
+++ trunk/src/y-mk/yocto/math/alg/ellipse.hpp
@@ -0,0 +1,34 @@
+#ifndef YOCTO_MATH_ALG_ELLIPSE_INCLUDED
+#define YOCTO_MATH_ALG_ELLIPSE_INCLUDED 1
+
+#include "yocto/math/v2d.hpp"
+#include "yocto/math/kernel/matrix.hpp"
+
+
+namespace yocto
+{
+    namespace math
+    {
+        
+        template <typename T>
+        class ellipse
+        {
+        public:
+            explicit ellipse();
+            virtual ~ellipse() throw();
+            
+            void reset() throw();
+            
+            void append( T x, T y ) throw();
+            
+            inline const matrix<T> &__S() const throw() { return S; }
+            
+        private:
+            matrix<T> S;
+            YOCTO_DISABLE_COPY_AND_ASSIGN(ellipse);
+        };
+        
+    }
+}
+
+#endif
