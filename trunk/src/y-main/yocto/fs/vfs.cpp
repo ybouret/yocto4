e5506cd7fb67e809259e4e05aea850277454e567
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-04-30 09:26:33 +0000

    helpers in vfs

--- trunk/src/y-main/yocto/fs/vfs.cpp
+++ trunk/src/y-main/yocto/fs/vfs.cpp
@@ -10,7 +10,6 @@ namespace yocto
     vfs::  vfs() throw() {}
     vfs:: ~vfs() throw() {}
     
-    
     void vfs:: create_sub_dir( const string &dirName )
     {
         string            org = dirName;
