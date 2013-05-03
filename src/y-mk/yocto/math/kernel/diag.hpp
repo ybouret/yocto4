803d427149085c67e7b0abb90b3c28c01b59dabc
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-05-03 08:23:01 +0000

    simplified diag API

--- src/y-mk/yocto/math/kernel/diag.hpp
+++ src/y-mk/yocto/math/kernel/diag.hpp
@@ -17,21 +17,31 @@ namespace yocto
              Given a matrix a[1..n][1..n], this routine replaces it by a balanced matrix with identical eigenvalues.
              A symmetric matrix is already balanced and is unaffected by this procedure.
              */
-            static void HessenbergBalance( matrix<T> &a ) throw();
+            static
+            void HessenbergBalance( matrix<T> &a ) throw();
             
             //! reduction to a real Hessenberg form
             /**
              matrix should be balanced.
              */
-            static void HessenbergReduce( matrix<T> &a ) throw();
+            static
+            void HessenbergReduce( matrix<T> &a ) throw();
             
             
             //! find the eigen values
             /**
              \param a a real matrix reduced to its Hessenberg form: destructed !
              */
-            static void HessenbergQR( matrix<T> &a, array<T> &wr, array<T> &wi );
+            static
+            bool HessenbergQR( matrix<T> &a, array<T> &wr, array<T> &wi ) throw();
             
+            static inline
+            bool eigenvalues( matrix<T> &a, array<T> &wr, array<T> &wi ) throw()
+            {
+                HessenbergBalance(a);
+                HessenbergReduce(a);
+                return HessenbergQR(a, wr, wi);
+            }
         };
     }
     
