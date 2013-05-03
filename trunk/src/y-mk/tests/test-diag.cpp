f85863e9d3e052f8e15e352ae6488c615632d12a
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-05-03 07:36:24 +0000

    debugged diag

--- trunk/src/y-mk/tests/test-diag.cpp
+++ trunk/src/y-mk/tests/test-diag.cpp
@@ -10,24 +10,28 @@ using namespace math;
 
 YOCTO_UNIT_TEST_IMPL(diag)
 {
-
+    
     matrix<double> a(4,4);
     const size_t   n = a.rows;
-    for(size_t i=1; i<=n; ++i )
+    for(size_t iter=1; iter <= 10; ++iter )
     {
-        for(size_t j=1; j<=n;++j)
+        for(size_t i=1; i<=n; ++i )
         {
-            a[i][j] = (0.5-alea<double>());// + (i==j?a.rows:0);
+            for(size_t j=1; j<=n;++j)
+            {
+                a[i][j] = (0.5-alea<double>());// + (i==j?a.rows:0);
+            }
         }
+        std::cerr << "a=" << a << std::endl;
+        diag<double>::balance(a);
+        std::cerr << "b=" << a << std::endl;
+        diag<double>::Hessenberg(a);
+        std::cerr << "c=" << a << std::endl;
+        vector<double> wr(n,0);
+        vector<double> wi(n,0);
+        diag<double>::eigenvalues(a, wr,wi);
+        std::cerr << "wr=" << wr << std::endl;
+        std::cerr << "wi=" << wi << std::endl;
     }
-    std::cerr << "a=" << a << std::endl;
-    diag<double>::balance(a);
-    std::cerr << "b=" << a << std::endl;
-    diag<double>::Hessenberg(a);
-    std::cerr << "c=" << a << std::endl;
-    vector<double> wr(n,0);
-    vector<double> wi(n,0);
-    diag<double>::eigenvalues(a, wr,wi);
-    
 }
 YOCTO_UNIT_TEST_DONE()
