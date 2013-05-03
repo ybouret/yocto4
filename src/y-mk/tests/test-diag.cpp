9f85082b8bc47ffd76faba24343b8096d5b1d06a
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-05-03 04:46:27 +0000

    buggy eigenvalues

--- src/y-mk/tests/test-diag.cpp
+++ src/y-mk/tests/test-diag.cpp
@@ -1,6 +1,7 @@
 #include "yocto/utest/run.hpp"
 #include "yocto/math/kernel/diag.hpp"
 #include "yocto/code/rand.hpp"
+#include "yocto/sequence/vector.hpp"
 
 using namespace yocto;
 using namespace math;
@@ -11,10 +12,10 @@ YOCTO_UNIT_TEST_IMPL(diag)
 {
 
     matrix<double> a(4,4);
-    
-    for(size_t i=1; i<=a.rows; ++i )
+    const size_t   n = a.rows;
+    for(size_t i=1; i<=n; ++i )
     {
-        for(size_t j=1; j<=a.cols;++j)
+        for(size_t j=1; j<=n;++j)
         {
             a[i][j] = (0.5-alea<double>());// + (i==j?a.rows:0);
         }
@@ -24,6 +25,9 @@ YOCTO_UNIT_TEST_IMPL(diag)
     std::cerr << "b=" << a << std::endl;
     diag<double>::Hessenberg(a);
     std::cerr << "c=" << a << std::endl;
+    vector<double> wr(n,0);
+    vector<double> wi(n,0);
+    diag<double>::eigenvalues(a, wr,wi);
     
 }
 YOCTO_UNIT_TEST_DONE()
