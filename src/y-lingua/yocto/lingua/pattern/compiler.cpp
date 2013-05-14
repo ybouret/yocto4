7e179268e6f52564fb75f9b9270ecfebf32ee338
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-05-14 17:48:08 +0000

    lingua: initial bracket

--- src/y-lingua/yocto/lingua/pattern/compiler.cpp
+++ src/y-lingua/yocto/lingua/pattern/compiler.cpp
@@ -21,256 +21,267 @@
 #define LBRACE '{'
 #define RBRACE '}'
 
+#define LBRACK '['
+#define RBRACK ']'
 
 namespace yocto
 {
-    namespace lingua
-    {
-        
-        namespace
-        {
-            static const char fn[] = "lingua::compile";
-            
-            static inline
-            bool sep_coma( char C) throw()
-            {
-                return ',' == C;
-            }
-            
-            static inline
-            bool sep_null( char C ) throw()
-            {
-                return 0 == C;
-            }
-            
-            static inline
-            string scan_braces(const char   * &curr,
-                               const char   * last)
-            {
-                assert( LBRACE == curr[0] );
-                const char *org = ++curr;
-                while(curr<last)
-                {
-                    if(RBRACE==curr[0])
-                    {
-                        return string(org,curr-org);
-                    }
-                    ++curr;
-                }
-                throw exception("%s(Unfinished Braces)",fn);
-            }
-            
-            static inline
-            pattern *compile_xp(const char   * &curr,
-                                const char   * last,
-                                const p_dict *dict,
-                                size_t       &depth )
-            {
-                
-                auto_ptr<logical> p( AND::create() );
-                while(curr<last)
-                {
-                    char C = *curr;
-                    switch(C)
-                    {
-                            //--------------------------------------------------
-                            //
-                            // SubExpressions
-                            //
-                            //--------------------------------------------------
-                        case '(':
-                            ++depth;
-                            YRX(std::cerr << "[XP] Start SubExpression/Depth=" << depth << std::endl);
-                            ++curr;
-                            p->append( compile_xp(curr,last,dict,depth) );
-                            break;
-                            
-                        case ')':
-                            YRX(std::cerr << "[XP] Finish SubExpression/Depth=" << depth << std::endl);
-                            if(depth<=0) throw exception("%s(Unexpected Right Paren)",fn);
-                            --depth;
-                            ++curr;
-                            goto FINISHED;
-                            
-                            //--------------------------------------------------
-                            //
-                            // Alternation
-                            //
-                            //--------------------------------------------------
-                        case '|': {
-                            YRX(std::cerr << "[XP] Alternation/Depth=" << depth << std::endl);
-                            ++curr;
-                            auto_ptr<pattern>  lhs( p.yield() );
-                            auto_ptr<pattern>  rhs( compile_xp(curr, last, dict, depth));
-                            p.reset( OR::create() );
-                            p->append(lhs.yield());
-                            p->append(rhs.yield());
-                        }
-                            goto FINISHED;
-                            
-                            //--------------------------------------------------
-                            //
-                            // Jokers
-                            //
-                            //--------------------------------------------------
-                        case '?':
-                            YRX(std::cerr << "[XP] joker '?'" << std::endl);
-                            if(p->operands.size<=0)
-                                throw exception("%s(No Left Expression for '?')",fn);
-                            else
-                            {
-                                p->append( optional::create(p->operands.pop_back() ));
-                            }
-                            ++curr;
-                            break;
-                            
-                        case '+':
-                            YRX(std::cerr << "[XP] joker '+'" << std::endl);
-                            if(p->operands.size<=0)
-                                throw exception("%s(No Left Expression for '+')",fn);
-                            else
-                            {
-                                p->append( one_or_more(p->operands.pop_back() ));
-                            }
-                            ++curr;
-                            break;
-                            
-                        case '*':
-                            YRX(std::cerr << "[XP] joker '*'" << std::endl);
-                            if(p->operands.size<=0)
-                                throw exception("%s(No Left Expression for '*')",fn);
-                            else
-                            {
-                                p->append( zero_or_more(p->operands.pop_back() ));
-                            }
-                            ++curr;
-                            break;
-                            
-                            //--------------------------------------------------
-                            //
-                            // braces
-                            //
-                            //--------------------------------------------------
-                        case LBRACE:
-                        {
-                            const string content( scan_braces(curr, last) );
-                            assert(RBRACE==curr[0]);
-                            ++curr;
-                            YRX(std::cerr << "[XP] [BRACES] {" << content << "}" << std::endl);
-                            if(content.size()<=0)
-                                throw exception("%s(Empty Braces)",fn);
-                            if(isdigit(content[0]))
-                            {
-                                //----------------------------------------------
-                                // assume counting joker
-                                //----------------------------------------------
-                                if( p->operands.size <= 0 )
-                                    throw exception("%s(No SubExpression before Braces)",fn);
-                                const char *orgA = &content[0];
-                                const char *endA = orgA;
-                                const char *endB = orgA + content.size();
-                                bool        comma = false;
-                                while(endA<endB)
-                                {
-                                    if(*endA==',')
-                                    {
-                                        comma = true;
-                                        break;
-                                    }
-                                    ++endA;
-                                }
-                                const string astr(orgA,endA-orgA);
-                                const size_t a = strconv::to<size_t>(astr,"First Brace Number");
-                                YRX( std::cerr << "\ta=" << astr << std::endl);
-                                if(comma)
-                                {
-                                    const char *orgB = endA+1;
-                                    if(orgB<endB)
-                                    {
-                                        const string bstr(orgB,endB-orgB);
-                                        const size_t b = strconv::to<size_t>(bstr,"Second Brace Number");
-                                        YRX(std::cerr << "\t=> between " <<a << " and " << b << " times" << std::endl);
-                                        p->append( counting::create( p->operands.pop_back(), a, b));
-                                    }
-                                    else
-                                    {
-                                        YRX(std::cerr<<"\t=> at least " << a << " times" <<std::endl);
-                                        p->append( at_least::create( p->operands.pop_back(), a));
-                                    }
-                                }
-                                else
-                                {
-                                    YRX(std::cerr << "\t=> exactly " << a << " times" << std::endl);
-                                    p->append( counting::create( p->operands.pop_back(), a, a));
-                                }
-                            }
-                            else
-                            {
-                                //----------------------------------------------
-                                // assume named joker from dictionary
-                                //----------------------------------------------
-                                if(!dict)
-                                    throw exception("%s(no dict for {%s})", fn, content.c_str());
-                                p->append( dict->create(content) );
-                            }
-                        }
-                            break;
-                            
-                            //--------------------------------------------------
-                            //
-                            // single chars
-                            //
-                            //--------------------------------------------------
-                        default:
-                            //--------------------------------------------------
-                            // a new single char
-                            //--------------------------------------------------
-                            YRX(std::cerr << "[XP] append single '" << C << "'" << std::endl);
-                            p->append( single::create(C) );
-                            ++curr;
-                    }
-                }
-                
-                if(depth>0)
-                    throw exception("%s(Unfinished SubExpression)",fn);
-                
-            FINISHED:
-                assert( p.is_valid() );
-                if(p->operands.size<=0)
-                    throw exception("%s(Empty SubExpression)",fn);
-                
-                p->optimize();
-                return pattern::simplify(p.yield());
-            }
-            
-            
-        }
-        
-        
-        ////////////////////////////////////////////////////////////////////////
-        //
-        //
-        // high-level
-        //
-        //
-        ////////////////////////////////////////////////////////////////////////
-        pattern *compile( const string &expr, const p_dict *dict )
-        {
-            size_t      depth = 0;
-            const char *curr  = expr.c_str();
-            const char *last  = curr + expr.size();
-            
-            return compile_xp(curr,
-                              last,
-                              dict,
-                              depth);
-        }
-        
-        pattern *compile( const char *expr, const p_dict *dict )
-        {
-            const string xp(expr);
-            return compile(xp,dict);
-        }
-        
-    }
+	namespace lingua
+	{
+
+		namespace
+		{
+	
+			static const char fn[] = "lingua::compile";
+
+
+			static inline
+			pattern *compile_braces(const char * &curr, const char * last)
+			{
+				
+				assert( LBRACK == curr[0] );
+				
+				return 0;
+			}
+
+			static inline
+				string scan_braces(const char   * &curr,
+				const char   * last)
+			{
+				assert( LBRACE == curr[0] );
+				const char *org = ++curr;
+				while(curr<last)
+				{
+					if(RBRACE==curr[0])
+					{
+						return string(org,curr-org);
+					}
+					++curr;
+				}
+				throw exception("%s(Unfinished Braces)",fn);
+			}
+
+			static inline
+				pattern *compile_xp(const char   * &curr,
+				const char   * last,
+				const p_dict *dict,
+				size_t       &depth )
+			{
+
+				auto_ptr<logical> p( AND::create() );
+				while(curr<last)
+				{
+					char C = *curr;
+					switch(C)
+					{
+						//--------------------------------------------------
+						//
+						// SubExpressions
+						//
+						//--------------------------------------------------
+					case '(':
+						++depth;
+						YRX(std::cerr << "[XP] Start SubExpression/Depth=" << depth << std::endl);
+						++curr;
+						p->append( compile_xp(curr,last,dict,depth) );
+						break;
+
+					case ')':
+						YRX(std::cerr << "[XP] Finish SubExpression/Depth=" << depth << std::endl);
+						if(depth<=0) throw exception("%s(Unexpected Right Paren)",fn);
+						--depth;
+						++curr;
+						goto FINISHED;
+
+						//--------------------------------------------------
+						//
+						// Alternation
+						//
+						//--------------------------------------------------
+					case '|': {
+						YRX(std::cerr << "[XP] Alternation/Depth=" << depth << std::endl);
+						++curr;
+						auto_ptr<pattern>  lhs( p.yield() );
+						auto_ptr<pattern>  rhs( compile_xp(curr, last, dict, depth));
+						p.reset( OR::create() );
+						p->append(lhs.yield());
+						p->append(rhs.yield());
+							  }
+							  goto FINISHED;
+
+							  //--------------------------------------------------
+							  //
+							  // Jokers
+							  //
+							  //--------------------------------------------------
+					case '?':
+						YRX(std::cerr << "[XP] joker '?'" << std::endl);
+						if(p->operands.size<=0)
+							throw exception("%s(No Left Expression for '?')",fn);
+						else
+						{
+							p->append( optional::create(p->operands.pop_back() ));
+						}
+						++curr;
+						break;
+
+					case '+':
+						YRX(std::cerr << "[XP] joker '+'" << std::endl);
+						if(p->operands.size<=0)
+							throw exception("%s(No Left Expression for '+')",fn);
+						else
+						{
+							p->append( one_or_more(p->operands.pop_back() ));
+						}
+						++curr;
+						break;
+
+					case '*':
+						YRX(std::cerr << "[XP] joker '*'" << std::endl);
+						if(p->operands.size<=0)
+							throw exception("%s(No Left Expression for '*')",fn);
+						else
+						{
+							p->append( zero_or_more(p->operands.pop_back() ));
+						}
+						++curr;
+						break;
+
+						//--------------------------------------------------
+						//
+						// bracket
+						//
+						//--------------------------------------------------
+					case LBRACK:
+						p->append(0);
+
+						break;
+
+						//--------------------------------------------------
+						//
+						// braces
+						//
+						//--------------------------------------------------
+					case LBRACE:
+						{
+							const string content( scan_braces(curr, last) );
+							assert(RBRACE==curr[0]);
+							++curr;
+							YRX(std::cerr << "[XP] [BRACES] {" << content << "}" << std::endl);
+							if(content.size()<=0)
+								throw exception("%s(Empty Braces)",fn);
+							if(isdigit(content[0]))
+							{
+								//----------------------------------------------
+								// assume counting joker
+								//----------------------------------------------
+								if( p->operands.size <= 0 )
+									throw exception("%s(No SubExpression before Braces)",fn);
+								const char *orgA = &content[0];
+								const char *endA = orgA;
+								const char *endB = orgA + content.size();
+								bool        comma = false;
+								while(endA<endB)
+								{
+									if(*endA==',')
+									{
+										comma = true;
+										break;
+									}
+									++endA;
+								}
+								const string astr(orgA,endA-orgA);
+								const size_t a = strconv::to<size_t>(astr,"First Brace Number");
+								YRX( std::cerr << "\ta=" << astr << std::endl);
+								if(comma)
+								{
+									const char *orgB = endA+1;
+									if(orgB<endB)
+									{
+										const string bstr(orgB,endB-orgB);
+										const size_t b = strconv::to<size_t>(bstr,"Second Brace Number");
+										YRX(std::cerr << "\t=> between " <<a << " and " << b << " times" << std::endl);
+										p->append( counting::create( p->operands.pop_back(), a, b));
+									}
+									else
+									{
+										YRX(std::cerr<<"\t=> at least " << a << " times" <<std::endl);
+										p->append( at_least::create( p->operands.pop_back(), a));
+									}
+								}
+								else
+								{
+									YRX(std::cerr << "\t=> exactly " << a << " times" << std::endl);
+									p->append( counting::create( p->operands.pop_back(), a, a));
+								}
+							}
+							else
+							{
+								//----------------------------------------------
+								// assume named joker from dictionary
+								//----------------------------------------------
+								if(!dict)
+									throw exception("%s(no dict for {%s})", fn, content.c_str());
+								p->append( dict->create(content) );
+							}
+						}
+						break;
+
+						//--------------------------------------------------
+						//
+						// single chars
+						//
+						//--------------------------------------------------
+					default:
+						//--------------------------------------------------
+						// a new single char
+						//--------------------------------------------------
+						YRX(std::cerr << "[XP] append single '" << C << "'" << std::endl);
+						p->append( single::create(C) );
+						++curr;
+					}
+				}
+
+				if(depth>0)
+					throw exception("%s(Unfinished SubExpression)",fn);
+
+FINISHED:
+				assert( p.is_valid() );
+				if(p->operands.size<=0)
+					throw exception("%s(Empty SubExpression)",fn);
+
+				p->optimize();
+				return pattern::simplify(p.yield());
+			}
+
+
+		}
+
+
+		////////////////////////////////////////////////////////////////////////
+		//
+		//
+		// high-level
+		//
+		//
+		////////////////////////////////////////////////////////////////////////
+		pattern *compile( const string &expr, const p_dict *dict )
+		{
+			size_t      depth = 0;
+			const char *curr  = expr.c_str();
+			const char *last  = curr + expr.size();
+
+			return compile_xp(curr,
+				last,
+				dict,
+				depth);
+		}
+
+		pattern *compile( const char *expr, const p_dict *dict )
+		{
+			const string xp(expr);
+			return compile(xp,dict);
+		}
+
+	}
 }
