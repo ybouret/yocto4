cecd15e37fe825b749ea42624d594d31b1071fc4
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-05-15 18:29:11 +0000

    lingua: initial lexeme

---
+++ src/y-lingua/yocto/lingua/lexeme.cpp
@@ -0,0 +1,34 @@
+#include "yocto/lingua/lexeme.hpp"
+
+namespace yocto 
+{
+	namespace lingua
+	{
+		lexeme:: ~lexeme() throw() {}
+
+		lexeme:: lexeme( const lexeme &other ) :
+		token(other),
+			prev(0),
+			next(0),
+			name(other.name),
+			line(other.line)
+		{
+
+		}
+
+		lexeme:: lexeme( const string &id, size_t il ) throw() :
+		token(),
+			prev(0),
+			next(0),
+			name(id),
+			line(il)
+		{
+
+		}
+
+		lexeme * lexeme:: clone() const
+		{
+			return new lexeme( *this );
+		}
+	}
+}
---
+++ src/y-lingua/yocto/lingua/lexeme.hpp
@@ -0,0 +1,35 @@
+#ifndef YOCTO_LINGUA_LEXEME_INCLUDED
+#define YOCTO_LINGUA_LEXEME_INCLUDED 1
+
+
+#include "yocto/lingua/token.hpp"
+#include "yocto/core/meta-list.hpp"
+
+namespace yocto 
+{
+	namespace lingua
+	{
+		class lexeme : public token
+		{
+		public:
+			lexeme       *prev;  //!< for list
+			lexeme       *next;  //!< for list
+			const string &name;  //!< name of the rule that created the lexeme
+			const size_t  line;  //!< line number
+
+			explicit lexeme( const string &id, size_t il ) throw();
+			virtual ~lexeme() throw();
+			lexeme( const lexeme &other );
+
+			lexeme *clone() const;
+
+		private:
+			YOCTO_DISABLE_ASSIGN(lexeme);
+		};
+
+		typedef core::meta_list<lexeme> lexemes;
+
+	}
+}
+
+#endif
