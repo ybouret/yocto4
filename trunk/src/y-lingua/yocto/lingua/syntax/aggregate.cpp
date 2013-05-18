d939c2aef702f5b4ff714bbb13a2c59aac2cb728
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-05-18 09:28:49 +0000

    need to change xnode

--- trunk/src/y-lingua/yocto/lingua/syntax/aggregate.cpp
+++ trunk/src/y-lingua/yocto/lingua/syntax/aggregate.cpp
@@ -1,4 +1,5 @@
 #include "yocto/lingua/syntax/aggregate.hpp"
+#include "yocto/exception.hpp"
 
 namespace yocto
 {
@@ -15,7 +16,16 @@ namespace yocto
             }
             
             
-            
+            bool aggregate:: match(Y_SYNTAX_MATCH_ARGS)
+            {
+                const size_t n = items.size();
+                if(n<=0)
+                    throw exception("syntax.aggregate.%s is empty", label.c_str());
+                xnode *node = xnode::create(label, 0, is_regular);
+                auto_ptr<xnode> q(node);
+                
+                return true;
+            }
             
 
         }
