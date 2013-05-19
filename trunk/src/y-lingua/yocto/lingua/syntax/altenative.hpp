97074c56578055243572ba6e7943b0b6e90d0097
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-05-19 12:05:07 +0000

    syntax: alt init commit

---
+++ trunk/src/y-lingua/yocto/lingua/syntax/altenative.hpp
@@ -0,0 +1,31 @@
+#ifndef YOCTO_LINGUA_SYNTAX_ALTERNATIVE_INCLUDED
+#define YOCTO_LINGUA_SYNTAX_ALTERNATIVE_INCLUDED 1
+
+#include "yocto/lingua/syntax/composite.hpp"
+
+namespace yocto
+{
+    namespace lingua
+    {
+        namespace syntax
+        {
+            
+            //! match a of possible rules
+            class alternative : public composite
+            {
+            public:
+                virtual ~alternative() throw();
+                Y_SYNTAX_RULE_API();
+                
+                explicit alternative(const string &id);
+                const node_property behavior;
+                
+            private:
+                YOCTO_DISABLE_COPY(alternative);
+            };
+        }
+        
+    }
+}
+
+#endif
