e3cf74f863f84ce880cd3dca7e45b3dddcc25b6d
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-05-21 06:32:27 +0000

    xnode abstract initial commit

---
+++ src/y-lingua/yocto/lingua/syntax/xnode-abstract.cpp
@@ -0,0 +1,20 @@
+#include "yocto/lingua/syntax/xnode.hpp"
+#include "yocto/code/cast.hpp"
+#include <cstring>
+
+namespace yocto
+{
+    namespace lingua
+    {
+        namespace syntax
+        {
+
+            xnode * xnode:: abstract(xnode *node)
+            {
+                return node;
+            }
+        }
+
+    }
+
+}
