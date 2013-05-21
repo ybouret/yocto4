1910b5c00e877e4134855bd91c3aaeeea90d9cfc
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-05-21 11:48:41 +0000

    lingua: test-json seems ok

--- src/y-lingua/tests/test-json.cpp
+++ src/y-lingua/tests/test-json.cpp
@@ -15,6 +15,78 @@ namespace
     public:
         explicit MyJSON() : parser( "JSON", "main" )
         {
+            //! lexer
+            syntax::terminal & LBRACE  = jettison("LBRACE", "\\{");
+            syntax::terminal & RBRACE  = jettison("RBRACE", "\\}");
+            syntax::terminal & LBRACK  = jettison("LBRACK", "\\[");
+            syntax::terminal & RBRACK  = jettison("RBRACK", "\\]");
+            syntax::terminal & STRING  = terminal("STRING", "[:cstring:]");
+            syntax::terminal & JTRUE   = univocal("true", "true");
+            syntax::terminal & JFALSE  = univocal("false", "false");
+            syntax::terminal & JNULL   = univocal("null","null");
+            syntax::terminal & JNUMBER = terminal("NUMBER","[:digit:]+");
+            syntax::terminal & COLUMN  = jettison("COLUMN",":");
+            syntax::terminal & COMMA   = jettison("COMMA", ",");
+            
+            Y_LEX_DISCARD(scanner, "BLANK", "[:blank:]");
+            Y_LEX_NO_ENDL(scanner);
+            
+            
+            //! grammar
+            syntax:: aggregate &EMPTY_OBJ =  agg("EMPTY_OBJ");
+            EMPTY_OBJ += LBRACE;
+            EMPTY_OBJ += RBRACE;
+            
+            syntax::aggregate &EMPTY_ARR = agg("EMPTY_ARR");
+            EMPTY_ARR += LBRACK;
+            EMPTY_ARR += RBRACK;
+            
+           
+            
+            syntax::aggregate   &OBJECT = agg("OBJECT");
+            syntax::aggregate   &ARRAY  = agg("ARRAY");
+            syntax::alternative &VALUE  = alt("VALUE");
+            VALUE |= STRING;
+            VALUE |= JNUMBER;
+            VALUE |= JTRUE;
+            VALUE |= JFALSE;
+            VALUE |= JNULL;
+            VALUE |= OBJECT;
+            VALUE |= EMPTY_OBJ;
+            VALUE |= ARRAY;
+            VALUE |= EMPTY_ARR;
+            
+            syntax::aggregate &PAIR= agg("PAIR");
+            PAIR += STRING;
+            PAIR += COLUMN;
+            PAIR += VALUE;
+            
+            syntax::aggregate &EXTRA_PAIR = agg("EXTRA_PAIR", syntax::is_merging_all);
+            EXTRA_PAIR += COMMA;
+            EXTRA_PAIR += PAIR;
+            
+            OBJECT += LBRACE;
+            OBJECT += PAIR;
+            OBJECT += rep("EXTRA_PAIRS", EXTRA_PAIR, 0);
+            OBJECT += RBRACE;
+            
+            syntax::aggregate &EXTRA_VALUE = agg("EXTRA_VALUE",syntax::is_merging_all);
+            EXTRA_VALUE += COMMA;
+            EXTRA_VALUE += VALUE;
+            
+            ARRAY += LBRACK;
+            ARRAY += VALUE;
+            ARRAY += rep("EXTRA_VALUES", EXTRA_VALUE,0);
+            ARRAY += RBRACK;
+            
+            syntax::alternative &JSON = alt("JSON");
+            JSON |= EMPTY_OBJ;
+            JSON |= EMPTY_ARR;
+            JSON |= OBJECT;
+            JSON |= ARRAY;
+            
+            set_root(JSON);
+            
         }
         
         virtual ~MyJSON() throw() {}
@@ -28,11 +100,20 @@ namespace
 
 YOCTO_UNIT_TEST_IMPL(json)
 {
+    
+    vfs &fs = local_fs::instance();
+    if(fs.is_reg("tree.dot" )) fs.remove_file("tree.dot");
+    if(fs.is_reg("tree.png"))  fs.remove_file("tree.png");
+
+    
     MyJSON json;
     source src;
     src.attach_stdin();
     
-    
+    auto_ptr<syntax::xnode> root( json(src) );
+    assert( root.is_valid() );
+    root->graphviz("tree.dot");
+    system("dot -Tpng -otree.png tree.dot");
     
 }
 YOCTO_UNIT_TEST_DONE()
