d6c246f67309c8664e70ceb487ac58258afdc406
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-05-23 08:44:04 +0000

    grammar compiler seems ok

--- trunk/src/y-lingua/yocto/lingua/compiler-tree.cpp
+++ trunk/src/y-lingua/yocto/lingua/compiler-tree.cpp
@@ -6,18 +6,108 @@ namespace yocto
     namespace lingua
     {
         
-        static inline
-        void __claim( syntax::xnode *parent, syntax::xnode::child_list &ch ) throw()
+        syntax::xnode * compiler:: simplify( syntax::xnode *node ) throw()
         {
-            while(ch.size)
+            assert(node);
+            if( !node->terminal )
             {
-                syntax::xnode *node = ch.pop_back();
-                node->parent = parent;
-                parent->children().push_front(node);
+                syntax::xnode::child_list &source = node->children();
+                syntax::xnode::child_list target;
+                while(source.size)
+                {
+                    syntax::xnode *sub = simplify(source.pop_front());
+                    if( sub->label == "ATOMS" )
+                    {
+                        syntax::xnode::child_list &ch = sub->children();
+                        assert(ch.size>0);
+                        if( ch.size == 1 )
+                        {
+                            while(ch.size)
+                            {
+                                syntax::xnode *n = ch.pop_front();
+                                n->parent = node;
+                                target.push_back(n);
+                            }
+                            delete sub;
+                            continue;
+                        }
+                    }
+                    
+                    target.push_back(sub);
+                }
+                source.swap_with(target);
             }
+            return node;
+
         }
         
-        syntax::xnode *compiler::rewrite( syntax::xnode *root )
+        // top down assembling same kind of nodes
+        syntax::xnode * compiler:: assemble( const char *label, syntax::xnode *node ) throw()
+        {
+            assert(node);
+            assert(label);
+            if( !node->terminal )
+            {
+                syntax::xnode::child_list &source = node->children();
+                syntax::xnode::child_list target;
+                const bool is_kind = label == node->label;
+                while(source.size)
+                {
+                    syntax::xnode *sub = assemble(label,source.pop_front());
+                    if( is_kind && label == sub->label )
+                    {
+                        syntax::xnode::child_list &ch = sub->children();
+                        while(ch.size)
+                        {
+                            syntax::xnode *n = ch.pop_front();
+                            n->parent = node;
+                            target.push_back(n);
+                        }
+                        delete sub;
+                    }
+                    else
+                        target.push_back(sub);
+                }
+                target.swap_with(source);
+            }
+            return node;
+        }
+        
+        //======================================================================
+        // contract consecutive groups on the same level
+        //======================================================================
+        syntax::xnode * compiler::contract_atoms( syntax::xnode *node ) throw()
+        {
+            assert(node);
+            if(!node->terminal)
+            {
+                syntax::xnode::child_list &source = node->children();
+                syntax::xnode::child_list  target;
+                while(source.size)
+                {
+                    syntax::xnode *sub = contract_atoms( source.pop_front() );
+                    syntax::xnode *prv = target.tail;
+                    if( "ATOMS" == sub->label && prv && "ATOMS" == prv->label )
+                    {
+                        syntax::xnode::child_list &ch = sub->children();
+                        while(ch.size)
+                        {
+                            syntax::xnode *n = ch.pop_front();
+                            prv->children().push_back(n);
+                            n->parent = prv;
+                        }
+                        delete sub;
+                    }
+                    else
+                        target.push_back(sub);
+                }
+                source.swap_with(target);
+            }
+            return node;
+        }
+        
+        
+        syntax::xnode *compiler::rewrite( syntax::xnode *root ) throw()
         {
             assert(root);
             if( ! root->terminal )
