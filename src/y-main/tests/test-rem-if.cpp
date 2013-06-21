5d0144b655780d4d02d48d427dfdb5c2ac20cc0e
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-06-21 06:51:39 +0000

    basic remove_if

---
+++ src/y-main/tests/test-rem-if.cpp
@@ -0,0 +1,50 @@
+#include "yocto/utest/run.hpp"
+#include "yocto/code/remove-if.hpp"
+#include "yocto/sequence/vector.hpp"
+
+
+#include "support.hpp"
+
+using namespace yocto;
+
+
+static inline bool is_bad_int( int x )
+{
+    return x > 3;
+}
+
+static inline bool is_bad_str( const string &s )
+{
+    return s.size() <= 0 || s[0] == 'A';
+}
+
+YOCTO_UNIT_TEST_IMPL(remove_if)
+{
+    vector<int> a;
+    
+    for( size_t i= 2 + alea_leq(20); i>0; --i )
+    {
+        a.push_back( int(alea_leq(15) ) );
+    }
+    
+    std::cerr << "a1=" << a << std::endl;
+    remove_if(a,is_bad_int);
+    std::cerr << "a2=" << a << std::endl;
+    
+    vector<string> b;
+    for(size_t i=2+alea_leq(20);i>0;--i)
+    {
+        string s;
+        for(size_t j=1+alea_lt(3);j>0;--j)
+        {
+            s += char( 'A' + alea_lt(2) );
+        }
+        b.push_back(s);
+    }
+    std::cerr << "b1=" << b << std::endl;
+    remove_if(b,is_bad_str);
+    std::cerr << "b2=" << b << std::endl;
+
+}
+YOCTO_UNIT_TEST_DONE()
+
---
+++ src/y-main/yocto/code/remove-if.hpp
@@ -0,0 +1,46 @@
+#ifndef YOCTO_CODE_REMOVE_IF_INCLUDED
+#define YOCTO_CODE_REMOVE_IF_INCLUDED
+
+#include "yocto/code/round.hpp"
+#include <cstring>
+
+namespace yocto
+{
+    template <typename T,typename FUNC> static inline
+    size_t remove_if( T *a, size_t n, FUNC &is_bad )
+    {
+        assert(!(a==0&&n>0));
+        size_t   n_ok = n;
+        size_t   curr = 0;
+        uint64_t wksp[ YOCTO_U64_FOR_ITEM(T) ];
+        void    *addr = &wksp[0];
+        
+        while(curr<n_ok)
+        {
+            T *source = &a[curr];
+            if( is_bad( *source) )
+            {
+                memmove(addr,          (void*)source,     sizeof(T)             );
+                memmove((void*)source, (void*)&source[1], (--n_ok-curr)*sizeof(T) );
+                memmove((void*)&a[n_ok],addr,sizeof(T));
+            }
+            else
+                ++curr;
+        }
+        return n_ok;
+    }
+    
+    template <typename ARRAY,typename FUNC> static inline
+    void remove_if( ARRAY &a, FUNC &is_bad)
+    {
+        size_t n = a.size();
+        if(n>0)
+        {
+            n = remove_if<typename ARRAY::type,FUNC>(&a[1], n, is_bad);
+            while(a.size()>n) a.pop_back();
+        }
+    }
+}
+
+
+#endif
