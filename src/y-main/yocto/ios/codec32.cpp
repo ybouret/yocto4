7c55d56f149b5022b78a266a653a5a358cf53264
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-06-26 06:46:09 +0000

    identity codec32

--- src/y-main/yocto/ios/codec32.cpp
+++ src/y-main/yocto/ios/codec32.cpp
@@ -1,4 +1,7 @@
 #include "yocto/ios/codec32.hpp"
+#include "yocto/exception.hpp"
+
+#include <cstring>
 
 namespace yocto
 {
@@ -7,5 +10,28 @@ namespace yocto
         
         codec32:: ~codec32() throw() {}
         codec32::  codec32() throw() {}
+        
+        
+        identity32:: identity32() throw() {}
+        
+        identity32:: ~identity32() throw() {}
+        
+        void identity32:: encode( Y_CODEC32_ARGS_NO_OPT )
+        {
+            Y_CODEC32_CHECK();
+            if(destlen<srclen)
+                throw exception("identity32.encode(destlen=%u<srclen=%u)", unsigned(destlen), unsigned(srclen) );
+            memcpy(destbuf,srcbuf,destlen=srclen);
+        }
+        
+        void identity32:: decode( Y_CODEC32_ARGS_NO_OPT )
+        {
+            Y_CODEC32_CHECK();
+            if(destlen<srclen)
+                throw exception("identity32.decode(destlen=%u<srclen=%u)", unsigned(destlen), unsigned(srclen) );
+            memcpy(destbuf,srcbuf,destlen=srclen);
+        }
+
+
     }
 }
