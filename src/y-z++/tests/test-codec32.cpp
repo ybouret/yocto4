e640ea98b73a4d7619382d78af8dbf776e889f85
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-06-26 07:26:52 +0000

    will test z++/codec32

---
+++ src/y-z++/tests/test-codec32.cpp
@@ -0,0 +1,16 @@
+#include "yocto/utest/run.hpp"
+#include "yocto/z++/codec32.hpp"
+
+#include <cstdlib>
+
+using namespace yocto;
+
+YOCTO_UNIT_TEST_IMPL(codec32)
+{
+    
+    zlib::codec32 z32;
+    
+    
+}
+YOCTO_UNIT_TEST_DONE()
+
--- src/y-z++/yocto/z++/codec32.cpp
+++ src/y-z++/yocto/z++/codec32.cpp
@@ -1,4 +1,6 @@
 #include "yocto/z++/codec32.hpp"
+#include "yocto/threading/mutex.hpp"
+#include "yocto/zlib/zlib.h"
 
 namespace yocto
 {
@@ -7,6 +9,42 @@ namespace yocto
         codec32:: codec32() throw() {}
         
         codec32:: ~codec32() throw() {}
+        
+        
+        int32_t codec32:: Encode(void           *destbuf,
+                                 uint32_t       &destlen,
+                                 const void     *srcbuf,
+                                 const uint32_t  srclen,
+                                 const int32_t   level) throw()
+        {
+            
+            Y_CODEC32_CHECK();
+            
+            Bytef       *dest    = (Bytef *)destbuf;
+            uLong        destLen = destLen;
+            const Bytef *source = (const Bytef *)srcbuf;
+            const uLong  sourceLen = srclen;
+            YOCTO_GIANT_LOCK();
+            const int    ans = compress2(dest, &destLen, source, sourceLen, int(level));
+            if(ans!=Z_OK)
+                return 1;
+            
+            return 0;
+        }
+        
+        uint32_t codec32:: EncodeSpace(uint32_t src) throw()
+        {
+            return compressBound(src);
+        }
+        
+        void codec32:: encode( Y_CODEC32_ARGS )
+        {
+        }
+        
+        void codec32:: decode( Y_CODEC32_ARGS_NO_OPT )
+        {
+            
+        }
         
     }
     
