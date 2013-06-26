3690ef17682101322bdd54f03261dbd7f0427415
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-06-26 06:46:09 +0000

    identity codec32

--- trunk/src/y-main/yocto/ios/codec32.hpp
+++ trunk/src/y-main/yocto/ios/codec32.hpp
@@ -30,6 +30,37 @@ namespace yocto
         private:
             YOCTO_DISABLE_COPY_AND_ASSIGN(codec32);
         };
+        
+#define Y_CODEC32_ARGS  \
+void          *destbuf, \
+uint32_t      &destlen, \
+const void    *srcbuf,  \
+const uint32_t srclen,  \
+void          *options
+        
+#define Y_CODEC32_ARGS_NO_OPT \
+void          *destbuf, \
+uint32_t      &destlen, \
+const void    *srcbuf,  \
+const uint32_t srclen,  \
+void          *
+        
+#define Y_CODEC32_CHECK() do { assert(!(destbuf==0&&destlen>0)); assert(!(srcbuf==0&&srclen>0)); } while(false)
+        
+#define Y_CODEC32_DECL() virtual void encode(Y_CODEC32_ARGS); virtual void decode(Y_CODEC32_ARGS)
+        
+        
+        class identity32 : public codec32
+        {
+        public:
+            explicit identity32() throw();
+            virtual ~identity32() throw();
+            
+            Y_CODEC32_DECL();
+            
+        private:
+            YOCTO_DISABLE_COPY_AND_ASSIGN(identity32);
+        };
     }
 }
 
