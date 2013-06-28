a1fb7d59c7fa06a207cc916637d69aa2ec71da9a
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-06-28 06:16:52 +0000

    removed codec32

--- trunk/src/y-z++/tests/test-codec32.cpp
+++ trunk/src/y-z++/tests/test-codec32.cpp
@@ -1,5 +1,4 @@
 #include "yocto/utest/run.hpp"
-#include "yocto/z++/codec32.hpp"
 #include "yocto/ios/file-loader.hpp"
 #include "yocto/memory/buffers.hpp"
 
@@ -15,22 +14,6 @@ YOCTO_UNIT_TEST_IMPL(codec32)
         ios::file_content content;
         ios::file_loader::append_to(content, argv[1]);
         std::cerr << "Input Size  = " << content.size() << std::endl;
-        zlib::codec32 z32;
-        
-        uint32_t nOutMax = z32.EncodeSpace(content.size());
-        std::cerr << "Encode Space= " << nOutMax << std::endl;
-        
-        memory::buffer_of<char,memory::global> encbuf( nOutMax );
-        
-        int32_t ans = z32.Encode(encbuf(), nOutMax, content(), content.size(), 9);
-        if( ans != 0 )
-        {
-            std::cerr << "Couldn't Encode" << std::endl;
-        }
-        else
-        {
-            std::cerr << "Encoded Size= " << nOutMax << std::endl;
-        }
     }
     
 }
