03b2ead27796af178f034b42cb10420afcd3802b
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-07-08 09:00:52 +0000

    removed old swamp

--- trunk/src/y-swamp/yocto/swamp/array2d.cpp
+++
@@ -1,9 +0,0 @@
-#include "yocto/swamp/array2d.hpp"
-#define SOURCE "array2d.cxx"
-
-#include  <iostream>
-#include "yocto/ios/ocstream.hpp"
-#include "yocto/math/complex.hpp"
-
-#include "array-inc.cxx"
-
--- trunk/src/y-swamp/yocto/swamp/level-set.cpp
+++
@@ -1,9 +0,0 @@
-#include "yocto/swamp/level-set.hpp"
-#include "yocto/exception.cpp"
-
-#define ZTYPE float
-#include "level-set.cxx"
-
-#undef ZTYPE
-#define ZTYPE double
-#include "level-set.cxx"
