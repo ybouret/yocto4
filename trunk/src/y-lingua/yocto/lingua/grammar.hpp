ee905e5b7c95cae11ae64b34b36159532284e4fa
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-07-08 10:22:52 +0000

    minor lingua updates

--- trunk/src/y-lingua/yocto/lingua/grammar.hpp
+++ trunk/src/y-lingua/yocto/lingua/grammar.hpp
@@ -19,6 +19,8 @@ namespace yocto
             
             //! create an empty grammar
             explicit grammar( const string &id );
+            
+            //! create an empty grammar
             explicit grammar( const char   *id );
             
             //! default ctor
