ae628cc994c5cd1555c4dc55bc7734f56d915cee
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-07-08 13:44:01 +0000

    minor doc update

--- src/y-lingua/yocto/lingua/pattern/dict.hpp
+++ src/y-lingua/yocto/lingua/pattern/dict.hpp
@@ -9,22 +9,35 @@ namespace yocto
     namespace lingua
     {
         
+        //! pattern dictionay
         class p_dict : public object
         {
         public:
             explicit p_dict() throw();
             virtual ~p_dict() throw();
             
+            //! insert a pattern, taken care of
             void insert( const string &id, pattern *p);
+            
+            //! insert a pattern, taken care of
             void insert( const char   *id, pattern *p);
             
+            //! compile the pattern from expression and insert it
             void insert( const string &id, const string &expr);
+            
+            //! compile the pattern from expression and insert it
             void insert( const char   *id, const char   *expr);
             
+            //! lookup by name
             bool has( const string &id ) const throw();
+            
+            //! lookup by name
             bool has( const char   *id ) const;
             
+            //! create by name, throw on error
             pattern *create( const string &id ) const;
+            
+            //! create by name, throw on error
             pattern *create( const char   *id ) const;
             
         private:
