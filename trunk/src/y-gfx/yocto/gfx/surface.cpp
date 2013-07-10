29ad8bc7ae275eeb713f03283bcc191a8ea14287
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-07-10 14:49:50 +0000

    minimal surface

--- trunk/src/y-gfx/yocto/gfx/surface.cpp
+++ trunk/src/y-gfx/yocto/gfx/surface.cpp
@@ -1,17 +1,119 @@
 #include "yocto/gfx/surface.hpp"
+#include "yocto/memory/global.hpp"
 
 namespace yocto
 {
     namespace gfx
     {
-        surface:: ~surface() throw() {}
-        
-        surface:: surface( const color_format &fmt, size_t width, size_t height ) :
-        format( fmt ),
-        pixels( new pixbuf(fmt.bytes_per_pixel,width,height) )
+        surface:: ~surface() throw()
         {
+            memory::kind<memory::global>::release_as<row>(rows, nrow);
         }
         
         
+        static inline
+        void * move8(void *p, size_t x) throw()
+        {
+            return static_cast<uint8_t*>(p)+x;
+        }
+        
+        static inline
+        void * move16(void *p,size_t x) throw()
+        {
+            return static_cast<uint16_t*>(p)+x;
+        }
+        
+        static inline
+        void * move24(void *p,size_t x) throw()
+        {
+            return static_cast<uint8_t*>(p)+3*x;
+        }
+        
+        static inline
+        void * move32(void *p,size_t x) throw()
+        {
+            return static_cast<uint32_t*>(p)+x;
+        }
+        
+        
+        
+        surface:: row:: row( size_t width, void *start, const size_t BPP) throw() :
+        w(width),
+        p(start),
+        move(0)
+        {
+            assert(w>0);
+            assert(p);
+            switch( BPP )
+            {
+                case 1:
+                    move = move8;
+                    break;
+                    
+                    
+                case 2:
+                    move = move16;
+                    break;
+                    
+                case 3:
+                    move = move24;
+                    break;
+                    
+                case 4:
+                    move = move32;
+                    break;
+            }
+        }
+        
+        
+        void * surface::row:: operator[](size_t i) throw()
+        {
+            assert(i<w);
+            assert(move);
+            return move(p,i);
+        }
+        
+        const void * surface::row:: operator[](size_t i) const throw()
+        {
+            assert(i<w);
+            assert(move);
+            return move((void*)p,i);
+        }
+        
+        
+        surface:: surface( const color_format &fmt, size_t width, size_t height ) :
+        format( fmt ),
+        pixels( new pixbuf(fmt.bytes_per_pixel,width,height) ),
+        w(pixels->w),
+        h(pixels->h),
+        nrow(h),
+        rows( memory::kind<memory::global>::acquire_as<row>(nrow) )
+        {
+            link(0,0);
+        }
+        
+        void surface:: link( unit_t xoff, unit_t yoff ) throw()
+        {
+            uint8_t *p = (uint8_t*)(pixels->get(xoff, yoff));
+            for(size_t j=0;j<h;++j)
+            {
+                new (rows+j) row(w,p,pixels->bytes_per_pixel);
+                p += pixels->stride;
+            }
+        }
+        
+        surface::row & surface:: operator[]( size_t j ) throw()
+        {
+            assert(j<h);
+            return rows[j];
+        }
+        
+        const surface::row & surface:: operator[]( size_t j ) const throw()
+        {
+            assert(j<h);
+            return rows[j];
+        }
+        
+               
     }
 }
--- trunk/src/y-gfx/yocto/gfx/surface.hpp
+++ trunk/src/y-gfx/yocto/gfx/surface.hpp
@@ -12,15 +12,42 @@ namespace yocto
         class surface : public object
         {
         public:
-            virtual ~surface() throw();
+            
+            class row
+            {
+            public:
+                row(size_t width, void *start, const size_t BPP) throw();
+                const size_t w;
+                
+                void * operator[](size_t i) throw();
+                const void * operator[](size_t i) const throw();
+                
+            private:
+                void        *p;
+                void  *    (*move)(void *,size_t);
+                
+                ~row() throw();
+                YOCTO_DISABLE_COPY_AND_ASSIGN(row);
+            };
             
             //! make a new memory surface
             explicit surface( const color_format &fmt, size_t width, size_t height);
+            virtual ~surface() throw();
             
+            
+            
+            row &       operator[](size_t j) throw();
+            const row & operator[](size_t j) const throw();
+
             const color_format format;
             const pixel_buffer pixels;
+            const size_t       w;
+            const size_t       h;
             
         private:
+            size_t nrow;
+            row   *rows;
+            void   link( unit_t xoff, unit_t yoff ) throw();
             YOCTO_DISABLE_COPY_AND_ASSIGN(surface);
         };
     }
