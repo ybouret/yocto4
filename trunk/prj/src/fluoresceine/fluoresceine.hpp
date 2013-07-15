accd2332a3a66f0cde89bc891277b6e93f080a63
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-07-15 12:13:49 +0000

    added fluoresceine for aqueous sample

---
+++ trunk/prj/src/fluoresceine/fluoresceine.hpp
@@ -0,0 +1,13 @@
+#ifndef FLUORESCEINE_INCLUDED
+#define FLUORESCEINE_INCLUDED 1
+
+#include "yocto/aqueous/initializer.hpp"
+#include "yocto/fs/vfs.hpp"
+#include "yocto/exception.hpp"
+
+#include <FL/fl_ask.H>
+
+using namespace yocto;
+using namespace aqueous;
+
+#endif
