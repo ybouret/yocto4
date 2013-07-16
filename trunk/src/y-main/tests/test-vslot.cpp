57d440891e8deadc601443162688a388ba6d63ec
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-07-16 08:05:57 +0000

    added info for vslot

--- trunk/src/y-main/tests/test-vslot.cpp
+++ trunk/src/y-main/tests/test-vslot.cpp
@@ -15,9 +15,9 @@ YOCTO_UNIT_TEST_IMPL(vslot)
 
     v1.release();
     
-    v1.make<short>(2);  std::cerr << v1.as<short>()  << std::endl;
-    v1.make<float>(4);  std::cerr << v1.as<float>()  << std::endl;
-    v1.make<double>(8); std::cerr << v1.as<double>() << std::endl;
+    v1.make<short>(2);        std::cerr << v1.as<short>()  << std::endl;
+    v1.make<float>(4);        std::cerr << v1.as<float>()  << std::endl;
+    v1.make<double>(8);       std::cerr << v1.as<double>() << std::endl;
     v1.make<string>("hello"); std::cerr << v1.as<string>() << std::endl;
 }
 YOCTO_UNIT_TEST_DONE()
