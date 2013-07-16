c12323248732f4e355e4477278b6a261083ed179
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-07-16 07:08:13 +0000

    vslot initial commit

---
+++ trunk/src/y-main/yocto/container/vslot.cpp
@@ -0,0 +1,94 @@
+#include "yocto/container/vslot.hpp"
+#include "yocto/code/round.hpp"
+#include "yocto/object.hpp"
+
+namespace yocto
+{
+    
+    
+    bool vslot:: is_allocated() const throw()
+    {
+        if( size > 0 )
+        {
+            assert(data!=0);
+            return true;
+        }
+        else
+        {
+            assert(0==data);
+            return false;
+        }
+    }
+    
+    bool vslot:: is_active() const throw()
+    {
+        if( type )
+        {
+            assert(is_allocated());
+            assert(kill!=0);
+            return 0;
+        }
+        else
+        {
+            assert(0==kill);
+            return false;
+        }
+    }
+    
+    void vslot:: allocate( size_t n )
+    {
+        assert(0==size);
+        assert(0==data);
+        assert(0==kill);
+        assert(0==type);
+        if(n>0)
+        {
+            data = object:: operator new(n);
+            size = n;
+        }
+        
+    }
+    
+    void vslot:: deallocate() throw()
+    {
+        assert(0==type);
+        assert(0==kill);
+        if(is_allocated())
+        {
+            object:: operator delete(data,size);
+            data = 0;
+            size = 0;
+        }
+    }
+    
+    void vslot:: free() throw()
+    {
+        if(is_active())
+        {
+            kill(data);
+            kill = 0;
+            type = 0;
+        }
+    }
+    
+    void vslot:: release() throw()
+    {
+        free();
+        deallocate();
+    }
+    
+    vslot:: ~vslot() throw()
+    {
+        release();
+    }
+    
+    vslot:: vslot() throw() :
+    size(0),
+    data(0),
+    type(0),
+    kill(0)
+    {
+    }
+    
+    
+}
