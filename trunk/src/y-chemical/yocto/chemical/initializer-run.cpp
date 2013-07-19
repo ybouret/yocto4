c3b2268231d99cca82eaac4c44b8b9812592d19c
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-07-19 08:20:45 +0000

    rewrite initializer

--- trunk/src/y-chemical/yocto/chemical/initializer-run.cpp
+++ trunk/src/y-chemical/yocto/chemical/initializer-run.cpp
@@ -2,6 +2,7 @@
 #include "yocto/exception.hpp"
 #include "yocto/math/kernel/svd.hpp"
 #include "yocto/math/kernel/algebra.hpp"
+#include "yocto/math/opt/bracket.hpp"
 
 namespace yocto
 {
@@ -13,202 +14,266 @@ namespace yocto
         
         
         
-        
-        static inline
-        void __project(vector_t       &X,
-                       const matrix_t &P,
-                       const matrix_t &P2,
-                       const lu_t     &L2,
-                       vector_t       &Y,
-                       const vector_t &Lam,
-                       vector_t       &Mu ) throw()
-        {
+        namespace  {
             
-            //==================================================================
-            // initialize first projection
-            //==================================================================
-            mkl::set(Mu, Lam);
-            mkl::mulsub(Mu, P, X);
-            L2.solve(P2, Mu);
-            mkl::mul_trn(Y, P, Mu);
-            //std::cerr << "Y=" << Y << std::endl;
-            mkl::add(X, Y);
-            double Ymin = mkl::norm2(Y);
-            
-            //==================================================================
-            // go on until norm decreases
-            //==================================================================
-            while(true)
+            class Initializer
             {
-                mkl::set(Mu, Lam);
-                mkl::mulsub(Mu, P, X);
-                L2.solve(P2, Mu);
-                mkl::mul_trn(Y, P, Mu);
-                //std::cerr << "Y=" << Y << std::endl;
-                mkl::add(X, Y);
-                const double Ytmp = mkl::norm2(Y);
-                if(Ytmp>=Ymin)
-                    break;
-                Ymin = Ytmp;
-            }
+            public:
+                equilibria  &cs;
+                initializer &cr;
+                const size_t N;
+                const size_t M;
+                const size_t Nc;
+                const double t;
+                matrix_t     P;
+                vector_t     Lam;
+                matrix_t     Q;
+                matrix_t     P2;
+                lu_t         L2;
+                vector_t     X0;
+                vector_t     X1;
+                vector_t     Y;
+                vector_t     Mu;
+                vector_t     V;
+                vector_t     dV;
+                
+                Initializer(equilibria        &user_cs,
+                            collection        &lib,
+                            initializer       &user_cr,
+                            const double       user_t,
+                            urand32           &ran) :
+                cs(user_cs),
+                cr(user_cr),
+                N(cs.size()),
+                M(lib.size()),
+                Nc(cr.size()),
+                t(user_t),
+                P(),
+                Lam(),
+                Q(),
+                P2(),
+                L2(),
+                X0(),
+                X1(),
+                Y(),
+                Mu(),
+                V(),
+                dV()
+                {
+                    //==========================================================
+                    // check rank
+                    //==========================================================
+                    if( N+Nc != M )
+                        throw exception("initialier: #species=%u != (#equilibria=%u+#constraints=%u)", unsigned(M), unsigned(N), unsigned(Nc) );
+                    
+                    //==========================================================
+                    // initialize
+                    //==========================================================
+                    cs.build_from(lib);
+                    
+                    //==========================================================
+                    // initialize
+                    //==========================================================
+                    cs.scale_all(t);
+                    
+                    //==========================================================
+                    // build agebraic constraints
+                    //==========================================================
+                    P.make(Nc,M);
+                    Lam.make(Nc,M);
+                    for(size_t i=1; i <= Nc; ++i )
+                    {
+                        const constraint &Q = * cr[i];
+                        Lam[i] = Q.value;
+                        for( constraint_weights::const_iterator weight=Q.begin(); weight != Q.end(); ++weight )
+                        {
+                            const species::ptr &sp = lib[weight->key];
+                            P[i][sp->indx] = *weight;
+                        }
+                    }
+                    std::cerr << "P=" << P << std::endl;
+                    std::cerr << "Lam=" << Lam << std::endl;
+                    
+                    //==========================================================
+                    //
+                    // build the orthogonal matrix by SVD
+                    //
+                    //==========================================================
+                    Q.make(N,M);
+                    {
+                        matrix_t F(M,M);
+                        for(size_t i=1;i<=Nc;++i)
+                        {
+                            for(size_t j=1;j<=M;++j)
+                                F[j][i] = P[i][j];
+                        }
+                        
+                        matrix_t __V(M,M);
+                        vector_t __W(M,0);
+                        if( !math::svd<double>::build(F, __W, __V) )
+                            throw exception("singular initializer for SVD");
+                        for(size_t i=1;i<=N;++i)
+                        {
+                            for(size_t j=1;j<=M;++j)
+                                Q[i][j] = F[j][i+Nc];
+                        }
+                    }
+                    std::cerr << "Q=" << Q << std::endl;
+                    
+                    //==========================================================
+                    //
+                    // build the partial Moore-Penrose Pseudo Inverse: inv(P*P')
+                    //
+                    //==========================================================
+                    L2.ensure(Nc);
+                    P2.make(Nc,Nc);
+                    mkl::mul_rtrn(P2,P,P);
+                    if( !L2.build(P2) )
+                        throw exception("singular initializer for pseudo-inverse");
+                    
+                    //==========================================================
+                    //
+                    // Final Memory Allocation
+                    //
+                    //==========================================================
+                    X0.make(M,0);
+                    X1.make(M,0);
+                    Y.make(M,0);
+                    Mu.make(Nc,0);
+                    V.make(N,0);
+                    dV.make(N,0);
+                    
+                    
+                    //==========================================================
+                    //
+                    // Initialize the algorithm: X0 + tQ.V
+                    //
+                    //==========================================================
+                    mkl::set(Mu,Lam);
+                    L2.solve(P2, Mu);
+                    mkl::mul_trn(X0,P,Mu);
+                    project(X0);
+                    std::cerr << "X0=" << X0 << std::endl;
+                    
+                    double amplitude = 0;
+                    for(size_t i=M;i>0;--i)
+                    {
+                        const double xm = Fabs(X0[i]);
+                        if(xm>amplitude) amplitude = xm;
+                    }
+                    
+                    //==========================================================
+                    //
+                    // Algorithm
+                    //
+                    //==========================================================
+                    bool converged = false;
+                    
+                    //----------------------------------------------------------
+                    // initialize step
+                    //----------------------------------------------------------
+                    for(size_t i=N;i>0;--i)
+                    {
+                        V[i] = amplitude * (0.5 - ran.get<double>());
+                    }
+                    std::cerr << "V=" << V << std::endl;
+                    
+                NEWTON_STEP:
+                    //----------------------------------------------------------
+                    // build composition: X1 = X0 + Q'*V;
+                    //----------------------------------------------------------
+                    build_composition();
+                    //std::cerr << "X1=" << X1 << std::endl;
+                    
+                    
+                    //----------------------------------------------------------
+                    // compute newton step
+                    //----------------------------------------------------------
+                    mkl::set(cs.C,X1);
+                    cs.compute_Gamma_and_Phi(t,false);
+                    mkl::mul_rtrn(cs.W, cs.Phi, Q);
+                    if( ! cs.LU.build(cs.W) )
+                    {
+                        std::cerr << "singular newton step" << std::endl;
+                        return;
+                    }
+                    mkl::neg(dV,cs.Gamma);
+                    cs.LU.solve(cs.W,dV);
+                    std::cerr << "V="  << V  << std::endl;
+                    std::cerr << "dV=" << dV << std::endl;
+                    
+                    converged = true;
+                    for(size_t i=N;i>0;--i)
+                    {
+                        V[i] += dV[i];
+                        if( Fabs(dV[i]) > Fabs( cs.ftol * V[i] ) )
+                            converged = false;
+                    }
+                    if(!converged) goto NEWTON_STEP;
+                    
+                    build_composition();
+                    mkl::set(cs.C,X1);
+                    
+                }
+                
+                
+                void build_composition()
+                {
+                    mkl::mul_trn(X1, Q, V);
+                    mkl::add(X1, X0);
+                    project(X1);
+                }
+                
+                
+                void project( vector_t &X ) throw()
+                {
+                    //==================================================================
+                    // initialize first projection
+                    //==================================================================
+                    mkl::set(Mu, Lam);
+                    mkl::mulsub(Mu, P, X);
+                    L2.solve(P2, Mu);
+                    mkl::mul_trn(Y, P, Mu);
+                    mkl::add(X, Y);
+                    double Ymin = mkl::norm1(Y);
+                    
+                    //==================================================================
+                    // go on while norm decreases
+                    //==================================================================
+                    while(true)
+                    {
+                        mkl::set(Mu, Lam);
+                        mkl::mulsub(Mu, P, X);
+                        L2.solve(P2, Mu);
+                        mkl::mul_trn(Y, P, Mu);
+                        mkl::add(X, Y);
+                        const double Ytmp = mkl::norm1(Y);
+                        if(Ytmp>=Ymin)
+                            break;
+                        Ymin = Ytmp;
+                    }
+                    
+                }
+                
+                
+                ~Initializer() throw()
+                {
+                    
+                }
+                
+                
+            private:
+                YOCTO_DISABLE_COPY_AND_ASSIGN(Initializer);
+            };
+            
         }
         
-#define __PROJ(X) __project(X,P,P2,L2,Y,Lam,Mu)
         
         
         void initializer:: operator()( equilibria &cs, collection &lib, const double t )
         {
-            //==================================================================
-            //
-            // initialize the equilibria
-            //
-            //==================================================================
-            cs.build_from(lib);
-            const size_t N = cs.size();
-            const size_t M = lib.size();
-            const size_t Nc = size();
-            if( N+Nc != M )
-                throw exception("initialier: #species=%u != (#equilibria=%u+#constraints=%u)", unsigned(M), unsigned(N), unsigned(Nc) );
             
-            cs.scale_all(t);
-            
-            
-            //==================================================================
-            //
-            // build the algebraic constraints
-            //
-            //==================================================================
-            matrix_t P(Nc,M);
-            vector_t Lam(Nc,0);
-            for(size_t i=1; i <= Nc; ++i )
-            {
-                const constraint &Q = * (*this)[i];
-                Lam[i] = Q.value;
-                for( constraint_weights::const_iterator weight=Q.begin(); weight != Q.end(); ++weight )
-                {
-                    const species::ptr &sp = lib[weight->key];
-                    P[i][sp->indx] = *weight;
-                }
-            }
-            std::cerr << "P=" << P << std::endl;
-            std::cerr << "Lam=" << Lam << std::endl;
-            
-            //==================================================================
-            //
-            // build the orthogonal matrix by SVD
-            //
-            //==================================================================
-            matrix_t Q(N,M);
-            {
-                matrix_t F(M,M);
-                for(size_t i=1;i<=Nc;++i)
-                {
-                    for(size_t j=1;j<=M;++j)
-                        F[j][i] = P[i][j];
-                }
-                
-                matrix_t __V(M,M);
-                vector_t __W(M,0);
-                if( !math::svd<double>::build(F, __W, __V) )
-                    throw exception("singular initializer for SVD");
-                for(size_t i=1;i<=N;++i)
-                {
-                    for(size_t j=1;j<=M;++j)
-                        Q[i][j] = F[j][i+Nc];
-                }
-            }
-            std::cerr << "Q=" << Q << std::endl;
-            
-            //==================================================================
-            //
-            // build the partial Moore-Penrose Pseudo Inverse
-            //
-            //==================================================================
-            lu_t     L2(Nc);
-            matrix_t P2(Nc,Nc);
-            mkl::mul_rtrn(P2,P,P);
-            if( !L2.build(P2) )
-                throw exception("singular initializer for pseudo-inverse");
-            
-            //==================================================================
-            //
-            // Initialize the algorithm
-            //
-            //==================================================================
-            vector_t  &X = cs.C;
-            vector_t  &Y = cs.dC;
-            matrix_t  &W = cs.W;
-            lu_t      &LU = cs.LU;
-            vector_t  Mu(Nc,0);
-            vector_t  dX(M,0);
-            vector_t &Gamma = cs.Gamma;
-            const double ftol = cs.ftol;
-            
-            
-            //==================================================================
-            //
-            // Algorithm
-            //
-            //==================================================================
-            
-            //------------------------------------------------------------------
-            //
-            // choose a starting point
-            //
-            //------------------------------------------------------------------
-            for(size_t i=1;i<=M;++i)
-                X[i] = 0;
-            for( equilibria::iterator eq= cs.begin(); eq != cs.end(); ++eq )
-            {
-                (**eq).append(X,ran);
-            }
-            std::cerr << "X0=" << X << std::endl;
-            __PROJ(X);
-            std::cerr << "X0p=" << X << std::endl;
-            
-        NEWTON_STEP:
-            {
-                //------------------------------------------------------------------
-                //
-                // compute the modified jacobian
-                //
-                //------------------------------------------------------------------
-                cs.compute_Gamma_and_Phi(t, false);
-                const double E0 = 0.5 * mkl::norm2(Gamma);
-                mkl::mul_rtrn(W, cs.Phi, Q);
-                
-                if(!LU.build(W))
-                {
-                    std::cerr << "singular composition" << std::endl;
-                    return;
-                }
-                
-                //------------------------------------------------------------------
-                //
-                // compute the DECREASE Y
-                //
-                //------------------------------------------------------------------
-                LU.solve(W,Gamma);
-                mkl::mul_trn(Y, Q,Gamma);
-                
-                //------------------------------------------------------------------
-                //
-                // evaluate the new coordinates and the effective step dX
-                //
-                //------------------------------------------------------------------
-                mkl::set(dX,X);
-                mkl::sub(X,Y);
-                __PROJ(X);
-                mkl::sub(dX,X);
-                std::cerr << "X="  << X  << std::endl;
-                std::cerr << "dX=" << dX << std::endl;
-                
-                for(size_t i=M;i>0;--i)
-                {
-                    if( Fabs(dX[i]) > Fabs( ftol * X[i] ) )
-                        goto NEWTON_STEP;
-                }
-            }
+            Initializer ini(cs,lib,*this,t,ran);
             
         }
         
