9958c27ac6a92123fdbe4a770ead779ec4957ef6
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-07-19 09:02:52 +0000

    initializer seems to work

--- src/y-chemical/tests/test-init.cpp
+++ src/y-chemical/tests/test-init.cpp
@@ -24,6 +24,9 @@ YOCTO_UNIT_TEST_IMPL(init)
     cs.add_water(lib,1e-14);
     cs.add_acid(lib, "acid", "AH", "A-", pow(10,-4.8));
     
+    std::cerr << cs << std::endl;
+
+    
     chemical::initializer ini;
     
     
@@ -43,6 +46,53 @@ YOCTO_UNIT_TEST_IMPL(init)
     std::cerr << "C=" << cs.C << std::endl;
     std::cerr << "S=" << S << std::endl;
     std::cerr << "pH=" << S.pH() << std::endl;
+}
+YOCTO_UNIT_TEST_DONE()
+
+YOCTO_UNIT_TEST_IMPL(init2)
+{
+    double C0 = 0.01;
+    if( argc > 1 )
+    {
+        C0 = strconv::to<double>(argv[1],"C0");
+    }
+    
+    
+    chemical::collection lib;
+    lib.add("H+");
+    lib.add("HO-");
+    lib.add("CO2");
+    lib.add("HCO3-");
+    lib.add("CO3--");
+    
+    
+    chemical::equilibria cs;
+    cs.add_water(lib,1e-14);
+    cs.add_acid(lib, "eq1", "CO2", "HCO3-", 4.16e-7);
+    cs.add_acid(lib, "eq2", "HCO3-", "CO3--", 4.69e-11);
+    
+    std::cerr << cs << std::endl;
+    
+    chemical::initializer ini;
+    
+    
+    ini.electroneutrality(lib);
+    {
+        chemical::constraint &mass = ini.create(C0);
+        mass["CO2"]   = 1;
+        mass["HCO3-"] = 1;
+        mass["CO3--"] = 1;
+    }
+
+    
+    ini(cs,lib,0.0);
+    chemical::solution S(lib);
+    S.load(cs.C);
+    
+    std::cerr << "C=" << cs.C << std::endl;
+    std::cerr << "S=" << S << std::endl;
+    std::cerr << "pH=" << S.pH() << std::endl;
+    
 }
 YOCTO_UNIT_TEST_DONE()
 
