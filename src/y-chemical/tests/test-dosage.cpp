f9f09c8eedd813ea65dcb6cdd5746d8b06273541
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-08-16 08:51:45 +0000

    need another stop for chemichal initializer ftol loop

--- src/y-chemical/tests/test-dosage.cpp
+++ src/y-chemical/tests/test-dosage.cpp
@@ -9,6 +9,9 @@ using namespace yocto;
 
 YOCTO_UNIT_TEST_IMPL(dosage)
 {
+    //--------------------------------------------------------------------------
+    // use lua to load parameters
+    //--------------------------------------------------------------------------
     Lua::State VM;
     lua_State *L = VM();
     
@@ -20,10 +23,38 @@ YOCTO_UNIT_TEST_IMPL(dosage)
     else
         throw exception("need a config file to do something");
     
+    //--------------------------------------------------------------------------
+    // load the species into collection
+    //--------------------------------------------------------------------------
     chemical::collection      lib;
     chemical:: _lua:: load(L, lib, "species");
-
     std::cerr << lib << std::endl;
-
+    
+    //--------------------------------------------------------------------------
+    // load the reactions
+    //--------------------------------------------------------------------------
+    chemical::equilibria cs;
+    chemical:: _lua::load(L, lib, cs,"eqs");
+    std::cerr  << cs << std::endl;
+    
+    //--------------------------------------------------------------------------
+    // prepare the initializer
+    //--------------------------------------------------------------------------
+    chemical::initializer ini;
+    ini.electroneutrality(lib);
+    chemical:: _lua::load(L,ini,"ini");
+    
+    std::cerr << ini << std::endl;
+    
+    //--------------------------------------------------------------------------
+    // find the initial composition
+    //--------------------------------------------------------------------------
+    cs.ftol = 1e-7;
+    ini(cs,lib,0.0);
+    
+    chemical::solution S(lib);
+    S.load(cs.C);
+    std::cerr << "pH=" << S.pH() << std::endl;
+    
 }
 YOCTO_UNIT_TEST_DONE()
