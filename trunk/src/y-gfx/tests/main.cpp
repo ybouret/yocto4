311500a6d6dfd0b207ccb73135cedb285d6fc2ac
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-08-20 08:06:59 +0000

    rebuilding gfx

--- trunk/src/y-gfx/tests/main.cpp
+++ trunk/src/y-gfx/tests/main.cpp
@@ -2,11 +2,6 @@
 
 YOCTO_UNIT_TEST_INIT(16)
 {
-    YOCTO_UNIT_TEST_DECL(fmt);
-    YOCTO_UNIT_TEST_DECL(loss);
-    YOCTO_UNIT_TEST_DECL(pixbuf);
-    YOCTO_UNIT_TEST_DECL(surface);
-    
 }
 YOCTO_UNIT_TEST_EXEC()
 
