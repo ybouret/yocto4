747fe8fecd2d908ca3f8896d0ed9fea8e0d92485
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-08-21 08:00:59 +0000

    WIP pixbuf

--- trunk/src/y-gfx/yocto/gfx/pixel-buffer.cpp
+++ trunk/src/y-gfx/yocto/gfx/pixel-buffer.cpp
@@ -1,10 +1,52 @@
 #include "yocto/gfx/pixel-buffer.hpp"
 #include "yocto/exception.hpp"
+#include "yocto/memory/global.hpp"
 
 namespace yocto
 {
     namespace gfx
     {
+        pixbuf:: ~pixbuf() throw()
+        {
+            assert(entry);
+            if(allocated)
+            {
+                assert(bytes);
+                memory::kind<memory::global>::release(entry, bytes);
+            }
+        }
+        
+        static inline
+        size_t __check_width(size_t w)
+        {
+            if(w<=0)
+                throw exception("gfx::pixbuf(no width)");
+            return w;
+        }
+        
+        static inline
+        size_t __check_height(size_t h)
+        {
+            if(h<=0)
+                throw exception("gfx::pixbuf(no height)");
+            return h;
+        }
+        
+        pixbuf:: pixbuf( size_t BytesPerPixel, size_t Width, size_t Height) :
+        metrics(BytesPerPixel),
+        counted(),
+        w( __check_width(  Width ) ),
+        h( __check_height( Height) ),
+        bytes_per_line( w * bytes_per_pixel ),
+        stride( bytes_per_line     ),
+        bytes(  bytes_per_line * h ),
+        entry(  memory::kind<memory::global>::acquire(bytes) ),
+        allocated(true)
+        {
+            
+        }
+
+        
         
     }
 }
--- trunk/src/y-gfx/yocto/gfx/pixel-buffer.hpp
+++ trunk/src/y-gfx/yocto/gfx/pixel-buffer.hpp
@@ -8,8 +8,30 @@ namespace yocto
 {
     namespace gfx
     {
-     
-              
+        
+        class pixbuf : public metrics, public counted
+        {
+        public:
+            const size_t w;              //!< width
+            const size_t h;              //!< height
+            const size_t bytes_per_line; //!< w * bytes_per_pixel
+            const size_t stride;         //!< may be larger than bytes_per_line
+            
+            //! allocate some pixels
+            explicit pixbuf( size_t BytesPerPixel, size_t Width, size_t Height);
+            virtual ~pixbuf() throw();
+            
+        protected:
+            size_t bytes;
+            void  *entry;
+            
+        public:
+            const bool allocated;
+            
+        private:
+            YOCTO_DISABLE_COPY_AND_ASSIGN(pixbuf);
+        };
+        
     }
 }
 
