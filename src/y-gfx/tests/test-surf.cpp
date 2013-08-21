9539e58ca9ea8de196d9e551c951da6715707a2f
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-08-21 09:09:50 +0000

    initial surface test

--- src/y-gfx/tests/test-surf.cpp
+++ src/y-gfx/tests/test-surf.cpp
@@ -3,9 +3,28 @@
 
 using namespace yocto;
 
+
+static inline
+void display( const gfx::surface &S )
+{
+    std::cerr << "width =" << S.w << std::endl;
+    std::cerr << "height=" << S.h << std::endl;
+    std::cerr << "stride=" << S.pixels->stride << std::endl;
+
+}
+
 YOCTO_UNIT_TEST_IMPL(surf)
 {
     
+    gfx::surface S1( gfx::color_format::ARGB32(), 100, 80);
+    display(S1);
+    
+    gfx::surface S2( gfx::color_format::ARGB16(), 100, 80);
+    display(S2);
+    
+    gfx::surface S3( gfx::color_format::RGB24(), 100, 80);
+    display(S3);
+
     
 }
 YOCTO_UNIT_TEST_DONE()
