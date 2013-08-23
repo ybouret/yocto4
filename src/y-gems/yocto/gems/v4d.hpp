337de90565fda20eb5e11b5b4461402734a54fd5
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-08-23 07:01:29 +0000

    updating gems

--- src/y-gems/yocto/gems/v4d.hpp
+++ src/y-gems/yocto/gems/v4d.hpp
@@ -1,6 +1,29 @@
 #ifndef YOCTO_GEMS_V4D_INCLUDED
 #define YOCTO_GEMS_V4D_INCLUDED 1
 
+#include "yocto/math/types.hpp"
+
+
+namespace yocto
+{
+ 
+    namespace gems
+    {
+        template <typename T>
+        class v4d
+        {
+        public:
+            T x,y,z,w;
+            inline  v4d() throw() : x(0), y(0), z(0), w(0) {}
+            inline  v4d(T X,T Y,T Z,T W=0) throw() : x(X), y(Y), z(Z), w(W) {}
+            inline ~v4d() throw() {}
+            
+            inline v4d( const v4d &other ) throw() :
+            x(other.x),y(other.y),z(other.z),w(other.w) {}
+            
+        };
+    }
+}
 
 
 #endif
