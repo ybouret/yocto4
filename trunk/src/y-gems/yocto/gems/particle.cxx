77a13b4e9c71834af3c1b70bd398b7a2dbb5c301
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-08-28 12:15:01 +0000

    gems update

---
+++ trunk/src/y-gems/yocto/gems/particle.cxx
@@ -0,0 +1,25 @@
+#include "yocto/math/ztype.hpp"
+#include "yocto/gems/particle.hpp"
+#include "yocto/math/types.hpp"
+
+namespace yocto
+{
+    namespace gems
+    {
+        using namespace math;
+        
+        template <>
+        particle<real_t>:: ~particle() throw() {}
+        
+        template <>
+        particle<real_t>:: particle() throw() :
+        r(), w( numeric<real_t>::one ),
+        v(), m( numeric<real_t>::one ),
+        a(),
+        id(0)
+        {
+        }
+        
+        
+    }
+}
--- trunk/src/y-gems/yocto/gems/particle.hpp
+++ trunk/src/y-gems/yocto/gems/particle.hpp
@@ -1,31 +1,27 @@
 #ifndef YOCTO_GEMS_PARTICLE_INCLUDED
 #define YOCTO_GEMS_PARTICLE_INCLUDED 1
 
-#include "yocto/gems/v4d.hpp"
+#include "yocto/gems/types.hpp"
+#include "yocto/math/v3d.hpp"
 
 namespace yocto
 {
     namespace gems
     {
      
+        using namespace math;
+        
         template <typename T>
         class particle
         {
         public:
-            v4d<T> r; //!< position + 1/mass
-            v4d<T> v; //!< velocity + mass
-            v3d<T> a; //!< acceleration/force accumulator
+            v3d<T> r; T w;
+            v3d<T> v; T m;
+            v3d<T> a;
+            uint_t id;
             
-            particle( T mass, bool is_mobile = true ) throw() :
-            r(0,0,0,is_mobile ? 1/mass : 0 ),
-            v(0,0,0,mass),
-            a(0,0,0,0)
-            {
-            }
-            
-            ~particle() throw()
-            {
-            }
+             particle() throw();
+            ~particle() throw();
             
         };
     }
