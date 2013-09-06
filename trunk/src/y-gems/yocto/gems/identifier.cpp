5e6763bb823905dbb04207b06f72d276c6c1cfd0
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-09-06 08:36:29 +0000

    factorized gems::identifier

---
+++ trunk/src/y-gems/yocto/gems/identifier.cpp
@@ -0,0 +1,26 @@
+#include "yocto/gems/identifier.hpp"
+
+namespace yocto
+{
+    namespace gems
+    {
+     
+        identifier :: ~identifier() throw()
+        {
+        }
+        
+        
+        identifier:: identifier( word_t u, word_t t) throw() :
+        uuid(u),
+        type(t)
+        {
+        }
+        
+        
+        const word_t & identifier:: key() const throw()
+        {
+            return uuid;
+        }
+        
+    }
+}
---
+++ trunk/src/y-gems/yocto/gems/identifier.hpp
@@ -0,0 +1,32 @@
+#ifndef YOCTO_GEMS_IDENTIFIER_INCLUDED
+#define YOCTO_GEMS_IDENTIFIER_INCLUDED 1
+
+#include "yocto/gems/types.hpp"
+#include "yocto/counted.hpp"
+#include "yocto/object.hpp"
+
+namespace yocto
+{
+    namespace gems
+    {
+        
+        class identifier : public object, public counted
+        {
+        public:
+            const word_t uuid;
+            const word_t type;
+            
+            explicit identifier( word_t u, word_t t) throw();
+            virtual ~identifier() throw();
+            
+            const word_t & key() const throw(); //!< uuid
+            
+        private:
+            YOCTO_DISABLE_COPY_AND_ASSIGN(identifier);
+        };
+        
+    }
+    
+}
+
+#endif
