96930bef9e75de18be3165529c8566907af963bb
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-09-06 08:29:13 +0000

    update gems::atom

--- src/y-gems/yocto/gems/atom.hpp
+++ src/y-gems/yocto/gems/atom.hpp
@@ -3,6 +3,8 @@
 
 #include "yocto/gems/types.hpp"
 #include "yocto/math/v3d.hpp"
+#include "yocto/intrusive-ptr.hpp"
+#include "yocto/counted.hpp"
 
 namespace yocto
 {
@@ -10,8 +12,29 @@ namespace yocto
     {
         using namespace math;
         
-        class atom : public counted_object
+        template <typename T>
+        class atom : public object, public counted
         {
+        public:
+            typedef intrusive_ptr<word_t,atom> ptr;
+            
+            const word_t uuid;
+            const word_t type;
+            v3d<T>       r;
+            v3d<T>       v;
+            v3d<T>       a;
+            const T      m; //!< mass
+            const T      w; //!< 1/mass
+            
+            explicit atom(word_t u, word_t t) throw();
+            virtual ~atom() throw();
+            void     set_mass(T mass) throw();
+            
+            const word_t & key() const throw();
+            
+        private:
+            atom& operator=(const atom &);
+            atom(const atom &other) throw();
             
         };
         
