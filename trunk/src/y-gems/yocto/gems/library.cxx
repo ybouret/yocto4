ae13296ba2a7d4f07baeb958805e1e9a03ac8c08
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-09-09 07:33:35 +0000

    updated gems::lib, need to rethink....

--- trunk/src/y-gems/yocto/gems/library.cxx
+++ trunk/src/y-gems/yocto/gems/library.cxx
@@ -1,5 +1,6 @@
 #include "yocto/math/ztype.hpp"
 #include "yocto/gems/library.hpp"
+#include "yocto/exception.hpp"
 
 namespace yocto
 {
@@ -9,6 +10,41 @@ namespace yocto
         template <>
         library<real_t>:: ~library() throw() {}
         
+        template <>
+        library<real_t>:: library() : atoms(), residues()
+        {
+        }
+        
+        template <>
+        library<real_t>::res_properties & library<real_t>:: declare_residue( word_t type, const string &name )
+        {
+            res_properties::ptr p( new res_properties(type,name) );
+            if( !residues.insert(p) ) throw exception("multiple residue type");
+            return *p;
+        }
+        
+        
+        template <>
+        library<real_t>::res_properties & library<real_t>:: declare_residue( word_t type, const char *name )
+        {
+            const string ID(name);
+            return declare_residue(type, ID);
+        }
+        
+        template <>
+        library<real_t>::atm_properties & library<real_t>:: declare_atom( word_t type, const string &name, real_t mass)
+        {
+            atm_properties::ptr p( new atm_properties(type,name,mass) );
+            if( !atoms.insert(p) ) throw exception("multiple atom type");
+            return *p;
+        }
+        
+        template <>
+        library<real_t>::atm_properties & library<real_t>:: declare_atom( word_t type, const char *name, real_t mass)
+        {
+            const string ID(name);
+            return declare_atom(type, ID, mass);
+        }
         
         
     }
--- trunk/src/y-gems/yocto/gems/library.hpp
+++ trunk/src/y-gems/yocto/gems/library.hpp
@@ -2,6 +2,7 @@
 #define YOCTO_GEMS_LIBRARY_INCLUDED 1
 
 #include "yocto/gems/residue.hpp"
+#include "yocto/associative/set.hpp"
 
 namespace yocto
 {
@@ -15,6 +16,21 @@ namespace yocto
             explicit library();
             virtual ~library() throw();
             
+            typedef typename atom<T>::properties    atm_properties;
+            typedef typename residue<T>::properties res_properties;
+            
+            typedef set<word_t,typename atm_properties::ptr,key_hasher<word_t,hashing::sfh>,allocator>    atmlib;
+            typedef set<word_t,typename res_properties::ptr,key_hasher<word_t,hashing::sfh>,allocator> reslib;
+
+            atmlib atoms;
+            reslib residues;
+            
+            res_properties & declare_residue( word_t type, const string &name );
+            res_properties & declare_residue( word_t type, const char   *name );
+
+            atm_properties & declare_atom(word_t type, const string &name, T mass );
+            atm_properties & declare_atom(word_t type, const char   *name, T mass );
+
         private:
             YOCTO_DISABLE_COPY_AND_ASSIGN(library);
         };
