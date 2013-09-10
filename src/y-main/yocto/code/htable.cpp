60b71d285faf4ae2e9aee2da26a2ea230c58e614
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-09-10 10:04:42 +0000

    htable metrics

---
+++ src/y-main/yocto/code/htable.cpp
@@ -0,0 +1,45 @@
+#include "yocto/code/htable.hpp"
+#include "yocto/exceptions.hpp"
+#include "yocto/code/primes.hpp"
+#include "yocto/code/utils.hpp"
+#include <cerrno>
+
+namespace yocto
+{
+    size_t htable:: compute_slots_for(size_t &items)
+    {
+        //----------------------------------------------------------------------
+        // compute the minimum #items according to specs
+        //----------------------------------------------------------------------
+        static const size_t min_items = min_slots * load_factor;
+        if(items<=0) return 0;
+        
+        //----------------------------------------------------------------------
+        // initialize #nodes
+        //----------------------------------------------------------------------
+        size_t nodes = max_of(min_items,items);
+        
+        //----------------------------------------------------------------------
+        // adjust to load_factor
+        //----------------------------------------------------------------------
+        while( 0 != ( nodes % load_factor) )
+            if( ++nodes < items ) throw libc::exception( ERANGE, "htable: #nodes overflow, level-1" );
+        
+        //----------------------------------------------------------------------
+        // get the corresponding prime size
+        //----------------------------------------------------------------------
+        size_t slots = next_prime( nodes/load_factor );
+        if(slots<min_slots) throw libc::exception( ERANGE, "htable: #slots overflow" );
+        
+        //----------------------------------------------------------------------
+        // recompute the effective #nodes
+        //----------------------------------------------------------------------
+        nodes = slots * load_factor;
+        if( nodes < items )
+            throw libc::exception( ERANGE, "htable: #nodes overflow, level-2");
+        
+        items = nodes;
+        return slots;
+    }
+    
+}
---
+++ src/y-main/yocto/code/htable.hpp
@@ -0,0 +1,22 @@
+#ifndef YOCTO_CODE_HTABLE_INCLUDED
+#define YOCTO_CODE_HTABLE_INCLUDED 1
+
+#include "yocto/os.hpp"
+
+namespace yocto
+{
+    
+    struct htable
+    {
+        static const size_t load_factor = 4;
+        static const size_t min_slots   = 3;
+        
+        static size_t compute_slots_for( size_t &items );
+        
+    };
+    
+}
+
+
+#endif
+
