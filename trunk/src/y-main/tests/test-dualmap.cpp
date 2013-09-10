84d116f5c24ef69878e50e0d508c7c5cdcacb1db
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-09-10 13:58:22 +0000

    update dualmap

--- trunk/src/y-main/tests/test-dualmap.cpp
+++ trunk/src/y-main/tests/test-dualmap.cpp
@@ -1,10 +1,17 @@
 #include "yocto/associative/dualmap.hpp"
 #include "yocto/utest/run.hpp"
+#include "yocto/string.hpp"
 
 using namespace yocto;
 
 YOCTO_UNIT_TEST_IMPL(dualmap)
 {
-
+    dualmap<int, string, float> dm;
+    
+    const string s = "toto";
+    
+    (void) dm.get_key_of(s);
+    (void) dm.get_sub_of(10);
+    
 }
 YOCTO_UNIT_TEST_DONE()
