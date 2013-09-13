9cb1e81d1ec641f77f43e2df71c8a6e0648caf7e
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-09-13 14:49:57 +0000

    using gems:: properties:: table

--- src/y-gems/yocto/gems/properties.cpp
+++ src/y-gems/yocto/gems/properties.cpp
@@ -1,4 +1,5 @@
 #include "yocto/gems/properties.hpp"
+#include "yocto/exception.hpp"
 
 namespace yocto
 {
@@ -19,6 +20,16 @@ namespace yocto
         
         const word_t & properties:: key()    const throw() { return type; }
         const string & properties:: subkey() const throw() { return name; }
+     
+        void properties:: check_pointer(const void *addr, const string &id)
+        {
+            if(!addr) throw exception("unregistered name='%s'", id.c_str());
+        }
+        
+        void properties:: check_pointer(const void *addr, word_t id)
+        {
+            if(!addr) throw exception("unregistered type=0x%08x", unsigned(id) );
+        }
         
     }
     
