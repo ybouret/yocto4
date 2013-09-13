567e236e23a11d1b098129702fc6257aab417338
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-09-13 11:43:15 +0000

    added nosy-ptr to dual set

--- src/y-main/tests/test-dualset.cpp
+++ src/y-main/tests/test-dualset.cpp
@@ -6,10 +6,11 @@
 #include "yocto/sequence/vector.hpp"
 
 #include "yocto/nosy-ptr.hpp"
+#include "yocto/counted.hpp"
 
 using namespace yocto;
 
-class Dummy
+class Dummy : public object, public counted
 {
 public:
     explicit Dummy( int a, const string &b , float x) :
@@ -90,17 +91,63 @@ YOCTO_UNIT_TEST_IMPL(dualset)
         else
         {
             if( !ds1.sub_remove(subs[i])) throw exception("missing remove subkey");
-
+            
         }
     }
     
     
-
+    
 }
 YOCTO_UNIT_TEST_DONE()
 
 YOCTO_UNIT_TEST_IMPL(dualset2)
 {
+    typedef dual_set<int, string, Dummy::Ptr> ds_t;
+    
+    
+    ds_t ds1;
+    
+    for(size_t i= 100 + alea_leq(100); i>0;--i)
+    {
+        const int    a = gen<int>::get();
+        const string b = gen<string>::get();
+        const float  x = gen<float>::get();
+        const Dummy::Ptr  d( new Dummy(a,b,x) );
+        if( !ds1.insert(d) )
+            std::cerr << "multiple dual " << a << "," <<b << std::endl;
+    }
+    
+    std::cerr << "size     = " << ds1.size()      << std::endl;
+    std::cerr << "capacity = " << ds1.capacity()  << std::endl;
+    std::cerr << "slots    = " << ds1.num_slots() << std::endl;
+    std::cerr << "bytes    = " << ds1.allocated_bytes() << std::endl;
+    vector<int>    keys;
+    vector<string> subs;
+    
+    for( ds_t::iterator i=ds1.begin(); i != ds1.end(); ++i )
+    {
+        keys.push_back( i->key() );
+        subs.push_back( i->subkey() );
+    }
+    
+    for(size_t i=1; i <= keys.size(); ++i )
+    {
+        if(!ds1.search(keys[i])) throw exception("missing key");
+        if(!ds1.sub_search(subs[i])) throw exception("missing subkey");
+    }
+    
+    for(size_t i=1; i <= keys.size(); ++i )
+    {
+        if( (i%2) )
+        {
+            if( !ds1.remove(keys[i])) throw exception("missing remove key");
+        }
+        else
+        {
+            if( !ds1.sub_remove(subs[i])) throw exception("missing remove subkey");
+            
+        }
+    }
     
     
 }
