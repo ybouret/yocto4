c1e9637627caa5fa542aba5f8de63423b2ee3176
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-09-18 12:52:40 +0000

    multi_map first insert

--- trunk/src/y-main/tests/test-multi-map.cpp
+++ trunk/src/y-main/tests/test-multi-map.cpp
@@ -4,6 +4,8 @@
 #include "yocto/code/primes.hpp"
 #include "yocto/code/rand.hpp"
 #include "yocto/sort/heap.hpp"
+#include "yocto/string.hpp"
+#include "support.hpp"
 
 using namespace yocto;
 
@@ -15,7 +17,7 @@ YOCTO_UNIT_TEST_IMPL(multi_map)
         const size_t q = prev_prime(n-1);
         indx.free();
         size_t       hkey   =  alea_leq(0xFFFFFFFF);
-        std::cerr << "Build with " << n << "/" << q << " : hkey=" << hkey << std::endl;
+        
         for(size_t i=0;i<n;++i)
         {
             const size_t j = hkey % n;
@@ -24,10 +26,20 @@ YOCTO_UNIT_TEST_IMPL(multi_map)
         }
         assert(indx.size()==n);
         hsort(indx);
-        //std::cerr << "indx=" << indx << std::endl;
         for(size_t i=1;i<=n;++i)
         {
             if(indx[i]!=i) throw exception("invalid index for n=%u", unsigned(n));
+        }
+    }
+    
+    multi_map<string,string> thesaurus;
+    for( size_t g=3+alea_leq(10); g>0; --g )
+    {
+        const string key = gen<string>::get();
+        for(size_t i=1+alea_leq(10);i>0;--i)
+        {
+            const string str = gen<string>::get();
+            thesaurus.insert(key,str);
         }
     }
 }
