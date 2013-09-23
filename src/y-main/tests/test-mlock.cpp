d04377136541aa5f873cb5d430f8f58f5aa24e66
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-09-23 09:32:46 +0000

    mlock for win32 seems ok

--- src/y-main/tests/test-mlock.cpp
+++ src/y-main/tests/test-mlock.cpp
@@ -1,18 +1,25 @@
 #include "yocto/utest/run.hpp"
 #include "yocto/memory/locked.hpp"
-
+#include <cstdlib>
 
 using namespace yocto;
 
 YOCTO_UNIT_TEST_IMPL(mlock)
 {
+	
     memory::locked &vmem = memory::locked::instance();
     std::cerr << "using " << vmem.name << std::endl;
     std::cerr << "page size=" << vmem.page_size << std::endl;
     size_t n = 1;
     void  *addr = vmem.acquire(n);
     std::cerr << "locked @" << addr << " +" << n << std::endl;
-    vmem.release(addr, n);
+    int         *p = (int *)addr;
+	const size_t m = n/sizeof(*p);
+	for(size_t i=0;i<m;++i)
+	{
+		p[i] = rand();
+	}
+	vmem.release(addr, n);
     
 }
 YOCTO_UNIT_TEST_DONE()
