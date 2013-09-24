9303b5413029c162a4e07726183b01be931d22fa
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-09-24 14:20:44 +0000

    need asynchronous encryption

--- trunk/src/y-kr/tests/test-ios.cpp
+++ trunk/src/y-kr/tests/test-ios.cpp
@@ -1,17 +1,38 @@
 #include "yocto/utest/run.hpp"
 #include "yocto/crypto/ios/encrypter.hpp"
 #include "yocto/crypto/ios/decrypter.hpp"
+#include "yocto/ios/imstream.hpp"
+#include "yocto/ios/osstream.hpp"
 
 using namespace yocto;
+
+static inline
+void __encrypt( ios::ostream &target, ios::istream &source, crypto::ios_cipher &enc)
+{
+    enc.restart();
+    enc.process(target,source,0);
+}
 
 YOCTO_UNIT_TEST_IMPL(ios)
 {
     const string key = "toto";
     crypto::ios_encrypter e1( crypto::ios128, "ecb", key);
     crypto::ios_decrypter d1( crypto::ios128, "ecb", key);
-
+    
     crypto::ios_encrypter e2( crypto::ios128, "cbc", key);
     crypto::ios_decrypter d2( crypto::ios128, "cbc", key);
-
+    
+    string       src = "QWERTYUIOPASDFGHJKLZXCVBNM";
+    src += src;
+    string       tgt;
+    
+    {
+        ios::imstream source(src);
+        tgt.clear();
+        ios::osstream target(tgt);
+        __encrypt(target, source, e1);
+        tgt.output_visible(std::cerr) << std::endl;
+    }
+    
 }
 YOCTO_UNIT_TEST_DONE()
