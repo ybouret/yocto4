ba8ce446591a7d676fe650efd2b2c8f1001a4da1
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-09-24 10:06:06 +0000

    pages ok

--- src/y-main/yocto/memory/page.cpp
+++ src/y-main/yocto/memory/page.cpp
@@ -4,6 +4,8 @@
 #include "yocto/code/utils.hpp"
 #include <cstring>
 
+#include <iostream>
+
 namespace yocto
 {
     namespace memory
@@ -19,9 +21,17 @@ namespace yocto
             return n;
         }
         
+        bool page:: is_empty(void) const throw()
+        {
+            return 0 == head->from && head->next == tail;
+        }
+
+        
+        
         page:: page( size_t nbytes ) :
+        safe(false),
         nblk( 2 + __rpbs(nbytes)/block_size ),
-        head( locked::instance().acquire_as<block_t>(nblk) ),
+        head( locked::instance().try_acquire_as<block_t>(nblk,(bool&)safe) ),
         tail( &head[nblk]-1 ),
         next(0),
         prev(0)
@@ -29,13 +39,17 @@ namespace yocto
             assert(nblk>2);
             
             //------------------------------------------------------------------
-            // format
+            // format head
             //------------------------------------------------------------------
             head->prev = 0;
             head->next = tail;
             head->from = 0;
             head->size = (nblk-2) * block_size;
             
+            assert(head->size == block_size * (static_cast<ptrdiff_t>(tail-head)-1));
+            //------------------------------------------------------------------
+            // format tail
+            //------------------------------------------------------------------
             tail->prev = head;
             tail->next = 0;
             tail->from = this;
@@ -45,6 +59,92 @@ namespace yocto
         page:: ~page() throw()
         {
             locked::location()->release_as<block_t>(head, nblk);
+        }
+        
+        void *page:: acquire(size_t &n) throw()
+        {
+            block_t *blk = head;
+            assert(blk!=NULL);
+            while(blk!=tail)
+            {
+                assert(blk->next!=0);
+                if(0 == blk->from && blk->size>=n)
+                {
+                    //==========================================================
+                    // found a block !
+                    //==========================================================
+                    const size_t usr_aligned = __rpbs(n);
+                    const size_t usr_nblocks = usr_aligned/block_size;
+                    const size_t blk_aligned = blk->size;
+                    const size_t blk_nblocks = blk_aligned/block_size;
+                    //std::cerr << "usr_aligned=" << usr_aligned << "/#blk=" << usr_nblocks  << std::endl;
+                    //std::cerr << "blk_aligned=" << blk_aligned << "/#blk=" << blk_nblocks  << std::endl;
+                    const size_t split_limit = usr_nblocks+1;
+                    if( blk_nblocks>split_limit)
+                    {
+                        //std::cerr  << "should split !" << std::endl;
+                        
+                        block_t *next = blk->next; assert(next!=0);
+                        block_t *dest = blk+split_limit;
+                        blk->next  = dest;
+                        dest->prev = blk;
+                        dest->next = next;
+                        dest->from = 0;     // mark as free
+                        next->prev = dest;
+                        
+                        blk->size  = usr_aligned;
+                        dest->size = block_size*(blk_nblocks-split_limit);
+                        assert( (static_cast<ptrdiff_t>(dest->next-dest)-1) * block_size == dest->size );
+                        //std::cerr << "blk size=" << blk->size  << std::endl;
+                        //std::cerr << "new size=" << dest->size <<  std::endl;
+                    }
+                    //else std::cerr << "should keep !" << std::endl;
+                    
+                    blk->from = this;
+                    n         = blk->size;
+                    void   *p = &blk[1];
+                    
+                    memset(p,0,n);
+                    return p;
+                }
+                
+                blk = blk->next;
+            }
+            return 0;
+        }
+        
+        page *page:: release( void *p ) throw()
+        {
+            assert(p);
+            block_t *blk = static_cast<block_t *>(p) - 1;
+            assert(blk->from); // must be allocated
+            assert(blk->next); // block can't be a tail
+            page    *P   = blk->from;
+            
+            blk->from = 0;
+            if(blk->next->from == 0 )
+            {
+                //std::cerr << "\t\t##MERGE NEXT" << std::endl;
+                __merge_next_of(blk);
+            }
+            if(blk->prev&&0==blk->prev->from)
+            {
+                //std::cerr << "\t\t##MERGE PREV" << std::endl;
+                __merge_next_of(blk->prev);
+            }
+            return P;
+        }
+
+        void page:: __merge_next_of( block_t *blk ) throw()
+        {
+            assert(blk);
+            assert(0==blk->from);
+            assert(blk->next);
+            assert(0==blk->next->from);
+            assert(blk->next->next!=0);
+            blk->next = blk->next->next;
+            blk->next->prev = blk;
+            blk->size = (static_cast<ptrdiff_t>(blk->next-blk)-1) * block_size;
         }
         
     }
--- src/y-main/yocto/memory/page.hpp
+++ src/y-main/yocto/memory/page.hpp
@@ -12,6 +12,8 @@ namespace yocto
         class page
         {
         public:
+            const bool safe;
+
             YOCTO_MAKE_OBJECT
             
             page(size_t nbytes);
@@ -21,17 +23,24 @@ namespace yocto
             {
                 block_t *prev;
                 block_t *next;
-                size_t   size;
                 page    *from;
+                size_t   size;
             };
             static const size_t block_size = sizeof(block_t);
+            
+            
+            void        *acquire( size_t &n ) throw();
+            static page *release( void *p ) throw();
+            bool         is_empty(void) const throw();
             
         private:
             YOCTO_DISABLE_COPY_AND_ASSIGN(page);
             size_t         nblk;
             block_t       *head;
             block_t       *tail;
-                        
+            
+            static void __merge_next_of( block_t *blk ) throw();
+            
         public:
             page *next;
             page *prev;
