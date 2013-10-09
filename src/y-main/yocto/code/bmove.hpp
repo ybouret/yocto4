804340830ec3fc6b15ceea07f286cd3a3b05b898
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-10-09 12:52:02 +0000

    speed up bmove

--- src/y-main/yocto/code/bmove.hpp
+++ src/y-main/yocto/code/bmove.hpp
@@ -55,6 +55,68 @@ typedef unsigned_int<N>::type word_t;\
             tgt[2] = src[2];
         }
         
+        
+        template <> inline void bmove<32>(void *a, const void *b) throw()
+        {
+            uint64_t       *tgt = (uint64_t *)a;
+            const uint64_t *src = (const uint64_t *)b;
+            tgt[0] = src[0];
+            tgt[1] = src[1];
+            tgt[2] = src[2];
+            tgt[3] = src[3];
+        }
+        
+        template <> inline void bmove<40>(void *a, const void *b) throw()
+        {
+            uint64_t       *tgt = (uint64_t *)a;
+            const uint64_t *src = (const uint64_t *)b;
+            tgt[0] = src[0];
+            tgt[1] = src[1];
+            tgt[2] = src[2];
+            tgt[3] = src[3];
+            tgt[4] = src[4];
+        }
+        
+        template <> inline void bmove<48>(void *a, const void *b) throw()
+        {
+            uint64_t       *tgt = (uint64_t *)a;
+            const uint64_t *src = (const uint64_t *)b;
+            tgt[0] = src[0];
+            tgt[1] = src[1];
+            tgt[2] = src[2];
+            tgt[3] = src[3];
+            tgt[4] = src[4];
+            tgt[5] = src[5];
+        }
+        
+        template <> inline void bmove<56>(void *a, const void *b) throw()
+        {
+            uint64_t       *tgt = (uint64_t *)a;
+            const uint64_t *src = (const uint64_t *)b;
+            tgt[0] = src[0];
+            tgt[1] = src[1];
+            tgt[2] = src[2];
+            tgt[3] = src[3];
+            tgt[4] = src[4];
+            tgt[5] = src[5];
+            tgt[6] = src[6];
+        }
+        
+        template <> inline void bmove<64>(void *a, const void *b) throw()
+        {
+            uint64_t       *tgt = (uint64_t *)a;
+            const uint64_t *src = (const uint64_t *)b;
+            tgt[0] = src[0];
+            tgt[1] = src[1];
+            tgt[2] = src[2];
+            tgt[3] = src[3];
+            tgt[4] = src[4];
+            tgt[5] = src[5];
+            tgt[6] = src[6];
+            tgt[7] = src[7];
+        }
+        
+        
     }
     
     template <typename T>
