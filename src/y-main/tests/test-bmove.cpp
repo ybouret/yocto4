d4e0a9817c1aee4f91a7c1c22ed567d69d4441b2
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-10-09 11:31:51 +0000

    initial bmove commit

---
+++ src/y-main/tests/test-bmove.cpp
@@ -0,0 +1,79 @@
+#include "yocto/utest/run.hpp"
+#include "yocto/code/bmove.hpp"
+#include "yocto/math/complex.hpp"
+#include "yocto/sys/wtime.hpp"
+#include "yocto/ios/ocstream.hpp"
+
+using namespace yocto;
+using namespace math;
+
+#define ITER_MAX (1024*1024)
+#define Y_BMOVE_TMX(N)  do {                         \
+const double ini = chrono.query();                   \
+for(int i=0;i<ITER_MAX;++i) core::bmove<N>(arr,brr); \
+tmx[N-1] = 1e-6 * (ITER_MAX/(chrono.query() - ini)); \
+std::cerr << "#" << N << " => " << tmx[N-1] << " Mmov/s" << std::endl;\
+ios::ocstream fp("mov.dat",true); fp("%g %g\n", double(N), tmx[N-1] ); \
+} while(false)
+
+YOCTO_UNIT_TEST_IMPL(bmove)
+{
+    
+    
+    uint8_t arr[64] = { 0 };
+    uint8_t brr[64] = { 0 };
+    double  tmx[64] = { 0 };
+    
+    wtime chrono;
+    chrono.start();
+    
+    ios::ocstream::overwrite("mov.dat");
+    /*         */    Y_BMOVE_TMX(1);  Y_BMOVE_TMX(2);  Y_BMOVE_TMX(3);  Y_BMOVE_TMX(4);  Y_BMOVE_TMX(5);  Y_BMOVE_TMX(6);  Y_BMOVE_TMX(7);  Y_BMOVE_TMX(8);  Y_BMOVE_TMX(9);
+    Y_BMOVE_TMX(10); Y_BMOVE_TMX(11); Y_BMOVE_TMX(12); Y_BMOVE_TMX(13); Y_BMOVE_TMX(14); Y_BMOVE_TMX(15); Y_BMOVE_TMX(16); Y_BMOVE_TMX(17); Y_BMOVE_TMX(18); Y_BMOVE_TMX(19);
+    Y_BMOVE_TMX(20); Y_BMOVE_TMX(21); Y_BMOVE_TMX(22); Y_BMOVE_TMX(23); Y_BMOVE_TMX(24); Y_BMOVE_TMX(25); Y_BMOVE_TMX(26); Y_BMOVE_TMX(27); Y_BMOVE_TMX(28); Y_BMOVE_TMX(29);
+    Y_BMOVE_TMX(30); Y_BMOVE_TMX(31); Y_BMOVE_TMX(32); Y_BMOVE_TMX(33); Y_BMOVE_TMX(34); Y_BMOVE_TMX(35); Y_BMOVE_TMX(36); Y_BMOVE_TMX(37); Y_BMOVE_TMX(38); Y_BMOVE_TMX(39);
+    Y_BMOVE_TMX(40); Y_BMOVE_TMX(41); Y_BMOVE_TMX(42); Y_BMOVE_TMX(43); Y_BMOVE_TMX(44); Y_BMOVE_TMX(45); Y_BMOVE_TMX(46); Y_BMOVE_TMX(47); Y_BMOVE_TMX(48); Y_BMOVE_TMX(49);
+    Y_BMOVE_TMX(50); Y_BMOVE_TMX(51); Y_BMOVE_TMX(52); Y_BMOVE_TMX(53); Y_BMOVE_TMX(54); Y_BMOVE_TMX(55); Y_BMOVE_TMX(56); Y_BMOVE_TMX(57); Y_BMOVE_TMX(58); Y_BMOVE_TMX(59);
+    Y_BMOVE_TMX(60); Y_BMOVE_TMX(61); Y_BMOVE_TMX(62); Y_BMOVE_TMX(63); Y_BMOVE_TMX(64);
+    
+    
+    {
+        char a=0;
+        char b='a';
+        bmove(a, b);
+        std::cerr << "a=" << a << std::endl;
+    }
+    
+    {
+        int16_t a=0;
+        int16_t b='a';
+        bmove(a, b);
+        std::cerr << "a=" << a << std::endl;
+    }
+    
+    {
+        uint32_t a=0;
+        uint32_t b='a';
+        bmove(a, b);
+        std::cerr << "a=" << a << std::endl;
+    }
+    
+    {
+        uint32_t a=0;
+        uint32_t b='a';
+        bmove(a, b);
+        std::cerr << "a=" << a << std::endl;
+    }
+    
+    {
+        uint64_t a=0;
+        uint64_t b='a';
+        bmove(a, b);
+        std::cerr << "a=" << a << std::endl;
+    }
+    
+    
+    
+    
+}
+YOCTO_UNIT_TEST_DONE()
