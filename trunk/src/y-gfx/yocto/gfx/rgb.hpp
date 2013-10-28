b0abb8e07db81eb9323fd0f18cc83f78ce86e075
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-10-28 13:53:29 +0000

    gfx/rgb

---
+++ trunk/src/y-gfx/yocto/gfx/rgb.hpp
@@ -0,0 +1,72 @@
+#ifndef YOCTO_GFX_RGB_INCLUDED
+#define YOCTO_GFX_RGB_INCLUDED 1
+
+#include "yocto/gfx/types.hpp"
+#include "yocto/container/sequence.hpp"
+
+namespace yocto
+{
+    namespace gfx
+    {
+        template <typename T>
+        struct alpha_max;
+        
+        template <>
+        struct alpha_max<uint8_t>
+        {
+            enum { value = 0xff };
+        };
+        
+        
+        template <>
+        struct alpha_max<float>
+        {
+            enum { value = 1 };
+        };
+
+        //! generic rbg(a)
+        template <typename T>
+        class rgb
+        {
+        public:
+            T r,g,b,a;
+            
+            inline rgb() throw() :
+            r(0),g(0),b(0),a( alpha_max<T>::value ) {}
+            
+            inline rgb( T R, T G, T B, T A ) throw() :
+            r(R), g(G), b(B), a(A) {}
+            
+            inline rgb( T R, T G, T B ) throw() :
+            r(R), g(G), b(B), a( alpha_max<T>::value ) {}
+            
+            inline rgb( const rgb &other ) throw() :
+            r(other.r),
+            g(other.g),
+            b(other.b),
+            a(other.a)
+            {
+            }
+            inline rgb & operator=( const rgb &other ) throw()
+            {
+                r = other.r;
+                g = other.g;
+                b = other.b;
+                a = other.a;
+                return *this;
+            }
+            
+            inline ~rgb() throw() {}
+            
+        };
+        
+        typedef rgb<uint8_t>  rgb_t;
+        typedef rgb<uint32_t> rgb_mask_t;
+        
+        rgb_t get_named_rgb(const string &id);
+        rgb_t get_named_rgb(const char   *id);
+        void  get_named_rgb(sequence<string> &names);
+    }
+}
+
+#endif
