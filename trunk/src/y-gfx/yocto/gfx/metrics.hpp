764f6f6753993eec5d5c02fb33a062a538d74b74
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-10-29 07:38:02 +0000

    added metrics for pixel_t

---
+++ trunk/src/y-gfx/yocto/gfx/metrics.hpp
@@ -0,0 +1,38 @@
+#ifndef YOCTO_GFX_METRICS_INCLUDED
+#define YOCTO_GFX_METRICS_INCLUDED 1
+
+#include "yocto/gfx/types.hpp"
+
+namespace yocto
+{
+    namespace gfx
+    {
+        //! common color_format/pixel_buffer info
+        class metrics
+        {
+        public:
+            explicit metrics( size_t Depth );
+            virtual ~metrics() throw();
+            metrics( const metrics &) throw();
+            
+            const size_t  depth;
+            
+            pixel_t     (*get_pixel)(const void *);
+            void        (*put_pixel)(void *,pixel_t);
+            
+            static void fmt(const uint32_t  mask,
+                            uint8_t       &bits,
+                            uint8_t       &shift,
+                            uint8_t       &loss,
+                            const char    *field);
+            
+        private:
+            YOCTO_DISABLE_ASSIGN(metrics);
+        };
+        
+        
+    }
+}
+
+#endif
+
