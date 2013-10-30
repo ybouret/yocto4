339b8bba90563a1c40c3074dee1df82b547957ad
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-10-30 12:40:21 +0000

    gfx surface WIP

--- trunk/src/y-gfx/yocto/gfx/surface.hpp
+++ trunk/src/y-gfx/yocto/gfx/surface.hpp
@@ -11,13 +11,31 @@ namespace yocto
         class surface : public format, public bitmap
         {
         public:
+            typedef arc_ptr<surface> pointer;
+            
+            class row
+            {
+            public:
+                row(void *addr, unit_t W) throw();
+                const unit_t w;
+                
+            private:
+                void *p; // row entry address
+                YOCTO_DISABLE_COPY_AND_ASSIGN(row);
+                ~row() throw();
+            };
+            
             static surface *create( const format fmt, unit_t W, unit_t H);
             virtual ~surface() throw();
             
         private:
             explicit surface( const format &fmt, unit_t W, unit_t H);
-
+            row   *rows;
+            size_t nrow;
             YOCTO_DISABLE_COPY_AND_ASSIGN(surface);
+            
+            void create_rows();
+            void delete_rows() throw();
             
         };
     }
