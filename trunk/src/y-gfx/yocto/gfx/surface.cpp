0b7f2e84344cc05fb77e9d29400c7febfa733e03
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-10-30 12:24:50 +0000

    initial surface

--- trunk/src/y-gfx/yocto/gfx/surface.cpp
+++ trunk/src/y-gfx/yocto/gfx/surface.cpp
@@ -5,6 +5,21 @@ namespace yocto
     namespace gfx
     {
         
+        surface * surface:: create(const format fmt, unit_t W, unit_t H)
+        {
+            return new surface(fmt,W,H);
+        }
+        
+        surface:: surface( const format &fmt, unit_t W, unit_t H ) :
+        format(fmt),
+        bitmap(fmt.bytes,W,H)
+        {
+        }
+        
+        surface:: ~surface() throw()
+        {
+        }
+        
     }
     
 }
--- trunk/src/y-gfx/yocto/gfx/surface.hpp
+++ trunk/src/y-gfx/yocto/gfx/surface.hpp
@@ -2,12 +2,24 @@
 #define YOCTO_GFX_SURFACE_INCLUDED 1
 
 #include "yocto/gfx/bitmap.hpp"
+#include "yocto/gfx/format.hpp"
 
 namespace yocto
 {
     namespace gfx
     {
-        
+        class surface : public format, public bitmap
+        {
+        public:
+            static surface *create( const format fmt, unit_t W, unit_t H);
+            virtual ~surface() throw();
+            
+        private:
+            explicit surface( const format &fmt, unit_t W, unit_t H);
+
+            YOCTO_DISABLE_COPY_AND_ASSIGN(surface);
+            
+        };
     }
     
 }
