14bc9e7b217e63583498cecda306bbc6a1bebf95
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-10-30 10:33:53 +0000

    start gfx surface

---
+++ src/y-gfx/yocto/gfx/surface.hpp
@@ -0,0 +1,16 @@
+#ifndef YOCTO_GFX_SURFACE_INCLUDED
+#define YOCTO_GFX_SURFACE_INCLUDED 1
+
+#include "yocto/gfx/bitmap.hpp"
+
+namespace yocto
+{
+    namespace gfx
+    {
+        
+    }
+    
+}
+
+#endif
+
