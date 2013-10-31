2e6b2d81e22bd1a1ac3e6af659fe8fe867559d99
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-10-31 10:36:32 +0000

    added bitmap32, to remove...

---
+++ src/y-gfx/yocto/gfx/bitmap32.hpp
@@ -0,0 +1,15 @@
+#ifndef YOCTO_GFX_BITMAP32_INCLUDED
+#define YOCTO_GFX_BITMAP32_INCLUDED 1
+
+#include "yocto/gfx/pixmap.hpp"
+#include "yocto/gfx/rgb.hpp"
+
+namespace yocto
+{
+    namespace gfx
+    {
+       
+    }
+}
+
+#endif
