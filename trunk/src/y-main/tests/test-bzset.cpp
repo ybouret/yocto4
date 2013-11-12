b980f3770b72dbac5d8da85f13853bad88838be5
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-11-12 21:15:44 +0000

    ok, performance check...

--- trunk/src/y-main/tests/test-bzset.cpp
+++ trunk/src/y-main/tests/test-bzset.cpp
@@ -6,26 +6,17 @@
 
 using namespace yocto;
 
-#define ITER_MAX (32*1024*1024)
-
-#define Y_BZSET_TMX(N) do { \
-for(size_t i=0;i<sizeof(arr)/sizeof(arr[0]);++i) arr[i] = uint8_t(i+1); \
-const double ini  = chrono.query();                  \
-const size_t iter = ITER_MAX/N;                      \
-for(size_t i=iter;i>0;--i) { core::bzset<N>(arr); }  \
-const double tmx = chrono.query() - ini;             \
-const double spd = (iter/tmx);                       \
-ios::ocstream fp("ldz.dat",true);                    \
-for(unsigned i=0;i<N;++i) if(0!=arr[i]) throw exception("invalid bzset<%u>@%u\n", unsigned(N), i);\
-for(size_t i=0;i<sizeof(arr)/sizeof(arr[0]);++i) arr[i] = uint8_t(i+1); \
-const double ini2 = chrono.query();                   \
-for(size_t i=iter;i>0;--i) { memset(arr,0,N); }         \
-const double tmx2 = chrono.query() - ini2;              \
-const double spd2 = (iter/tmx2);                        \
-fp("%u %.5g %.5g\n", unsigned(N), 1e-6*spd, 1e-6*spd2); \
-std::cerr << '.';                                       \
-std::cerr.flush();                                      \
-} while(false)
+#define Y_BZSET_TMX(N) \
+	do{ \
+	for(size_t i=0;i<sizeof(arr)/sizeof(arr[0]);++i) arr[i] = uint8_t(i+1);\
+	YOCTO_PERFORMANCE(speed,duration,core::bzset<N>(arr),chrono);          \
+	for(size_t i=0;i<sizeof(arr)/sizeof(arr[0]);++i) arr[i] = uint8_t(i+1);\
+	YOCTO_PERFORMANCE(speed2,duration,memset(arr,0,N),chrono);             \
+	ios::ocstream fp("ldz.dat",true);                                 \
+	fp("%u %.5g %.5g\n", unsigned(N), 1e-6*speed, 1e-6*speed2);            \
+	std::cerr << '.';                                                      \
+	std::cerr.flush();                                                      \
+	} while(false)
 
 YOCTO_UNIT_TEST_IMPL(bzset)
 {
@@ -34,8 +25,10 @@ YOCTO_UNIT_TEST_IMPL(bzset)
     chrono.start();
     
     ios::ocstream::overwrite("ldz.dat");
-    
-    
+    double speed          = 0;
+	const double duration = 0.05;
+    double speed2         = 0;
+
     /*         */    Y_BZSET_TMX(1);  Y_BZSET_TMX(2);  Y_BZSET_TMX(3);  Y_BZSET_TMX(4);  Y_BZSET_TMX(5);  Y_BZSET_TMX(6);  Y_BZSET_TMX(7);  Y_BZSET_TMX(8);  Y_BZSET_TMX(9);
     Y_BZSET_TMX(10); Y_BZSET_TMX(11); Y_BZSET_TMX(12); Y_BZSET_TMX(13); Y_BZSET_TMX(14); Y_BZSET_TMX(15); Y_BZSET_TMX(16); Y_BZSET_TMX(17); Y_BZSET_TMX(18); Y_BZSET_TMX(19);
     Y_BZSET_TMX(20); Y_BZSET_TMX(21); Y_BZSET_TMX(22); Y_BZSET_TMX(23); Y_BZSET_TMX(24); Y_BZSET_TMX(25); Y_BZSET_TMX(26); Y_BZSET_TMX(27); Y_BZSET_TMX(28); Y_BZSET_TMX(29);
