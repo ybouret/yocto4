49cf2794a7a9d8cad5e468e9e5cc460e5882239f
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-11-12 19:05:41 +0000

    changed wtime implementation

--- src/y-main/yocto/sys/rt-clock.hpp
+++ src/y-main/yocto/sys/rt-clock.hpp
@@ -5,22 +5,21 @@
 
 namespace yocto
 {
-    class rt_clock
-    {
-    public:
-        explicit rt_clock();
-        virtual ~rt_clock() throw();
-        
-        uint64_t ticks();
-        void     calibrate(); //!< called during constructor
-        double   operator()( uint64_t num_ticks) const;
-        
-    private:
-        YOCTO_DISABLE_COPY_AND_ASSIGN(rt_clock);
-        int64_t data[4];
-        
-    };
-    
+	class rt_clock
+	{
+	public:
+		explicit rt_clock();
+		virtual ~rt_clock() throw();
+
+		static uint64_t ticks();     //!< system ticks
+		void            calibrate(); //!< called during constructor, needed to transforms ticks to seconds.
+		double          operator()( uint64_t num_ticks) const;
+
+	private:
+		YOCTO_DISABLE_COPY_AND_ASSIGN(rt_clock);
+		int64_t data[4];
+	};
+
 }
 
 #endif
