af5d9fddbbea8ce2c5db8cc97c9bc35e8199b78c
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2013-11-21 13:51:02 +0000

    Need to find a better Newton's termination in chemical initializer

--- src/chemical/yocto/chemical/collection.hpp
+++ src/chemical/yocto/chemical/collection.hpp
@@ -44,16 +44,16 @@ inline const species::ptr &method() const { return (*this)[name]; }
             //------------------------------------------------------------------
             // species must be declared before use !
             //------------------------------------------------------------------
-            YOCTO_CHEMICAL_NAMED_SPECIES(proton,      "H+")
-            YOCTO_CHEMICAL_NAMED_SPECIES(hydroxy,     "HO-")
-            YOCTO_CHEMICAL_NAMED_SPECIES(chloride,    "Cl-")
-            YOCTO_CHEMICAL_NAMED_SPECIES(sodium,      "Na+")
-            YOCTO_CHEMICAL_NAMED_SPECIES(potassium,   "K+")
-            YOCTO_CHEMICAL_NAMED_SPECIES(CO2,         "CO2")
+            YOCTO_CHEMICAL_NAMED_SPECIES(proton,      "H+"   )
+            YOCTO_CHEMICAL_NAMED_SPECIES(hydroxy,     "HO-"  )
+            YOCTO_CHEMICAL_NAMED_SPECIES(chloride,    "Cl-"  )
+            YOCTO_CHEMICAL_NAMED_SPECIES(sodium,      "Na+"  )
+            YOCTO_CHEMICAL_NAMED_SPECIES(potassium,   "K+"   )
+            YOCTO_CHEMICAL_NAMED_SPECIES(CO2,         "CO2"  )
             YOCTO_CHEMICAL_NAMED_SPECIES(bicarbonate, "HCO3-")
             YOCTO_CHEMICAL_NAMED_SPECIES(carbonate,   "CO3--")
-            YOCTO_CHEMICAL_NAMED_SPECIES(amonium,     "NH4+")
-            YOCTO_CHEMICAL_NAMED_SPECIES(amoniac,     "NH3")
+            YOCTO_CHEMICAL_NAMED_SPECIES(amonium,     "NH4+" )
+            YOCTO_CHEMICAL_NAMED_SPECIES(amoniac,     "NH3"  )
             
             species &add(const string &name); //!< one of the predefined species in ztable
             species &add(const char   *name); //!< one of the predefined species in ztable
