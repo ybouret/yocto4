98bdef57034a54603a5b0c28c16e493653434053
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-01-02 15:43:56 +0000

    upgraded RKCK

---
+++ src/mk/yocto/math/ode/explicit/rkck.cxx
@@ -0,0 +1,92 @@
+#include "yocto/math/ztype.hpp"
+#include "yocto/math/ode/explicit/rkck.hpp"
+
+#include "yocto/math/ode/rkck.hpp"
+#include "yocto/math/ztype.hpp"
+
+namespace yocto
+{
+	namespace math
+	{
+		
+		namespace ode
+		{
+			
+			template <>
+			RKCK<real_t>:: ~RKCK() throw()
+			{
+			}
+			
+			template <>
+			RKCK<real_t>:: RKCK() :
+            Explicit::step<real_t>(6),
+			ytmp( next_array() ),
+			ak2(  next_array()   ),
+			ak3(  next_array()   ),
+			ak4(  next_array()   ),
+			ak5(  next_array()   ),
+			ak6(  next_array()   )
+			{
+			}
+			
+			template <>
+			void RKCK<real_t>:: operator()(array<real_t>        &yout,
+										   array<real_t>        &yerr,
+										   equation             &drvs,
+										   const real_t          x,
+										   const real_t          h,
+										   const array<real_t>  &y,
+										   const array<real_t>  &dydx
+										   )
+			{
+				static const real_t a2  = REAL(0.2), a3=REAL(0.3), a4=REAL(0.6), a5=REAL(1.0), a6=REAL(0.875);
+				static const real_t b21 = REAL(0.2);
+				static const real_t b31 = REAL(3.0)/REAL(40.0),        b32=REAL(9.0)/REAL(40.0);
+				static const real_t b41 = REAL(0.3),                   b42=REAL(-0.9),              b43=REAL(1.2);
+				static const real_t b51 = REAL(-11.0)/REAL(54.0),      b52=REAL(2.5),               b53 = REAL(-70.0)/REAL(27.0),  b54=REAL(35.0)/REAL(27.0);
+				static const real_t b61 = REAL(1631.0)/REAL(55296.0),  b62=REAL(175.0)/REAL(512.0), b63=REAL(575.0)/REAL(13824.0), b64=REAL(44275.0)/REAL(110592.0), b65=(253.0)/REAL(4096.0);
+				static const real_t c1=REAL(37.0)/REAL(378.0), c3=REAL(250.0)/REAL(621.0), c4=REAL(125.0)/REAL(594.0), c6=REAL(512.0)/REAL(1771.0);
+				static const real_t dc1=c1-REAL(2825.0)/REAL(27648.0), dc3=c3-REAL(18575.0)/REAL(48384.0), dc4=c4-REAL(13525.0)/REAL(55296.0), dc5 = REAL(-277.0)/REAL(14336.0), dc6=c6-REAL(0.25);
+				
+				const size_t n = y.size();
+				
+				
+				for (size_t i=n;i>0;--i)
+					ytmp[i]=y[i]+b21*h*dydx[i];
+				
+				drvs(ak2,x+a2*h,ytmp);
+				for (size_t i=n;i>0;--i)
+					ytmp[i]=y[i]+h*(b31*dydx[i]+b32*ak2[i]);
+				
+				drvs(ak3,x+a3*h,ytmp);
+				
+				for (size_t i=n;i>0;--i)
+					ytmp[i]=y[i]+h*(b41*dydx[i]+b42*ak2[i]+b43*ak3[i]);
+				
+				drvs(ak4,x+a4*h,ytmp);
+				
+				for (size_t i=n;i>0;--i)
+					ytmp[i]=y[i]+h*(b51*dydx[i]+b52*ak2[i]+b53*ak3[i]+b54*ak4[i]);
+				
+				drvs(ak5,x+a5*h,ytmp);
+				
+				for (size_t i=n;i>0;--i)
+					ytmp[i]=y[i]+h*(b61*dydx[i]+b62*ak2[i]+b63*ak3[i]+b64*ak4[i]+b65*ak5[i]);
+				
+				drvs(ak6,x+a6*h,ytmp);
+				
+				for (size_t i=n;i>0;--i)
+					yout[i]=y[i]+h*(c1*dydx[i]+c3*ak3[i]+c4*ak4[i]+c6*ak6[i]);
+				
+				for (size_t i=n;i>0;--i)
+					yerr[i]=h*(dc1*dydx[i]+dc3*ak3[i]+dc4*ak4[i]+dc5*ak5[i]+dc6*ak6[i]);
+				
+			}
+			
+			
+		}
+		
+	}
+	
+}
+
---
+++ src/mk/yocto/math/ode/explicit/rkck.hpp
@@ -0,0 +1,44 @@
+#ifndef YOCTO_ODE_RKCK_INCLUDED
+#define YOCTO_ODE_RKCK_INCLUDED 1
+
+
+#include "yocto/math/ode/explicit/step.hpp"
+
+namespace yocto
+{
+	namespace math
+	{
+		
+		namespace ode
+		{
+            
+            
+            //! Runge-Kutta Cash-Karp 4/5 step
+            template <typename T>
+            class RKCK : public Explicit::step<T>
+            {
+            public:
+                typedef typename field<T>::explicit_type equation;
+                explicit RKCK();
+                virtual ~RKCK() throw();
+                
+                virtual void operator()(array<T>        &yout,
+                                        array<T>        &yerr,
+                                        equation        &drvs,
+                                        const T          x,
+                                        const T          h,
+                                        const array<T>  &y,
+                                        const array<T>  &dydx );
+                
+            private:
+                YOCTO_DISABLE_COPY_AND_ASSIGN(RKCK);
+                lw_array<T> &ytmp, &ak2, &ak3, &ak4, &ak5, &ak6;
+            };
+            
+		}
+		
+	}
+	
+}
+
+#endif
