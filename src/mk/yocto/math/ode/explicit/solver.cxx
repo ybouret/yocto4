1d2e8a90d667c67b091bd39d15ffb82f7f2045b3
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-01-03 09:43:14 +0000

    upgraded explicit solver

---
+++ src/mk/yocto/math/ode/explicit/solver.cxx
@@ -0,0 +1,101 @@
+#include "yocto/math/ztype.hpp"
+#include "yocto/math/ode/explicit/solver.hpp"
+#include "yocto/math/types.hpp"
+#include "yocto/exception.hpp"
+
+namespace yocto
+{
+    namespace math
+    {
+        namespace ode
+        {
+            template <>
+			void explicit_solver<real_t>::
+            operator()(equation                   &drvs,
+                       explicit_controler<real_t> &ctrl,
+                       explicit_step<real_t>      &forward,
+                       array<real_t>              &ystart,
+                       const real_t                x1,
+                       const real_t                x2,
+                       real_t                      &h1,
+                       callback                    *cb
+                       )
+			{
+				const size_t   n    = ystart.size();
+				const real_t  _TINY = Fabs( TINY );
+				
+				//--------------------------------------------------------------
+				// sanity check
+				//--------------------------------------------------------------
+				assert( common_size()         == n );
+				assert( ctrl.common_size()    == n );
+				assert( forward.common_size() == n );
+				
+				
+				//--------------------------------------------------------------
+				// initialize
+				//--------------------------------------------------------------
+				real_t h = x2 < x1 ? -Fabs(h1) : Fabs(h1);
+				real_t x = x1;
+				for( size_t i=n;i>0;--i) y[i] = ystart[i];
+				
+				//--------------------------------------------------------------
+				// Main Loop
+				//--------------------------------------------------------------
+				for(;;)
+				{
+					//----------------------------------------------------------
+					// initialize derivatives @x
+					//----------------------------------------------------------
+					drvs( dydx, x, y );
+					for( size_t i=n; i>0; --i )
+						yscal[i] = Fabs(y[i]) + Fabs( h * dydx[i] ) + _TINY;
+					
+					//----------------------------------------------------------
+					// check no overshooting
+					//----------------------------------------------------------
+					const real_t xm = x+h;
+					if( (xm-x2) * (xm-x1) >= 0 ) h = x2 - x;
+					
+					//----------------------------------------------------------
+					// forward with control
+					//----------------------------------------------------------
+					real_t h_did = 0, h_next = 0;
+					ctrl( forward, y, dydx, drvs, x, h, h_did, h_next, yscal, eps,cb);
+					
+#if 0
+					if( Fabs(h_did) < Fabs(h) )
+					{
+						// had to reduce
+					}
+#endif
+					
+					//----------------------------------------------------------
+					// check if done
+					//----------------------------------------------------------
+					if( (x-x1) * (x-x2) >= 0 )
+					{
+						break;
+					}
+					
+					//----------------------------------------------------------
+					// check if not too small
+					//----------------------------------------------------------
+					if( Fabs(h_next) < Fabs(hmin) )
+						throw exception( "h=%g<hmin=%g in ode::solver", h, hmin );
+					
+					h=h_next;
+				}
+				
+				//--------------------------------------------------------------
+				// success
+				//--------------------------------------------------------------
+				for( size_t i=n; i >0; --i )
+					ystart[i] = y[i];
+				h1 = h;
+				
+			}
+            
+        }
+    }
+}
---
+++ src/mk/yocto/math/ode/explicit/solver.hpp
@@ -0,0 +1,102 @@
+#ifndef YOCTO_MATH_ODE_EXPLICIT_SOLVER_INCLUDED
+#define YOCTO_MATH_ODE_EXPLICIT_SOLVER_INCLUDED 1
+
+#include "yocto/math/ode/explicit/controler.hpp"
+#include "yocto/math/ode/solver-data.hpp"
+
+namespace yocto
+{
+    namespace math
+    {
+        namespace ode
+        {
+            //! explicit solver
+            /**
+             needs to bee prepared before integration.
+             */
+            template <typename T>
+			class explicit_solver : public solver_data<T>
+			{
+			public:
+				typedef typename field<T>::explicit_type equation;
+                typedef typename field<T>::callback_type callback;
+                
+				explicit explicit_solver( T user_eps );
+				virtual ~explicit_solver() throw();
+				
+                
+                //! ystart@x1 -> x1
+                /**
+                 \param drvs    differential equation
+                 \param ctrl    an adaptive controler
+                 \param forward an embedded step
+                 \param ystart  initial values
+                 \param x1      initial parameter
+                 \param x2      final   parameter
+                 \param h1      guess step, updated
+                 */
+				void operator()(equation              &drvs,
+								explicit_controler<T> &ctrl,
+								explicit_step<T>      &forward,
+								array<T>              &ystart,
+								const T                x1,
+								const T                x2,
+								T                     &h1,
+                                callback              *cb
+								);
+				
+				
+				
+			private:
+				YOCTO_DISABLE_COPY_AND_ASSIGN(explicit_solver);
+			};
+            
+            
+            //! parametrized driver
+            template <
+			typename T,
+			template <typename> class CTRL,
+			template <typename> class STEP
+			> class explicit_driver : public explicit_solver<T>
+			{
+			public:
+				typedef typename field<T>::explicit_type equation;
+                typedef typename field<T>::callback_type callback;
+
+				explicit explicit_driver( T user_eps ) : explicit_solver<T>( user_eps ), ctrl_(), step_() {}
+				virtual ~explicit_driver() throw() {}
+				
+                //! allocate all memory
+				inline void start( size_t nvar )
+				{
+					this->prepare( nvar );
+					ctrl_.prepare( nvar );
+					step_.prepare( nvar );
+				}
+				
+                //! make a step
+				inline void operator()(equation   &drvs,
+									   array<T>   &ystart,
+									   const T     x1,
+									   const T     x2,
+									   T          &h1,
+                                       callback   *cb
+									   )
+				{
+                    explicit_solver<T> &self = *this;
+					self( drvs, ctrl_, step_, ystart, x1, x2, h1,cb);
+				}
+				
+			private:
+				CTRL<T> ctrl_;
+				STEP<T> step_;
+                YOCTO_DISABLE_COPY_AND_ASSIGN(explicit_driver);
+			};
+            
+            
+        }
+    }
+}
+
+
+#endif
