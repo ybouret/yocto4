f7c09ed268d17baceb3edd593cde7572ec6e11e8
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-01-06 08:20:25 +0000

    initial chemical boot

--- src/chemical/yocto/chemical/boot.cpp
+++ src/chemical/yocto/chemical/boot.cpp
@@ -4,9 +4,65 @@ namespace yocto
 {
     namespace chemical
     {
-
-
+        
+        ////////////////////////////////////////////////////////////////////////
+        //
+        //
+        //
+        ////////////////////////////////////////////////////////////////////////
+        boot:: constituent:: constituent(const species::ptr &sp,
+                                         const double        w ) throw() :
+        spec(sp),
+        weight(w)
+        {
+            assert(fabs(w)>0);
+        }
+        
+        boot:: constituent:: constituent( const constituent &other ) throw() :
+        spec( other.spec ),
+        weight( other.weight )
+        {
+        }
+        
+        boot:: constituent:: ~constituent() throw() {}
+        
+        const string & boot:: constituent:: key() const throw()
+        {
+            return spec->name;
+        }
+        
+        ////////////////////////////////////////////////////////////////////////
+        //
+        //
+        //
+        ////////////////////////////////////////////////////////////////////////
+        boot:: constraint:: constraint( const double v ) throw() :
+        constituents(),
+        value(v)
+        {
+        }
+        
+        boot:: constraint:: ~constraint() throw() {}
+        
+        
+        ////////////////////////////////////////////////////////////////////////
+        //
+        //
+        //
+        ////////////////////////////////////////////////////////////////////////
+        boot:: loader:: loader() throw() : constraints() {}
+        boot:: loader:: ~loader() throw() {}
+        
+        
+        boot::constraint & boot:: loader:: add(double value)
+        {
+            const constraint::ptr p( new constraint(value) );
+            push_back(p);
+            return *back();
+        }
+        
+        
     }
-
+    
 }
 
--- src/chemical/yocto/chemical/boot.hpp
+++ src/chemical/yocto/chemical/boot.hpp
@@ -2,7 +2,9 @@
 #define YOCTO_CHEMICAL_BOOT_INCLUDED 1
 
 #include "yocto/chemical/equilibria.hpp"
-#include "yocto/associative/map.hpp"
+#include "yocto/associative/set.hpp"
+#include "yocto/memory/pooled.hpp"
+#include "yocto/ptr/shared.hpp"
 
 namespace yocto
 {
@@ -10,20 +12,60 @@ namespace yocto
     {
         
         
-        class boot
+        struct boot
         {
-        public:
-            class constraint
+            
+            //! a constituent: species + weight
+            class constituent : public object, public counted
             {
             public:
+                explicit constituent( const species::ptr &sp, const double w ) throw();
+                virtual ~constituent() throw();
+                constituent( const constituent &) throw();
+                
+                const species::ptr spec;
+                const double       weight;
+                const string &     key() const throw();
+                
+                typedef intr_ptr<string,constituent> ptr;
+                
+            private:
+                YOCTO_DISABLE_ASSIGN(constituent);
+            };
+            
+            typedef key_hasher<string>        hasher_type;
+            typedef memory::pooled::allocator allocator_type;
+            typedef set<string,constituent::ptr,hasher_type,allocator_type> constituents;
+            
+            //! a constraint: constituents + values
+            class constraint : public constituents
+            {
+            public:
+                explicit constraint(const double v) throw();
+                virtual ~constraint() throw();
+                
+                double value;
+                
+                typedef shared_ptr<constraint> ptr;
                 
             private:
                 YOCTO_DISABLE_COPY_AND_ASSIGN(constraint);
             };
             
+            typedef vector<constraint::ptr,allocator_type> constraints;
             
-        private:
-            YOCTO_DISABLE_COPY_AND_ASSIGN(boot);
+            //! a vector of constraints
+            class loader : public constraints
+            {
+            public:
+                explicit loader() throw();
+                virtual ~loader() throw();
+                
+                constraint & add(double value);
+                
+            private:
+                YOCTO_DISABLE_COPY_AND_ASSIGN(loader);
+            };
             
         };
     }
