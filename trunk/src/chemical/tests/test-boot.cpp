7cc60d8fc4a6dc0b036e1513871152e6cc5a6d3b
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-01-06 08:20:25 +0000

    initial chemical boot

--- trunk/src/chemical/tests/test-boot.cpp
+++ trunk/src/chemical/tests/test-boot.cpp
@@ -6,6 +6,12 @@ using namespace yocto;
 
 YOCTO_UNIT_TEST_IMPL(boot)
 {
+    std::cerr << "sizeof(boot:constituent)=" << sizeof(chemical::boot::constituent) << std::endl;
+    std::cerr << "sizeof(boot:constraint)="  << sizeof(chemical::boot::constraint) << std::endl;
+    std::cerr << "sizeof(boot:loader)="       << sizeof(chemical::boot::loader) << std::endl;
 
+    chemical::boot::loader ini;
+    
+    
 }
 YOCTO_UNIT_TEST_DONE()
