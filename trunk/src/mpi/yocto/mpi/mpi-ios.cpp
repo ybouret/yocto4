bd930dd315ac9c3e3c9b727b0d7114cd4e318c49
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-01-27 13:22:19 +0000

    work on endian

--- trunk/src/mpi/yocto/mpi/mpi-ios.cpp
+++ trunk/src/mpi/yocto/mpi/mpi-ios.cpp
@@ -37,24 +37,6 @@ namespace yocto
         return *fp;
     }
     
-    
-    static inline
-    void __put_io_key( const mpi::io_key_t k, void *data )
-    {
-        assert(data);
-        const mpi::io_key_t k_net = swap_be_as(k);
-        memcpy(data,&k_net,sizeof(mpi::io_key_t));
-    }
-    
-    static inline
-    mpi::io_key_t __get_io_key( const void *data )
-    {
-        assert(data);
-        mpi::io_key_t k_net(0);
-        memcpy( &k_net, data, sizeof(mpi::io_key_t));
-        return swap_be_as(k_net);
-    }
-    
     void mpi::ostream:: write(char C)
     {
         static const char iosize = sizeof(io_key_t)+1;
@@ -69,7 +51,7 @@ namespace yocto
             for(int r=1;r<MPI.CommWorldSize;++r)
             {
                 MPI.Recv(iobuff,iosize, MPI_BYTE, r, tag, MPI_COMM_WORLD, status);
-                const io_key_t k = __get_io_key(iobuff);
+                const io_key_t k = query_be<io_key_t>(iobuff);
                 if(k!=key)
                     throw yocto::exception("MPI I/O: invalid key during write char");
                 fp->write(iobuff[sizeof(io_key_t)]);
@@ -78,7 +60,7 @@ namespace yocto
         else
         {
             assert(!MPI.IsFirst);
-            __put_io_key(key,iobuff);
+            store_be<io_key_t>(key,iobuff);
             //*(io_key_t *)iobuff = swap_be_as<io_key_t>(key);
             iobuff[sizeof(io_key_t)]=C;
             MPI.Send(iobuff, iosize, MPI_BYTE, 0, tag, MPI_COMM_WORLD);
