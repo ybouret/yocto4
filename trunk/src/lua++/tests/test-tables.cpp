2aac4007269d349f2b413bb10edfdb660bf8bedd
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-01-27 08:25:27 +0000

    lua++ tables

---
+++ trunk/src/lua++/tests/test-tables.cpp
@@ -0,0 +1,33 @@
+#include "yocto/utest/run.hpp"
+#include "yocto/lua/lua-state.hpp"
+#include "yocto/lua/lua-config.hpp"
+
+
+using namespace yocto;
+
+YOCTO_UNIT_TEST_IMPL(tables)
+{
+    Lua::State vm;
+    lua_State *L = vm();
+    
+    lua_settop(L, 0);
+    
+    lua_createtable(L, 0, 5);
+    
+    
+    lua_pushinteger(L, 1); // key
+    lua_pushinteger(L, 2); // value
+    
+    lua_rawset(L, -3);
+    
+    lua_setglobal(L, "toto");
+
+    
+    for( int i=1; i < argc; ++i )
+    {
+        const char *filename = argv[i];
+        Lua::Config::DoFile( L, filename );
+    }
+    
+}
+YOCTO_UNIT_TEST_DONE()
