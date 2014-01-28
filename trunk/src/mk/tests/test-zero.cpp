16e07ce22901c7baef4ef4e93809523e523dbc35
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-01-28 08:43:07 +0000

    modified zfind, need to check files including it

--- trunk/src/mk/tests/test-zero.cpp
+++ trunk/src/mk/tests/test-zero.cpp
@@ -14,9 +14,29 @@ static inline T F( T x )
 
 YOCTO_UNIT_TEST_IMPL(zero)
 {
-	zfind<float>             solve(1e-5f);
-	numeric<float>::function func( cfunctor(F<float>) );
-	const float z = solve( func, -numeric<float>::pi/2, numeric<float>::pi/2 );
-	std::cerr << "F(" << z << ")=" << func(z) << std::endl;
+    {
+        zfind<float>             solve(1e-5f);
+        numeric<float>::function func( cfunctor(F<float>) );
+        const float z = solve( func, -numeric<float>::pi/2, numeric<float>::pi/2 );
+        std::cerr << "F(" << z << ")=" << func(z) << std::endl;
+    }
+    
+    {
+        zfind<double> solve(1e-5);
+        numeric<double>::function func( cfunctor(F<double>) );
+        triplet<double> x = { 0.0, 0, 0.5 };
+        triplet<double> f = { 0, 0, 0 };
+        if( !solve.lookup(func, x, f))
+        {
+            std::cerr << "Couldn't find a zero" << std::endl;
+        }
+        else
+        {
+            std::cerr << "found x=" << x << " and f=" << f << std::endl;
+            const double z = solve.run(func, x, f);
+            std::cerr << "F(" << z << ")=" << func(z) << "/" << f.b << std::endl;
+        }
+    }
+    
 }
 YOCTO_UNIT_TEST_DONE()
--- trunk/src/mk/yocto/math/fcn/zfind.hpp
+++ trunk/src/mk/yocto/math/fcn/zfind.hpp
@@ -1,6 +1,6 @@
 /** \file  zfind.hpp
-    \brief Root Finding by Ridders' Method.
-*/
+ \brief Root Finding by Ridders' Method.
+ */
 
 #ifndef YOCTO_ZFIND_INCLUDED
 #define YOCTO_ZFIND_INCLUDED 1
@@ -8,14 +8,16 @@
 #include "yocto/math/triplet.hpp"
 
 namespace yocto {
-
+    
 	namespace math {
-
+        
+        
+        
 		//! find zero between x.a and x.c
 		/**
-			f.a and f.c must be provided and f.a * f.c <= 0
-			xerr>=0
-		*/
+         f.a and f.c must be provided and f.a * f.c <= 0
+         xerr>=0
+         */
 		template <typename T>
 		class zfind
 		{
@@ -23,27 +25,47 @@ namespace yocto {
             explicit zfind( T default_xerr ) throw();
             virtual ~zfind() throw();
             
-			T xerr; //!< error on x
-
+			T       xerr;   //!< error on x
+            T       growth; //!< for bracketing: default is 1.6180339887
+            size_t  trials; //!< for bracketing: max trials, default is 50
+            
 			//! find zero between x.a and x.c
 			/**
-				f.a and f.c must be provided and f.a * f.c <= 0
-			*/
-			void run( typename numeric<T>::function &proc, triplet<T> &x, triplet<T> &f) const;
-			T    get( typename numeric<T>::function &proc, T xlo, T xhi ) const;
-
-			inline T operator()( typename numeric<T>::function &proc, T xlo, T xhi ) const
-			{
-				return this->get( proc, xlo, xhi );
-			}
-
+             f.a and f.c must be provided and f.a * f.c <= 0
+             x.b and f.b are the results
+             it may fail if proc is not continuous...
+             return x.b
+             */
+			T run( typename numeric<T>::function &proc, triplet<T> &x, triplet<T> &f) const;
+			
+            //! best effort solving
+            /**
+             \param proc a continuous function
+             \param xa   an initial range boundary
+             \param xc   an initial range boundary
+             */
+            T operator()(typename numeric<T>::function &proc,
+                         T xa,
+                         T xc) const;
+			
+            
+            //! starting from x.a and x.c, try to bracket one zero
+            /**
+             \param proc a function
+             \param x    the initial [x.a:x.c] range
+             \param f    an uninitialized triplet
+             f is initialized and updated.
+             */
+            bool lookup( typename numeric<T>::function &proc, triplet<T> &x, triplet<T> &f) const;
+            
+            
         private:
             YOCTO_DISABLE_COPY_AND_ASSIGN(zfind);
 		};
-
+        
 		//YOCTO_DMC_MATH(struct zfind)
 	}
-
+    
 }
 
 #endif
