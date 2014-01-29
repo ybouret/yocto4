944a42c083ab127d60bd18e8dac659e41a4f2376
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-01-29 17:05:12 +0000

    upgraded eqsolve and simu times

--- src/main/yocto/math/round.hpp
+++ src/main/yocto/math/round.hpp
@@ -8,13 +8,25 @@ namespace yocto
     namespace math
     {
         template <typename T>
-        T log_round( T a ) throw()
+        inline T log_round( T a ) throw()
         {
             assert( a > 0 );
             const double a_log = Floor( Log10(a) );
-            const double a_one = Floor( a * Pow(10.0,-a_log));
-            return a_one * Pow(10.0,a_log);
+            const double a_one = Floor( a * Pow( T(10.0),-a_log));
+            return a_one * Pow( T(10.0),a_log);
         }
+     
+        template <typename T>
+        inline void simulation_times( T &dt, T &dt_save, size_t &every )
+        {
+            assert(dt>0);
+            dt = log_round(dt);
+            if(dt_save<=dt) dt_save = dt;
+            every   = size_t(Floor(dt_save/dt));
+            if(every<1) every=1;
+            dt_save = every * dt;
+        }
+        
     }
 }
 
