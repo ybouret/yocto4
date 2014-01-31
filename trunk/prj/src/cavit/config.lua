609f3c5ae24be49227c5848d29b1b268a19c4bf8
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-01-31 10:22:36 +0000

    with lambda1

--- trunk/prj/src/cavit/config.lua
+++ trunk/prj/src/cavit/config.lua
@@ -1,4 +1,12 @@
 
+exp = math.exp;
+
+-- spontaneous
 function lambda0(t)
-return math.exp(0.1*math.cos(t));
+return 1+0.1*t;
+end
+
+-- addendum: tau=t-t_cav
+function lambda1(tau,d)
+return 100*exp(-2*tau);
 end
