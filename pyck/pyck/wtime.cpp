e9676465d3866f93124ab190930a8f77696a1963
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-02-03 08:09:34 +0000

    upgraded PYCK

--- pyck/pyck/wtime.cpp
+++ pyck/pyck/wtime.cpp
@@ -1,25 +1,7 @@
 #include "./wtime.hpp"
-#include "./exception.hpp"
-#include "./mutex.hpp"
-
-#if defined(PYCK_BSD)
-#include <cerrno>
-#include <sys/time.h>
-#endif
-
-#if defined(PYCK_WIN)
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-#endif
 
 WallTime:: WallTime() throw() :
-#if defined(PYCK_BSD)
-data()
-#endif
-#if defined(PYCK_WIN)
-	quad(0),
-	freq(0)
-#endif
+last(0)
 {
 }
 
@@ -27,92 +9,26 @@ WallTime:: ~WallTime() throw()
 {
 }
 
-#if defined(PYCK_WIN)
-static void __wtime_fetch( int64_t &Q )
-{
-	if( ! ::QueryPerformanceCounter( (LARGE_INTEGER *) &Q )  )
-	{
-		throw Exception( ::GetLastError(), "::QueryPerformanceCounter" );
-	}
-}
-#endif
-
-#if defined(PYCK_BSD)
-static void __wtime_fetch( struct timeval *tv )
-{
-	if( 0 != gettimeofday( tv, NULL) )
-	{
-		throw Exception( errno, "gettimeofday" );
-	}
-}
-#endif
-
 void WallTime:: start()
 {
-#if defined(PYCK_WIN)
-	static const long double l_one = 1;
-	quad = 0;
-	freq = 0;
-	LARGE_INTEGER F;
-	PYCK_GIANT_LOCK();
-	if( ! :: QueryPerformanceFrequency( &F ) )
-	{
-		throw Exception(::GetLastError(), "::QueryPerformanceFrequency");
-	}
-	freq = l_one / static_cast<long double>( F.QuadPart );
-	__wtime_fetch( quad );
-#endif
-
-#if defined(PYCK_BSD)
-	__wtime_fetch( (struct timeval *)(void*)data );
-#endif
+    last = Ticks();
 }
 
 double WallTime:: query() const
 {
-#if defined(PYCK_WIN)
-	int64_t now = 0;
-	__wtime_fetch(now);
-	return static_cast<double>( freq * static_cast<long double>(now-quad) );
-#endif
-
-#if defined(PYCK_BSD)
-	const struct timeval *old = (const struct timeval *)(void*)data;
-	struct timeval now;
-	__wtime_fetch( &now );
-	const long int sec_old  = old->tv_sec;
-	const long int sec_now  = now.tv_sec;
-	const long int usec_old = old->tv_usec;
-	const long int usec_now = now.tv_usec;
-	return double(sec_now-sec_old) + 1e-6 * double(usec_now-usec_old);
-#endif
-}
-
-uint64_t WallTime:: Ticks()
-{
-#if defined(PYCK_WIN)
-	int64_t ans = 0;
-	__wtime_fetch( ans );
-	return ans;
-#endif
-
-#if defined(PYCK_BSD)
-	static const uint64_t mega = 1000000;
-	struct timeval now;
-	__wtime_fetch( &now );
-	return static_cast<uint64_t>(now.tv_usec) + mega * static_cast<uint64_t>(now.tv_sec);
-#endif
+    const RunTimeClock &clk = *this;
+    return clk( Ticks() - last );
 }
 
 uint32_t WallTime:: Seed()
 {
-	union
+	const union
 	{
 		uint64_t  ticks;
 		uint32_t  dw[2];
-	} ini = { WallTime::Ticks() };
+	} ini = { RunTimeClock:: Ticks() };
 
-	uint32_t seed = IntegerHash32( ini.dw[0] ) ^ IntegerHash32( ini.dw[1] );
+	const uint32_t seed = IntegerHash32( ini.dw[0] ) ^ IntegerHash32( ini.dw[1] );
 	return seed;
 }
 
--- pyck/pyck/wtime.hpp
+++ pyck/pyck/wtime.hpp
@@ -1,31 +1,23 @@
 #ifndef PYCK_WTIME_INCLUDED
 #define PYCK_WTIME_INCLUDED 1
 
-#include "./os.hpp"
+#include "./rt-clock.hpp"
 
-class WallTime
+class WallTime : public RunTimeClock
 {
 public:
 	explicit WallTime() throw();
 	virtual ~WallTime() throw();
+    
 	void   start();
 	double query() const;
-	static uint64_t Ticks();
+	
 	static uint32_t Seed();
 
 private:
 	WallTime( const WallTime & );
 	WallTime &operator=( const WallTime & );
-
-#if defined(PYCK_BSD)
-	int64_t data[2];
-#endif
-
-#if defined(PYCK_WIN)
-	int64_t      quad;
-	long double  freq;
-#endif
-
+    uint64_t last;
 };
 
 #endif
