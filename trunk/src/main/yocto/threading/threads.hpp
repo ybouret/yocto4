3630c434790dca48e3a80f5b0beddd6f77a6ab89
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-02-10 08:18:52 +0000

    simplified threading

--- trunk/src/main/yocto/threading/threads.hpp
+++ trunk/src/main/yocto/threading/threads.hpp
@@ -54,29 +54,33 @@ namespace yocto
 			};
             
             
-            
+            //! the functionoid must be valid during all thread execution
 			template <typename FUNCTION>
-			void start( const FUNCTION &fn )
+			void start( FUNCTION &fn )
 			{
 				YOCTO_LOCK(access);
 				thread *thr = query();
 				try
 				{
-					thr->code.build< exec0<FUNCTION>, FUNCTION>( fn );
+                    const exec0<FUNCTION> todo(fn);
+					thr->code.make< exec0<FUNCTION> >( todo );
 					launch( execute0<FUNCTION>, & (thr->code.as< exec0<FUNCTION> >()) );
 					push_back(thr);
 				}
 				catch(...) { pool.store(thr); throw; }
 			}
             
+            //! the functionoid and the parameter must be valid during  the whole thread
 			template <typename FUNCTION, typename T>
-			void start( const FUNCTION &function, typename type_traits<T>::parameter_type param1 )
+			void start(FUNCTION &function,
+                       T        &param1 )
 			{
 				YOCTO_LOCK(access);
 				thread *thr = query();
 				try
 				{
-					thr->code.build< exec1<FUNCTION,T>, FUNCTION, T>( function, param1);
+                    const exec1<FUNCTION,T> todo(function,param1);
+					thr->code.make< exec1<FUNCTION,T> >( todo );
 					launch( execute1<FUNCTION,T>, &( thr->code.as< exec1<FUNCTION,T> >() ));
 					push_back(thr);
 				}
@@ -96,14 +100,15 @@ namespace yocto
 			class exec0
 			{
 			public:
-				inline  exec0( const FUNCTION &function ) : fn_(function) {}
+				inline  exec0( FUNCTION &function ) throw(): fn_(function)  {}
+                inline  exec0( const exec0 &other ) throw(): fn_(other.fn_) {}
 				inline ~exec0() throw() {}
                 
 				inline void run() { fn_(); }
                 
 			private:
-				FUNCTION fn_;
-				YOCTO_DISABLE_COPY_AND_ASSIGN(exec0);
+				FUNCTION &fn_;
+				YOCTO_DISABLE_ASSIGN(exec0);
 			};
             
             
@@ -129,17 +134,20 @@ namespace yocto
 			class exec1
 			{
 			public:
-				inline  exec1(const FUNCTION                         &function,
-                              typename type_traits<T>::parameter_type param1 ) :
+				inline  exec1(FUNCTION &function,
+                              T        &param1 ) throw() :
                 fn_(function), p1_(param1) {}
+                inline  exec1(const exec1 &other ) throw() :
+                fn_(other.fn_), p1_(other.p1_) {}
+                
 				inline ~exec1() throw() {}
                 
 				inline void run() { fn_(p1_); }
                 
 			private:
-				FUNCTION                              fn_;
-				typename type_traits<T>::mutable_type p1_;
-				YOCTO_DISABLE_COPY_AND_ASSIGN(exec1);
+				FUNCTION &fn_;
+				T        &p1_;
+				YOCTO_DISABLE_ASSIGN(exec1);
 			};
             
 			template <typename FUNCTION, typename T>
