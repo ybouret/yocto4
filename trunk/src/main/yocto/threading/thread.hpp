d98ce0fe22fa27b4810b43293ea4e8e51faab107
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-02-10 07:52:20 +0000

    upgraded api

--- trunk/src/main/yocto/threading/thread.hpp
+++ trunk/src/main/yocto/threading/thread.hpp
@@ -6,105 +6,106 @@
 
 namespace yocto
 {
-
+    
 	namespace threading
 	{
-
-
+        
+        
+        //! forward declaration
 		class threads;
-
+        
 		//! a generic thread
 		class thread
 		{
 		public:
 			//! the default prototype
-			typedef void (*proc_t)(void *);
-
+			typedef void (*procedure)(void *);
+            
 #if defined(YOCTO_BSD)
 			//! pthread
 			typedef pthread_t handle_t;
 #endif
-
+            
 #if defined(YOCTO_WIN)
 			//! win32
 			typedef HANDLE handle_t;
 #endif
-
+            
 			//! shared access
 			mutex  &access;
-
+            
 			//! get memory and link access
 			static thread *create_with( mutex & shared_access );
-
+            
 			//! finish MUST be called before !
 			static void    destruct( thread *thr ) throw();
-
+            
 			//! launch a thread
 			/**
-			the stop flag is set to false.
-			\warning the user_proc/user_data must be available until finish!
-			*/
-			void launch( proc_t user_proc, void *user_data );
-
-
-			//! help for thread to finish
+             the stop flag is set to false.
+             \warning the user_proc/user_data must be available until finish!
+             */
+			void launch( procedure user_proc, void *user_data );
+            
+            
+			//! wait for thread to finish
 			/**
-			if the thread is active,
-			the stop flag is set to true and
-			the shutdown procedure called.
-			*/
+             if the thread is active,
+             the stop flag is set to true and
+             the shutdown procedure called.
+             */
 			void finish() throw();
-
+            
 			//! get system level handle
 			handle_t        get_handle() const throw();
-
+            
 			//! get system level handle without the wrapper
 			static handle_t get_current_handle() throw();
-
+            
 			void on_cpu( size_t cpu_id );
 			static void assign_cpu( thread::handle_t , size_t cpu_id );
 			static void foreach_cpu( thread::handle_t, void (*proc)(size_t cpu_id,void*), void *);
-
-
-
+            
+            
+            
 		private:
 			thread( mutex &shared_access ) throw();
 			YOCTO_DISABLE_COPY_AND_ASSIGN(thread);
 			~thread() throw();
 			friend class threads;
-
-			handle_t handle;
-			proc_t   proc;    //!< routine to start
-			void    *data;    //!< arguments for the routine
-			vslot    code;    //!< store executable code
-
-			void     clear() throw();
-
-
+            
+			handle_t  handle;
+			procedure proc;    //!< routine to start
+			void     *data;    //!< arguments for the routine
+			vslot     code;    //!< store executable code
+            
+			void     clear() throw(); //!< cleanup after finish
+            
+            
 #if defined(YOCTO_BSD)
 			static void * entry( void * ) throw();
 #endif
-
+            
 #if defined(YOCTO_WIN)
 			static DWORD WINAPI entry( LPVOID ) throw();
 #endif
-
-
+            
+            
 		public:
 			const bool stop; //!< internal flag
-			thread    *next;
-			thread    *prev;
-
+			thread    *next; //!< for threads
+			thread    *prev; //!< for threads
+            
 		};
-
+        
 		void assign_current_thread_on( size_t cpu_id );
-
-
-
+        
+        
+        
 	}
-
-
-
+    
+    
+    
 }
 
 #endif
--- trunk/src/main/yocto/threading/threads.hpp
+++ trunk/src/main/yocto/threading/threads.hpp
@@ -8,110 +8,59 @@
 
 namespace yocto
 {
-
+    
 	namespace threading
 	{
-
-
-#define YOCTO_THREADS_CREATE(VAR,CODE) do { \
-	thread *VAR = query(); \
-	try { do { CODE; } while(false); push_back(VAR); } \
-	catch(...){ pool.store(VAR); throw; } \
-	} while(false)
-
+        
 		//! low level memory for threads
 		class threads : public core::list_of<thread>
 		{
 		public:
 			mutex access;
-
+            
 			//! construct and give name to access
 			explicit threads(const char *id) throw();
-
+            
 			//! construct, give name to access and reserve(n)
 			explicit threads(const char *id, size_t n) throw();
-
+            
 			//! finish and clean all
 			virtual ~threads() throw();
-
+            
 			//! launch a generic procedure
-			void launch( thread::proc_t proc, void *data);
-
+			void launch( thread::procedure proc, void *data);
+            
 			//! finish a specific thread, removed from the list
 			void finish( thread *thr ) throw();
-
+            
 			//! finish all
 			void finish() throw();
-
+            
 			//! release pool
 			void trim() throw();
-
+            
 			//! prepare some memory
 			void reserve(size_t n);
-
-
+            
+            //! auto cleanup
 			class failsafe : public auto_clean<threads>
 			{
 			public:
 				explicit failsafe( threads &host ) throw();
 				virtual ~failsafe() throw();
-
+                
 			private:
 				YOCTO_DISABLE_COPY_AND_ASSIGN(failsafe);
 			};
-
-			template <typename FUNCTION>
-			class exec0
-			{
-			public:
-				inline exec0( const FUNCTION &function ) : fn_(function)
-				{
-				}
-
-				inline ~exec0() throw() {}
-
-				inline void run()
-				{
-					fn_();
-				}
-
-			private:
-				FUNCTION fn_;
-				YOCTO_DISABLE_COPY_AND_ASSIGN(exec0);
-			};
-
-			template <typename FUNCTION, typename T>
-			class exec1
-			{
-			public:
-				inline exec1(const FUNCTION                         &function,
-					typename type_traits<T>::parameter_type param1
-					) :
-				fn_(function),
-					p1_(param1)
-				{
-				}
-
-				inline ~exec1() throw() {}
-
-				inline void run()
-				{
-					fn_(p1_);
-				}
-
-			private:
-				FUNCTION                              fn_;
-				typename type_traits<T>::mutable_type p1_;
-				YOCTO_DISABLE_COPY_AND_ASSIGN(exec1);
-			};
-
-
+            
+            
+            
 			template <typename FUNCTION>
 			void start( const FUNCTION &fn )
 			{
 				YOCTO_LOCK(access);
 				thread *thr = query();
-				try 
+				try
 				{
 					thr->code.build< exec0<FUNCTION>, FUNCTION>( fn );
 					launch( execute0<FUNCTION>, & (thr->code.as< exec0<FUNCTION> >()) );
@@ -119,7 +68,7 @@ namespace yocto
 				}
 				catch(...) { pool.store(thr); throw; }
 			}
-
+            
 			template <typename FUNCTION, typename T>
 			void start( const FUNCTION &function, typename type_traits<T>::parameter_type param1 )
 			{
@@ -127,19 +76,39 @@ namespace yocto
 				thread *thr = query();
 				try
 				{
-					thr->code.build< exec1<FUNCTION,T>, T>( function, param1);
-					launch( execute1<FUNCTION,T>, & code.as< exec1<FUNCTION,T> >() );
+					thr->code.build< exec1<FUNCTION,T>, FUNCTION, T>( function, param1);
+					launch( execute1<FUNCTION,T>, &( thr->code.as< exec1<FUNCTION,T> >() ));
 					push_back(thr);
 				}
 				catch(...)  { pool.store(thr); throw; }
 			}
-
+            
 		private:
 			thread *query();
 			core::pool_of<thread> pool;
 			YOCTO_DISABLE_COPY_AND_ASSIGN(threads);
+            
+            //__________________________________________________________________
+            //
+            // Functionoid
+            //__________________________________________________________________
 			template <typename FUNCTION>
-			static inline void execute0( void *args )
+			class exec0
+			{
+			public:
+				inline  exec0( const FUNCTION &function ) : fn_(function) {}
+				inline ~exec0() throw() {}
+                
+				inline void run() { fn_(); }
+                
+			private:
+				FUNCTION fn_;
+				YOCTO_DISABLE_COPY_AND_ASSIGN(exec0);
+			};
+            
+            
+			template <typename FUNCTION>
+			static inline void execute0( void *args ) throw()
 			{
 				try
 				{
@@ -148,12 +117,33 @@ namespace yocto
 				}
 				catch(...)
 				{
-
+                    
 				}
 			}
-
+            
+            //__________________________________________________________________
+            //
+            // Functionoid + argument
+            //__________________________________________________________________
 			template <typename FUNCTION, typename T>
-			static inline void execute1( void *args )
+			class exec1
+			{
+			public:
+				inline  exec1(const FUNCTION                         &function,
+                              typename type_traits<T>::parameter_type param1 ) :
+                fn_(function), p1_(param1) {}
+				inline ~exec1() throw() {}
+                
+				inline void run() { fn_(p1_); }
+                
+			private:
+				FUNCTION                              fn_;
+				typename type_traits<T>::mutable_type p1_;
+				YOCTO_DISABLE_COPY_AND_ASSIGN(exec1);
+			};
+            
+			template <typename FUNCTION, typename T>
+			static inline void execute1( void *args ) throw()
 			{
 				try
 				{
@@ -162,13 +152,13 @@ namespace yocto
 				}
 				catch(...)
 				{
-
+                    
 				}
 			}
 		};
-
+        
 	}
-
+    
 }
 
 
