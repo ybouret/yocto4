909923b407fc31e1af3f8998b90248ad89b01179
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-02-11 11:31:51 +0000

    server WIP

--- src/thr/yocto/threading/server.hpp
+++ src/thr/yocto/threading/server.hpp
@@ -3,17 +3,49 @@
 
 #include "yocto/threading/threads.hpp"
 #include "yocto/threading/condition.hpp"
+#include "yocto/functor.hpp"
 
 namespace yocto
 {
     namespace threading
     {
-        class task_server
+        
+        
+        
+        class server
         {
         public:
+            explicit server();
+            virtual ~server() throw();
+            
+            typedef functor<void,null_type> job;
+        
+            class task
+            {
+            public:
+                task *next;
+                task *prev;
+            private:
+                job   work;
+                task( const job & );
+                ~task() throw();
+                YOCTO_DISABLE_COPY_AND_ASSIGN(task);
+                friend class threading::server;
+            };
+            
+            void enqueue( const job &J );
+            
             
         private:
+            threads workers;
             
+        public:
+            mutex  &access;
+            
+        private:
+            YOCTO_DISABLE_COPY_AND_ASSIGN(server);
+            core::list_of<task> tasks;
+            core::pool_of<task> tpool;
         };
     }
 }
