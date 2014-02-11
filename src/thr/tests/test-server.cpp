6a252eb198b5aca3726d8eb771952c48635ddd4c
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-02-11 10:32:55 +0000

    init thread server

---
+++ src/thr/tests/test-server.cpp
@@ -0,0 +1,11 @@
+#include "yocto/utest/run.hpp"
+#include "yocto/threading/server.hpp"
+
+using namespace yocto;
+using namespace threading;
+
+YOCTO_UNIT_TEST_IMPL(server)
+{
+    
+}
+YOCTO_UNIT_TEST_DONE()
---
+++ src/thr/yocto/threading/server.hpp
@@ -0,0 +1,22 @@
+#ifndef YOCTO_THREADING_SERVER_INCLUDED
+#define YOCTO_THREADING_SERVER_INCLUDED 1
+
+#include "yocto/threading/threads.hpp"
+#include "yocto/threading/condition.hpp"
+
+namespace yocto
+{
+    namespace threading
+    {
+        class task_server
+        {
+        public:
+            
+        private:
+            
+        };
+    }
+}
+
+#endif
+
