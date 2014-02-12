0b876d69d7dee54374c288efd444e22b19116d1c
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-02-12 16:21:25 +0000

    upgrading, need to implement samples

--- src/mk/tests/test-least-squares.cpp
+++ src/mk/tests/test-least-squares.cpp
@@ -47,7 +47,7 @@ YOCTO_UNIT_TEST_IMPL(least_squares)
     
     SamplePtr S1( new  Sample(t1,x1,z1) );
     SamplePtr S2( new  Sample(t2,x2,z2) );
-
+    
     const size_t nvar = 3;
     const size_t npar = 2;
     
@@ -82,6 +82,7 @@ YOCTO_UNIT_TEST_IMPL(least_squares)
     vector<double> aerr( a.size(), 0 );
     
     lsf.verbose = true;
+    lsf.ftol    = 1e-7;
     lsf(F,samples,a,used,aerr);
     
     std::cerr << "aorg=" << a    << std::endl;
@@ -89,3 +90,239 @@ YOCTO_UNIT_TEST_IMPL(least_squares)
     
 }
 YOCTO_UNIT_TEST_DONE()
+
+
+#include "yocto/code/rand.hpp"
+#include "yocto/code/ipower.hpp"
+#include "yocto/sort/heap.hpp"
+#include "yocto/ios/ocstream.hpp"
+
+namespace
+{
+	
+	
+	
+	template <typename T>
+	class Poly
+	{
+	public:
+		explicit Poly() throw() {}
+		virtual ~Poly() throw() {}
+		
+		inline T Eval( double x, const array<T> &a )
+		{
+			T ans = 0;
+			for( size_t i=1; i <= a.size(); ++i )
+			{
+				ans += a[i] * ipower(x,i-1);
+			}
+			return ans;
+		}
+		
+		bool ToDo(typename       least_squares<T>::function &F,
+                  const typename least_squares<T>::samples  &Samples)
+		{
+			
+			return true;
+		}
+		
+		
+	private:
+		YOCTO_DISABLE_COPY_AND_ASSIGN(Poly);
+	};
+	
+}
+
+YOCTO_UNIT_TEST_IMPL(lsf_poly)
+{
+	
+	const size_t N = 10 + alea_leq(290);
+	vector<double> X( N, 0 );
+	vector<double> Y( N, 0 );
+	vector<double> Z( N, 0 );
+	
+	for( size_t i=1; i <= N; ++i )
+	{
+		X[i] = 6.14 * alea<double>();
+		Y[i] = Cos( X[i] ) + 0.2 * (0.5 - alea<double>() );
+		
+	}
+	
+	{
+		hsort( X, Y, __compare<double> );
+		ios::ocstream fp( "lsf.dat", false );
+		for( size_t i=1; i <= N; ++i )
+		{
+			fp("%g %g\n", X[i], Y[i] );
+		}
+	}
+	
+    vector< least_squares<double>::sample::pointer > Samples;
+    {
+        least_squares<double>::sample::pointer S( new least_squares<double>::sample( X, Y, Z ) );
+        Samples.push_back(S);
+    }
+	Poly<double>            P;
+    
+	least_squares<double>::callback cb( &P, & Poly<double>::ToDo );
+	least_squares<double>::function F(  &P, & Poly<double>::Eval );
+    least_squares<double> LeastSquare;
+	
+    
+	for( size_t nv = 1; nv <= 5; ++nv )
+	{
+		std::cerr << "-- nvar=" << nv << std::endl;
+		vector<double> aorg(nv,0);
+		vector<bool>   used(nv,true);
+		vector<double> aerr(nv,0);
+		
+		if( nv > 2 && alea<double>() > 0.7 )
+			used[ 1 + alea_lt(nv) ] = false;
+		
+		LeastSquare.ftol = 1e-7;
+		if(  LeastSquare(F, Samples, aorg, used, aerr , NULL)  != least_squares_failure )
+		{
+			for( size_t i=1; i <= nv; ++i )
+			{
+				std::cerr << "a[" << i << "]=" << aorg[i] << " +/- " << aerr[i] << std::endl;
+			}
+			ios::ocstream fp( vformat("lsf.%u.dat", unsigned(nv)), false );
+			for( size_t i=1; i <= N; ++i )
+			{
+				fp("%g %g %g\n", X[i], Y[i], Z[i] );
+			}
+		}
+	}
+	
+	
+}
+YOCTO_UNIT_TEST_DONE()
+
+namespace
+{
+    static inline double gaussian( double x, double mu, double sig )
+    {
+        const double dx = x-mu;
+        return Exp( - (dx*dx)/(sig+sig) );
+    }
+    
+    template <typename T>
+    class Gauss
+    {
+    public:
+        
+        Gauss() throw()
+        {
+        }
+        
+        ~Gauss() throw()
+        {
+        }
+        
+        T Eval( T x, const array<T> &a )
+        {
+            const size_t nexp = a.size()/3;
+            size_t i=1;
+            T ans(0);
+            for( size_t j=1; j <= nexp; ++j )
+            {
+                const T A   = a[i++];
+                const T mu  = a[i++];
+                const T sig = a[i++];
+                ans += A * gaussian(x, mu, sig);
+            }
+            return ans;
+        }
+        
+        bool ToDo(typename       least_squares<T>::function &F,
+                  const typename least_squares<T>::samples  &Samples)
+        {
+            const array<T> &a = Samples[1]->u;
+            const size_t nexp = a.size()/3;
+            size_t i=1;
+            for( size_t j=1; j <= nexp; ++j )
+            {
+                const T A   = a[i++];
+                const T mu  = a[i++];
+                const T sig = a[i++];
+                if(j>1) std::cerr << "+";
+                std::cerr << "(" << A << ")*G(" << mu << "," << sig << ")";
+            }
+            std::cerr << std::endl;
+            return true;
+        }
+    };
+    
+}
+
+YOCTO_UNIT_TEST_IMPL(lsf_gauss)
+{
+	const size_t   N = 10 + alea_leq(1000);
+	vector<double> X( N, 0 );
+	vector<double> Y( N, 0 );
+	vector<double> Z( N, 0 );
+	
+	const double x1 = 0.21;
+	const double s1 = 0.02;
+	const double a1 = 1.1;
+	
+	const double x2 = 0.78;
+	const double s2 = 0.01;
+	const double a2 = 2.3;
+	
+	for( size_t i=1; i <= N; ++i )
+	{
+		X[i] = double(i-1)/(N-1);
+		Y[i] = a1 * gaussian( X[i], x1, s1) + a2 * gaussian( X[i], x2, s2 ) + 0.2 * ( 0.5 - alea<double>());
+		
+	}
+	
+	{
+		ios::ocstream fp( "mexp.dat", false );
+		for( size_t i=1; i <= N; ++i )
+		{
+			fp("%g %g\n", X[i], Y[i] );
+		}
+	}
+	
+	vector<double> a(6,0);
+	vector<bool>   used(6,true);
+	vector<double> aerr(6,0);
+    
+    vector< least_squares<double>::sample::pointer > Samples;
+    {
+        least_squares<double>::sample::pointer S( new least_squares<double>::sample( X, Y, Z ) );
+        Samples.push_back(S);
+    }
+
+	Gauss<double>                   G;
+	least_squares<double>::function F( &G, & Gauss<double>::Eval );
+    least_squares<double>::callback cb( &G, & Gauss<double>::ToDo );
+	least_squares<double>           LeastSquare;
+	a[1] = 1;
+	a[2] = 0.25;
+	a[3] = 0.01;
+	
+	a[4] = 1;
+	a[5] = 0.75;
+	a[6] = 0.01;
+	
+	
+	LeastSquare.h = 1e-4;
+	
+	if( LeastSquare( F, Samples, a, used, aerr, &cb) != least_squares_failure )
+	{
+		for( size_t i=1; i <= 6; ++i )
+		{
+			std::cerr << "a[" << i << "]=" << a[i] << " +/- " << aerr[i] << std::endl;
+		}
+		ios::ocstream fp( "mexp.6.dat", false );
+		for( size_t i=1; i <= N; ++i )
+		{
+			fp("%g %g %g\n", X[i], Y[i], Z[i] );
+		}
+	}
+}
+YOCTO_UNIT_TEST_DONE()
+
+
