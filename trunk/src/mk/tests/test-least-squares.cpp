b038e9f0eef370a7279d9ea70ae54dfcaf20784b
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-02-12 10:53:36 +0000

    start least_squares

---
+++ trunk/src/mk/tests/test-least-squares.cpp
@@ -0,0 +1,13 @@
+#include "yocto/utest/run.hpp"
+#include "yocto/math/fit/least-squares.hpp"
+
+
+using namespace yocto;
+using namespace math;
+
+
+YOCTO_UNIT_TEST_IMPL(least_squares)
+{
+    
+}
+YOCTO_UNIT_TEST_DONE()
