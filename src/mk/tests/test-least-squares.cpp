e59d64698f65b0ff8c5428328582759ef4a4b412
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-02-12 15:37:45 +0000

    least squares WIP

--- src/mk/tests/test-least-squares.cpp
+++ src/mk/tests/test-least-squares.cpp
@@ -1,13 +1,91 @@
 #include "yocto/utest/run.hpp"
 #include "yocto/math/fit/least-squares.hpp"
-
+#include "yocto/sequence/lw-array.hpp"
 
 using namespace yocto;
 using namespace math;
 
 
+namespace
+{
+    struct diffusion
+    {
+        
+        double compute( double t, const array<double> &a )
+        {
+            const double slope = a[1];
+            const double t0    = a[2];
+            return sqrt( slope*(t-t0) );
+        }
+        
+    };
+}
+
 YOCTO_UNIT_TEST_IMPL(least_squares)
 {
+    
+    
+    const double _t1[] = { 30,80,140,200,270,320,430,550,640,720,830,890 };
+    const double _x1[] = { 4.414520,5.011710,5.632319,6.194379,6.721311,7.330211,8.009368,8.735363,9.297424,9.707260,10.339578,10.878220};
+    
+    const double _t2[] = { 60,120,200,270,360,460,580,670,790,920,1040 };
+    const double _x2[] = { 5.199063,5.854801,6.662763,7.365340,8.067916,8.782201,9.578454,10.175644,10.878220,11.651054,12.213115};
+    
+    const lw_array<double> t1( (double*)_t1, sizeof(_t1)/sizeof(_t1[0]) );
+    const lw_array<double> x1( (double*)_x1, sizeof(_x1)/sizeof(_x1[0]) );
+    const lw_array<double> t2( (double*)_t2, sizeof(_t2)/sizeof(_t2[0]) );
+    const lw_array<double> x2( (double*)_x2, sizeof(_x2)/sizeof(_x2[0]) );
+    
+    vector<double> z1(t1.size(),0);
+    vector<double> z2(t2.size(),0);
+    
+    typedef least_squares<double> LSF;
+    typedef LSF::sample           Sample;
+    typedef Sample::pointer       SamplePtr;
+    diffusion     diff;
+    LSF::function F( &diff, & diffusion::compute );
+    
+    SamplePtr S1( new  Sample(t1,x1,z1) );
+    SamplePtr S2( new  Sample(t2,x2,z2) );
+
+    const size_t nvar = 3;
+    const size_t npar = 2;
+    
+    S1->prepare(nvar,npar);
+    S2->prepare(nvar,npar);
+    {
+        S1->Gamma[1][1] = 1;
+        S1->Gamma[2][3] = 1;
+        std::cerr << "Gamma1=" << S1->Gamma << std::endl;
+    }
+    
+    {
+        S2->Gamma[1][2] = 1;
+        S2->Gamma[2][3] = 1;
+        std::cerr << "Gamma2=" << S2->Gamma << std::endl;
+    }
+    
+    vector<double> a(3,0);
+    vector<bool>   used(3,true);
+    a[1] = 0.10;
+    a[2] = 0.10;
+    a[3] = 1;
+    
+    LSF lsf;
+    
+    vector<SamplePtr> samples(2,as_capacity);
+    samples.push_back(S1);
+    samples.push_back(S2);
+    
+    used[3] = true;
+    
+    vector<double> aerr( a.size(), 0 );
+    
+    lsf.verbose = true;
+    lsf(F,samples,a,used,aerr);
+    
+    std::cerr << "aorg=" << a    << std::endl;
+    std::cerr << "aerr=" << aerr << std::endl;
     
 }
 YOCTO_UNIT_TEST_DONE()
--- src/mk/yocto/math/fit/least-squares.hpp
+++ src/mk/yocto/math/fit/least-squares.hpp
@@ -6,65 +6,103 @@
 #include "yocto/ptr/arc.hpp"
 #include "yocto/counted.hpp"
 #include "yocto/functor.hpp"
+#include "yocto/math/fcn/derivative.hpp"
+#include "yocto/math/kernel/crout.hpp"
 
 namespace yocto
 {
- 
+    
     namespace math
     {
         
-        namespace fit
+        enum least_squares_result
         {
+            least_squares_success, //!< everyting OK
+            least_squares_failure, //!< singular point
+            least_squares_spurious //!< numerical problem
+        };
+        
+        
+        template <typename T>
+        class least_squares
+        {
+        public:
             
-            template <typename T>
-            class least_squares
+            typedef functor<T,TL2(T,const array<T>&)> function;
+            
+            class sample : public object, public counted
             {
             public:
+                const array<T> &X;
+                const array<T> &Y;
+                array<T>       &Z;
+                const size_t    N; //!< when prepared
                 
-                typedef functor<T,TL2(T,const array<T>&)> function;
+                virtual ~sample() throw();
+                explicit sample( const array<T> &aX, const array<T> &aY, array<T> &aZ ) throw();
                 
-                class sample : public object, public counted
-                {
-                public:
-                    const array<T> &X;
-                    const array<T> &Y;
-                    array<T>       &Z;
-                    const size_t    N; //!< when prepared
-                    
-                    virtual ~sample() throw();
-                    explicit sample( const array<T> &aX, const array<T> &aY, array<T> &aZ ) throw();
-                    
-                    typedef arc_ptr<sample> pointer;
-                    
-                    vector<T> beta;   //!< partial gradient
-                    matrix<T> alpha;  //!< partial curvature
-                    matrix<T> Gamma;  //!< variables
-                    vector<T> u;      //!< reduced variables
-                    vector<T> dFdu;   //!< reduced gradient
-                    T         D2;     //!< least squares value
-                    
-                    T compute_least_squares( function &F, const array<T> &a );
-                    
-                    //! prepare all
-                    /**
-                     \param nvar total number of variables
-                     \param npar number of parameters for the fit function
-                     */
-                    void prepare( size_t nvar, size_t npar );
-                    void release() throw();
-                    
-                private:
-                    YOCTO_DISABLE_COPY_AND_ASSIGN(sample);
-                };
+                typedef arc_ptr<sample> pointer;
                 
+                vector<T> beta;   //!< partial gradient
+                matrix<T> alpha;  //!< partial curvature
+                matrix<T> Gamma;  //!< variables
+                vector<T> u;      //!< reduced variables
+                vector<T> dFdu;   //!< reduced gradient
+                vector<T> dFda;   //!< full gradient
+                T         D2;     //!< least squares value
                 
-                virtual ~least_squares() throw();
+                T compute_D2( function &F, const array<T> &a );
+                T compute_D2( function &F, const array<T> &a, const array<bool> &used, derivative<T> &drvs, const T h);
+                
+                //! prepare all
+                /**
+                 \param nvar total number of variables
+                 \param npar number of parameters for the fit function
+                 */
+                void prepare( size_t nvar, size_t npar );
+                void release() throw();
                 
             private:
-                
+                YOCTO_DISABLE_COPY_AND_ASSIGN(sample);
             };
             
-        }
+            typedef array<typename sample::pointer> samples;
+            
+            explicit least_squares();
+            virtual ~least_squares() throw();
+            
+            T             D2;    //!< global D2
+            derivative<T> drvs;  //!< to compute derivatives
+            T             h;     //!< for derivatives, default = 1e-4
+            T             ftol;  //!< for convergence, default = numeric<T>::ftol
+            bool          verbose; //!< little printout, default is false
+            
+            least_squares_result operator()(function                        &F,
+                                            samples                         &Samples,
+                                            array<T>                        &Aorg,
+                                            const array<bool>               &Used,
+                                            array<T>                        &Aerr
+                                            );
+            
+        private:
+            function                        *proc;
+            array<typename sample::pointer> *data;
+            size_t                           nvar;
+            size_t                           nums;
+            matrix<T>                        alpha;
+            matrix<T>                        curv;
+            vector<T>                        beta;
+            array<T>                        *aorg;
+            const array<bool>               *used;
+            vector<T>                        atmp;
+            crout<T>                         LU;
+            
+            T    compute_D2_org();
+            T    compute_D2_tmp();
+            bool build_curvature(T lambda);
+        };
+        
+        
         
     }
     
