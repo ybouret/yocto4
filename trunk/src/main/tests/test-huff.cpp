7b945b3dd7934038d910ee3bd6281e12328710fa
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-02-27 16:09:59 +0000

    huffman: codes need to be rewritten

--- trunk/src/main/tests/test-huff.cpp
+++ trunk/src/main/tests/test-huff.cpp
@@ -14,14 +14,15 @@ YOCTO_UNIT_TEST_IMPL(huff)
     ios::icstream     input( ios::cstdin );
     char C = 0;
     
+    bool use2 = false;
     
     Huffman::encoder H1;
     Huffman::encoder H2;
-
+    
     move_to_front    mtf;
     ios::ocstream fp1("huff1.dat",false);
     ios::ocstream fp2("huff2.dat",false);
-
+    
     size_t ni = 0;
     size_t n1 = 0;
     size_t n2 = 0;
@@ -29,42 +30,57 @@ YOCTO_UNIT_TEST_IMPL(huff)
     {
         ++ni;
         H1.write(C);
-        H2.write( mtf.encode(C) );
+        if(use2)
+        {
+            H2.write( mtf.encode(C) );
+        }
         while( H1.query(C) )
         {
             fp1.write(C);
             ++n1;
         }
-        
-        while( H1.query(C) )
+        if(use2)
         {
-            fp2.write(C);
-            ++n2;
+            while( H2.query(C) )
+            {
+                fp2.write(C);
+                ++n2;
+            }
         }
         
     }
     H1.flush();
-    H2.flush();
     while( H1.query(C) )
     {
         fp1.write(C);
         ++n1;
     }
-    while( H2.query(C) )
+    
+    if(use2)
     {
-        fp2.write(C);
-        ++n2;
+        H2.flush();
+        while( H2.query(C) )
+        {
+            fp2.write(C);
+            ++n2;
+        }
     }
-
+    
     std::cerr << "#input   = " << ni << std::endl;
     std::cerr << "#output1 = " << n1 << std::endl;
-    std::cerr << "#output2 = " << n2 << std::endl;
+    if(use2)
+    {
+        std::cerr << "#output2 = " << n2 << std::endl;
+    }
     
     std::cerr << "H1:" << std::endl;
     std::cerr << H1.alphabet() << std::endl;
     
-    std::cerr << "H2:" << std::endl;
-    std::cerr << H2.alphabet() << std::endl;
+    if(use2)
+    {
+        std::cerr << "H2:" << std::endl;
+        std::cerr << H2.alphabet() << std::endl;
+    }
     
 }
 YOCTO_UNIT_TEST_DONE()
