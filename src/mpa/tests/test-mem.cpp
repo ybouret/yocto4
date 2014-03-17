16042455aee3958f698d981d711f0274d0d8c3ce
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-03-17 11:29:39 +0000

    mpa WIP

--- src/mpa/tests/test-mem.cpp
+++ src/mpa/tests/test-mem.cpp
@@ -1,17 +1,19 @@
 #include "yocto/utest/run.hpp"
-#include "yocto/mpa/types.hpp"
+#include "yocto/mpa/natural.hpp"
 #include "yocto/code/rand.hpp"
+#include "yocto/sequence/vector.hpp"
 
 using namespace yocto;
 
-namespace
-{
-    
-}
 
 YOCTO_UNIT_TEST_IMPL(mem)
 {
-    
+    vector<mpn> u;
+    for(size_t i=1000;i>0;--i)
+    {
+        const mpn x( alea_leq(512), as_capacity);
+        u.push_back(x);
+    }
 }
 YOCTO_UNIT_TEST_DONE()
 
