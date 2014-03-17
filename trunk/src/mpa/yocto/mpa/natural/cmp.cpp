4d272b3ab3be633737601834025c6d4acaee6fbd
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-03-17 14:49:43 +0000

    mpa WIP

---
+++ trunk/src/mpa/yocto/mpa/natural/cmp.cpp
@@ -0,0 +1,41 @@
+#include "yocto/mpa/natural.hpp"
+#include "yocto/code/bswap.hpp"
+#include "yocto/code/round.hpp"
+
+namespace yocto
+{
+    namespace mpa
+    {
+        
+        int natural:: compare( const natural &lhs, const natural &rhs ) throw()
+        {
+            YOCTO_CHECK_MPN(&lhs);
+            YOCTO_CHECK_MPN(&rhs);
+            const size_t L = lhs.size;
+            const size_t R = rhs.size;
+            if(L<R)
+            {
+                return -1;
+            }
+            else
+            {
+                if(R<L)
+                    return 1;
+                else
+                {
+                    const uint8_t *l = lhs.byte;
+                    const uint8_t *r = rhs.byte;
+                    size_t i = L;
+                    while(i-->0)
+                    {
+                        const int ll = l[i];
+                        const int rr = r[i];
+                        if(ll!=rr) return ll-rr;
+                    }
+                    return 0; // all bytes equal
+                }
+            }
+        }
+    }
+
+}
