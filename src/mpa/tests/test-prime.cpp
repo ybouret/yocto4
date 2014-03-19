b86fb483bc309baf42d1e3f1c68d68912c785eb6
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-03-19 14:02:33 +0000

    rsa/crt implemented

--- src/mpa/tests/test-prime.cpp
+++ src/mpa/tests/test-prime.cpp
@@ -49,20 +49,31 @@ YOCTO_UNIT_TEST_IMPL(rsa)
     mpn d = mpn::mod_inv(e, phi);
     std::cerr << "d=" << d << std::endl;
     const size_t mbits = n.bits() -1;
-    std::cerr << "#n = " << n.bits() << std::endl;
-    for(size_t i=0; i <100; ++i )
+    //std::cerr << "#n = " << n.bits() << std::endl;
+    
+    const mpn dp = d % p1;
+    const mpn dq = d % q1;
+    const mpn iq = mpn::mod_inv(q, p);
+    
+    for(size_t i=0; i <10; ++i )
     {
         const mpn M = mpn::rand(mbits);
         const mpn C = mpn::mod_exp(M, e, n);
         const mpn P = mpn::mod_exp(C,d,n);
-        if(false)
-        {
-            std::cerr << "C=" << C << std::endl;
-            std::cerr << "M=" << M << std::endl;
-            std::cerr << "P=" << P << std::endl;
-        }
-        if( P != M)
-            throw exception("rsa failure");
+        
+        
+        mpn       M1 = mpn::mod_exp(C, dp, p);
+        const mpn M2 = mpn::mod_exp(C, dq, q);
+        while(M1<M2) M1 += p;
+        M1 -= M2;
+        M1 *= iq;
+        const mpn h = M1 % p;
+        const mpn Q = M2 + h * q;
+        
+        std::cerr << M << " => " << C << " => " << P  << " / " << Q << std::endl;
+        
+        if( P != M) throw exception("rsa failure");
+        if( Q != M) throw exception("crt failure");
     }
     
     
