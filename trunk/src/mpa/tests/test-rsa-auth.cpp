9e6e4fd9f9bba064a106f29706eb7ee24f005a88
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-03-24 14:37:00 +0000

    basic RSA encoder

--- trunk/src/mpa/tests/test-rsa-auth.cpp
+++ trunk/src/mpa/tests/test-rsa-auth.cpp
@@ -1,6 +1,7 @@
 #include "yocto/utest/run.hpp"
-#include "yocto/mpa/rsa/keys.hpp"
+#include "yocto/mpa/rsa/codec.hpp"
 #include "yocto/ios/ocstream.hpp"
+#include "yocto/ios/icstream.hpp"
 #include "yocto/ios/imstream.hpp"
 #include "yocto/sequence/vector.hpp"
 
@@ -26,6 +27,20 @@ YOCTO_UNIT_TEST_IMPL(rsa_auth)
         }
     }
     std::cerr << "Loaded #" << Keys.size() << " keys" << std::endl;
+    
+    string line;
+    ios::icstream inp( ios::cstdin );
+    RSA::encoder  enc(Keys[1]);
+    while( line.clear(), inp.read_line(line) >= 0 )
+    {
+        for(size_t iter=1;iter<=4;++iter)
+        {
+            const string s_enc = enc.to_string(line);
+            s_enc.output_visible(std::cerr<<"Enc=") << std::endl;
+        }
+    }
+    
+    
     
 }
 YOCTO_UNIT_TEST_DONE()
