9e0e4d335e5bb3e1dc80b48b5ad37984fc9e9a9f
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-03-26 09:35:30 +0000

    mpa integer WIP

--- trunk/src/mpa/yocto/mpa/integer/add.cpp
+++ trunk/src/mpa/yocto/mpa/integer/add.cpp
@@ -4,10 +4,92 @@ namespace yocto
 {
     namespace mpa
     {
+        
         integer integer::add(const  integer &lhs, const  integer &rhs)
         {
-            
-            return integer();
+            switch(lhs.s)
+            {
+                    //__________________________________________________________
+                    // lhs
+                case __negative:
+                    //__________________________________________________________
+                    switch (rhs.s) {
+                        case __negative:
+                        {
+                            const natural sum = lhs.n + rhs.n;
+                            return integer( __negative, sum );
+                        }
+                            
+                        case __zero:
+                            return lhs;
+                            
+                        case __positive:
+                            switch( int2sign( natural::compare(lhs.n, rhs.n)))
+                        {
+                            case __negative:
+                            {
+                                const natural dif = rhs.n - lhs.n;
+                                return integer(__positive,dif);
+                            }
+                            case __zero:
+                                return integer();
+                                
+                            case __positive:
+                            {
+                                const natural dif = lhs.n - rhs.n;
+                                return integer(__negative,dif);
+                            }
+                        }
+                    }
+                    
+                    //__________________________________________________________
+                    // lhs
+                case __zero:
+                    //__________________________________________________________
+                    return rhs;
+                    
+                    //__________________________________________________________
+                    // lhs
+                case __positive:
+                    //__________________________________________________________
+                    switch (rhs.s) {
+                        case __negative:
+                            switch( int2sign( natural::compare(lhs.n, rhs.n)))
+                        {
+                            case __negative:
+                            {
+                                const natural dif = rhs.n - lhs.n;
+                                return integer(__negative,dif);
+                            }
+                                
+                            case __zero:
+                                return integer();
+                                
+                            case __positive:
+                            {
+                                const natural dif = lhs.n - rhs.n;
+                                return integer(__positive,dif);
+                            }
+                        }
+                            
+                        case __zero:
+                            return lhs;
+                            
+                        case __positive:
+                        {
+                            const natural sum = lhs.n + rhs.n;
+                            return integer( __positive, sum );
+                        }
+                    }
+                    
+            }
         }
+        
+        
+        integer operator+(const integer &lhs, const integer &rhs)
+        {
+            return integer::add(lhs,rhs);
+        }
+        
     }
 }
