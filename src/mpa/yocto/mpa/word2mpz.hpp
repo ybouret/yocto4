d7c5a581860fb76a0ea1a5e43f3f45e3cbe91d89
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-03-26 08:11:45 +0000

    WIP integer

---
+++ src/mpa/yocto/mpa/word2mpz.hpp
@@ -0,0 +1,18 @@
+#ifndef YOCTO_MPA_WORD2MPZ_INCLUDED
+#define YOCTO_MPA_WORD2MPZ_INCLUDED 1
+
+#include "yocto/mpa/integer.hpp"
+#include "yocto/code/round.hpp"
+#include "yocto/code/endian.hpp"
+#include "yocto/code/cast.hpp"
+
+namespace yocto
+{
+    namespace mpa
+    {
+
+    }
+
+}
+
+#endif
