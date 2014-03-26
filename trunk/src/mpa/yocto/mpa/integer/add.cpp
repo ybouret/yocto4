39ab68c96e1e4043097e00890a9e08ca974edcd9
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-03-26 08:11:45 +0000

    WIP integer

---
+++ trunk/src/mpa/yocto/mpa/integer/add.cpp
@@ -0,0 +1,13 @@
+#include "yocto/mpa/word2mpz.hpp"
+
+namespace yocto
+{
+    namespace mpa
+    {
+        integer integer::add(const  integer &lhs, const  integer &rhs)
+        {
+            
+            return integer();
+        }
+    }
+}
