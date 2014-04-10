8af7f95a0fc49d212416d9640e6d5003a4e32915
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-04-10 08:22:53 +0000

    corrected memory leak for win32

--- trunk/src/main/yocto/fs/local-fs.cpp
+++ trunk/src/main/yocto/fs/local-fs.cpp
@@ -21,74 +21,74 @@
 
 namespace yocto
 {
-
-
+    
+    
 	const char local_fs::name[] = "local-fs";
-
+    
 	local_fs:: local_fs() throw()
 	{
 	}
-
+    
 	local_fs:: ~local_fs() throw()
 	{
-
+        
 	}
-
+    
 	vfs::entry::attribute local_fs:: query_attribute( const string &path, bool &is_link ) const throw()
 	{
 		is_link = false;
-
+        
 #if defined(YOCTO_WIN)
 		const DWORD m = ::GetFileAttributes( TEXT(&path[0]) );
-
+        
 		if( m == INVALID_FILE_ATTRIBUTES )
 			return vfs::entry::no_ent;
 		if( m &  FILE_ATTRIBUTE_DIRECTORY )
 			return vfs::entry::is_dir;
 		if( m & (FILE_ATTRIBUTE_NORMAL|FILE_ATTRIBUTE_ARCHIVE) )
 			return vfs::entry::is_reg;
-
+        
 		return vfs::entry::is_unk;
 #endif
-
-
+        
+        
 #if defined(YOCTO_BSD)
 		struct stat buf;
 		const mode_t *m = &(buf.st_mode);
-
-
-
+        
+        
+        
 		if( lstat( &path[0], &buf ) != 0 )
 			return  vfs::entry::no_ent;
-
-
-
+        
+        
+        
 		if( S_ISLNK( (*m) ) ) {
 			/* follow link */
 			is_link = true;
-
+            
 			if( stat( &path[0], &buf ) !=0 )
 				return  vfs::entry::no_ent; /* TODO: loop ? */
 		}
-
-
+        
+        
 		if( S_ISDIR( (*m) ) )
 			return  vfs::entry::is_dir;
-
+        
 		if( S_ISREG( (*m) ) )
 			return  vfs::entry::is_reg;
-
-
+        
+        
 		return vfs::entry::is_unk;
 #endif /* BSD */
 	}
-
+    
 	void local_fs:: create_dir(  const string &dirname, const bool allow_already_exists )
 	{
 #if defined(YOCTO_BSD)
 		YOCTO_GIANT_LOCK();
 		if( mkdir( &dirname[0],  (S_IRUSR | S_IWUSR | S_IXUSR) | (S_IRGRP | S_IXGRP ) ) < 0 ) {
-
+            
 			const int err = errno;
 			if( err == EEXIST &&  allow_already_exists ) {
 				return;
@@ -96,14 +96,14 @@ namespace yocto
 			else {
 				throw libc::exception( err, "create_dir(%s)", &dirname[0]);
 			}
-
+            
 		}
 #endif
-
+        
 #if defined(YOCTO_WIN)
 		YOCTO_GIANT_LOCK();
 		const DWORD attr = ::GetFileAttributes( &dirname[0] );
-
+        
 		if( (attr != INVALID_FILE_ATTRIBUTES) && ( (attr & FILE_ATTRIBUTE_DIRECTORY) != 0 ) ) {
 			if( !allow_already_exists  )
 			{
@@ -112,15 +112,15 @@ namespace yocto
 			else
 				return;
 		}
-
+        
 		if( ! ::CreateDirectory( &dirname[0], NULL ) )
 		{
 			throw win32::exception( ::GetLastError(), "create_dir(%s)", &dirname[0] );
 		}
 #endif
-
+        
 	}
-
+    
 	void local_fs:: remove_dir(  const string &dirname   )
 	{
 		YOCTO_GIANT_LOCK();
@@ -129,14 +129,14 @@ namespace yocto
 			throw libc::exception( errno, "remove_dir(%s)", &dirname[0] );
 		}
 #endif
-
+        
 #if defined(YOCTO_WIN)
 		if(! ::RemoveDirectory( &dirname[0] ) ) {
 			throw win32::exception( ::GetLastError(), "remove_dir(%s)", &dirname[0] );
 		}
 #endif
 	}
-
+    
 	void local_fs:: remove_file( const string &filename )
 	{
 		YOCTO_GIANT_LOCK();
@@ -146,7 +146,7 @@ namespace yocto
 			throw libc::exception( errno, "unlink(%s)", &filename[0] );
 		}
 #endif
-
+        
 #if defined(YOCTO_WIN)
 		if( ! ::DeleteFile( &filename[0] ) )
 		{
@@ -154,11 +154,11 @@ namespace yocto
 		}
 #endif
 	}
-
+    
 	uint64_t local_fs:: get_file_size( const string &path ) const
 	{
 		YOCTO_GIANT_LOCK();
-
+        
 #if defined(YOCTO_BSD)
 #  if defined(YOCTO_APPLE) || defined(YOCTO_FREEBSD)
 #    define Y_FS_STAT stat
@@ -175,30 +175,32 @@ namespace yocto
 			throw libc::exception( EINVAL, "stat: not a regular file");
 		return s.st_size;
 #endif
-
+        
 #if defined(YOCTO_WIN)
-			LARGE_INTEGER nLargeInteger = { 0 } ;
-			HANDLE hFile = CreateFile(path.c_str(),
-				GENERIC_READ,
-				FILE_SHARE_READ,
-				NULL,
-				OPEN_EXISTING,
-				FILE_ATTRIBUTE_READONLY,
-				NULL);
-			if( INVALID_HANDLE_VALUE == hFile )
-			{
-				throw win32::exception( ::GetLastError(), "OpenExistingFile" );
-			}
-			
-			if( !::GetFileSizeEx(hFile, &nLargeInteger) )
-			{
-				throw win32::exception( ::GetLastError(), "GetFileSizeEx" );
-			}
-
-			return uint64_t(nLargeInteger.QuadPart);
+        LARGE_INTEGER nLargeInteger = { 0 } ;
+        HANDLE hFile = CreateFile(path.c_str(),
+                                  GENERIC_READ,
+                                  FILE_SHARE_READ,
+                                  NULL,
+                                  OPEN_EXISTING,
+                                  FILE_ATTRIBUTE_READONLY,
+                                  NULL);
+        if( INVALID_HANDLE_VALUE == hFile )
+        {
+            throw win32::exception( ::GetLastError(), "OpenExistingFile" );
+        }
+        
+        if( !::GetFileSizeEx(hFile, &nLargeInteger) )
+        {
+            ::CloseHandle(hFile);
+            throw win32::exception( ::GetLastError(), "GetFileSizeEx" );
+        }
+        
+        ::CloseHandle(hFile);
+        return uint64_t(nLargeInteger.QuadPart);
 #endif
 		throw exception("get_file_size not implemented");
 	}
-
-
+    
+    
 }
