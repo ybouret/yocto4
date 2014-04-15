569202dac4b08a423066e40b195d7016096cdc5d
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-04-15 13:43:24 +0000

    cyclic smoother is ok

--- src/mk/yocto/math/sig/smoother.cxx
+++ src/mk/yocto/math/sig/smoother.cxx
@@ -1,5 +1,13 @@
 #include "yocto/math/ztype.hpp"
+#include "yocto/math/types.hpp"
 #include "yocto/math/sig/smoother.hpp"
+#include "yocto/exception.hpp"
+#include "yocto/code/utils.hpp"
+#include "yocto/code/ipower.hpp"
+#include "yocto/math/kernel/crout.hpp"
+
+#include "yocto/ios/ocstream.hpp"
+
 
 namespace yocto
 {
@@ -11,8 +19,215 @@ namespace yocto
         }
         
         template <>
-        smoother<real_t>:: smoother()
+        smoother<real_t>:: smoother() throw() :
+        lower_range(0),
+        upper_range(0),
+        degree(0),
+        v()
         {
+            
+        }
+        
+        namespace
+        {
+            static inline int __compare_x( const v2d<real_t> &lhs, const v2d<real_t> &rhs ) throw()
+            {
+                return (lhs.x < rhs.x) ? -1 : (rhs.x<lhs.x? 1 : 0);
+            }
+            
+        }
+        
+        
+        template <>
+        real_t smoother<real_t>:: operator()(size_t i,
+                                             const array<real_t>    &X,
+                                             const array<real_t>    &Y,
+                                             const extender<real_t> &E,
+                                             real_t                 *dYdX)
+        {
+            assert(X.size()==Y.size());
+            assert(i>0);
+            assert(i<=X.size());
+            assert(upper_range>=0);
+            assert(lower_range>=0);
+            
+            //__________________________________________________________________
+            //
+            // Initialize
+            //__________________________________________________________________
+            const ptrdiff_t N = ptrdiff_t(X.size());
+            v.free();
+            switch(N)
+            {
+                case 0:
+                    if(dYdX) *dYdX = 0;
+                    return 0;
+                    
+                case 1:
+                    if(dYdX) *dYdX = 0;
+                    return Y[1];
+                    
+                default:
+                    break;
+            }
+            
+            vtx_t tmp(0,Y[i]);
+            v.push_back(tmp);
+            const real_t xi = X[i];
+            
+            //__________________________________________________________________
+            //
+            // Collect Points
+            //__________________________________________________________________
+            if(extend_cyclic==E.lower)
+            {
+                assert(extend_cyclic==E.upper);
+                const size_t nmin = dYdX ? 1 : 0;
+                
+                {
+                    size_t nl = 0;
+                    for(ptrdiff_t j=i-1;;--j)
+                    {
+                        const real_t xj = E.get_x(j, X, N);
+                        const real_t dx = xj - xi;
+                        if( nl >= nmin && Fabs(dx) > lower_range)
+                            break;
+                        tmp.x = dx;
+                        tmp.y = E.get_y(j,Y,N);
+                        v.push_back(tmp);
+                        ++nl;
+                    }
+                }
+                
+                {
+                    size_t nr = 0;
+                    for(ptrdiff_t j=i+1;;++j)
+                    {
+                        const real_t xj = E.get_x(j, X, N);
+                        const real_t dx = xj - xi;
+                        if( nr >= nmin && Fabs(dx) > lower_range)
+                            break;
+                        tmp.x = dx;
+                        tmp.y = E.get_y(j,Y,N);
+                        v.push_back(tmp);
+                        ++nr;
+                    }
+                }
+                assert(v.size()>=1+2*nmin);
+                
+            }
+            else
+            {
+                throw exception("not implemented");
+            }
+            
+            //__________________________________________________________________
+            //
+            // window size
+            //__________________________________________________________________
+            const size_t W = v.size();
+            
+            //__________________________________________________________________
+            //
+            // polynomial #coefficients
+            //__________________________________________________________________
+            const size_t   m = min_of<size_t>(degree+1,W);
+            
+            
+            //__________________________________________________________________
+            //
+            // prepare the rhs
+            //__________________________________________________________________
+            vector<real_t>  a(m,REAL(0.0));
+            
+            for(size_t r=1;r<=m;++r)
+            {
+                const size_t p=r-1;
+                real_t       s=0;
+                list<vtx_t>::iterator i = v.begin();
+                for(size_t j=W;j>0;--j,++i)
+                {
+                    const vtx_t &q = *i;
+                    s += q.y * ipower(q.x,p);
+                }
+                a[r] = s;
+            }
+            
+            //__________________________________________________________________
+            //
+            // prepare the momenta
+            //__________________________________________________________________
+            matrix<real_t> mu(m,m);
+            for(size_t r=1;r<=m;++r)
+            {
+                for(size_t c=r;c<=m;++c)
+                {
+                    const size_t p=(r+c-2);
+                    real_t       s=0;
+                    list<vtx_t>::iterator i = v.begin();
+                    for(size_t j=W;j>0;--j,++i)
+                    {
+                        const vtx_t &q = *i;
+                        s += ipower(q.x,p);
+                    }
+                    mu[r][c] = mu[c][r] = s;
+                }
+            }
+            
+            //__________________________________________________________________
+            //
+            // solve the polynomial coefficients
+            //__________________________________________________________________
+            crout<real_t> lu(m);
+            if( !lu.build(mu) )
+                throw exception("singular smoothing window around X[%d]=%g", int(i), xi);
+            crout<real_t>::solve(mu,a);
+            //std::cerr << "a=" << a << std::endl;
+            if( dYdX )
+            {
+                assert(m>=2);
+                *dYdX = a[2];
+            }
+            
+            if(i==1||i==N||i==N/2)
+            {
+                
+                ios::ocstream fp( vformat("window%d.dat",int(i)), false);
+                v.sort( __compare_x );
+                for(list<vtx_t>::iterator i=v.begin();i!=v.end();++i)
+                {
+                    const vtx_t &q = *i;
+                    fp("%g %g\n", q.x+xi, q.y);
+                }
+            }
+            
+            return a[1];
+        }
+        
+        
+        template <>
+        void smoother<real_t>:: operator()(array<real_t>          &Z,
+                                           const array<real_t>    &X,
+                                           const array<real_t>    &Y,
+                                           const extender<real_t> &E,
+                                           array<real_t>          *dZdX)
+        {
+            assert(Z.size()==X.size());
+            assert(Z.size()==Y.size());
+            const size_t N = Z.size();
+            smoother<real_t> &self = *this;
+            if(dZdX)
+            {
+                assert(dZdX->size()==Z.size());
+                array<real_t> &diff = *dZdX;
+                for(size_t i=1;i<=N;++i)
+                    Z[i] = self(i,X,Y,E, &diff[i]);
+            }
+            else
+            {
+                for(size_t i=1;i<=N;++i)
+                    Z[i] = self(i,X,Y,E, 0);
+            }
             
         }
         
--- src/mk/yocto/math/sig/smoother.hpp
+++ src/mk/yocto/math/sig/smoother.hpp
@@ -2,18 +2,42 @@
 #define YOCTO_MK_SIG_SMOOTHER_INCLUDED 1
 
 #include "yocto/math/sig/extender.hpp"
+#include "yocto/sequence/list.hpp"
+#include "yocto/math/v2d.hpp"
 
 namespace yocto
 {
     namespace math
     {
         
-        template <typename>
+        template <typename T>
         class smoother
         {
         public:
-            explicit smoother();
+            typedef v2d<T> vtx_t;
+            
+            explicit smoother() throw();
             virtual ~smoother() throw();
+            
+            T           lower_range;
+            T           upper_range;
+            size_t      degree;
+            
+            list<vtx_t> v;
+            
+            T operator()(size_t             i,
+                         const array<T>    &X,
+                         const array<T>    &Y,
+                         const extender<T> &E,
+                         T                 *dYdX);
+            
+            void operator()(array<T>          &Z,
+                            const array<T>    &X,
+                            const array<T>    &Y,
+                            const extender<T> &E,
+                            array<T>          *dZdX);
+            
+            
             
         private:
             YOCTO_DISABLE_COPY_AND_ASSIGN(smoother);
