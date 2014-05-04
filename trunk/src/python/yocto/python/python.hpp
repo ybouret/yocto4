0fb31b7763e7a8d723e512042d30d520258cc31a
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-05-04 09:04:53 +0000

    basic python

--- trunk/src/python/yocto/python/python.hpp
+++ trunk/src/python/yocto/python/python.hpp
@@ -2,9 +2,26 @@
 #define YOCTO_PYTHON_INCLUDED
 
 #include <Python.h>
+#include "yocto/threading/singleton.hpp"
 
 namespace yocto
 {
+ 
+    class python : public singleton<python>
+    {
+    public:
+        
+        static const char name[];
+        void Initialize();
+        void Finalize() throw();
+        
+    private:
+        YOCTO_DISABLE_COPY_AND_ASSIGN(python);
+        friend class singleton<python>;
+        explicit python() throw();
+        virtual ~python() throw();
+        static const threading::longevity life_time = 10;
+    };
     
 }
 
