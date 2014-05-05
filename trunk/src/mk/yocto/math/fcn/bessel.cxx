879ef5d2c1f877c39fc5a11bc1bc93c1f1500a46
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-05-05 08:50:10 +0000

    update bessel

--- trunk/src/mk/yocto/math/fcn/bessel.cxx
+++ trunk/src/mk/yocto/math/fcn/bessel.cxx
@@ -6,7 +6,7 @@ namespace yocto
 {
     namespace math
     {
-        
+        ////////////////////////////////////////////////////////////////////////
         template <>
         real_t bessel<real_t>::j0( real_t x )
         {
@@ -35,11 +35,13 @@ namespace yocto
             return real_t(ans);
         }
         
+        ////////////////////////////////////////////////////////////////////////
         template <>
         real_t bessel<real_t>::y0( real_t x )
         {
             double z;
             double xx,y,ans,ans1,ans2;
+            assert(x>0);
             
             if (x < 8.0)
             {
@@ -59,6 +61,43 @@ namespace yocto
                 ans = sqrt(0.636619772/x)*(sin(xx)*ans1+z*cos(xx)*ans2);
             }
             return real_t(ans);
+        }
+        
+        ////////////////////////////////////////////////////////////////////////
+        template <>
+        real_t bessel<real_t>::i0( real_t x )
+        {
+            double ax,ans;
+            double y;
+            
+            if ((ax=fabs(x)) < 3.75)
+            {
+                y=x/3.75;
+                y*=y;
+                ans=1.0+y*(3.5156229+y*(3.0899424+y*(1.2067492
+                                                     +y*(0.2659732+y*(0.360768e-1+y*0.45813e-2)))));
+            } else {
+                y=3.75/ax;
+                ans=(exp(ax)/sqrt(ax))*(0.39894228+y*(0.1328592e-1
+                                                      +y*(0.225319e-2+y*(-0.157565e-2+y*(0.916281e-2 +y*(-0.2057706e-1+y*(0.2635537e-1+y*(-0.1647633e-1 +y*0.392377e-2))))))));
+            }
+            return ans;
+        }
+        
+        ////////////////////////////////////////////////////////////////////////
+        template <>
+        real_t bessel<real_t>::k0( real_t x )
+        {
+            assert(x>0);
+            double y,ans;
+            if (x <= 2.0) {
+                y=x*x/4.0;
+                ans=(-log(x/2.0)*bessel<double>::i0(x))+(-0.57721566+y*(0.42278420 +y*(0.23069756+y*(0.3488590e-1+y*(0.262698e-2 +y*(0.10750e-3+y*0.74e-5))))));
+            } else {
+                y=2.0/x;
+                ans=(exp(-x)/sqrt(x))*(1.25331414+y*(-0.7832358e-1 +y*(0.2189568e-1+y*(-0.1062446e-1+y*(0.587872e-2 +y*(-0.251540e-2+y*0.53208e-3))))));
+            }
+            return ans;
         }
         
     }
