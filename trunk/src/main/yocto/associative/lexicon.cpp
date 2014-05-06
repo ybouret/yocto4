b580ea8d9c57da81ae1775092b129af9ea4ef247
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-05-06 13:07:08 +0000

    lexicon seems ok

--- trunk/src/main/yocto/associative/lexicon.cpp
+++ trunk/src/main/yocto/associative/lexicon.cpp
@@ -1,10 +1,18 @@
 #include "yocto/associative/lexicon.hpp"
 
+#include <iostream>
+
 namespace yocto
 {
     namespace hidden
     {
         const char lexicon_name[] = "lexicon";
         
+        void lexicon_hasher_is_invalid()
+        {
+            std::cerr << "You Need To Define a Specific Hasher" << std::endl;
+            exit(1);
+        }
+
     }
 }
