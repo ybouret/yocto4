5cce2479d5b0d7cf96496809769de38e4492ddf3
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-05-06 09:48:49 +0000

    lexicon WIP

--- src/main/yocto/associative/lexicon.hpp
+++ src/main/yocto/associative/lexicon.hpp
@@ -5,9 +5,12 @@
 #include "yocto/associative/key-hasher.hpp"
 #include "yocto/core/list.hpp"
 #include "yocto/core/pool.hpp"
+#include "yocto/code/utils.hpp"
 
 namespace yocto
 {
+    
+    
     //! each object IS a key
     /**
      The key part of each object MUST be constant...
@@ -21,7 +24,10 @@ namespace yocto
     {
     public:
         YOCTO_ASSOCIATIVE_KEY_T;
-        
+        static const size_t LOAD = 4;
+        //======================================================================
+        // dynamic node to store data
+        //======================================================================
         class DataNode
         {
         public:
@@ -66,13 +72,21 @@ namespace yocto
                 return node;
             }
             
+            inline void cache()
+            {
+                pool.store( object::acquire1<DataNode>() );
+            }
+            
         private:
             YOCTO_DISABLE_COPY_AND_ASSIGN(DataNodePool);
             core::pool_of<DataNode> pool;
+
         };
         
         
-        
+        //======================================================================
+        // dynamic node to store hash key
+        //======================================================================
         class HashNode
         {
         public:
@@ -106,6 +120,12 @@ namespace yocto
                 return new ( pool.size ? pool.query() : object::acquire1<HashNode>() ) HashNode(hkey,addr);
             }
             
+            inline void cache()
+            {
+                pool.store( object::acquire1<HashNode>() );
+            }
+            
+            
         private:
             YOCTO_DISABLE_COPY_AND_ASSIGN(HashNodePool);
             core::pool_of<HashNode> pool;
@@ -113,20 +133,154 @@ namespace yocto
         
         typedef core::list_of<HashNode> HashSlot;
         
+        //======================================================================
+        // API
+        //======================================================================
+        inline explicit lexicon() throw() :
+        ldata(),
+        nslot(0),
+        hmask(nslot-1),
+        slots(0),
+        count(0),
+        hash(),
+        hmem()
+        {
+        }
+        
+        inline virtual ~lexicon() throw()
+        {
+            __release();
+        }
+        
+        inline void free() throw() { __free(); }
+        
+        inline type * search( param_key k )
+        {
+            const size_t h    = hash(k);
+            HashSlot    *slot = 0;
+            DataNode    *node = lookup(h, k, slot);
+            if(node)
+            {
+                return & (node->data);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        
+        
         
     private:
-        core::list_of<DataNode>       data_list;
+        core::list_of<DataNode>       ldata;  //!< key ordered data
+        size_t                        nslot;  //!< a power of two >= data_list/LOAD;
+        size_t                        hmask;  //!< num_slots-1
+        HashSlot                     *slots;  //!< memory
+        size_t                        count;  //!< for memory
         
-        
-    public:
         DataNodePool                  data_nodes;
         core::pool_of<HashNode>       hash_nodes;
         
     private:
         YOCTO_DISABLE_COPY_AND_ASSIGN(lexicon);
-       
+        
         mutable KEY_HASHER            hash;
         ALLOCATOR                     hmem;
+        
+        inline void __free() throw()
+        {
+            for(size_t i=0;i<nslot;++i)
+            {
+                HashSlot &src = slots[i];
+                while(src.size) hash_nodes.store(src.pop_back());
+            }
+            while(ldata.size)
+            {
+                data_nodes.store( ldata.pop_back() );
+            }
+        }
+        
+        inline void __release() throw()
+        {
+            __free();
+            hmem.template release_as<HashSlot>(slots,count);
+            nslot = 0;
+            hmask = nslot-1;
+        }
+        
+        //! reserve supplementary #nodes
+        inline void reserve_nodes( size_t n )
+        {
+            assert(data_nodes.pool.size==hash_nodes.pool.size);
+            while(n-->0)
+            {
+                {
+                    data_nodes.cache();
+                }
+                try
+                {
+                    hash_nodes.cache();
+                }
+                catch(...)
+                {
+                    object::release1( data_nodes.pool.query() );
+                    throw;
+                }
+            }
+        }
+        
+        inline void rescale_slots( const size_t n )
+        {
+            // acquire new slots
+            const size_t new_nslot = next_power_of_two( max_of<size_t>(n,16) );
+            size_t       new_count = new_nslot;
+            HashSlot    *new_slots = hmem.template acquire_as<HashSlot>(new_count);
+            const size_t new_hmask = new_nslot - 1;
+            
+            // transfert nodes
+            for(size_t i=0;i<nslot;++i)
+            {
+                HashSlot &src = slots[i];
+                while(src.size)
+                {
+                    HashNode *node = src.pop_front();
+                    HashSlot &tgt  = new_slots[ node->hkey & new_hmask ];
+                    tgt.push_back(node);
+                }
+            }
+            
+            // exchange memory
+            hmem.template release_as<HashSlot>(slots,count);
+            nslot = new_nslot;
+            slots = new_slots;
+            count = new_count;
+            hmask = new_hmask;
+        }
+        
+        inline DataNode *lookup( const size_t h, const_key &k, HashSlot * &slot ) const throw()
+        {
+            assert(0==slot);
+            if(nslot<=0)
+            {
+                return 0;
+            }
+            else
+            {
+                slot = &slots[ h & hmask ];
+                for( const HashNode *node = slot->head;node;node=node->next)
+                {
+                    assert(node->addr);
+                    const DataNode *dn = node->addr;
+                    if(dn->key() == k)
+                    {
+                        return (DataNode *)dn;
+                    }
+                }
+                return 0;
+            }
+        }
+        
+        
     };
     
     
