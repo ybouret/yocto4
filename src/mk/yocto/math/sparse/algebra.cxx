4efa8f37061d535eb190f2970a471df03ee18f8e
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-05-09 11:53:05 +0000

    sparse matrix transposition

--- src/mk/yocto/math/sparse/algebra.cxx
+++ src/mk/yocto/math/sparse/algebra.cxx
@@ -52,6 +52,52 @@ namespace yocto
         }
         
         
+        template <>
+        void sp_algebra<z_type>::mul_trn(array<z_type> &v, const sp_matrix<z_type> &M, const array<z_type> &u) throw()
+        {
+            static const z_type __zero__ = numeric<z_type>::zero;
+            assert(v.size()==M.cols);
+            assert(u.size()==M.rows);
+            for(size_t r=v.size();r>0;--r)
+            {
+                v[r] = __zero__;
+            }
+            
+            for(size_t i=M.count();i>0;--i)
+            {
+                const item_type &it = M[i];
+                v[it.col] += it.value * u[it.row];
+            }
+        }
+        
+        template <>
+        void sp_algebra<z_type>::muladd_trn(array<z_type> &v, const sp_matrix<z_type> &M, const array<z_type> &u) throw()
+        {
+            assert(v.size()==M.cols);
+            assert(u.size()==M.rows);
+            
+            for(size_t i=M.count();i>0;--i)
+            {
+                const item_type &it = M[i];
+                v[it.col] += it.value * u[it.row];
+            }
+        }
+        
+        template <>
+        void sp_algebra<z_type>::mulsub_trn(array<z_type> &v, const sp_matrix<z_type> &M, const array<z_type> &u) throw()
+        {
+            assert(v.size()==M.cols);
+            assert(u.size()==M.rows);
+            
+            for(size_t i=M.count();i>0;--i)
+            {
+                const item_type &it = M[i];
+                v[it.col] -= it.value * u[it.row];
+            }
+        }
+
+
+        
     }
     
 }
--- src/mk/yocto/math/sparse/matrix.cxx
+++ src/mk/yocto/math/sparse/matrix.cxx
@@ -125,6 +125,68 @@ namespace yocto
         }
         
         
+        template <>
+        sp_matrix<z_type>:: sp_matrix(const sp_matrix &M) :
+        rows(M.rows),
+        cols(M.cols),
+        nmax(M.nmax),
+        items(M.items)
+        {
+            
+        }
+        
+        template <>
+        sp_matrix<z_type>:: sp_matrix( const matrix<z_type> &M, real_type threshold ) :
+        rows(M.rows),
+        cols(M.cols),
+        nmax(M.size),
+        items()
+        {
+            // first pass: count non zero
+            size_t nz = 0;
+            for(size_t i=M.rows;i>0;--i)
+            {
+                for(size_t j=M.cols;j>0;--j)
+                {
+                    if( Fabs(M[i][j]) > threshold ) ++nz;
+                }
+            }
+            items.reserve(nz);
+            
+            
+            // second pass: register
+            for(size_t i=1;i<=M.rows;++i)
+            {
+                for(size_t j=1;j<=M.cols;++j)
+                {
+                    const z_type &m = M[i][j];
+                    if( Fabs(m) > threshold )
+                    {
+                        const item_type it(i,j,m);
+                        if( !items.insert(it))
+                            throw exception("unexpected sparse matrix constructor failure");
+                    }
+                }
+            }
+            
+        }
+        
+        namespace
+        {
+            static inline void __transpose__( sp_key &k ) throw()
+            {
+                cswap_const(k.row,k.col);
+            }
+        }
+        
+        template <>
+        void sp_matrix<z_type>:: transpose() throw()
+        {
+            items.alter_keys(__transpose__);
+            cswap_const(rows,cols);
+        }
+
+        
     }
     
 }
