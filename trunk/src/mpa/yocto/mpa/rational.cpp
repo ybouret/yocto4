6aba287b12353beb23ab3f2121793ecbb8c3648d
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-05-12 21:09:31 +0000

    rational WIP: need to put binomial in natural

--- trunk/src/mpa/yocto/mpa/rational.cpp
+++ trunk/src/mpa/yocto/mpa/rational.cpp
@@ -1,4 +1,7 @@
 #include "yocto/mpa/rational.hpp"
+#include "yocto/exceptions.hpp"
+#include <cerrno>
+#include <iostream>
 
 namespace yocto
 {
@@ -8,8 +11,8 @@ namespace yocto
         
         
         rational:: rational() :
-        num(0),
-        den(1)
+        num(  int64_t(0) ),
+        den( uint64_t(1) )
         {
         }
         
@@ -17,14 +20,30 @@ namespace yocto
         num(N),
         den(D)
         {
-            
+            simplify();
         }
         
         void rational::simplify()
         {
-            assert(den>0);
+            if(den<=0)
+                throw libc::exception(EDOM,"rational division by zero");
             natural::simplify( (natural&)(num.n),(natural &)den);
         }
+        
+        rational:: rational( const int64_t __num ) :
+        num(__num),
+        den(1)
+        {
+        }
+        
+        rational:: rational( const  int64_t __num, const uint64_t __den ) :
+        num(__num),
+        den(__den)
+        {
+            simplify();
+        }
+        
+        
         
         rational:: ~rational() throw()
         {
@@ -62,6 +81,59 @@ namespace yocto
             return rational(N,D);
         }
         
+        //______________________________________________________________________
+        //
+        // SUB
+        //______________________________________________________________________
+        rational rational::sub( const rational &lhs, const rational &rhs )
+        {
+            const integer LD(lhs.den);
+            const integer RD(rhs.den);
+            const integer N = (lhs.num*RD) - (rhs.num*LD);
+            const natural D = rhs.den * lhs.den;
+            return rational(N,D);
+        }
+
+        //______________________________________________________________________
+        //
+        // MUL
+        //______________________________________________________________________
+        rational rational:: mul(const rational &lhs, const rational &rhs )
+        {
+
+            const integer N = lhs.num * rhs.num;
+            const natural D = lhs.den * rhs.den;
+            return rational(N,D);
+        }
+
+        rational rational:: binomial( uint64_t n, uint64_t p )
+        {
+            if(p>n) throw libc::exception( EDOM, "invalid binomial coefficients");
+            natural NN = natural::factorial(n);
+            natural D  = natural::factorial(p);
+            natural::simplify(NN, D);
+            D  *= natural::factorial(n-p);
+            const integer N(NN);
+            return rational(N,D);
+        }
+        
+        rational rational:: power( const rational &q, uint64_t p )
+        {
+            integer N = 1;
+            natural D = 1;
+            while(p-->0)
+            {
+                N *= q.num;
+                D *= q.den;
+            }
+            return rational(N,D);
+        }
+        
+        std::ostream & operator<<(std::ostream &os, const rational &q)
+        {
+            os << '(' << q.num << '/' << q.den << ')';
+            return os;
+        }
         
     }
     
