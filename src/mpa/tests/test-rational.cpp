e91e24346e59d4dd6e347aa5a10b6c2b4a62c1ad
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-05-12 21:09:31 +0000

    rational WIP: need to put binomial in natural

---
+++ src/mpa/tests/test-rational.cpp
@@ -0,0 +1,21 @@
+#include "yocto/utest/run.hpp"
+#include "yocto/mpa/rational.hpp"
+
+using namespace yocto;
+
+YOCTO_UNIT_TEST_IMPL(rational)
+{
+    mpq q;
+    std::cerr << "q=" << q << std::endl;
+    q = mpq(123,9);
+    std::cerr << "q=" << q << std::endl;
+    mpq p = mpq(-2,6);
+    std::cerr << "p=" << p << std::endl;
+    std::cerr << "p+q=" << p+q << std::endl;
+    
+    p = mpq::binomial(20,5);
+    std::cerr << "p=" << p << std::endl;
+    
+}
+YOCTO_UNIT_TEST_DONE()
+
--- src/mpa/yocto/mpa/rational.hpp
+++ src/mpa/yocto/mpa/rational.hpp
@@ -21,16 +21,73 @@ namespace yocto
             rational( const rational &other );
             rational &operator=(const rational &);
             void xch(rational &other) throw();
-        
-            static rational add( const rational &lhs, const rational &rhs);
+            rational(const int64_t __num);
+            rational(const int64_t __num,const uint64_t __den);
             
+            // ADD
+            static rational add( const rational &lhs, const rational &rhs);
+            inline friend rational operator+(const rational &lhs, const rational &rhs)
+            {
+                return add(lhs,rhs);
+            }
+            
+            inline rational & operator+=( const rational &rhs )
+            {
+                rational tmp = add(*this,rhs);
+                xch(tmp);
+                return *this;
+            }
+            
+            
+            // SUB
+            static rational sub( const rational &lhs, const rational &rhs);
+            inline friend rational operator-(const rational &lhs, const rational &rhs)
+            {
+                return sub(lhs,rhs);
+            }
+            
+            inline rational & operator-=( const rational &rhs )
+            {
+                rational tmp = sub(*this,rhs);
+                xch(tmp);
+                return *this;
+            }
+
+            
+            
+            // MUL
+            static rational mul(const rational &lhs, const rational &rhs );
+            inline friend rational operator*(const rational &lhs, const rational &rhs)
+            {
+                return mul(lhs,rhs);
+            }
+            
+            inline  rational &operator*=(const rational &rhs)
+            {
+                rational tmp = mul(*this,rhs);
+                xch(tmp);
+                return *this;
+            }
+            
+            // algebra
             void simplify();
+            
+            // binomial coefficients
+            static rational binomial( uint64_t n, uint64_t p );
+            
+            // power
+            static rational power( const rational &q, uint64_t p );
+            
+            friend std::ostream & operator<<(std::ostream &, const rational &q);
+            
             
         private:
             explicit rational( const integer &N, const natural &D); //simplify
         };
         
     }
+    
+    typedef mpa::rational mpq;
 }
 
 #endif
