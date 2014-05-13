bd4892454044d4da60e383a0f1c75769585cea30
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-05-13 12:23:32 +0000

    C style combination

--- trunk/src/main/tests/test-comb.cpp
+++ trunk/src/main/tests/test-comb.cpp
@@ -6,27 +6,19 @@ using namespace yocto;
 
 YOCTO_UNIT_TEST_IMPL(comb)
 {
-    
-    static const size_t nmax = 1024;
-    
+        
     
     size_t n = (argc > 1) ? strconv::to<size_t>(argv[1],"n") : 10 ;
     size_t k = (argc > 2) ? strconv::to<size_t>(argv[2],"k") :  3 ;
-    if(n>nmax) n = nmax;
-    if(k>n)    k = n;
+    combination C(n,k);
     
-    size_t  count = 0;
-    size_t *comb  = (size_t *)malloc(k*sizeof(size_t));
-    if(!comb)
-        throw exception("Out of Memory");
-    
-    combination::init(comb,k,n);
+    size_t count = 0;
     do
     {
         ++count;
+        std::cerr << C << std::endl;
     }
-    while( combination::next(comb,k,n) );
-    free(comb);
+    while( C.next() );
     
     std::cerr << "C(" << n << "," << k << ")=" << count << std::endl;
     
--- trunk/src/main/yocto/code/combination.hpp
+++ trunk/src/main/yocto/code/combination.hpp
@@ -7,12 +7,27 @@
 namespace yocto
 {
     
-    struct combination
+    class combination
     {
+    public:
+        const size_t n;
+        const size_t k;
         
-        static void init( size_t comb[], const size_t k, const size_t n ) throw();
-        static bool next( size_t comb[], const size_t k, const size_t n ) throw();
+        void init() throw();
+        bool next() throw();
         
+        explicit combination(const size_t an, const size_t ak);
+        virtual ~combination() throw();
+        
+        //! value of comb[0<=i<k]
+        size_t operator[](size_t i) const throw();
+        
+        friend std::ostream & operator<<( std::ostream &os, const combination &C );
+        
+    private:
+        YOCTO_DISABLE_ASSIGN(combination);
+        size_t         *comb;
+        const ptrdiff_t nmk;
     };
     
 }
