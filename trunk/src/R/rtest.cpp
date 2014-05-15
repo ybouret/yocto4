c89bc852b21e406da046f5b2166c4050e854b7c3
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-05-15 11:51:56 +0000

    creating R++

---
+++ trunk/src/R/rtest.cpp
@@ -0,0 +1,2 @@
+#include "yocto/R/R++.hpp"
+
---
+++ trunk/src/R/yocto/R/R++.hpp
@@ -0,0 +1,9 @@
+#ifndef YOCTO_RXX_INCLUDED
+#define YOCTO_RXX_INCLUDED 1
+
+#include <R.h>
+#include <Rinternals.h>
+#include "yocto/exception.hpp"
+
+
+#endif
