793e8ce3a09ef6d482b80cccbaf8aad933a328c9
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-05-30 13:09:20 +0000

    WIP pixmap

--- src/gfx/tests/test-pixmap.cpp
+++ src/gfx/tests/test-pixmap.cpp
@@ -1,12 +1,21 @@
 #include "yocto/utest/run.hpp"
 #include "yocto/gfx/pixmap.hpp"
+#include "yocto/gfx/rgb.hpp"
 
 using namespace yocto;
+
+float rgb2g(const gfx::rgb_t &c)
+{
+    return gfx::conv::greyscale_f(c.r, c.g, c.b);
+}
 
 YOCTO_UNIT_TEST_IMPL(pixmap)
 {
     
-    yocto::gfx::pixmap<char> chpx(250,200);
-    
+    gfx::pixmap<char>       chpx(250,200);
+    gfx::pixmap<double>     dpx(100,300);
+    gfx::pixmap<gfx::rgb_t> rgbpx(40,50);
+    functor<float,TL1(gfx::rgb_t)> filter( cfunctor(rgb2g) );
+    gfx::pixmap<float>      fpx(rgbpx,filter);
 }
 YOCTO_UNIT_TEST_DONE()
