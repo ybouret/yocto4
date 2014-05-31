8942cab69a8d834db6d9d3a2fa6af6534fa65b67
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-05-31 08:33:05 +0000

    upgrading format

--- trunk/src/gfx/yocto/gfx/format.hpp
+++ trunk/src/gfx/yocto/gfx/format.hpp
@@ -15,16 +15,24 @@ namespace yocto
         class format
         {
         public:
-            const mask_t mask;
-            const size_t depth;
-            const rgba_t bits;
-            const rgba_t shift;
-            const rgba_t loss;
-            const size_t bpp;
+            typedef pixel_t (*get_proc)(const void *);
+            typedef void    (*put_proc)(void *,pixel_t);
+           
+            const mask_t   mask;
+            const size_t   depth;
+            const get_proc get_pixel;
+            const put_proc put_pixel;
+            const rgba_t   bits;
+            const rgba_t   shift;
+            const rgba_t   loss;
+            const size_t   bpp;
             
             format( uint32_t Rmask, uint32_t Gmask, uint32_t Bmask, uint32_t Amask );
             ~format() throw();
             format( const format &fmt ) throw();
+            
+           
+            
             
         private:
             YOCTO_DISABLE_ASSIGN(format);
