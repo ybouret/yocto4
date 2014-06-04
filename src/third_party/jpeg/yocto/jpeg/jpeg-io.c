5d861195f252f876778dfb1b8bacdba5bed1520e
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-06-04 11:38:37 +0000

    jpeg-io from libjpeg

---
+++ src/third_party/jpeg/yocto/jpeg/jpeg-io.c
@@ -0,0 +1,24 @@
+
+#include "jpeglib.h"
+
+JSAMPLE * image_buffer = 0;
+int image_height;	/* Number of rows in image */
+int image_width;		/* Number of columns in image */
+
+GLOBAL(int)
+read_JPEG_file (char * filename);
+GLOBAL(void)
+write_JPEG_file (char * filename, int quality);
+
+int main(int argc, char *argv[])
+{
+    
+    if(argc>1)
+    {
+        read_JPEG_file(argv[1]);
+        write_JPEG_file("toto.jpg", 70);
+    }
+    
+    return 0;
+    
+}
