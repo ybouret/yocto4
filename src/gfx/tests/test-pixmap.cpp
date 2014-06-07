9bdab4ea456db3a101b4acaa34b0028fcae35441
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-06-07 09:15:12 +0000

    corrected bug

--- src/gfx/tests/test-pixmap.cpp
+++ src/gfx/tests/test-pixmap.cpp
@@ -11,11 +11,16 @@ float rgb2g(const gfx::rgb_t &c)
 
 YOCTO_UNIT_TEST_IMPL(pixmap)
 {
-    
+    std::cerr << "pixmap<char>" << std::endl;
     gfx::pixmap<char>       chpx(250,200);
+    std::cerr << "pixmap<double>" << std::endl;
     gfx::pixmap<double>     dpx(100,300);
+    std::cerr << "pixmap<rgb_t>" << std::endl;
     gfx::pixmap<gfx::rgb_t> rgbpx(40,50);
+    
+    std::cerr << "pixmap/filter" << std::endl;
     functor<float,TL1(gfx::rgb_t)> filter( cfunctor(rgb2g) );
-    gfx::pixmap<float>      fpx(rgbpx,filter);
+    gfx::pixmap<float>             fpx(rgbpx,filter);
+    std::cerr << "done" << std::endl;
 }
 YOCTO_UNIT_TEST_DONE()
