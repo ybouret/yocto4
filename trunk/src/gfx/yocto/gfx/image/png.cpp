a65c43399ddeba58db09f8390e62991213cf0226
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-06-10 09:31:18 +0000

    preliminary load png is OK

--- trunk/src/gfx/yocto/gfx/image/png.cpp
+++ trunk/src/gfx/yocto/gfx/image/png.cpp
@@ -50,13 +50,13 @@ namespace yocto
                 size_t     wlen;
                 void      *wksp;
                 
-                void allocate( unit_t width, unit_t height )
+                void allocate( unit_t width, unit_t height, unit_t num_channels )
                 {
                     const size_t rows_offset = 0;
                     const size_t rows_length = sizeof(png_bytep) * height;
                     
                     const size_t data_offset = memory::align(rows_length+rows_offset);
-                    const unit_t row_bytes   = 4 * width;
+                    const unit_t row_bytes   = num_channels * width;
                     const size_t data_length = height * row_bytes * sizeof(png_byte);
                     
                     wlen = memory::align(data_offset+data_length);
@@ -156,9 +156,13 @@ namespace yocto
             
             
             png_set_strip_16(png_ptr); //!< strip 16->8 bits
+            png_set_packing(png_ptr);
             png_set_expand(png_ptr);   //!< expand all to RGBA...
             png_read_update_info(png_ptr, info_ptr);
+            const unit_t num_channels = png_get_channels(png_ptr, info_ptr);
+            std::cerr << "#channels=" << num_channels << std::endl;
             bit_depth  = png_get_bit_depth(png_ptr, info_ptr);
+            
             if(bit_depth!=8)
             {
                 png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
@@ -171,13 +175,19 @@ namespace yocto
                 throw exception("%s(unhandled #passes=%d)",fn, number_of_passes);
             }
             
+            if(num_channels<3)
+            {
+                png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+                throw exception("%s(unhandled #channels=%d)",fn, int(num_channels));
+            }
+            
             
             auto_ptr<bitmap> bmp;
             PNG_Mem          mem;
             try
             {
                 bmp.reset(new bitmap(depth,width,height));
-                mem.allocate(width,height);
+                mem.allocate(width,height,num_channels);
             }
             catch(...)
             {
@@ -201,11 +211,12 @@ namespace yocto
             //
             // processing...
             //__________________________________________________________________
+            unit_t y=height;
             for(unit_t j=0;j<height;++j)
             {
                 const png_byte *q = mem.rows[j];
-                uint8_t        *p = static_cast<uint8_t*>(bmp->get_line(j));
-                for(unit_t i=0;i<width;++i,q += 4, p += depth)
+                uint8_t        *p = static_cast<uint8_t*>(bmp->get_line(--y));
+                for(unit_t i=0;i<width;++i,q += num_channels, p += depth)
                 {
                     const rgba_t C( q[0], q[1], q[2], q[3]);
                     proc(p,C,args);
