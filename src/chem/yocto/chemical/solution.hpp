494ce8851a3b45ed5bafb247ac6333d76f1a7010
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-06-25 07:18:56 +0000

    chem sol

--- src/chem/yocto/chemical/solution.hpp
+++ src/chem/yocto/chemical/solution.hpp
@@ -14,6 +14,8 @@ namespace yocto
         {
         public:
             explicit solution( const collection &from );
+            solution(const solution &other);
+            
             virtual ~solution() throw();
             const collection &lib;
             
@@ -24,8 +26,10 @@ namespace yocto
             virtual void        free() throw();
             virtual void        release() throw();
             
-            double       & operator[]( const string &id );
-            const double & operator[]( const string &id ) const;
+            double       & operator()( const string &id );
+            const double & operator()( const string &id ) const;
+            double       & operator()( const char   *id );
+            const double & operator()( const char   *id ) const;
 
             void output( std::ostream &) const;
             inline friend std::ostream & operator<<( std::ostream &os, const solution &self)
@@ -34,13 +38,21 @@ namespace yocto
                 return os;
             }
             
+            void ldz() throw();
+            void load( const array<double> &X) throw();
+            void save( array<double> &X) const throw();
+            
+            solution & operator=( const solution & ); //!< checking compatible
+            solution & operator=( const array<double> &) throw();
+            
+            double pH() const; //!< "H+" must exists...
+            
             
         private:
             const size_t n;
             size_t       m; //!< memory
             double      *C; //!< conc
             
-            YOCTO_DISABLE_ASSIGN(solution);
             virtual const double *get_item() const throw();
         };
     }
