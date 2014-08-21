1b94c4651cca0ad9e6dcb1e9387e98b4deeb443c
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-08-21 15:17:44 +0000

    init effectors API

--- src/chem/yocto/chemical/effector.cpp
+++ src/chem/yocto/chemical/effector.cpp
@@ -21,7 +21,6 @@ namespace yocto
         // Effectors
         //
         //======================================================================
-
         effectors:: effectors() throw() : effector::database()
         {
             
@@ -30,6 +29,24 @@ namespace yocto
         effectors:: ~effectors() throw()
         {
             
+        }
+        
+        void effectors:: compute_rate( YOCTO_CHEMICAL_EFFECTOR_ARGS, solution &Stmp )
+        {
+            const size_t M = dSdt.size();
+            dSdt.ldz();
+            const size_t neff = size();
+            iterator     iter = begin();
+            for( size_t i=neff;i>0;--i,++iter)
+            {
+                const effector &eff = **iter;
+                eff.call(Stmp, t, zeta, S, S_out);
+                const double fac = eff.Vmax;
+                for(size_t j=M;j>0;--j)
+                {
+                    dSdt[j] += fac * Stmp[j];
+                }
+            }
         }
     
         
