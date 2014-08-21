4472c72f61bb31fced02b06eed66e7086ab5dadb
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-08-21 15:08:12 +0000

    effectors init

--- src/chem/yocto/chemical/effector.hpp
+++ src/chem/yocto/chemical/effector.hpp
@@ -18,14 +18,27 @@ namespace yocto
             
             const string &key() const throw();
             typedef intr_ptr<string,effector> pointer;
+            typedef set<string,pointer>       database;
             
-            
+            //! compute the rate WITHOUT the Vmax
+            virtual void call( array<double> &dSdt, double t, const array<double> &S, const array<double> &S_out ) const = 0;
             
         protected:
             explicit effector( const string & );
             
         private:
             YOCTO_DISABLE_COPY_AND_ASSIGN(effector);
+        };
+        
+        
+        class effectors : public effector::database
+        {
+        public:
+            explicit effectors() throw();
+            virtual ~effectors() throw();
+            
+        private:
+            YOCTO_DISABLE_COPY_AND_ASSIGN(effectors);
         };
         
     }
