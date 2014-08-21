557af3bba158a77b541b284925e1a4c0658a493b
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-08-21 07:16:07 +0000

    a little tao

--- src/mk/yocto/math/kernel/tao.hpp
+++ src/mk/yocto/math/kernel/tao.hpp
@@ -3,6 +3,8 @@
 
 #include "yocto/math/types.hpp"
 #include "yocto/type/traits.hpp"
+#include "yocto/math/kernel/matrix.hpp"
+#include "yocto/code/unroll.hpp"
 
 namespace yocto
 {
@@ -11,14 +13,57 @@ namespace yocto
         
         struct tao
         {
+            
+#define YOCTO_TAO_LOOP(N,FUNC) YOCTO_LOOP_FUNC(N,Y_TAO_##FUNC,1)
+            
             template <typename ARRAY>
-            static inline void set( ARRAY &a, typename ARRAY::param_type x )
+            static inline void ld( ARRAY &a, typename ARRAY::param_type x ) throw()
             {
-                for(size_t i=a.size();i>0;--i)
-                {
-                    a[i] = x;
-                }
+#define Y_TAO_LD(I) a[I] = x
+                YOCTO_TAO_LOOP(a.size(),LD);
             }
+            
+            template <typename ARR, typename BRR>
+            static inline void set( ARR &a, const BRR &b ) throw()
+            {
+                assert(a.size()<=b.size());
+#define Y_TAO_SET(I) a[I] = b[I]
+                YOCTO_TAO_LOOP(a.size(),SET);
+            }
+            
+            template <typename ARR, typename BRR>
+            static inline void neg( ARR &a, const BRR &b ) throw()
+            {
+                assert(a.size()<=b.size());
+#define Y_TAO_NEG(I) a[I] = -static_cast<typename ARR::type>(b[I])
+                YOCTO_TAO_LOOP(a.size(),NEG);
+            }
+
+            
+            template<typename ARR, typename BRR>
+            static inline void mulset( ARR &a, typename ARR::param_type x, const BRR &b ) throw()
+            {
+                assert(a.size()<=b.size());
+#define Y_TAO_MULSET(I) a[I] = x*b[I]
+                YOCTO_TAO_LOOP(a.size(),MULSET);
+            }
+            
+            template<typename ARR, typename BRR>
+            static inline void muladd( ARR &a, typename ARR::param_type x, const BRR &b ) throw()
+            {
+                assert(a.size()<=b.size());
+#define Y_TAO_MULADD(I) a[I] += x*b[I]
+                YOCTO_TAO_LOOP(a.size(),MULADD);
+            }
+            
+            template<typename ARR, typename BRR>
+            static inline void mulsub( ARR &a, typename ARR::param_type x, const BRR &b ) throw()
+            {
+                assert(a.size()<=b.size());
+#define Y_TAO_MULSUB(I) a[I] -= x*b[I]
+                YOCTO_TAO_LOOP(a.size(),MULSUB);
+            }
+            
         };
         
     }
