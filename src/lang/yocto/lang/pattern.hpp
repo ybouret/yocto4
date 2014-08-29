4b70b36a41032804d893ca02cde7bdc53041efa2
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-08-29 09:29:53 +0000

    start writing patterns

---
+++ src/lang/yocto/lang/pattern.hpp
@@ -0,0 +1,54 @@
+#ifndef YOCTO_LANG_PATTERN_INCLUDED
+#define YOCTO_LANG_PATTERN_INCLUDED 1
+
+#include "yocto/lang/source.hpp"
+#include "yocto/core/meta-list.hpp"
+
+namespace yocto
+{
+    namespace lang
+    {
+        
+        class pattern : public token
+        {
+        public:
+            virtual ~pattern() throw();
+            
+            //! clone interface
+            virtual pattern * clone() const = 0;
+            
+            //! true is a match is possible
+            /**
+             must left the source untouched otherwise !
+             */
+            virtual bool      match( source &src, ios::istream & ) = 0;
+            
+            //! clear all internal data
+            /**
+             default is this->clear
+             */
+            virtual void      reset() throw();
+            
+            pattern       *next;
+            pattern       *prev;
+            const uint32_t type; //!< UUID
+            
+        protected:
+            explicit pattern( const uint32_t user_type ) throw();
+            
+        private:
+            YOCTO_DISABLE_COPY_AND_ASSIGN(pattern);
+        };
+        
+#define YOCTO_LANG_PATTERN_API \
+virtual pattern *clone() const; \
+vortual bool     match(source&,ios::istream&)
+        
+        
+        typedef core::meta_list<pattern> p_list;
+        
+    }
+}
+
+#endif
+
