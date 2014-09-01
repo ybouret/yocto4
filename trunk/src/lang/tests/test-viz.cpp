1b57009aadb0a37d37456d694878a432465d56f7
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-09-01 08:05:20 +0000

    basic viz test

--- trunk/src/lang/tests/test-viz.cpp
+++ trunk/src/lang/tests/test-viz.cpp
@@ -5,11 +5,27 @@
 #include "yocto/utest/run.hpp"
 
 
+#include "yocto/ptr/auto.hpp"
 
 using namespace yocto;
+using namespace lang;
 
-YOCTO_UNIT_TEST_IMPL(pattern)
+
+static inline
+void doviz( const char *name, pattern *p )
 {
+    auto_ptr<pattern> q(p);
+    string fn = name;
+    fn += ".dot";
+    q->graphviz(fn);
+}
+
+#define YVIZ(NAME) doviz( #NAME, posix:: NAME() )
+
+YOCTO_UNIT_TEST_IMPL(viz)
+{
+    YVIZ(lower);
+    YVIZ(upper);
     
 }
 YOCTO_UNIT_TEST_DONE()
