40c73b611f0c9a4fbcff9f704fdd5eb80c821056
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-09-01 11:16:27 +0000

    typo

--- trunk/src/lang/yocto/lang/pattern/compiler.cpp
+++ trunk/src/lang/yocto/lang/pattern/compiler.cpp
@@ -2,52 +2,56 @@
 
 namespace yocto
 {
-    namespace lang
-    {
-        
-        namespace
-        {
-            class Compiler
-            {
-            public:
-                
-                const char   *curr;
-                const char   *last;
-                const p_dict *dict;
-                int           level;
+	namespace lang
+	{
 
-                inline ~Compiler() throw()
-                {
-                
-                }
-                
-                inline Compiler(const char *usrCurr, const char *usrLast, const p_dict *usrDict) throw() :
-                curr(usrCurr),
-                last(usrLast),
-                dict(usrDict),
-				level(0)
-                {
-                }
-                
-                
-                
-            private:
-                YOCTO_DISABLE_COPY_AND_ASSIGN(Compiler);
-            };
-        }
-        
-        
-        pattern *compile( const string &expr, const p_dict *dict)
-        {
-            Compiler CC(expr.c_str(),expr.c_str()+expr.size(),dict);
-            return 0;
-        }
-        
-        pattern *compile( const char   *expr, const p_dict *dict)
-        {
-            const string Expr(expr);
-            return compile(Expr,dict);
-        }
-        
-    }
+		namespace
+		{
+			class Compiler
+			{
+			public:
+
+				const char   *curr;
+				const char   *last;
+				const p_dict *dict;
+				p_list        stack;
+				int           level;
+
+				inline ~Compiler() throw()
+				{
+
+				}
+
+				inline Compiler(const char *usrCurr, const char *usrLast, const p_dict *usrDict):
+				curr(usrCurr),
+					last(usrLast),
+					dict(usrDict),
+					level(0),
+					stack()
+				{
+					// initialize
+					
+				}
+
+
+
+			private:
+				YOCTO_DISABLE_COPY_AND_ASSIGN(Compiler);
+			};
+		}
+
+
+		pattern *compile( const string &expr, const p_dict *dict)
+		{
+			Compiler CC(expr.c_str(),expr.c_str()+expr.size(),dict);
+			return 0;
+		}
+
+		pattern *compile( const char   *expr, const p_dict *dict)
+		{
+			const string Expr(expr);
+			return compile(Expr,dict);
+		}
+
+	}
 }
