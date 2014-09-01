9d6b2c24b1b4126ab0f2f3ac336a3318de1e7701
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-09-01 08:48:50 +0000

    viz tests

--- trunk/src/lang/tests/test-viz.cpp
+++ trunk/src/lang/tests/test-viz.cpp
@@ -17,7 +17,15 @@ void doviz( const char *name, pattern *p )
     auto_ptr<pattern> q(p);
     string fn = name;
     fn += ".dot";
+	string img = name;
+	img += ".png";
+
     q->graphviz(fn);
+
+	string cmd = "dot -Tpng -o " + img + " " + fn;
+	std::cerr << "Executing '" << cmd << "'" << std::endl;
+	system( cmd.c_str() );
+
 }
 
 #define YVIZ(NAME) doviz( #NAME, posix:: NAME() )
@@ -26,6 +34,17 @@ YOCTO_UNIT_TEST_IMPL(viz)
 {
     YVIZ(lower);
     YVIZ(upper);
-    
+    YVIZ(digit);
+	YVIZ(alpha);
+	YVIZ(alnum);
+	YVIZ(xdigit);
+	YVIZ(space);
+	YVIZ(blank);
+	YVIZ(word);
+	YVIZ(punct);
+	YVIZ(dot);
+	YVIZ(cstring);
+	YVIZ(endl);
+
 }
 YOCTO_UNIT_TEST_DONE()
