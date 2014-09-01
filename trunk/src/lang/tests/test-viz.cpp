acc2c5092d4c34e87e3467b379ff4fe611a9ed32
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-09-01 07:58:34 +0000

    need to test viz

---
+++ trunk/src/lang/tests/test-viz.cpp
@@ -0,0 +1,15 @@
+#include "yocto/lang/pattern/basic.hpp"
+#include "yocto/lang/pattern/joker.hpp"
+#include "yocto/lang/pattern/logic.hpp"
+#include "yocto/lang/pattern/posix.hpp"
+#include "yocto/utest/run.hpp"
+
+
+
+using namespace yocto;
+
+YOCTO_UNIT_TEST_IMPL(pattern)
+{
+    
+}
+YOCTO_UNIT_TEST_DONE()
