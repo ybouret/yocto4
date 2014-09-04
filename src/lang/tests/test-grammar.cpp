90915097a79a15df1fa91a14c7fd4ae9b209cc75
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-09-04 15:27:42 +0000

    seems ok for syntax jokers

--- src/lang/tests/test-grammar.cpp
+++ src/lang/tests/test-grammar.cpp
@@ -3,6 +3,8 @@
 
 #include "yocto/ios/icstream.hpp"
 
+#include <cstdlib>
+
 using namespace yocto;
 using namespace lang;
 
@@ -37,17 +39,48 @@ namespace
 YOCTO_UNIT_TEST_IMPL(grammar)
 {
     
+    
     ios::icstream Input( ios::cstdin );
     source        Source;
     MyLex         Lexer;
     grammar       G("Grammar");
     
-    G.term("identifier");
-
+    syntax::rule &identifier = G.term("identifier");
+    syntax::rule &optid      = G.opt(identifier);
+    syntax::rule &z_id       = G.zero_or_more("zom_id",identifier);
+    syntax::rule &o_id       = G.one_or_more("oom_id",identifier);
+    
+    if(argc>1)
+    {
+        const string arg = argv[1];
+        if(arg=="?")
+            G.set_root(optid);
+        
+        if(arg=="*")
+        {
+            G.set_root(z_id);
+        }
+        
+        if(arg=="+")
+        {
+            G.set_root(o_id);
+        }
+        
+    }
+    
+    G.show_rules();
+    
     auto_ptr<syntax::xnode> Tree( G.accept(Lexer, Source, Input) );
-    
-    
-    
+    std::cerr << "((" << G.name << ")) success" << std::endl;
+    if(Tree.is_valid())
+    {
+        Tree->graphviz("xnode.dot");
+        system("dot -Tpng -o xnode.png xnode.dot");
+    }
+    else
+    {
+        
+    }
     
     
     
---
+++ src/lang/yocto/lang/syntax/joker.cpp
@@ -0,0 +1,117 @@
+#include "yocto/lang/syntax/joker.hpp"
+
+namespace yocto
+{
+    namespace lang
+    {
+        
+        namespace syntax
+        {
+            
+            joker:: ~joker() throw()
+            {
+                
+            }
+            
+            joker:: joker( const string &id, rule &other ) :
+            rule(id),
+            sub(other)
+            {
+            }
+            
+            
+        }
+        
+    }
+    
+}
+
+namespace yocto
+{
+    namespace lang
+    {
+        
+        namespace syntax
+        {
+            
+            optional:: ~optional() throw()
+            {
+                
+            }
+            
+            optional:: optional(const string &id, rule &other ) :
+            joker(id,other)
+            {
+            }
+            
+            YOCTO_LANG_SYNTAX_RULE_MATCH_IMPL(optional)
+            {
+                syntax::xtree Node = 0;
+                if( sub.match(Lexer, Source, Input, Node))
+                {
+                    grow(Tree,Node);
+                }
+                return true;
+            }
+        }
+        
+    }
+    
+}
+
+
+namespace yocto
+{
+    namespace lang
+    {
+        
+        namespace syntax
+        {
+            
+            at_least:: ~at_least() throw()
+            {
+            }
+            
+            
+            at_least:: at_least(const string &id, rule &other, size_t count ) :
+            joker(id,other),
+            value(count)
+            {
+                
+            }
+            
+            YOCTO_LANG_SYNTAX_RULE_MATCH_IMPL(at_least)
+            {
+                syntax::xtree SubTree = syntax::xnode::create(label);
+                
+                for(;;)
+                {
+                    syntax::xtree Node = 0;
+                    if( sub.match(Lexer,Source,Input,Node) )
+                    {
+                        SubTree->add(Node);
+                        continue;
+                    }
+                    else
+                        break;
+                }
+                
+                if(SubTree->children().size>=value)
+                {
+                    grow(Tree,SubTree);
+                    return true;
+                }
+                else
+                {
+                    syntax::xnode::restore(Lexer, SubTree);
+                    return false;
+                }
+                
+            }
+            
+        }
+        
+    }
+    
+}
+
