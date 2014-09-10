41c7e1528125e1ab0be9d13b9911ce80bc64ff16
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-09-10 09:07:34 +0000

    ok for simple grammar error reporting

--- src/lang/yocto/lang/gramrun.cpp
+++ src/lang/yocto/lang/gramrun.cpp
@@ -12,9 +12,43 @@ namespace yocto
             
             syntax::xtree Tree = 0;
             syntax::rule *Root = rules.head; assert(Root);
+            
             if(!Root->match(Lexer,Source,Input,Tree))
             {
-                throw exception("((%s)) accept failure", name.c_str());
+                std::cerr << "Grammar Failure..." << std::endl;
+                std::cerr << "\t#lexemes=" << Lexer.cache_size() << std::endl;
+                const lexeme *last = Lexer.cache_tail();
+                switch( Lexer.cache_size() )
+                {
+                    case 0:
+                        throw exception("%d: ((%s)) unknown syntax error", Lexer.line, name.c_str());
+                        
+                    case 1:
+                        assert(last);
+                    {
+                        const string content1 = last->to_string();
+                        throw exception("%d: ((%s)) unexpected %s '%s'", last->line, name.c_str(), last->label.c_str(), content1.c_str() );
+                    }
+                        break;
+                        
+                    default:
+                        break;
+                }
+                assert(last);
+                assert(last->prev);
+                const lexeme *prev    = last->prev;
+                const string  content = last->to_string();
+                const string  prev_content = prev->to_string();
+                throw exception("%d: ((%s)) unexpected %s '%s' after %s '%s' (line %d)",
+                                last->line,
+                                name.c_str(),
+                                last->label.c_str(),
+                                content.c_str(),
+                                prev->label.c_str(),
+                                prev_content.c_str(),
+                                prev->line);
+                
+                
             }
             
             auto_ptr<syntax::xnode> TreeGuard(Tree);
