b46e78a5f999c67561c42398166a1155be92f249
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-09-11 06:36:45 +0000

    init gen, need to think about structure

--- src/lang/tests/test-gen.cpp
+++ src/lang/tests/test-gen.cpp
@@ -4,6 +4,8 @@
 #include "yocto/ios/icstream.hpp"
 #include "yocto/fs/local-fs.hpp"
 
+#include <cstdlib>
+
 using namespace yocto;
 using namespace lang;
 
@@ -17,10 +19,23 @@ namespace
 
 YOCTO_UNIT_TEST_IMPL(gen)
 {
+    
+    vfs &fs = local_fs::instance();
+    
+    fs.try_remove_file("xnode.dot");
+    fs.try_remove_file("xnode.png");
+    
+    
     generator     G;
     ios::icstream fp( ios::cstdin );
     
-    
+    auto_ptr<syntax::xnode> Tree( G.run(fp) );
+    if(Tree.is_valid())
+    {
+        Tree->graphviz("xnode.dot");
+        system("dot -Tpng -o xnode.png xnode.dot");
+    }
+
 }
 YOCTO_UNIT_TEST_DONE()
 
--- src/lang/yocto/lang/generator.cpp
+++ src/lang/yocto/lang/generator.cpp
@@ -1,4 +1,6 @@
 #include "yocto/lang/generator.hpp"
+#include "yocto/lang/lexical/plugin/cstring.hpp"
+#include "yocto/lang/lexical/plugin/comment.hpp"
 
 namespace yocto
 {
@@ -10,18 +12,29 @@ namespace yocto
         }
         
         generator:: generator() :
-        parser("generator","scan")
+        parser("generator","scanning")
         {
             //__________________________________________________________________
             //
             // Generator Grammar
             //__________________________________________________________________
+            Terminal  &ID     = terminal("ID","[_[:alpha:]][:word:]+");
+            Terminal  &COLON  = jettison(":",':');
+            Terminal  &END    = jettison(";",';');
+            Rule      &STRING = plug_term(new lexical::cstring("STRING",*this) );
+            Alternate &EXPR = alt();
+            EXPR << ID << STRING;
             
+            Aggregate &RULE = assemble("RULE");
+            RULE << ID << COLON << EXPR << END;
+            
+            set_root( zero_or_more(RULE) );
             
             //__________________________________________________________________
             //
-            // final
+            // Lexical
             //__________________________________________________________________
+            plug_meta( new lexical::comment("COMMENT",*this,"//") );
             scanner.make("blank", "[:blank:]", discard);
             scanner.make("endl" , "[:endl:]",  newline);
 
