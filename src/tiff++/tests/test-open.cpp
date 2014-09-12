031a819b4556f2207621f71dc951137f1bce2b39
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-09-12 08:53:43 +0000

    start tiff, need to move into gfx

---
+++ src/tiff++/tests/test-open.cpp
@@ -0,0 +1,9 @@
+#include "yocto/tiff++/io.hpp"
+#include "yocto/utest/run.hpp"
+
+
+YOCTO_UNIT_TEST_IMPL(open)
+{
+    
+}
+YOCTO_UNIT_TEST_DONE()
---
+++ src/tiff++/yocto/tiff++/io.hpp
@@ -0,0 +1,17 @@
+#ifndef YOCTO_TIFF_IO_INCLUDED
+#define YOCTO_TIFF_IO_INCLUDED 1
+
+#include "yocto/os.hpp"
+#include <tiffio.h>
+
+namespace yocto
+{
+    
+    namespace gfx
+    {
+        
+    }
+    
+}
+
+#endif
