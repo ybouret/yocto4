469d4cd577ac161112732547101d7be0e59e6812
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-09-16 14:39:31 +0000

    need to write parser tree before gen, to optimize

--- trunk/src/lang/yocto/lang/genebuild.cpp
+++ trunk/src/lang/yocto/lang/genebuild.cpp
@@ -1,6 +1,7 @@
 #include "yocto/lang/generator.hpp"
 #include "yocto/associative/set.hpp"
 #include "yocto/exception.hpp"
+#include "yocto/sequence/list.hpp"
 
 #include <cstdlib>
 
@@ -11,61 +12,6 @@ namespace yocto
         namespace
         {
             
-            class vrule;
-            
-            class vrule_node : public object
-            {
-            public:
-                vrule_node  *next;
-                vrule_node  *prev;
-                const vrule *handle;
-                
-                explicit vrule_node(vrule *h) throw() :
-                next(0), prev(0), handle(h)
-                {
-                    assert(handle);
-                }
-                
-                virtual ~vrule_node() throw()
-                {
-                }
-                
-                
-            private:
-                YOCTO_DISABLE_COPY_AND_ASSIGN(vrule_node);
-            };
-            
-            typedef core::list_of_cpp<vrule_node> vrule_list;
-            
-            
-            class vrule : public object, public counted
-            {
-            public:
-                const syntax::xnode *node;
-                const string         name;
-                const string &key() const throw() { return name; }
-                
-                explicit vrule(const syntax::xnode *nd) throw() :
-                node(nd),
-                name(nd->lxm()->to_string())
-                {
-                }
-                
-                virtual ~vrule() throw()
-                {
-                }
-                
-                typedef intr_ptr<string,vrule> pointer;
-                typedef set<string,pointer>    database;
-                
-                vrule_list parents;
-                vrule_list children;
-                
-                
-            private:
-                YOCTO_DISABLE_COPY_AND_ASSIGN(vrule);
-                
-            };
             
         }
         
@@ -88,50 +34,15 @@ namespace yocto
             root->graphviz("xnode.dot");
             system("dot -Tpng -o xnode.png xnode.dot");
             
-            //__________________________________________________________________
-            //
-            // first pass: build database of rules
-            //__________________________________________________________________
-            vrule::database vdb;
-            {
-                const syntax::xnode::child_list &ch = root->children();
-                vdb.reserve(ch.size);
-                for(const syntax::xnode *node = ch.head; node; node=node->next)
-                {
-                    if(node->label == "RULE" )
-                    {
-                        const syntax::xnode::child_list &content = node->children();
-                        assert(content.size>0);
-                        const syntax::xnode *nd = content.head;
-                        assert(nd->label == "ID" );
-                        assert(nd->terminal);
-                        const vrule::pointer p( new vrule(nd) );
-                        std::cerr << "Registering rule '"  << p->name << "'" << std::endl;
-                        if( !vdb.insert(p) )
-                        {
-                            throw exception("multiple rule '%s'", p->name.c_str());
-                        }
-                        continue;
-                    }
-                    
-                    throw exception("cannot process '%s'", node->label.c_str());
-                }
-            }
             
             //__________________________________________________________________
             //
-            // second pass: dependency and terminals
+            // generate parser
             //__________________________________________________________________
-            for( vrule::database::iterator i=vdb.begin();i!=vdb.end();++i)
-            {
-                vrule *parent = &(**i);
-                std::cerr << "\tprocessing " << parent->name << std::endl;
-                for(const syntax::xnode *node = parent->node->next; node; node=node->next)
-                {
-                    std::cerr << "\t\t" << node->label << std::endl;
-                }
-            }
+            auto_ptr<parser> P( new parser("generated","scanner") );
             
+            
+
             
         }
         
