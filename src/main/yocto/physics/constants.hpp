18eb119e5e12f60af4bbd309537da9a1a8170743
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-09-18 07:44:48 +0000

    some constants

---
+++ src/main/yocto/physics/constants.hpp
@@ -0,0 +1,12 @@
+#ifndef YOCTO_PHYSICS_CONSTANTS_INCLUDED
+#define YOCTO_PHYSICS_CONSTANTS_INCLUDED 1
+
+#define Y_ELEMENTARY_CHARGE 1.60217657e-19
+#define Y_AVOGADRO          6.02214129e+23
+#define Y_FARADAY           96485.3399
+#define Y_EPSILON0          8.85418782e-12
+#define Y_C                 299792458
+#define Y_kB                1.3806488e-23
+#define Y_R                 8.3144621
+
+#endif
