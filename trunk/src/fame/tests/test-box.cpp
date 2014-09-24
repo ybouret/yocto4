9d082b5c74fe6a1ab6a322bab6df6b813f5f67d7
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-09-24 14:07:55 +0000

    need to test box mapping output

--- trunk/src/fame/tests/test-box.cpp
+++ trunk/src/fame/tests/test-box.cpp
@@ -10,12 +10,19 @@ YOCTO_UNIT_TEST_IMPL(box)
     
     box<1,float> b1(1,-1);
     std::cerr << "b1=" << b1 << std::endl;
+    array1D<double> x("x",layout1D(-5,5));
+    b1.map(x);
+    std::cerr << "x=" << x << std::endl;
     
     box<2,double> b2( math::v2d<double>(1,-2), math::v2d<double>(-1,3) );
+    array1D<double> y("y",layout1D(-6,6));
     std::cerr << "b2=" << b2 << std::endl;
-
-    box<3,double> b3( math::v3d<double>(1,-2,1.2), math::v3d<double>(-1,3,-0.7) );
-    std::cerr << "b3=" << b3 << std::endl;
+    b2.map(x,y);
     
+    box<3,double> b3( math::v3d<double>(1,-2,1.2), math::v3d<double>(-1,3,-0.7) );
+    array1D<double> z("z",layout1D(-10,10));
+    std::cerr << "b3=" << b3 << std::endl;
+    b3.map(x,y,z);
+
 }
 YOCTO_UNIT_TEST_DONE()
