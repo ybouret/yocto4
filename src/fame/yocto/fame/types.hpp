81b938ee7f98ee9620152fd0618f570934d07a43
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-09-24 08:24:49 +0000

    fame initial commit

---
+++ src/fame/yocto/fame/types.hpp
@@ -0,0 +1,106 @@
+#ifndef YOCTO_FAME_TYPES_INCLUDED
+#define YOCTO_FAME_TYPES_INCLUDED 1
+
+#include "yocto/ordered/sorted-vector.hpp"
+#include "yocto/math/v3d.hpp"
+
+namespace yocto
+{
+    //! Fields And Meshes Explorer
+    namespace fame
+    {
+        //______________________________________________________________________
+        //
+        // definitions
+        //______________________________________________________________________
+        typedef ptrdiff_t              unit_t;
+        typedef memory::global         memory_kind;
+        typedef memory_kind::allocator memory_allocator;
+        
+        //______________________________________________________________________
+        //
+        // vertex for dimension
+        //______________________________________________________________________
+        template <size_t,typename>
+        struct vertex_for; //! root template
+        
+        template <typename T> struct vertex_for<1,T> { typedef T type; };
+        template <typename T> struct vertex_for<2,T> { typedef math::v2d<T> type; };
+        template <typename T> struct vertex_for<3,T> { typedef math::v3d<T> type; };
+        
+        
+        //______________________________________________________________________
+        //
+        // logical coordinates
+        //______________________________________________________________________
+        typedef unit_t            coord1D;
+        typedef math::v2d<unit_t> coord2D;
+        typedef math::v3d<unit_t> coord3D;
+        
+        template <size_t>
+        struct coord_for;
+        template <> struct coord_for<1> { typedef coord1D type; };
+        template <> struct coord_for<2> { typedef coord2D type; };
+        template <> struct coord_for<3> { typedef coord3D type; };
+        
+        
+        
+        //! extract coord from compound COORD
+        template <typename COORD>
+        inline unit_t & __coord( COORD &C, size_t dim ) throw()
+        {
+            assert(dim<sizeof(COORD)/sizeof(unit_t));
+            return *(((unit_t *)&C)+dim);
+        }
+        
+        //! extract coord from compound COORD (const)
+        template <typename COORD>
+        inline const unit_t & __coord( const COORD &C, size_t dim ) throw()
+        {
+            assert(dim<sizeof(COORD)/sizeof(unit_t));
+            return *(((unit_t *)&C)+dim);
+        }
+        
+        //! compare coordinates (integral types)
+        template <typename COORD>
+        inline bool are_same_coord( const COORD &A, const COORD &B ) throw()
+        {
+            const unit_t * a = & __coord(A,0);
+            const unit_t * b = & __coord(B,0);
+            for(size_t i=0;i<sizeof(COORD)/sizeof(unit_t);++i)
+            {
+                if(a[i]!=b[i])
+                    return false;
+            }
+            return true;
+        }
+        
+        typedef sorted_vector<
+        unit_t,
+        comparator<unit_t>,
+        memory_allocator>  sorted_offsets;
+        
+        //! sorted list of offsets
+        class offsets_list : public sorted_offsets
+        {
+        public:
+            explicit offsets_list() throw();
+            explicit offsets_list(size_t num);
+            virtual ~offsets_list() throw();
+            offsets_list( const offsets_list & );
+            
+            //! permissive storage
+            void store( size_t indx );
+            
+        private:
+            YOCTO_DISABLE_ASSIGN(offsets_list);
+        };
+        
+        
+        
+        
+    }
+}
+
+#endif
+
