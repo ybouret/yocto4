e5103b139d8d0bebc3ebe311fa9d2b8998df3c51
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-09-26 13:01:14 +0000

    new genbuild

--- src/lang/yocto/lang/genebuild.cpp
+++ src/lang/yocto/lang/genebuild.cpp
@@ -16,8 +16,11 @@ namespace yocto
             enum vnode_type
             {
                 vnode_rule,
-                vnode_term
+                vnode_term,
+                vnode_joker
             };
+            
+            
             
             class vnode;
             
@@ -29,36 +32,21 @@ namespace yocto
             public:
                 typedef vnode *                  ptr;
                 
-                const syntax::xnode *node;
                 const string         name;
+                const syntax::xnode *node;
                 const vnode_type     type;
                 vnode               *next;
                 vnode               *prev;
                 alist                children;
-                bool                 built;
                 
                 //! a rule
-                explicit vnode( const syntax::xnode *nd ) :
+                explicit vnode(const string &id, const syntax::xnode *nd,  vnode_type nt ) :
+                name(id),
                 node(nd),
-                name(nd->lxm()->to_string()),
-                type(vnode_rule),
+                type(nt),
                 next(0),
                 prev(0),
-                children(),
-                built(false)
-                {
-                    
-                }
-                
-                //! a terminal = expression
-                explicit vnode( const syntax::xnode *nd, const string &expr ) :
-                node(nd),
-                name(expr),
-                type(vnode_term),
-                next(0),
-                prev(0),
-                children(),
-                built(true)
+                children()
                 {
                     
                 }
@@ -77,12 +65,12 @@ namespace yocto
             {
             public:
                 vlist  vr;   //!< virtual top level rules
-                vmap   vm;   //!< named rules
                 vlist  vl;   //!< local rules
+                vmap   vm;   //!< global map
                 int    vi;   //!< index for rule naming
                 p_dict dict; //!< for storing patterns
                 
-                inline vcontext() : vr(), vm(), vl(), vi(0), dict()
+                inline vcontext() : vr(), vl(), vi(0), dict()
                 {
                     
                 }
@@ -92,131 +80,46 @@ namespace yocto
                     
                 }
                 
-                //______________________________________________________________
-                //
-                // top level rules collection
-                //______________________________________________________________
-                inline void collect_rules_from(const syntax::xnode *root )
+                vnode *find_in( vlist &l, const string &id ) throw()
                 {
-                    
-                    // TODO: better
-                    if(root->count()<=0)
-                        throw exception("no rules");
-                    
-                    // first pass: named rules
-                    for(const syntax::xnode *r=root->head();r;r=r->next)
+                    for(vnode *vn = l.head; vn; vn=vn->next)
                     {
-                        if( "RULE" == r->label )
-                        {
-                            const syntax::xnode *nd = r->head();
-                            assert("ID"==nd->label);
-                            vnode *vn = new vnode(nd);
-                            vr.push_back(vn);
-                            if(!vm.insert(vn->name,vn))
-                            {
-                                throw exception("multiple top level rule '%s'", vn->name.c_str());
-                            }
-                            std::cerr << "registered '" << vr.tail->name << "'" << std::endl;
-                            continue;
-                        }
-                        
-                        throw exception("can't handle %s", r->label.c_str());
+                        if(vn->name == id) return vn;
                     }
-                    
-                    // second pass: virtual tree
-                    build(vr.head);
+                    return 0;
                 }
                 
-                //______________________________________________________________
-                //
-                // virtual tree building
-                //______________________________________________________________
-                inline void build(vnode *vn)
+                vnode *find_rule(const string &id) throw()
                 {
-                    assert(vn);
-                    switch(vn->type)
-                    {
-                        case vnode_rule:
-                            build_rule(vn);
-                            break;
-                            
-                        default:
-                            throw exception("can't handle this type");
-                    }
+                    return find_in(vr,id);
                 }
                 
-                //______________________________________________________________
-                //
-                // parsing a rule
-                //______________________________________________________________
-                inline void build_rule(vnode *vn)
+                inline void register_rule(const syntax::xnode *xn)
                 {
-                    assert(vn);
-                    assert(vnode_rule==vn->type);
-                    const syntax::xnode *xn = vn->node;
-                    
-                    //__________________________________________________________
-                    //
-                    // first xnode must be ID since it's a rule
-                    //__________________________________________________________
+                    assert(xn);
                     assert("ID"==xn->label);
-                    if(!vn->built)
+                    const string id = xn->lxm()->to_string();
+                    std::cerr <<  "-- new rule " << id << std::endl;
+                    if(find_rule(id))
                     {
-                        std::cerr << "building rule " << vn->name << std::endl;
-                        build_children(vn,xn->next);
+                        throw exception("%d: multiple rule '%s'", xn->lxm()->line, id.c_str());
                     }
+                    vnode *vn = new vnode(id,xn->next,vnode_rule);
+                    vr.push_back(vn);
                 }
                 
-                inline void build_children(vnode *parent, const syntax::xnode *xn)
+                
+                inline void collect_rules( const syntax::xnode *rules )
                 {
-                    for(;xn;xn=xn->next)
+                    assert( !rules->terminal );
+                    assert("RULES" == rules->label );
+                    for( const syntax::xnode *xn = rules->head(); xn; xn=xn->next)
                     {
-                        const string &label = xn->label;
-                        std::cerr << "\t\t" << label << std::endl;
+                        assert("RULE" == xn->label);
+                        register_rule(xn->head());
                         
-                        if( "ID" == label )
-                        {
-                            //__________________________________________________
-                            //
-                            // a sub rule
-                            //__________________________________________________
-                            const string id = xn->lxm()->to_string();
-                            std::cerr << "\t\t|_" << id << std::endl;
-                            vnode::ptr *ppvn = vm.search(id);
-                            if(!ppvn)
-                            {
-                                throw exception("'%s' refers to unknown rule '%s'",  parent->name.c_str(), id.c_str() );
-                            }
-                            vnode::ptr vn = *ppvn;
-                            parent->children.append(vn);
-                            build_rule(vn); // recursive
-                            continue;
-                        }
-                        
-                        
-                        if( "string" == label )
-                        {
-                            //__________________________________________________
-                            //
-                            // a string to be compiled
-                            //__________________________________________________
-                            const string expr = xn->lxm()->to_string();
-                            std::cerr << "\t\t|_'" << expr << "'" << std::endl;
-                            if( !dict.has(expr) )
-                            {
-                                dict.add(expr,expr);
-                            }
-                            
-                            vnode *vn = new vnode(xn,expr);
-                            vl.push_back(vn);
-                            parent->children.append(vn);
-                            continue;
-                        }
                     }
-                    parent->built = true;
                 }
-                
-                
                 
             private:
                 YOCTO_DISABLE_COPY_AND_ASSIGN(vcontext);
@@ -250,7 +153,7 @@ namespace yocto
             // Let's rebuild the tree: gather top level rules
             //__________________________________________________________________
             vcontext ctx;
-            ctx.collect_rules_from(root);
+            ctx.collect_rules(root);
             
             //__________________________________________________________________
             //
