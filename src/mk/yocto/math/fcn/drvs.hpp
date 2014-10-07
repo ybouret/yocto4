0b94246209ae03410cda38e70d5d9bad844925d4
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-10-07 11:50:44 +0000

    cleanup code

--- src/mk/yocto/math/fcn/drvs.hpp
+++ src/mk/yocto/math/fcn/drvs.hpp
@@ -17,22 +17,40 @@ namespace yocto
             virtual ~derivative() throw();
             
             T diff( typename numeric<T>::function &F, T x, T h, T &err);
-            void diff( T &dFdx, T &d2Fdx2, typename numeric<T>::function &F, T x, const T Fx, T h, T &err);
             
             T operator()( typename numeric<T>::function &F, T x, T h );
             
-            void operator()(T &dFdx, T &d2Fdx2, typename numeric<T>::function &F, T x, const T Fx, T h);
             
+        protected:
+            matrix<T> a;
             
         private:
             YOCTO_DISABLE_COPY_AND_ASSIGN(derivative);
-            matrix<T> a;
-            matrix<T> b;
             
         public:
             const T dtol; //!< approx minimal fractional tolerance on dFdx
             const T hopt; //!< optimal step for a scaling of x_c=1
             const T hmin; //!< should be minimal initial step
+        };
+        
+        
+        template <typename T>
+        class derivative2 : public derivative<T>
+        {
+        public:
+            explicit derivative2();
+            virtual ~derivative2() throw();
+            
+            void diff( T &dFdx, T &d2Fdx2, typename numeric<T>::function &F, T x, const T Fx, T h, T &err);
+            void operator()(T &dFdx, T &d2Fdx2, typename numeric<T>::function &F, T x, const T Fx, T h);
+
+            
+        protected:
+            matrix<T> b;
+            
+        private:
+            YOCTO_DISABLE_COPY_AND_ASSIGN(derivative2);
+            
         };
         
         
