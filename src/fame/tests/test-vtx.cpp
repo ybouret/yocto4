0b373f475909036630a9612721c1ba5fb9f444f3
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-10-13 12:15:48 +0000

    created points

--- src/fame/tests/test-vtx.cpp
+++ src/fame/tests/test-vtx.cpp
@@ -1,5 +1,5 @@
 #include "yocto/utest/run.hpp"
-#include "yocto/fame/vertices.hpp"
+#include "yocto/fame/points.hpp"
 
 
 using namespace yocto;
@@ -9,9 +9,9 @@ using namespace math;
 
 YOCTO_UNIT_TEST_IMPL(vtx)
 {
-    //vertices<1,float>  v1f("v1f", layout1D(-5,5) );
-    //vertices<2,double> v2d("v2d", layout1D(1,10) );
-    //vertices<3,double> v3d("v3d", layout1D(1,20) );
+    
+    Point<1,float>::List points;
+    
     
 }
 YOCTO_UNIT_TEST_DONE()
