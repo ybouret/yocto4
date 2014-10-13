5efc23510de988b3c60ace843f4c575f283d775e
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-10-13 10:44:48 +0000

    updated

--- src/fame/yocto/fame/vertices.hpp
+++ src/fame/yocto/fame/vertices.hpp
@@ -9,29 +9,8 @@ namespace yocto
     namespace fame
     {
         
-        template <size_t DIM,typename T>
-        class vertices : public array1D< typename vertex_for<DIM,T>::type >
-        {
-        public:
-            typedef typename vertex_for<DIM,T>::type type;
-            typedef array1D<type>                    array_type;
-            
-            explicit vertices(const string     &user_name,
-                              const layout1D    L) :
-            array_type(user_name,L,0)
-            {
-            }
-            
-            virtual ~vertices() throw()
-            {
-            }
-            
-            
-        private:
-            YOCTO_DISABLE_COPY_AND_ASSIGN(vertices);
-        };
         
-        
+    
     }
 }
 
