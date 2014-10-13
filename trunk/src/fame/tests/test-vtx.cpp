1be32ebe9798f97b0dc3c83a20f8344401ba066a
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-10-13 10:44:48 +0000

    updated

--- trunk/src/fame/tests/test-vtx.cpp
+++ trunk/src/fame/tests/test-vtx.cpp
@@ -9,9 +9,9 @@ using namespace math;
 
 YOCTO_UNIT_TEST_IMPL(vtx)
 {
-    vertices<1,float>  v1f("v1f", layout1D(-5,5) );
-    vertices<2,double> v2d("v2d", layout1D(1,10) );
-    vertices<3,double> v3d("v3d", layout1D(1,20) );
+    //vertices<1,float>  v1f("v1f", layout1D(-5,5) );
+    //vertices<2,double> v2d("v2d", layout1D(1,10) );
+    //vertices<3,double> v3d("v3d", layout1D(1,20) );
     
 }
 YOCTO_UNIT_TEST_DONE()
