740dbd3a2fb0408ecb6614a0709df204f6a67d2c
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-10-14 08:34:31 +0000

    forget optimize...

--- src/mk/tests/test-opt.cpp
+++ src/mk/tests/test-opt.cpp
@@ -53,7 +53,7 @@ YOCTO_UNIT_TEST_IMPL(opt)
     std::cerr << "Double: " << std::endl;
     optim<double>(minimize<double>,xtol);
     optim<double>(minimize2<double>,xtol);
-    optim<double>(minimize3<double>,xtol);
+    //optim<double>(minimize3<double>,xtol);
 }
 YOCTO_UNIT_TEST_DONE()
 
