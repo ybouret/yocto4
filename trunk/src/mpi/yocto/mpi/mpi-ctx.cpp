bbaa5a65f484d2014d685d64e361a63b122678ea
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-10-14 15:51:48 +0000

    mpi update

--- trunk/src/mpi/yocto/mpi/mpi-ctx.cpp
+++ trunk/src/mpi/yocto/mpi/mpi-ctx.cpp
@@ -2,14 +2,18 @@
 
 namespace yocto
 {
-    mpi::comm_world:: comm_world( const mpi &MPI ) :
-    size( MPI.CommWorldSize ),
-    rank( MPI.CommWorldRank ),
-    last( MPI.CommWorldLast ),
-    first( MPI.IsFirst ),
-    final( MPI.IsFinal ),
-    parallel(MPI.IsParallel)
+    mpi:: comm_world:: comm_world( const mpi &ref ) :
+    MPI( ref ),
+    size(     MPI.CommWorldSize ),
+    rank(     MPI.CommWorldRank ),
+    last(     MPI.CommWorldLast ),
+    first(    MPI.IsFirst       ),
+    final(    MPI.IsFinal       ),
+    parallel( MPI.IsParallel    )
     {
     }
+    
+    mpi:: comm_world:: ~comm_world() throw() {}
+    
     
 }
