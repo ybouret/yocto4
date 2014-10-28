18da04413965e6d7b592d63612d56a5137e439dd
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-10-28 10:16:04 +0000

    basic split

--- trunk/src/fame/yocto/fame/split.cpp
+++ trunk/src/fame/yocto/fame/split.cpp
@@ -8,13 +8,38 @@ namespace yocto
 {
     namespace fame
     {
-        layout1D Link::Split(const layout1D &full_layout,
-                             int rank,
-                             int size )
+        
+        static inline
+        int __get_prev( int rank, int size ) throw()
+        {
+            assert(size>0);
+            assert(rank>=0);
+            assert(rank<size);
+            return (rank <= 0) ? (size-1) : rank-1;
+        }
+        
+        static inline
+        int __get_next( int rank, int size ) throw()
+        {
+            assert(size>0);
+            assert(rank>=0);
+            assert(rank<size);
+            return (rank>=size-1) ? 0 : rank+1;
+        }
+        
+        
+        
+        layout1D Split(const layout1D &full_layout,
+                       int             rank,
+                       int             size,
+                       bool            pbc,
+                       Links          &links)
         {
             
             assert(rank>=0);
             assert(size>0);
+            links.reset();
+            
             if(size>1)
             {
                 unit_t offset = full_layout.lower;
@@ -22,6 +47,31 @@ namespace yocto
                 if(length<=0)
                     throw libc::exception( EDOM, "layout is too small");
                 core::mpi_split(rank, size, offset, length);
+                if(rank==0)
+                {
+                    if(pbc)
+                    {
+                        links.append(__get_prev(rank,size), Link::Prev);
+                    }
+                    links.append(__get_next(rank, size), Link::Next);
+                }
+                else
+                {
+                    if(rank==size-1)
+                    {
+                        links.append(__get_prev(rank,size),Link::Prev);
+                        if(pbc)
+                        {
+                            links.append(__get_next(rank,size),Link::Next);
+                        }
+                    }
+                    else
+                    {
+                        links.append(__get_prev(rank,size),Link::Prev);
+                        links.append(__get_next(rank,size),Link::Next);
+                    }
+                }
+                
                 return layout1D(offset,(offset+length)-1);
             }
             else
