7e54c51ae604e3625cc8fdcb8af691fc714c791e
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-10-28 10:16:04 +0000

    basic split

--- src/fame/tests/test-split.cpp
+++ src/fame/tests/test-split.cpp
@@ -7,14 +7,15 @@ using namespace fame;
 YOCTO_UNIT_TEST_IMPL(split)
 {
     const layout1D l1 = layout1D(-5,5);
+    Links lx;
     for(int size=1;size<=5;++size)
     {
         std::cerr << std::endl;
         std::cerr << "size="  << size << " : full= " << l1 << std::endl;
         for(int rank=0;rank<size;++rank)
         {
-            const layout1D l = Link::Split(l1, rank, size);
-            std::cerr << "\trank="  << rank << " : " << l << std::endl;
+            const layout1D l = Split(l1, rank, size,true,lx);
+            std::cerr << "\trank="  << rank << " : " << l << ",\tlinks=" << lx << std::endl;
         }
     }
     
--- src/fame/yocto/fame/split.hpp
+++ src/fame/yocto/fame/split.hpp
@@ -20,19 +20,59 @@ namespace yocto
             const int      rank;
             const Position pos;
             
-            explicit Link( int r, Position p) throw() : rank(r), pos(p) {}
+            Link() throw() : rank(-1), pos(Prev) {}
+            Link( int r, Position p) throw() : rank(r), pos(p) {}
             ~Link() throw() {}
             Link(const Link &link) throw() : rank(link.rank), pos(link.pos) {}
             
-            static
-            layout1D Split(const layout1D &full_layout,
-                           int rank,
-                           int size );
+            friend std::ostream & operator<<(std::ostream &os, const Link &l )
+            {
+                os << l.rank << "@" << ((l.pos==Prev) ? "Prev" : "Next");
+                return os;
+            }
             
         private:
             YOCTO_DISABLE_ASSIGN(Link);
         };
         
+        class Links
+        {
+        public:
+            const size_t count;
+            inline Links() throw() : count(0), link() {}
+            inline ~Links() throw() {}
+            
+            void reset() throw() { (int&)count = 0; }
+            
+            void append(const int rank, const Link::Position pos) throw()
+            {
+                assert(count<2);
+                (int            &)(link[count].rank) = rank;
+                (Link::Position &)(link[count].pos)  = pos;
+                ++(size_t &)count;
+            }
+            
+            friend std::ostream & operator<<(std::ostream &os, const Links &l )
+            {
+                os << "[[";
+                for(size_t i=0;i<l.count;++i)
+                {
+                    os << ' ' << l.link[i];
+                }
+                os << " ]]";
+                return os;
+            }
+            
+        private:
+            Link link[2];
+            YOCTO_DISABLE_COPY_AND_ASSIGN(Links);
+        };
+        
+        layout1D Split(const layout1D &full_layout,
+                       int             rank,
+                       int             size,
+                       bool            pbc,
+                       Links          &links);
         
         
         
