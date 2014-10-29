1fc4f2a02cd7a31f24032fa4bb0baa9beccc5003
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-10-29 13:36:34 +0000

    rewrote quad1d split

---
+++ src/fame/yocto/fame/split/quad1d.cpp
@@ -0,0 +1,99 @@
+#include "yocto/fame/split/quad1d.hpp"
+#include "yocto/core/mpi-split.hpp"
+#include "yocto/exceptions.hpp"
+
+#include <cerrno>
+
+namespace yocto
+{
+    
+    namespace fame
+    {
+        
+        static inline
+        int __get_prev( int rank, int size ) throw()
+        {
+            assert(size>0);
+            assert(rank>=0);
+            assert(rank<size);
+            return (rank <= 0) ? (size-1) : rank-1;
+        }
+        
+        static inline
+        int __get_next( int rank, int size ) throw()
+        {
+            assert(size>0);
+            assert(rank>=0);
+            assert(rank<size);
+            return (rank>=size-1) ? 0 : rank+1;
+        }
+        
+        
+        layout1D quad_split(const layout1D &full,
+                            const int       rank,
+                            const int       size,
+                            const bool      pbc,
+                            quad_links     &links )
+        {
+            
+            assert(size>0);
+            assert(rank<size);
+            links.reset();
+            
+            //__________________________________________________________________
+            //
+            // get data
+            //__________________________________________________________________
+            
+            unit_t offset = full.lower;
+            unit_t length = full.width;
+            if(length<size)
+            {
+                throw libc::exception(EDOM,"quad_split1d: layout is too small");
+            }
+            
+            //__________________________________________________________________
+            //
+            // split
+            //__________________________________________________________________
+            core::mpi_split(rank, size, offset, length);
+            assert(length>0);
+            
+            //__________________________________________________________________
+            //
+            // building links
+            //__________________________________________________________________
+            if(rank==0)
+            {
+                if(pbc)
+                {
+                    links.append(quad_link::is_prev,__get_prev(rank,size));
+                }
+                links.append(quad_link::is_next, __get_next(rank,size));
+            }
+            else
+            {
+                if(size-1==rank)
+                {
+                    links.append(quad_link::is_prev,__get_prev(rank,size));
+                    if(pbc)
+                    {
+                        links.append(quad_link::is_next, __get_next(rank,size));
+                    }
+                }
+                else
+                {
+                    assert(rank>0);
+                    assert(size>1);
+                    assert(rank<size-1);
+                    links.append(quad_link::is_prev,rank-1);
+                    links.append(quad_link::is_next,rank+1);
+                }
+            }
+            
+            return layout1D(offset,(offset+length)-1);
+        }
+        
+    }
+    
+}
