03d12725027c9c3c7c8b0f8f2a770f937eeb4333
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-10-29 15:04:13 +0000

    better split1d/2d

--- trunk/src/fame/tests/test-split.cpp
+++ trunk/src/fame/tests/test-split.cpp
@@ -1,6 +1,6 @@
 #include "yocto/fame/split/quad1d.hpp"
 #include "yocto/utest/run.hpp"
-#include "yocto/ios/ocstream.hpp"
+#include "yocto/fame/split/quad2d.hpp"
 
 using namespace yocto;
 using namespace fame;
@@ -9,20 +9,44 @@ using namespace fame;
 
 YOCTO_UNIT_TEST_IMPL(split)
 {
-    const layout1D l1 = layout1D(-5,5);
     quad_links xlinks;
     
-    for(int size=1;size<=8;++size)
     {
-        std::cerr << std::endl;
-        std::cerr << "size=" << size << ", full=" << l1 << std::endl;
-        for(int rank=0;rank<size;++rank)
+        const layout1D l1 = layout1D(-5,5);
+        
+        for(int size=1;size<=8;++size)
         {
-            const layout1D s = quad_split(l1, rank, size, true,xlinks);
-            std::cerr << "\trank=" << rank << ",\t sub=" << s << ",\t links=" << xlinks << std::endl;
+            std::cerr << std::endl;
+            std::cerr << "size=" << size << ", full=" << l1 << std::endl;
+            for(int rank=0;rank<size;++rank)
+            {
+                const layout1D s = quad_split(l1, rank, size, true,xlinks);
+                std::cerr << "\trank=" << rank << ",\t sub=" << s << ",\t links=" << xlinks << std::endl;
+            }
         }
     }
     
+    quad_links ylinks;
+    
+    {
+        const layout2D l2 = layout2D( coord2D(-5,-6), coord2D(5,6) );
+        coord2D    sizes;
+        coord2D    ranks;
+        for(int size=1;size<=16;++size)
+        {
+            std::cerr << std::endl;
+            std::cerr << "size=" << size << ", full=" << l2 << std::endl;
+            for(int rank=0;rank<size;++rank)
+            {
+                const layout2D s = quad_split(l2, rank, size, true,xlinks, true,ylinks,ranks,sizes);
+                std::cerr << "\trank = " << rank << " : " << ranks << " / " << sizes << ", sub=" << s
+                << " xlinks=" << xlinks << ", ylinks=" << ylinks
+                << std::endl;
+                
+            }
+            
+        }
+    }
     
 }
 YOCTO_UNIT_TEST_DONE()
---
+++ trunk/src/fame/yocto/fame/split/quad2d.hpp
@@ -0,0 +1,28 @@
+#ifndef YOCTO_FAME_SPLIT_QUAD2D_INCLUDED
+#define YOCTO_FAME_SPLIT_QUAD2D_INCLUDED 1
+
+#include "yocto/fame/split/quad-links.hpp"
+#include "yocto/fame/layout.hpp"
+
+namespace yocto
+{
+    
+    namespace fame
+    {
+        
+        layout2D quad_split(const layout2D &full,
+                            const int       rank,
+                            const int       size,
+                            const bool      xpbc,
+                            quad_links     &xlinks,
+                            const bool      ypbc,
+                            quad_links     &ylinks,
+                            coord2D        &ranks,
+                            coord2D        &sizes);
+        
+    }
+    
+}
+
+#endif
+
