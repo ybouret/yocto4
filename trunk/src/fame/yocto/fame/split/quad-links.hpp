19f0665904edce4f367b6b8a47749bbc093529e3
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-10-29 13:10:15 +0000

    upgrading fame

---
+++ trunk/src/fame/yocto/fame/split/quad-links.hpp
@@ -0,0 +1,50 @@
+#ifndef YOCTO_FAME_SPLIT_QUAD_LINKS_INCLUDED
+#define YOCTO_FAME_SPLIT_QUAD_LINKS_INCLUDED 1
+
+#include "yocto/fame/types.hpp"
+
+namespace yocto
+{
+    namespace fame
+    {
+        //! a quad link in one direction
+        class quad_link
+        {
+        public:
+            enum position
+            {
+                is_prev,
+                is_next
+            };
+            position pos;
+            int      rank;
+            
+            quad_link() throw();
+            quad_link(position p, int r) throw();
+            ~quad_link() throw();
+            quad_link(const quad_link &) throw();
+            quad_link &operator=(const quad_link &) throw();
+        };
+        
+        //! quad links in one direction
+        class quad_links
+        {
+        public:
+            const size_t count;
+            quad_links()  throw();
+            ~quad_links() throw();
+            
+            void append( quad_link::position pos, int rank ) throw();
+            
+            quad_link & operator[](size_t i) throw();
+            const quad_link & operator[](size_t i) const throw();
+            
+        private:
+            quad_link link[2];
+            YOCTO_DISABLE_COPY_AND_ASSIGN(quad_links);
+        };
+    }
+}
+
+#endif
+
