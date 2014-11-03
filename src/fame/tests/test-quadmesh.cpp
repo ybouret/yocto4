6bc1fc2fcb0e1ad367adfa8e67a5d559b85f41ff
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-11-03 10:31:17 +0000

    upgrading to quadmesh...

---
+++ src/fame/tests/test-quadmesh.cpp
@@ -0,0 +1,12 @@
+#include "yocto/fame/mesh/quad.hpp"
+#include "yocto/utest/run.hpp"
+
+using namespace yocto;
+using namespace fame;
+
+YOCTO_UNIT_TEST_IMPL(quadmesh)
+{
+    
+}
+YOCTO_UNIT_TEST_DONE()
+
---
+++ src/fame/yocto/fame/mesh/quad.hpp
@@ -0,0 +1,26 @@
+#ifndef YOCTO_FAME_QUADMESH_INCLUDED
+#define YOCTO_FAME_QUADMESH_INCLUDED 1
+
+#include "yocto/fame/mesh.hpp"
+#include "yocto/fame/split/quad1d.hpp"
+
+namespace yocto
+{
+    namespace fame
+    {
+        
+        //! 
+        template <typename Layout>
+        class QuadMesh : public Layout
+        {
+        public:
+            
+            
+        private:
+            
+        };
+        
+    }
+}
+
+#endif
