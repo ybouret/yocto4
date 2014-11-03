c656ec74aa459a648711e22f70d058a5690ed913
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-11-03 12:13:02 +0000

    basic quad grid

--- src/fame/tests/test-quadmesh.cpp
+++ src/fame/tests/test-quadmesh.cpp
@@ -4,8 +4,61 @@
 using namespace yocto;
 using namespace fame;
 
-YOCTO_UNIT_TEST_IMPL(quadmesh)
+YOCTO_UNIT_TEST_IMPL(grid)
 {
+    
+    int ng = 1;
+    
+    const bool periodic[3] = { true, true, true };
+    
+    {
+        const layout1D L(1,30);
+        for(int size=1;size<=8;++size)
+        {
+            std::cerr << std::endl;
+            std::cerr << "size=" << size << " : full=" << L << std::endl;
+            for(int rank=0;rank<size;++rank)
+            {
+                QuadGrid<layout1D> grid(L,rank,size,ng,periodic);
+                std::cerr << "\trank=" << rank << " is " << grid.ranks << "/" << grid.sizes << std::endl;
+            }
+        }
+    }
+    
+    
+    {
+        const layout2D L( coord2D(1,1), coord2D(30,40) );
+        for(int size=1;size<=8;++size)
+        {
+            std::cerr << std::endl;
+            std::cerr << "size=" << size << " : full=" << L << std::endl;
+            for(int rank=0;rank<size;++rank)
+            {
+                QuadGrid<layout2D> grid(L,rank,size,ng,periodic);
+                std::cerr << "\trank=" << rank << " is " << grid.ranks << "/" << grid.sizes << std::endl;
+            }
+        }
+    }
+    
+    {
+    
+        const layout3D L( coord3D(1,1,1), coord3D(30,40,50) );
+        for(int size=1;size<=9;++size)
+        {
+            std::cerr << std::endl;
+            std::cerr << "size=" << size << " : full=" << L << std::endl;
+            for(int rank=0;rank<size;++rank)
+            {
+                QuadGrid<layout3D> grid(L,rank,size,ng,periodic);
+                std::cerr << "\trank=" << rank << " is " << grid.ranks << "/" << grid.sizes << std::endl;
+                std::cerr << "\t\tsub = " << grid << std::endl;
+                std::cerr << "\t\tout = " << grid.outline << std::endl;
+            }
+        }
+    }
+
+
+    
     
 }
 YOCTO_UNIT_TEST_DONE()
--- src/fame/yocto/fame/mesh/quad-grid.hpp
+++ src/fame/yocto/fame/mesh/quad-grid.hpp
@@ -2,21 +2,120 @@
 #define YOCTO_FAME_QUADGRID_INCLUDED 1
 
 #include "yocto/fame/mesh.hpp"
+#include "yocto/fame/split/build-quad-ghosts.hpp"
+
 #include "yocto/fame/split/quad1d.hpp"
+#include "yocto/fame/split/quad2d.hpp"
+#include "yocto/fame/split/quad3d.hpp"
 
 namespace yocto
 {
     namespace fame
     {
         
-        //! 
         template <typename Layout>
-        class QuadGrid : public Layout
+        class QuadGridData
         {
         public:
+            explicit QuadGridData() throw() : ranks(), sizes(), qlinks()  {}
+            virtual ~QuadGridData() throw() {}
+            
+            const typename Layout::coord ranks;
+            const typename Layout::coord sizes;
+            
+            
+        protected:
+            quad_links qlinks[ Layout::DIMENSIONS ];
+            
+        private:
+            YOCTO_DISABLE_COPY_AND_ASSIGN(QuadGridData);
+        };
+        
+        //!
+        template <typename Layout>
+        class QuadGrid : public QuadGridData<Layout>, public Layout
+        {
+        public:
+            static const size_t DIMENSIONS = Layout::DIMENSIONS;
+            typedef typename Layout::coord Coord;
+            typedef quad_ghosts<Layout>    Ghosts;
+            typedef typename Ghosts::list  GhostsList;
+            
+            
+        public:
+            GhostsList   local_ghosts;
+            GhostsList   async_ghosts;
+            const Layout outline;
+            
+            explicit QuadGrid(const Layout &full,
+                              const int     rank,
+                              const int     size,
+                              const int     ng,
+                              const bool   *pbc) :
+            QuadGridData<Layout>(),
+            Layout( __split(full,rank,size,pbc, this->qlinks, this->ranks, this->sizes, int2type<DIMENSIONS>() ) ),
+            local_ghosts(),
+            async_ghosts(),
+            outline( build_quad_ghosts<Layout>::outline_for(rank, *this, 1, this->qlinks, local_ghosts, async_ghosts ) )
+            {
+                
+            }
+            
             
             
         private:
+            YOCTO_DISABLE_COPY_AND_ASSIGN(QuadGrid);
+            
+            static inline
+            Layout __split(const Layout &full,
+                           const int     rank,
+                           const int     size,
+                           const bool   *pbc,
+                           quad_links   *links,
+                           const Coord  &__ranks,
+                           const Coord  &__sizes,
+                           int2type<1> )
+            {
+                const bool xpbc = (pbc ? pbc[0] : false);
+                (Coord &)__ranks = rank;
+                (Coord &)__sizes = size;
+                return quad1D::split(full, rank, size, xpbc, links[0]);
+            }
+            
+            static inline
+            Layout __split(const Layout &full,
+                           const int     rank,
+                           const int     size,
+                           const bool   *pbc,
+                           quad_links   *links,
+                           const Coord & __ranks,
+                           const Coord & __sizes,
+                           int2type<2> )
+            {
+                const bool has_pbc = (pbc!=NULL);
+                const bool xpbc = has_pbc ? pbc[0] : false;
+                const bool ypbc = has_pbc ? pbc[1] : false;
+                return quad2D::split(full, rank, size, xpbc, links[0], ypbc, links[1],(Coord &)__ranks, (Coord &)__sizes);
+            }
+            
+            static inline
+            Layout __split(const Layout &full,
+                           const int     rank,
+                           const int     size,
+                           const bool   *pbc,
+                           quad_links   *links,
+                           const Coord & __ranks,
+                           const Coord & __sizes,
+                           int2type<3> )
+            {
+                const bool has_pbc = (pbc!=NULL);
+                const bool xpbc = has_pbc ? pbc[0] : false;
+                const bool ypbc = has_pbc ? pbc[1] : false;
+                const bool zpbc = has_pbc ? pbc[2] : false;
+                return quad3D::split(full, rank, size, xpbc, links[0], ypbc, links[1], zpbc, links[2], (Coord &)__ranks, (Coord &)__sizes);
+            }
+
+            
             
         };
         
