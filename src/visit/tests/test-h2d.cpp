9ef21a5090bd5f9a50ba09ad711cb39acda5e29d
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-11-10 10:55:30 +0000

    seems ok with heat2D

--- src/visit/tests/test-h2d.cpp
+++ src/visit/tests/test-h2d.cpp
@@ -4,6 +4,7 @@
 #include "yocto/utest/run.hpp"
 #include "yocto/code/rand.hpp"
 #include "yocto/code/ipower.hpp"
+#include "yocto/fame/array2d.hpp"
 
 #include <cmath>
 
@@ -13,10 +14,11 @@ using namespace fame;
 
 namespace
 {
-    typedef layout1D                       Layout;
+    typedef layout2D                       Layout;
+    typedef coord2D                        Coord;
     typedef RectilinearMesh<Layout,double> MeshType;
     
-    static const bool the_pbc[1] = { true };
+    static const bool the_pbc[2] = { true, true };
     
     class Heat : public arrays, public MeshType
     {
@@ -25,18 +27,16 @@ namespace
                       const int     sim_rank,
                       const int     sim_size) :
         arrays(),
-        MeshType( *this, "mesh",full,the_pbc,sim_rank,sim_size,1),
-        curv("T")
+        MeshType( *this, "mesh",full,the_pbc,sim_rank,sim_size,1)
         {
-            store( new array1D<double>("T",outline) );
-            store( new array1D<double>("L",outline) );
+            store( new array2D<double>("T",outline) );
+            store( new array2D<double>("L",outline) );
         }
         
         virtual ~Heat() throw()
         {
         }
         
-        CurvInfo curv; //! for visit
         uniform_generator<double> ran;
         
     private:
@@ -50,14 +50,20 @@ namespace
         explicit HeatSim( const mpi &ref , const Layout &full) :
         VisIt::Simulation(ref),
         Heat(full,par_rank,par_size),
-        T( (*this)["T"].as< array1D<double> >() ),
-        L( (*this)["L"].as< array1D<double> >() ),
+        T( (*this)["T"].as< array2D<double> >() ),
+        L( (*this)["L"].as< array2D<double> >() ),
+        dx( 1.0 / full.width.x),
+        dy( 1.0 / full.width.y),
         handles()
         {
-            const double dx = 1.0 / full.width;
-            for(unit_t i=outline.lower;i<=outline.upper;++i)
+            for(unit_t i=outline.lower.x;i<=outline.upper.x;++i)
             {
                 X()[i] = (i-1)*dx;
+            }
+            
+            for(unit_t j=outline.lower.y;j<=outline.upper.y;++j)
+            {
+                Y()[j] = (j-1)*dy;
             }
             
             handles.append(&T);
@@ -72,8 +78,10 @@ namespace
         {
         }
         
-        array1D<double> &T;
-        array1D<double> &L;
+        array2D<double> &T;
+        array2D<double> &L;
+        const double     dx;
+        const double     dy;
         linear_handles   handles;
         
         void exchange()
@@ -84,9 +92,12 @@ namespace
         void reset()
         {
             MPI.Printf(stderr, "reset...\n");
-            for(unit_t i=lower;i<=upper;++i)
+            for(unit_t j=lower.y;j<=upper.y;++j)
             {
-                T[i] = ipower(sin( 2*M_PI*X()[i] ),1) + 0.05 * (0.5-ran());
+                for(unit_t i=lower.x;i<=upper.x;++i)
+                {
+                    T[j][i] = sin( 2*M_PI*X()[i] ) * sin( 2*M_PI*Y()[j]) + 0.05 * (0.5-ran());
+                }
             }
             exchange();
             cycle=0;
@@ -99,24 +110,40 @@ namespace
         {
             //
             add_generic_command("quit",md);
-
+            
             //
             add_generic_command("reset",md);
             
-            // curv
-            visit:: add_curv_meta_data(md, curv);
+            visit::add_mesh_meta_data(md, *this, par_size);
+            
+            visit::add_variable_meta_data(md, T, "mesh");
+            
         }
         
-        virtual visit_handle get_curve( const string &name ) const
+        virtual visit_handle get_mesh( int domain, const string &name) const
         {
             
-            if(name == "T" )
+            if(name=="mesh")
             {
-                return visit::get_curv(X(),T);
+                return visit::get_mesh(*this,domain);
             }
+            
+            
             return VISIT_INVALID_HANDLE;
         }
-
+        
+        virtual visit_handle get_variable( int domain, const string &name ) const
+        {
+            if( name == "T" )
+            {
+                return visit::get_variable_data(T,domain);
+            }
+            
+            
+            return VISIT_INVALID_HANDLE;
+        }
+        
+        
         virtual void execute(const string cmd, const array<string> &args)
         {
             if( cmd == "reset" )
@@ -128,28 +155,39 @@ namespace
         virtual void step()
         {
             runTime = cycle;
-            
+            const double lrho = ipower(dx/dy,2);
             // we start from a valid state
-            for(unit_t i=lower;i<=upper;++i)
+            for(unit_t j=lower.y;j<=upper.y;++j)
             {
-                L[i] = (T[i-1] - 2.0 *T[i] + T[i+1]);
+                for(unit_t i=lower.x;i<=upper.x;++i)
+                {
+                    L[j][i] = (T[j][i-1] - 2.0 *T[j][i] + T[j][i+1])
+                    + lrho * ( T[j-1][i] - 2.0 * T[j][i] + T[j+1][i]);
+                }
             }
             
-            for(unit_t i=lower;i<=upper;++i)
+            
+            for(unit_t j=lower.y;j<=upper.y;++j)
             {
-                T[i] += 0.1 * L[i];
+                for(unit_t i=lower.x;i<=upper.x;++i)
+                {
+                    T[j][i] += 0.02 * L[j][i];
+                }
             }
             
             exchange();
-        
-            for(unit_t i=lower;i<=upper;++i)
+            
+            
+            for(unit_t j=lower.y;j<=upper.y;++j)
             {
-                const double u =T[i];
-                T[i] += 0.04 * (u-u*u*u);
+                for(unit_t i=lower.x;i<=upper.x;++i)
+                {
+                    const double u =T[j][i];
+                    T[j][i] += 0.04 * (u-u*u*u);
+                }
             }
             
             exchange();
-
             
         }
         
@@ -159,10 +197,10 @@ namespace
     
 }
 
-YOCTO_UNIT_TEST_IMPL(h1d)
+YOCTO_UNIT_TEST_IMPL(h2d)
 {
-    const string sim_name    = "Heat1D";
-    const string sim_comment = "Visit+Fame=>Heat 1D";
+    const string sim_name    = "Heat2D";
+    const string sim_comment = "Visit+Fame=>Heat 2D";
     const string sim_path    = ".";
     
     YOCTO_MPI(MPI_THREAD_SINGLE);
@@ -171,9 +209,12 @@ YOCTO_UNIT_TEST_IMPL(h1d)
     VisIt::TraceFile trace(MPI,"trace.dat");
     VisIt::SetupParallel(MPI,sim_name,sim_comment,sim_path,NULL);
     
-    const Layout full(1,1000);
+    const Layout full( Coord(1,1), Coord(100,110) );
     
     HeatSim sim(MPI,full);
+    //MPI.Printf(stderr, "sizes= %d x %d\n", sim.sizes.x, sim.sizes.y);
+    
+    //return 0;
     
     VisIt::Loop(sim);
     
