37365edb77ef5da2cd536f2a3b9ae0245fa48194
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-11-14 16:07:03 +0000

    start eqs

--- trunk/src/chem/yocto/chemical/lua/io-eqs.cpp
+++ trunk/src/chem/yocto/chemical/lua/io-eqs.cpp
@@ -1,6 +1,8 @@
 #include "yocto/chemical/lua/io.hpp"
 #include "yocto/exception.hpp"
 
+#include "yocto/lua/lua-maths.hpp"
+
 namespace yocto
 {
     namespace chemical
@@ -10,7 +12,27 @@ namespace yocto
             
             class lua_equilibrium : public equilibrium
             {
+            public:
+                explicit lua_equilibrium(const string &id,
+                                         lua_State    *L,
+                                         const string &fn) :
+                equilibrium(id),
+                F(L,fn,true)
+                {
+                }
                 
+                virtual ~lua_equilibrium() throw()
+                {
+                }
+                
+                mutable Lua::Function<double> F;
+                
+            private:
+                YOCTO_DISABLE_COPY_AND_ASSIGN(lua_equilibrium);
+                virtual double getK(double t) const
+                {
+                    return F(t);
+                }
             };
             
         }
