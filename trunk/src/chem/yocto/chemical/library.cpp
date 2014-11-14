0081bf53d2cb91ad942232a62cbdc05e8a5effa8
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-11-14 14:13:55 +0000

    chem WIP

---
+++ trunk/src/chem/yocto/chemical/library.cpp
@@ -0,0 +1,136 @@
+#include "yocto/chemical/library.hpp"
+#include "yocto/exception.hpp"
+
+namespace yocto
+{
+    namespace chemical
+    {
+        
+        library:: library() throw() :
+        db(),
+        max_name_length(0)
+        {
+        }
+        
+        library:: ~library() throw()
+        {
+        }
+        
+        library::iterator library::begin() throw()
+        {
+            return db.begin();
+        }
+        
+        library::iterator library::end() throw()
+        {
+            return db.end();
+        }
+        
+        library::const_iterator library::begin() const throw()
+        {
+            return db.begin();
+        }
+        
+        library::const_iterator library::end() const throw()
+        {
+            return db.end();
+        }
+        
+        species & library:: operator[](const string &id )
+        {
+            species::pointer *pp = db.search(id);
+            if(!pp)
+                throw exception("no library['%s']", id.c_str());
+            return **pp;
+        }
+        
+        const species & library:: operator[](const string &id ) const
+        {
+            const species::pointer *pp = db.search(id);
+            if(!pp)
+                throw exception("no const library['%s']", id.c_str());
+            return **pp;
+        }
+        
+        species & library:: operator[](const char   *id)
+        {
+            const string ID(id);
+            return (*this)[ID];
+        }
+        
+        const species & library:: operator[](const char   *id) const 
+        {
+            const string ID(id);
+            return (*this)[ID];
+        }
+        
+        species & library:: add(const string &name, const int z)
+        {
+            if( refcount() > 0 )
+                throw exception("library is locked while adding '%s',z=%d", name.c_str(), z);
+            
+            species::pointer p( new species(name,z) );
+            if(!db.insert(p))
+                throw exception("library.insert(multiple '%s')",name.c_str());
+            
+            (size_t &)(p->indx) = db.size();
+            if(name.size()>max_name_length) max_name_length = name.size();
+            return *p;
+        }
+        
+        species & library:: add(const char *name, const int z)
+        {
+            const string NAME(name);
+            return add(NAME,z);
+        }
+
+        void library:: find_max_name_length() throw()
+        {
+            max_name_length = 0;
+            size_t j        = 0;
+            for(iterator i=begin();i!=end();++i)
+            {
+                species &sp = **i;
+                (size_t &)(sp.indx) = ++j;
+                if(sp.name.length()>max_name_length) max_name_length = sp.name.length();
+            }
+        }
+        
+        void library:: remove(const string &name)
+        {
+            if( refcount() > 0 )
+                throw exception("library is locked while removind '%s'", name.c_str());
+            if( db.remove(name) )
+            {
+                find_max_name_length();
+            }
+            
+        }
+        
+        void library:: remove(const char *name)
+        {
+            const string NAME(name);
+            remove(NAME);
+        }
+
+        void library:: output(std::ostream &os) const
+        {
+            
+            os << "{" << std::endl;
+            for(const_iterator i=begin();i!=end();++i)
+            {
+                const species &sp = **i;
+                os << sp.name;
+                for(size_t j=sp.name.size();j<max_name_length;++j)
+                {
+                    os << ' ';
+                }
+                os << " : " << sp.z << std::endl;
+            }
+            os << "}";
+        }
+
+        
+    }
+
+}
---
+++ trunk/src/chem/yocto/chemical/library.hpp
@@ -0,0 +1,65 @@
+#ifndef YOCTO_CHEMICAL_LIBRARY_INCLUDED
+#define YOCTO_CHEMICAL_LIBRARY_INCLUDED 1
+
+#include "yocto/chemical/species.hpp"
+#include <iostream>
+
+namespace yocto
+{
+    namespace chemical
+    {
+        
+        //! a library of species
+        /**
+         counted is used by solution
+         */
+        class library : public counted
+        {
+        public:
+            typedef species::database       db_type;
+            typedef db_type::iterator       iterator;
+            typedef db_type::const_iterator const_iterator;
+            
+            explicit library() throw();
+            virtual ~library() throw();
+            size_t size() const throw(); //!< #species
+            
+            species &      operator[](const string &id);
+            const species &operator[](const string &id) const;
+            species &      operator[](const char   *id);
+            const species &operator[](const char   *id) const;
+            
+            species & add(const string &name, const int z);
+            species & add(const char   *name, const int z);
+            
+            void      remove(const string &id);
+            void      remove(const char   *id);
+            
+            iterator begin() throw();
+            iterator end()   throw();
+            
+            const_iterator begin() const throw();
+            const_iterator end()   const throw();
+            
+            void output(std::ostream &os) const;
+            
+            friend inline std::ostream & operator<<( std::ostream &os, const library &lib )
+            {
+                lib.output(os);
+                return os;
+            }
+            
+        private:
+            YOCTO_DISABLE_COPY_AND_ASSIGN(library);
+            species::database db;
+            size_t            max_name_length;
+            void   find_max_name_length() throw();
+            
+        };
+        
+    }
+}
+
+
+#endif
+
