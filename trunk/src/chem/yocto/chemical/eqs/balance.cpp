5c1bac057a8f5822f7b7338a6628a1eba4d78122
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-11-18 10:18:41 +0000

    will clarify the thing

--- trunk/src/chem/yocto/chemical/eqs/balance.cpp
+++ trunk/src/chem/yocto/chemical/eqs/balance.cpp
@@ -30,44 +30,39 @@ namespace yocto
             size_t ans=0;
             assert(M==C.size());
             
-            Ineg.free();
-            
+            Jneg.free();
             for(size_t i=1;i<=M;++i)
             {
-                flag[i] = false;
                 if(active[i])
                 {
                     const double Ci = C[i];
                     if(Ci<0)
                     {
                         ++ans;
-                        Ineg.push_back(i);
-                        flag[i] = true;
+                        Jneg.push_back(i);
                     }
                 }
             }
-            assert(Ineg.size()==ans);
+            assert(Jneg.size()==ans);
             return ans;
         }
         
         double equilibria:: computeE( array<ptrdiff_t> &dEdC )
         {
             assert(M==dEdC.size());
-            Ineg.free();
+            Jneg.free();
             double ans = 0;
             for(size_t i=1;i<=M;++i)
             {
                 dEdC[i] = 0;
-                flag[i] = false;
                 if(active[i])
                 {
                     const double Ci = C[i];
                     if(Ci<0)
                     {
-                        Ineg.push_back(i);
+                        Jneg.push_back(i);
                         ans -= Ci;
                         dEdC[i] = 1;
-                        flag[i] = true;
                     }
                 }
                 
@@ -99,7 +94,7 @@ namespace yocto
                 ++count;
                 std::cerr << "==== #" << count << std::endl;
                 
-                const size_t Q =Ineg.size(); assert(Q>0);
+                const size_t Q =Jneg.size(); assert(Q>0);
                 tao::mul(beta,Nu,dEdC);
                 std::cerr << "E=" << E << std::endl;
                 std::cerr << "dEdC=" << dEdC << std::endl;
@@ -112,7 +107,7 @@ namespace yocto
                     bool                  isOn = false;
                     for(size_t j=Q;j>0;--j)
                     {
-                        if(pEq->involves(Ineg[j]))
+                        if(pEq->involves(Jneg[j]))
                         {
                             // the equilibrium MAY be online
                             pEq->compute_limits(C);
@@ -221,6 +216,7 @@ namespace yocto
                 
             }
             
+            tao::set(C0,C);
             
             
             
