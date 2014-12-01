9b3c8dbf6c39e026b3f3cb605ceb0a9eb9b74a03
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-12-01 15:00:22 +0000

    added solution(s)

---
+++ src/chem/yocto/chemical/solution.hpp
@@ -0,0 +1,63 @@
+#ifndef YOCTO_CHEMICAL_SOLUTION_INCLUDED
+#define YOCTO_CHEMICAL_SOLUTION_INCLUDED 1
+
+#include "yocto/chemical/library.hpp"
+#include "yocto/sequence/array.hpp"
+
+namespace yocto
+{
+    
+    namespace chemical
+    {
+        
+        class solution : public array<double>
+        {
+        public:
+            explicit solution( library &from );
+            solution(const solution &other);
+            
+            virtual ~solution() throw();
+            library &lib;
+            
+            virtual const char *name() const throw();
+            virtual size_t      size() const throw();
+            virtual size_t      capacity() const throw();
+            virtual void        reserve(size_t );
+            virtual void        free() throw();
+            virtual void        release() throw();
+            
+            double       & operator()( const string &id );
+            const double & operator()( const string &id ) const;
+            double       & operator()( const char   *id );
+            const double & operator()( const char   *id ) const;
+            
+            void output( std::ostream &) const;
+            inline friend std::ostream & operator<<( std::ostream &os, const solution &self)
+            {
+                self.output(os);
+                return os;
+            }
+            
+            void ldz() throw();
+            void load( const array<double> &X) throw();
+            void save( array<double> &X) const throw();
+            
+            solution & operator=( const solution & ); //!< checking compatible
+            solution & operator=( const array<double> &) throw();
+            double     charge() const throw();
+            
+            double pH() const; //!< "H+" must exists...
+            
+            
+        private:
+            const size_t n; //!< #components
+            size_t       m; //!< for memory
+            double      *C; //!< conc
+            
+            virtual const double *get_item() const throw();
+        };
+    }
+}
+
+#endif
+
