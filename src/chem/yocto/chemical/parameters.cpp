ccbbecae7addb560683d1d460376de62e7d920cf
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-12-03 07:45:04 +0000

    upgrading

--- src/chem/yocto/chemical/parameters.cpp
+++ src/chem/yocto/chemical/parameters.cpp
@@ -1,4 +1,5 @@
 #include "yocto/chemical/parameters.hpp"
+#include "yocto/exception.hpp"
 
 namespace yocto
 {
@@ -11,13 +12,43 @@ namespace yocto
         }
         
         parameters:: parameters(const library &user_lib) :
-        nvar(0),
         lib(user_lib),
         db(8,as_capacity)
         {
         }
         
+        size_t parameters:: count() const throw() { return db.size(); }
         
+        void parameters:: append(const string &name)
+        {
+            const size_t the_index = lib.size() + db.size() + 1;
+            if( !db.insert(name, the_index) )
+            {
+                throw exception("parameters.append(multiple '%s')", name.c_str());
+            }
+        }
+        
+        
+        void parameters:: append(const char *name)
+        {
+            const string NAME(name);
+            append(NAME);
+        }
+        
+        size_t parameters:: operator[](const string &name) const
+        {
+            const size_t *p = db.search(name);
+            if(!p) throw exception("no parameters['%s']",name.c_str());
+            return *p;
+        }
+        
+        size_t parameters:: operator[](const char   *name) const
+        {
+            const string NAME(name);
+            return (*this)[NAME];
+        }
+
+
     }
     
 }
