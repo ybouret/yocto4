9146fb3bd045b483f6ca6d19dc7efa8f863b67b1
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-12-10 14:48:12 +0000

    gems WIP

---
+++ trunk/src/gems/yocto/gems/forces/field.hpp
@@ -0,0 +1,27 @@
+#ifndef YOCTO_GEMS_FORCES_FIELD_INCLUDED
+#define YOCTO_GEMS_FORCES_FIELD_INCLUDED 1
+
+#include "yocto/gems/forces/unary.hpp"
+#include "yocto/gems/forces/binary.hpp"
+
+namespace yocto
+{
+    namespace gems
+    {
+    
+        class forces_field
+        {
+        public:
+            
+            
+        private:
+            YOCTO_DISABLE_COPY_AND_ASSIGN(forces_field);
+            
+        };
+    }
+    
+}
+
+
+#endif
+
