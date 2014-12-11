0160c172029800a780d2cf00abf4119ee01e8337
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-12-11 10:33:22 +0000

    gems WIP

--- src/gems/yocto/gems/forces/binary.hpp
+++ src/gems/yocto/gems/forces/binary.hpp
@@ -29,9 +29,24 @@ namespace yocto
         class spring_force : public binary_force<T>
         {
         public:
+            const T k;
+            const T l;
+            
+            virtual T operator()( atom<T> &p, atom<T> &q ) const
+            {
+                const v3d<T> dr(p,q);
+                const T      pq2   = dr.norm2();
+                const T      pq    = Sqrt(pq2);
+                const T      delta = pq - l;
+                const T      k_del = k * delta;
+                const v2d<T> f     = (k_del/pq) * dr;
+                p.f += f;
+                q.f -= f;
+                return T(0.5)*k_del * delta;
+            }
             
         private:
-            
+            YOCTO_DISABLE_COPY_AND_ASSIGN(spring_force);
         };
         
     }
