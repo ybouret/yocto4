606c9d50a455c7310c3b2fe702b21eb81658c3ae
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2014-12-31 09:41:42 +0000

    reincluded jpeg/png

---
+++ src/gfx/yocto/gfx/image.cpp
@@ -0,0 +1,18 @@
+#include "yocto/gfx/image.hpp"
+
+namespace yocto
+{
+    namespace gfx
+    {
+
+        image::format:: format(const char *id) : name(id)
+        {
+        }
+        
+        image::format:: ~format() throw()
+        {
+        }
+        
+        
+    }
+}
