b8dc7953fa0c9bc600a59e7affc69f6f2548fdc5
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2015-01-13 16:52:41 +0000

    start using a filter

--- trunk/src/gfx/yocto/gfx/ops/filter.hpp
+++ trunk/src/gfx/yocto/gfx/ops/filter.hpp
@@ -29,6 +29,27 @@ namespace yocto
 
             inline void fill_with( const T a ) throw() { for(size_t i=0;i<9;++i) value[i] = a; }
 
+            YOCTO_DISABLE_COPY_AND_ASSIGN(filter);
+        };
+
+        template <typename T, size_t N>
+        class NDfilter
+        {
+        public:
+            T value[9*N];
+            inline  NDfilter() throw() : value() { memset(value,0,sizeof(value)); }
+            inline ~NDfilter() throw() {}
+
+            inline T * operator()(int x, int y) throw()
+            {
+                assert( abs(x)<=1 );
+                assert( abs(y)<=1 );
+                const int offset = N*(3*(y+1)+(x+1));
+                return &value[offset];
+            }
+
+            YOCTO_DISABLE_COPY_AND_ASSIGN(NDfilter);
+
         };
 
         template <typename T,typename S,typename V,size_t N>
@@ -43,22 +64,24 @@ namespace yocto
             const unit_t w = source.w;
             const unit_t h = source.h;
 
-            filter<S> s;
+            NDfilter<S,N> s;
+
+            std::cerr << "Using NDfilter" << N << ", size=" << sizeof(s) << std::endl;
             for(unit_t jm=-1,j=0,jp=1;j<h;++jm,++j,++jp)
             {
                 for(unit_t im=-1,i=0,ip=1;i<w;++im,++i,++ip)
                 {
-                    source.safe_copy( &s(-1,-1), im, jm );
-                    source.safe_copy( &s(-1, 0), im, j  );
-                    source.safe_copy( &s(-1, 1), im, jp );
+                    source.safe_copy( s(-1,-1), im, jm );
+                    source.safe_copy( s(-1, 0), im, j  );
+                    source.safe_copy( s(-1, 1), im, jp );
 
-                    source.safe_copy( &s(0,-1), i, jm );
-                    source.safe_copy( &s(0, 0), i, j  );
-                    source.safe_copy( &s(0, 1), i, jp );
+                    source.safe_copy( s(0,-1), i, jm );
+                    source.safe_copy( s(0, 0), i, j  );
+                    source.safe_copy( s(0, 1), i, jp );
 
-                    source.safe_copy( &s(1,-1), ip, jm );
-                    source.safe_copy( &s(1, 0), ip, j  );
-                    source.safe_copy( &s(1, 1), ip, jp );
+                    source.safe_copy( s(1,-1), ip, jm );
+                    source.safe_copy( s(1, 0), ip, j  );
+                    source.safe_copy( s(1, 1), ip, jp );
                 }
             }
             
