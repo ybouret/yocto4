3559bb88eb9785b384788595cdacd51ad29feb48
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2015-01-16 07:57:40 +0000

    linux upgrade

--- src/main/yocto/math/types.cpp
+++ src/main/yocto/math/types.cpp
@@ -15,19 +15,19 @@ namespace yocto
         template <> const double           numeric<double>::zero = 0.0;
         template <> const complex<float>   numeric< complex<float> >::zero(0.0f,0.0f);
         template <> const complex<double>  numeric< complex<double> >::zero(0.0,0.0);
-        template <> const ptrdiff_t        numeric<long>::zero = 0;
+        template <> const unit_t           numeric<unit_t>::zero = 0;
         
         template <> const float            numeric<float>::one  = 1.0f;
         template <> const double           numeric<double>::one = 1.0;
         template <> const complex<float>   numeric< complex<float> >::one(1.0f,0.0f);
         template <> const complex<double>  numeric< complex<double> >::one(1.0,0.0);
-        template <> const ptrdiff_t        numeric<long>::one = 1;
+        template <> const unit_t           numeric<unit_t>::one = 1;
         
         template <> const float            numeric<float>::two  = 2.0f;
         template <> const double           numeric<double>::two = 2.0;
         template <> const complex<float>   numeric< complex<float> >::two(2.0f,0.0f);
         template <> const complex<double>  numeric< complex<double> >::two(2.0,0.0);
-        template <> const ptrdiff_t        numeric<long>::two = 2;
+        template <> const unit_t           numeric<unit_t>::two = 2;
 
     }
     
