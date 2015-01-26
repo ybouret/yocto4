c9a7bb9fd3210de2eedea955d67d25c0af2578f1
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2015-01-26 10:32:17 +0000

    optional plugin loading done...

--- src/main/dll/loader.cpp
+++ src/main/dll/loader.cpp
@@ -8,45 +8,60 @@ using namespace yocto;
 
 int main( int argc, char *argv[] )
 {
-    
+
     try
     {
+        std::cerr << "sizeof(rtld_content)="   << sizeof(rtld_content)   << std::endl;
+        std::cerr << "sizeof(interface<ops>)=" << sizeof(interface<ops>) << std::endl;
         if( argc < 2 )
             throw exception("Usage: loader module.dll");
         const string soname = argv[1];
-        std::cerr << "-- Loading " << soname << std::endl;
-		dylib *dll = dylib_load( soname.c_str(), dylib_full );
-        const module  m( dll );
-        std::cerr << "loaded <" << soname << ">" << std::endl;
-        
-        std::cerr << "-- Linking 'sum'" << std::endl;
-        int (*sum)(int a, int b)  = 0;
-        m.link( sum, "sum" );
-		if( !sum )
-			throw exception("Can't find 'sum'");
-        std::cerr << "sum@" << (void*)sum << std::endl;
-        std::cerr << "-- Executing function..." << std::endl;
-        std::cerr << sum(1,2) << std::endl;
-        
-        std::cerr << "-- Loading it a second time" << std::endl;
-        const module m2( dll );
+
+        if(false)
+        {
+            std::cerr << "-- Loading " << soname << std::endl;
+            dylib *dll = dylib_load( soname.c_str(), dylib_full );
+            const module  m( dll );
+            std::cerr << "loaded <" << soname << ">" << std::endl;
+
+            std::cerr << "-- Linking 'sum'" << std::endl;
+            int (*sum)(int a, int b)  = 0;
+            m.link( sum, "sum" );
+            if( !sum )
+                throw exception("Can't find 'sum'");
+            std::cerr << "sum@" << (void*)sum << std::endl;
+            std::cerr << "-- Executing function..." << std::endl;
+            std::cerr << sum(1,2) << std::endl;
+
+            std::cerr << "-- Loading it a second time" << std::endl;
+            const module m2( dll );
+        }
 
         std::cerr << "loader name: " << rtld_content::loader_name << std::endl;
         std::cerr << "-- Loading plugin" << std::endl; std::cerr.flush();
         interface<ops> I(soname);
-        std::cerr << "-- Plugin is Loaded" << std::endl;
-		std::cerr.flush();
-		
+        std::cerr << "#entries=" << I.num_entries << std::endl;
+        
+        if(I.is_loaded())
+        {
+            std::cerr << "-- Plugin is Loaded" << std::endl;
+        }
+        else
+        {
+            std::cerr << "-- Plugin is not loaded: " << I.when() << std::endl;
+            return 0;
+        }
+        std::cerr.flush();
+
         assert(I->add);
         assert(I->mul);
-        
-        const interface<ops>::pointer p = I.handle;
-        
-        std::cerr << "add: " << p->add(2,3) << std::endl;
-        std::cerr << "mul: " << p->mul(2,3) << std::endl;
 
-        
-        
+
+        std::cerr << "add: " << I->add(2,3) << std::endl;
+        std::cerr << "mul: " << I->mul(2,3) << std::endl;
+
+
+
         std::cerr << "-- Done" << std::endl;
         return 0;
     }
--- src/main/yocto/rtld/interface.cpp
+++ src/main/yocto/rtld/interface.cpp
@@ -6,33 +6,37 @@ namespace yocto
 
     rtld_content:: ~rtld_content() throw()
     {
-    }
-
-
-    static inline
-    dylib *load_module(const char *soname )
-    {
-        char errbuf[256];
-        const size_t errlen = sizeof(errbuf);
-        memset(errbuf,0,errlen);
-        dylib *so = dylib_load(soname, dylib_full, errbuf, errlen);
-        if(!so)
+        if(dll)
         {
-            throw imported::exception("dylib_load failure","%s",errbuf);
+            if(dylib_decr(dll)<=0)
+            {
+                dylib_free(dll);
+            }
         }
-        return so;
+        dll = 0;
     }
-
+    
     rtld_content:: rtld_content(const string &soname, const char *api_name) :
     uid(api_name),
-    dll( load_module(soname.c_str()) )
+    msg( max_message_length ),
+    dll( dylib_load(soname.c_str(), dylib_full, msg(), msg.length()))
     {
+        if(dll)
+        {
+            dylib_incr(dll);
+        }
+    }
+
+    bool rtld_content:: is_loaded() const throw()
+    {
+        return 0!=dll;
     }
 
 #define yld_xstr(s) yld__str(s)
 #define yld__str(s) #s
 
     const char rtld_content:: loader_name[] = yld_xstr(YOCTO_RTLD_LOADER);
-    
+
+
     
 }
