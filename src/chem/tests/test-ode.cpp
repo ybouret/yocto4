23294ab5610838f73e2bf1f4e5fad8404a8b2e98
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2015-02-03 14:25:10 +0000

    tested Psi

--- src/chem/tests/test-ode.cpp
+++ src/chem/tests/test-ode.cpp
@@ -13,14 +13,14 @@ using namespace chemical;
 using namespace math;
 
 namespace {
-    
-    
+
+
     class ChemSys
     {
     public:
         static const char  *p_names[];
         static const size_t p_count;
-        
+
         __lua::Library    lib;
         __lua::Equilibria eqs;
         __lua::Effectors  edb;
@@ -32,7 +32,7 @@ namespace {
         diff_solver       odeint;
         double            diff_h;
         vector<double>    C;
-        
+
         explicit ChemSys( lua_State *L ) :
         lib( L, "species"),
         eqs( L, "eqs", lib),
@@ -47,7 +47,7 @@ namespace {
         C(nvar,0)
         {
             odeint.start(nvar);
-            
+
             {
                 boot loader;
                 __lua::load(L, loader, "ini",lib);
@@ -55,7 +55,7 @@ namespace {
                 lib.display(std::cerr << "C_in=",C0) << std::endl;
                 std::cerr << "charge_in=" << lib.charge(C0) << std::endl;
             }
-            
+
             {
                 boot loader;
                 __lua::load(L, loader, "out",lib);
@@ -63,28 +63,28 @@ namespace {
                 lib.display(std::cerr << "C_out=",Cout) << std::endl;
                 std::cerr << "charge_out=" << lib.charge(C0) << std::endl;
             }
-            
+
         }
-        
+
         virtual ~ChemSys() throw()
         {
         }
-        
+
         void rate( array<double> &dYdt, double t, const array<double> &Y )
         {
             // get chemical rated
             edb.rate(dYdt, t, Y, Cout, params);
             eqs.absorb(t,dYdt,Y);
-            
+
         }
-        
+
         void reset( double zeta )
         {
             tao::set(C,C0);
             diff_h = 1e-6;
         }
-        
-        
+
+
         void init_file(const string &filename) const
         {
             ios::ocstream fp(filename,false);
@@ -99,7 +99,7 @@ namespace {
             }
             fp("\n");
         }
-        
+
         void save_file(const string &filename, const double t) const
         {
             ios::ocstream fp(filename,true);
@@ -110,52 +110,62 @@ namespace {
             }
             fp("\n");
         }
-        
+
         void process(const string &filename, const double tmax, double dt)
         {
             reset(0);
             init_file(filename);
             double t = 0;
             save_file(filename,t);
-            
+
             while(t<tmax)
             {
                 const double t1 = t + dt;
                 odeint(diffeq,C,t,t1,diff_h,&eqs.callback);
-               
+
                 t = t1;
-                
+
                 save_file(filename,t);
             }
         }
-        
+
     private:
         YOCTO_DISABLE_COPY_AND_ASSIGN(ChemSys);
     };
-    
+
     const char * ChemSys:: p_names[] = { "zeta", "V"  };
     const size_t ChemSys:: p_count   = sizeof(ChemSys:: p_names)/sizeof(ChemSys:: p_names[0]);
-    
+
 }
 
 YOCTO_UNIT_TEST_IMPL(ode)
 {
+
+    {
+        ios::ocstream fp("psi.dat",false);
+        for(double zeta=-5.0;zeta<=5.0;zeta+=0.01)
+        {
+            fp("%g %g\n", zeta, GHK_Psi(zeta) );
+        }
+
+    }
+
     library    lib;
     equilibria eqs;
-    
+
     Lua::State VM;
     lua_State *L = VM();
-    
+
     __lua::register_functions(L);
-    
+
     if(argc>1)
     {
         Lua::Config::DoFile(L,argv[1]);
     }
-    
-    
+
+
     ChemSys       cs(L);
-    
+
     cs.process("ode-raw.dat", 1, 1e-3);
     
     
