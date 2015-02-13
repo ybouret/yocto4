e663091066bc0d85b585108011eb4079a4297b4c
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2015-02-13 08:54:56 +0000

    approx CWT

--- trunk/src/mk/yocto/math/wavelet/cwt.hpp
+++ trunk/src/mk/yocto/math/wavelet/cwt.hpp
@@ -2,6 +2,7 @@
 #define YOCTO_MATH_WAVELET_CWT_INCLUDED 1
 
 #include "yocto/math/kernel/matrix.hpp"
+#include "yocto/math/fcn/intg.hpp"
 #include "yocto/math/types.hpp"
 
 namespace yocto
@@ -9,55 +10,74 @@ namespace yocto
     namespace math
     {
 
-        namespace wavelet
+        template <typename T>
+        struct wavelet
         {
+
+            typedef typename numeric<T>::function function;
 
             //!
             /**
              \param x must be sorted
-             \param y functions values @x
+             \param y signal values @x
              */
-            template <typename T>
-            inline void cwt(const array<T> &x,
-                            const array<T> &y,
-                            typename numeric<T>::function &Psi,
-                            matrix<T>      &W
-                            )
+            inline static
+            void cwt(const array<T> &x,
+                     const array<T> &y,
+                     function       &Psi,
+                     array<T>       &shifts,
+                     array<T>       &scales,
+                     matrix<T>      &W
+                     )
             {
+                integrator<T> intg;
                 assert(x.size()==y.size());
+                assert(x.size()==shifts.size());
+                assert(x.size()==scales.size());
                 assert(x.size()>1);
-                const size_t n = x.size();
+                const size_t n        = x.size();
                 const T      width    = x[n] - x[1]; assert(width>0);
-                const T      freq_max = (n-1)/width;
                 W.make(n,n);
+
+                // precompute scales
+                for(size_t j=1;j<=n;++j)
+                {
+                    scales[j] =(j*width)/n;
+                }
+
+                // run
                 for(size_t i=1;i<=n;++i)
                 {
                     // shift
-                    const T s = x[i]-x[1];
-
+                    const T shift = x[i]-x[1];
+                    shifts[i]  = shift;
                     for(size_t j=1;j<=n;++j)
                     {
-                        const T omega = (j*freq_max)/n;
-
+                        const T scale = scales[j];
                         T sum(0);
 
-                        for(size_t k=1;j<n;++k)
+                        for(size_t k=1,kp=2;k<n;++k,++kp)
                         {
                             const T xk  = x[k];
-                            const T xkp = x[k+1];
+                            const T xkp = x[kp];
                             const T dx  = xkp - xk;
-                            sum += dx*(y[k]*Psi((xk-s)/omega)+y[k+1]*Psi((xkp-s)/omega));
+                            const T yk  = y[k];
+                            const T ykp = y[kp];
+
+                            sum += dx*(yk*Psi((xk-shift)/scale)+ykp*Psi((xkp-shift)/scale));
                         }
 
-                        sum /= 2*Sqrt(omega);
+                        const T factor = Sqrt(scale);
+                        sum /= factor+factor;
 
                         W[i][j] = sum;
                     }
-
+                    
                 }
             }
-
-        }
+            
+        };
+        
     }
 }
 
