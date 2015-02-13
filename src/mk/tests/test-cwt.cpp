8e4dcd6528b937246e0c952b415741e43e8f9559
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2015-02-13 08:54:56 +0000

    approx CWT

--- src/mk/tests/test-cwt.cpp
+++ src/mk/tests/test-cwt.cpp
@@ -1,10 +1,65 @@
 #include "yocto/math/wavelet/cwt.hpp"
 #include "yocto/utest/run.hpp"
+#include "yocto/sequence/vector.hpp"
+#include "yocto/sort/quick.hpp"
+#include "yocto/code/rand.hpp"
+#include "yocto/ios/ocstream.hpp"
 
 using namespace yocto;
+using namespace math;
+
+static inline
+double MexicanHat( double u )
+{
+    const double u2 = u*u;
+    return (1.0-u2)*exp(-u2/2.0);
+}
 
 YOCTO_UNIT_TEST_IMPL(cwt)
 {
-    
+    const size_t N = 100;
+    vector<double> x(N,as_capacity);
+    vector<double> y(N,as_capacity);
+
+    const double width = 10;
+    alea_init();
+    for(size_t i=1;i<=N;++i)
+    {
+        const double xx = alea<double>() * width;
+        const double yy = 0.4 + sin(xx);// + 1.2 * sin(2.1*xx);
+        x.push_back(xx);
+        y.push_back(yy);
+    }
+
+    co_qsort(x,y);
+    {
+        ios::ocstream fp("wdat.dat",false);
+        for(size_t i=1;i<=N;++i)
+        {
+            fp("%g %g\n", x[i], y[i]);
+        }
+    }
+
+    vector<double> shifts(N,0);
+    vector<double> scales(N,0);
+
+    matrix<double> W;
+    numeric<double>::function Psi( cfunctor(MexicanHat) );
+    wavelet<double>::cwt(x, y, Psi,shifts,scales, W);
+
+    {
+        ios::ocstream fp("cw.dat",false);
+        for(size_t i=1;i<=N;++i)
+        {
+            const double xx = shifts[i];
+            for(size_t j=1;j<=N;++j)
+            {
+                const double yy = scales[j];
+                const double ww = W[i][j];
+                fp("%g %g %g\n",xx,yy,ww*ww);
+            }
+            fp("\n");
+        }
+    }
 }
 YOCTO_UNIT_TEST_DONE()
