ad889760b8f57cb51c62c6604ca5045ab6361451
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2015-02-19 15:20:15 +0000

    trial for 2d peaks, failure, need to think

--- src/mk/tests/test-spike2d.cpp
+++ src/mk/tests/test-spike2d.cpp
@@ -1,11 +1,37 @@
 #include "yocto/math/alg/spike2d.hpp"
 #include "yocto/utest/run.hpp"
+#include "yocto/math/kernel/matrix.hpp"
+#include "yocto/sequence/vector.hpp"
+#include <cmath>
 
 using namespace yocto;
 using namespace math;
 
+static inline double get_id(double x) { return x; }
+
 YOCTO_UNIT_TEST_IMPL(spike2d)
 {
-    
+
+    typedef spike2D<double,size_t> spike_t;
+    typedef spike_t::pointer       spike_p;
+
+    const size_t    n=30;
+    const size_t    m=40;
+    matrix<double>  M(n,m);
+    vector<spike_p> spikes;
+
+    for(size_t i=1;i<=n;++i)
+    {
+        const double x = (i*6.3)/n;
+        for(size_t j=1;j<=m;++j)
+        {
+            const double y = (j*6.3)/n;
+            M[i][j] = sin(x) * sin(y);
+        }
+    }
+
+
+    spike_t::detect(spikes, M, 1, n, 1, m, get_id);
+
 }
 YOCTO_UNIT_TEST_DONE()
