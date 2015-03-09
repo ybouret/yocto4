8c30bacc71ef8b0bf9bf914c6b1bd7dbcbd9c16c
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2015-03-09 16:25:22 +0000

    basic spike 1d detection, need to check

--- trunk/src/mk/yocto/math/alg/spike.cpp
+++ trunk/src/mk/yocto/math/alg/spike.cpp
@@ -7,7 +7,31 @@ namespace yocto
         spike::index_node::  index_node(size_t i) throw() : value(i), next(0), prev(0) {}
         spike::index_node:: ~index_node() throw() {}
 
+        int spike::index_node::compare(const index_node *lhs, const index_node *rhs) throw()
+        {
+            const size_t L = lhs->value;
+            const size_t R = rhs->value;
+            return (L<R) ? -1 : ( (R<L) ? 1  : 0 );
+        }
     }
 
 }
 
+namespace yocto
+{
+    namespace math
+    {
+        spike:: spike( size_t pos ) throw() :
+        position(pos),
+        indices()
+        {
+
+        }
+
+        spike:: ~spike() throw()
+        {
+        }
+
+
+    }
+}
