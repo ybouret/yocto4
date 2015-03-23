b33015a2e892cac431867517fcf93768095bda13
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2015-03-23 10:44:52 +0000

    upgrading

--- prj/src/bitrev/genbitrev.cpp
+++ prj/src/bitrev/genbitrev.cpp
@@ -2,6 +2,7 @@
 #include "yocto/sys/wtime.hpp"
 #include "yocto/math/types.hpp"
 #include "yocto/sequence/vector.hpp"
+#include "yocto/string/conv.hpp"
 
 using namespace yocto;
 using namespace math;
@@ -11,7 +12,7 @@ using namespace math;
 
 template <typename T>
 static inline
-double perf( size_t p )
+double perf( const size_t p )
 {
     wtime chrono;
     chrono.start();
@@ -49,19 +50,146 @@ double perf( size_t p )
     return speed;
 }
 
+#include "yocto/sort/quick.hpp"
+
+static inline
+void compute_swaps( const size_t p, sequence<size_t> &swaps )
+{
+    const size_t size = 1 << p;
+    const size_t n    = size << 1;
+
+    swaps.free();
+
+    vector<size_t> indx(n,as_capacity);
+    vector<size_t> jndx(n,as_capacity);
+
+    size_t nops = 0;
+
+    register size_t j=1;
+    for (register size_t i=1; i<n; i+=2)
+    {
+        if (j > i)
+        {
+            //core::bswap<2*sizeof(T)>( &arr[i], &arr[j] );
+            //std::cerr << i  << ", " << j << std::endl;
+            indx.push_back(i);
+            jndx.push_back(j);
+            ++nops;
+        }
+        size_t m = size; // m=  n / 2;
+        while (m >= 2 && j > m)
+        {
+            j -=  m;
+            m >>= 1;
+        }
+        j += m;
+    }
+    assert(indx.size()==nops);
+    assert(jndx.size()==nops);
+    co_qsort(indx, jndx);
+    std::cerr << " nops" << size << " = " << nops << std::endl;
+    std::cerr << "words" << size << " = " << nops * 2 << std::endl;
+    //std::cerr << indx << std::endl;
+    //std::cerr << jndx << std::endl;
+    //return nops;
+    for(size_t i=1;i<=nops;++i)
+    {
+        swaps.push_back(indx[i]);
+        swaps.push_back(jndx[i]);
+    }
+}
+
+#include <cstring>
+#include "yocto/ios/ocstream.hpp"
 
 YOCTO_PROGRAM_START()
 {
-    const size_t pmax = 12;
+    size_t pmax = 5;
+    if(argc>1)
+    {
+        pmax = strconv::to<size_t>(argv[1],"pmax");
+    }
+
+    if(argc>2&& !strcmp(argv[2],"perf"))
+    {
+        for(size_t p=0;p<=pmax;++p)
+        {
+            (void) perf<float>(p);
+        }
+        std::cerr << std::endl;
+        for(size_t p=0;p<=pmax;++p)
+        {
+            (void) perf<double>(p);
+        }
+        std::cerr << std::endl;
+    }
+
+    vector<size_t> swaps(65536,as_capacity);
+    ios::ocstream hdr("xbitrev.hpp",false);
+    ios::ocstream src("xbitrev.cpp",false);
+
+    hdr << "#ifndef YOCTO_XBITREV_INCLUDED\n";
+    hdr << "#define YOCTO_XBITREV_INCLUDED 1\n\n";
+    hdr << "#include \"yocto/code/bswap.hpp\"\n";
+    hdr << "namespace yocto { namespace math {\n\n";
+
+    src << "#include \"./xbitrev.hpp\"\n";
+    src << "namespace yocto { namespace math {\n\n";
+
+    hdr << "\tstruct xbitrev {\n";
 
     for(size_t p=0;p<=pmax;++p)
     {
-        (void) perf<float>(p);
+        const size_t sz = 1 << p;
+        compute_swaps(p,swaps);
+        const size_t nw = swaps.size();
+        //const size_t ns = nw/2;
+        hdr("\t\tstatic const size_t indx%04u[%4u];\n",unsigned(sz),unsigned(nw));
+        src("\tconst size_t xbitrev::indx%04u[%4u]= {",unsigned(sz),unsigned(nw) );
+        for(size_t i=1;i<=nw;++i)
+        {
+            if( 0 == ((i-1)&15) )
+            {
+                src << "\n\t";
+            }
+            src("%4u", unsigned(swaps[i]));
+            if(i<nw)
+            {
+                src << ',';
+            }
+        }
+
+        src("\t};\n\n");
+
     }
-    std::cerr << std::endl;
-    for(size_t p=0;p<=pmax;++p)
-    {
-        (void) perf<double>(p);
-    }
+
+    // generic code
+    hdr << "\n\t\ttemplate <typename T> inline void run_safe( T arr[], const size_t size) throw() {\n";
+    hdr << "\t\t\t const    size_t n = size << 1;\n";
+    hdr << "\t\t\t register size_t j = 1;\n";
+    hdr << "\t\t\t for(register size_t i=1; i<n; i+=2)\n";
+    hdr << "\t\t\t {\n";
+    hdr << "\t\t\t     if(j>i)\n";
+    hdr << "\t\t\t     {\n";
+    hdr << "\t\t\t         core::bswap<2*sizeof(T)>( &arr[i], &arr[j] );\n";
+    hdr << "\t\t\t     }\n";
+    hdr << "\t\t\t    register size_t m = size;\n";
+    hdr << "\t\t\t    while( (m>=2) && (j>m) )\n";
+    hdr << "\t\t\t    {\n";
+    hdr << "\t\t\t       j -=  m;\n";
+    hdr << "\t\t\t       m >>= 1;\n";
+    hdr << "\t\t\t    }\n";
+    hdr << "\t\t\t    j += m;\n";
+    hdr << "\t\t\t }\n";
+
+    hdr << "\t\t}\n\n";
+
+    hdr << "\t};\n";
+
+    hdr << "\n} }\n";
+    src << "\n} }\n";
+    hdr << "#endif\n";
+    
+    
 }
 YOCTO_PROGRAM_END()
