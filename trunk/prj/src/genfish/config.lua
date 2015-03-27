5878548323f2371473088a0e27ce04441bd1410e
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2015-03-27 12:42:14 +0000

    genfish update

--- trunk/prj/src/genfish/config.lua
+++ trunk/prj/src/genfish/config.lua
@@ -9,18 +9,18 @@ return t/0.2*( 0.2969*sqrt(z)-0.1260*z - 0.3516*z*z + 0.2843*z*z*z -0.1015*z*z*z
 end
 
 function Up(z)
-return 3*Width(z);
+return 2*Width(z);
 end
 
 function Down(z)
-return 2*Width(z);
+return 1.5*Width(z);
 end
 
 -- Mix
 function Mix(u)
-return u^2;
+return u;
 end
 
 
---N = 200;
-N = 100;
+N      = 50;
+Length = 100;
