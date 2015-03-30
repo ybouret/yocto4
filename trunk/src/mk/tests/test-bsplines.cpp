a1c7f6e2b96d226be43f6c0668fdd406456c6d96
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2015-03-30 08:09:41 +0000

    init bspline

--- trunk/src/mk/tests/test-bsplines.cpp
+++ trunk/src/mk/tests/test-bsplines.cpp
@@ -5,6 +5,8 @@
 
 #include "yocto/ios/ocstream.hpp"
 #include "yocto/math/types.hpp"
+#include "yocto/math/v2d.hpp"
+#include "yocto/code/rand.hpp"
 
 using namespace yocto;
 using namespace math;
@@ -12,7 +14,33 @@ using namespace math;
 YOCTO_UNIT_TEST_IMPL(bsplines)
 {
     
+    typedef v2d<double> vtx_t;
     
+    size_t m=7;
+    vector<vtx_t>  P(m,as_capacity);
+    vector<double> t(m,as_capacity);
+    
+    t.push_back(0);
+    P.push_back(vtx_t(0,0));
+    
+    for(size_t i=1;i<m;++i)
+    {
+        const double tt = t.back() + 0.1 + alea<double>();
+        const vtx_t  pp( alea<double>()-0.5, alea<double>()-0.5);
+        
+        t.push_back(tt);
+        P.push_back(pp);
+    }
+    
+    const double tmax = t.back();
+    std::cerr << "0 -> " << tmax << std::endl;
+    
+    size_t N = 5;
+    for(size_t i=0;i<=N;++i)
+    {
+        const double tt = (tmax*i)/N;
+        Bsplines<double,vtx_t>::compute(tt, t, P, 3);
+    }
     
     
 }
--- trunk/src/mk/yocto/math/dat/b-splines.hpp
+++ trunk/src/mk/yocto/math/dat/b-splines.hpp
@@ -18,11 +18,49 @@ namespace yocto {
             /**
              \param t assume increasingly ordered
              */
-            T compute( const T x, const array<T> &t, const array<U> &P, const size_t n )
+            static T compute( const T x, const array<T> &t, const array<U> &P, const size_t n )
             {
                 assert(t.size()>=2);
                 assert(t.size()==P.size());
                 assert(n<=t.size());
+                
+                const size_t m = t.size();
+                
+                //-- find the localisation
+                size_t jlo = 1;
+                size_t jhi = m;
+                if(x<=t[1])
+                {
+                    jhi = 2;
+                }
+                else
+                {
+                    if(x>=t[n])
+                    {
+                        jlo = m-1;
+                        jhi = m;
+                    }
+                    else
+                    {
+                        while(jhi-jlo>1)
+                        {
+                            const size_t jmid = (jhi+jlo)>>1;
+                            const T      tmid = t[jmid];
+                            if(x>tmid)
+                            {
+                                jlo = jmid;
+                            }
+                            else
+                            {
+                                jhi = jmid;
+                            }
+                        }
+                        
+                    }
+                }
+                
+                std::cerr << t[jlo] << " <= " << x << " <= " << t[jhi] << std::endl;
+                
                 
                 return 0;
             }
