33dda44f46900f8542a05be3a59469c4ec491d34
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2015-03-30 11:28:04 +0000

    WIP bsplines

--- trunk/src/mk/yocto/math/dat/b-splines.hpp
+++ trunk/src/mk/yocto/math/dat/b-splines.hpp
@@ -3,6 +3,7 @@
 
 #include "yocto/math/types.hpp"
 #include "yocto/sequence/array.hpp"
+#include "yocto/code/utils.hpp"
 
 namespace yocto {
 
@@ -23,54 +24,65 @@ namespace yocto {
                 assert(t.size()>=2);
                 assert(t.size()==P.size());
                 assert(n<=t.size());
-                
+
                 const size_t m = t.size();
-                
+
+                //______________________________________________________________
+                //
                 //-- find the localisation
+                //______________________________________________________________
                 size_t jlo = 1;
-                size_t jhi = m;
-                if(x<=t[1])
                 {
-                    jhi = 2;
-                }
-                else
-                {
-                    if(x>=t[n])
+                    size_t jhi = m;
+                    if(x<=t[1])
                     {
-                        jlo = m-1;
-                        jhi = m;
+                        jhi = 2;
                     }
                     else
                     {
-                        while(jhi-jlo>1)
+                        if(x>=t[n])
                         {
-                            const size_t jmid = (jhi+jlo)>>1;
-                            const T      tmid = t[jmid];
-                            if(x>tmid)
-                            {
-                                jlo = jmid;
-                            }
-                            else
-                            {
-                                jhi = jmid;
-                            }
+                            jlo = m-1;
+                            jhi = m;
                         }
-                        
+                        else
+                        {
+                            while(jhi-jlo>1)
+                            {
+                                const size_t jmid = (jhi+jlo)>>1;
+                                const T      tmid = t[jmid];
+                                if(x>tmid)
+                                {
+                                    jlo = jmid;
+                                }
+                                else
+                                {
+                                    jhi = jmid;
+                                }
+                            }
+
+                        }
                     }
+
+                    std::cerr << t[jlo] << " <= " << x << " <= " << t[jhi] << std::endl;
                 }
-                
-                std::cerr << t[jlo] << " <= " << x << " <= " << t[jhi] << std::endl;
-                
-                
+
+                //______________________________________________________________
+                //
+                //-- algorithm
+                //______________________________________________________________
+                const size_t lmin = 1+n;
+                const size_t l    = max_of(jlo,lmin);
+                std::cerr << "\tl=" << l << std::endl;
                 return 0;
             }
-
-
-
+            
+            
+            
         private:
             YOCTO_DISABLE_COPY_AND_ASSIGN(Bsplines);
         };
-
+        
     }
 }
 
