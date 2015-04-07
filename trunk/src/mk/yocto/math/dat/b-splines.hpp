d665634e27eded17c1f069080a51a9d0e14e5cfc
yann.bouret@gmail.com <yann.bouret@gmail.com@6d5338fb-0854-7327-6163-5cad528299e9>
2015-04-07 15:28:21 +0000

    approx bspline tangent

--- trunk/src/mk/yocto/math/dat/b-splines.hpp
+++ trunk/src/mk/yocto/math/dat/b-splines.hpp
@@ -3,8 +3,6 @@
 
 #include "yocto/math/types.hpp"
 #include "yocto/sequence/vector.hpp"
-#include "yocto/math/fcn/drvs.hpp"
-#include "yocto/math/fcn/intg.hpp"
 #include "yocto/code/utils.hpp"
 
 namespace yocto {
@@ -12,71 +10,73 @@ namespace yocto {
     namespace math
     {
 
-        template <typename U>
-        struct Cubic_Bsplines
+
+
+
+        //______________________________________________________________________
+        //
+        // Return a point on the cubic b-splines: x=0: begin, x=1: end
+        //______________________________________________________________________
+        template <typename T,typename U>
+        inline U Cubic_Bsplines( T x, const array<U> &Points ) throw()
         {
+            const unit_t num_points   = Points.size();
+            const unit_t num_segments = num_points + 1;
+            unit_t start_cv = -2;
 
-            //__________________________________________________________________
+            //______________________________________________________________
             //
-            // Get Points to fill segment knots
-            //__________________________________________________________________
-            static inline
-            const U &GetPoint(  const array<U> &Points,  unit_t i ) throw()
-            {
-                assert(Points.size()>0);
-                return Points[ clamp<unit_t>(1,i,Points.size()) ];
-            }
+            // x: 0 -> 1: find the segment
+            //______________________________________________________________
+            x = clamp<T>(0,x,1);
+            const T      xscaled = x*num_segments;
+            const unit_t segment = Floor( xscaled );
+            const T      t       = (xscaled-segment);
 
-            //__________________________________________________________________
+            //______________________________________________________________
             //
-            // Return a point on the cubic b-splines: x=0: begin, x=1: end
-            //__________________________________________________________________
-            template <typename T> static inline
-            U Get(  T x, const array<U> &Points ) throw()
-            {
-                const unit_t num_points   = Points.size();
-                const unit_t num_segments = num_points + 1;
-                unit_t start_cv = -2;
+            // prepare points
+            //______________________________________________________________
+            start_cv += segment;
 
-                //______________________________________________________________
-                //
-                // x: 0 -> 1: find the segment
-                //______________________________________________________________
-                x = clamp<T>(0,x,1);
-                const T      xscaled = x*num_segments;
-                const unit_t segment = Floor( xscaled );
-                const T      t       = (xscaled-segment);
+            const T t2 = t*t;
+            const T it = T(1.0)-t;
+            const T t3 = t * t2;
 
-                //______________________________________________________________
-                //
-                // prepare points
-                //______________________________________________________________
-                start_cv += segment;
+            //______________________________________________________________
+            //
+            // calculate blending functions for cubic bspline
+            //______________________________________________________________
+            const T b0 = it*it*it/T(6.0);
+            const T b1 = (3*t3 - 6*t2 + T(4.0))/T(6.0);
+            const T b2 = (-3*t3 +3*t2 + 3*t + T(1.0))/T(6.0);
+            const T b3 =  t3/T(6.0);
 
-                const T t2 = t*t;
-                const T it = T(1.0)-t;
-                const T t3 = t * t2;
+            return
+            b0 * Points[clamp<unit_t>(1,start_cv+1,num_points)] +
+            b1 * Points[clamp<unit_t>(1,start_cv+2,num_points)] +
+            b2 * Points[clamp<unit_t>(1,start_cv+3,num_points)] +
+            b3 * Points[clamp<unit_t>(1,start_cv+4,num_points)];
+        }
 
-                //______________________________________________________________
-                //
-                // calculate blending functions for cubic bspline
-                //______________________________________________________________
-                const T b0 = it*it*it/T(6.0);
-                const T b1 = (3*t3 - 6*t2 + T(4.0))/T(6.0);
-                const T b2 = (-3*t3 +3*t2 + 3*t + T(1.0))/T(6.0);
-                const T b3 =  t3/T(6.0);
+        //______________________________________________________________________
+        //
+        // Tangent vector approximation
+        //______________________________________________________________________
+        template <typename T,typename U>
+        inline U Cubic_Bsplines_Tangent( T x, const array<U> &Points ) throw()
+        {
+            //! scaling for x=0.5
+            static const T h_opt = Pow(numeric<T>::epsilon,T(1.0)/T(3.0))*T(0.5);
 
-                return
-                b0 * GetPoint(Points,start_cv+1) +
-                b1 * GetPoint(Points,start_cv+2) +
-                b2 * GetPoint(Points,start_cv+3) +
-                b3 * GetPoint(Points,start_cv+4);
-
-            }
+            volatile T tmp = x + h_opt;
+            const    T h   = tmp-x;
+            const    U lo  = Cubic_Bsplines<T,U>(x-h,Points);
+            const    U hi  = Cubic_Bsplines<T,U>(x+h,Points);
+            return (T(1.0)/(h+h)) * (hi-lo);
+        }
 
 
-
-        };
 
         template <
         typename T,
@@ -87,84 +87,65 @@ namespace yocto {
         public:
             typedef T                     type;
             typedef VTX<T>                vtx_t;
+            static const size_t           DIM = sizeof(vtx_t)/sizeof(type);
             typedef vector<vtx_t>         vector_type;
-            typedef Cubic_Bsplines<vtx_t> approx_type;
 
-            explicit CubicApproximation() throw() :
-            vector_type() {}
+            typename numeric<T>::function dS;
+
+            explicit CubicApproximation() :
+            vector_type(),
+            dS( this, & CubicApproximation:: dLength )
+            {}
             virtual ~CubicApproximation() throw()  {}
-            inline   CubicApproximation(const CubicApproximation &other) : vector_type(other) {}
+            inline   CubicApproximation(const CubicApproximation &other) :
+            vector_type(other),
+            dS(other.dS)
+            {}
 
 
             inline vtx_t Compute( const T x ) const throw()
             {
-                return approx_type::template Get<T>(x,*this);
+                return Cubic_Bsplines(x,*this);
+            }
+
+            inline vtx_t Tangent( const T x ) const throw()
+            {
+                return Cubic_Bsplines_Tangent(x,*this);
             }
 
             inline T X( const T x ) const throw()
             {
-                return approx_type::template Get<T>(x,*this).x;
+                return Cubic_Bsplines(x,*this).x;
             }
 
             inline T Y( const T x ) const throw()
             {
-                return approx_type::template Get<T>(x,*this).y;
+                return Cubic_Bsplines(x,*this).y;
             }
 
             inline T Z( const T x ) const throw()
             {
-                return approx_type::template Get<T>(x,*this).z;
+                return Cubic_Bsplines(x,*this).z;
             }
-
 
         private:
             YOCTO_DISABLE_ASSIGN(CubicApproximation);
 
+            inline T dLength( const T x ) const throw()
+            {
+                const vtx_t Q = Cubic_Bsplines_Tangent(x,*this);
+                T sum(0);
+                const T *q = (const T *)&Q;
+                for(size_t i=0;i<DIM;++i)
+                {
+                    sum += Square(q[i]);
+                }
+                return Sqrt(sum);
+            }
+
         };
 
 
-        template <typename APPROX>
-        class CubicDifferential
-        {
-        public:
-            typedef typename APPROX::type type;
-
-            template <typename HOST_POINTER,typename METHOD_POINTER>
-            inline CubicDifferential( HOST_POINTER host, METHOD_POINTER getU, METHOD_POINTER getV ) :
-            U(host,getU),
-            V(host,getV),
-            dS(    this, & CubicDifferential::call_dS    ),
-            drvs(),
-            intg()
-            {
-            }
-
-            virtual ~CubicDifferential() throw() {}
-
-
-            typename numeric<type>::function U;
-            typename numeric<type>::function V;
-            typename numeric<type>::function dS;
-            derivative<type> drvs;
-            integrator<type> intg;
-
-
-            inline type arc_length( type a, type b )
-            {
-                return intg(a,b,dS,1e-4);
-            }
-
-        private:
-            YOCTO_DISABLE_COPY_AND_ASSIGN(CubicDifferential);
-            inline type call_dS( const type x )
-            {
-                static const type ftol(1e-4);
-                const type dU = drvs(U,x,ftol);
-                const type dV = drvs(V,x,ftol);
-                return Hypotenuse(dU, dV);
-            }
-            
-        };
         
         
     }
