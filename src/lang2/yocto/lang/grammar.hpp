d5846b4b21d487696cc84cb2889efe395813d1a8
ybouret <yann.bouret@gmail.com>
2015-04-27 10:31:36 +0200

    WIP grammar

--- src/lang2/yocto/lang/grammar.hpp
+++ src/lang2/yocto/lang/grammar.hpp
@@ -7,6 +7,21 @@ namespace yocto
 {
     namespace lang
     {
+
+        class grammar : public object
+        {
+        public:
+            explicit grammar(const string &id);
+            virtual ~grammar() throw();
+
+            const string name;
+
+
+        private:
+            YOCTO_DISABLE_COPY_AND_ASSIGM(grammar);
+
+        };
+
     }
     
 }
