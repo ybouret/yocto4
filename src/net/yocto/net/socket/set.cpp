3dcbe884cb02b2776042f9732e4126830fbc9e31
ybouret <yann.bouret@gmail.com>
2015-05-26 18:25:21 +0200

    socket set WIP

--- src/net/yocto/net/socket/set.cpp
+++ src/net/yocto/net/socket/set.cpp
@@ -1,5 +1,4 @@
 #include "yocto/net/socket/set.hpp"
-
 namespace yocto
 {
     namespace network
@@ -22,3 +21,102 @@ namespace yocto
         
     }
 }
+
+
+#include "yocto/code/utils.hpp"
+#include "yocto/exceptions.hpp"
+#include <cerrno>
+
+namespace yocto
+{
+    namespace network
+    {
+        socket_set:: ~socket_set() throw()
+        {
+            
+        }
+        
+        socket_set:: socket_set() :
+        sock_reg_(),
+        recv_set_(),
+        send_set_()
+        {
+            
+        }
+        
+        
+
+        socket_set::socket_set( size_t n ) :
+        sock_reg_( min_of<size_t>(n,max_size), as_capacity ),
+        recv_set_(),
+        send_set_()
+        {
+        }
+        
+        
+        size_t socket_set:: size() const throw() {
+            return sock_reg_.size();
+        }
+        
+        size_t socket_set:: capacity() const throw()
+        {
+            return min_of( max_size, sock_reg_.capacity() );
+        }
+        
+        const char * socket_set::name() const throw() { return "socket_set"; }
+        
+        void socket_set:: insert( socket &s )
+        {
+            //----------------------------------------------------------
+            // use O/S hard limit
+            //----------------------------------------------------------
+            if( sock_reg_.size() >= max_size )
+                throw libc::exception( ERANGE, "socket_size::max_size(%d) exceeded", int(max_size));
+            
+            //----------------------------------------------------------
+            // then register the descriptor
+            //----------------------------------------------------------
+            //const bsd::socket_t sock = s.socket_;
+            if( !sock_reg_.insert( &s ) ) {
+                throw libc::exception( EINVAL, "socket already in socket_set" );
+            }
+        }
+        
+        void socket_set:: reserve( size_t n )
+        {
+            if( n + capacity() >= max_size )
+            {
+                throw libc::exception(ERANGE,
+                                      "socket_size.reserve(%u) from capacity=%u exceeds max_size(%d)",
+                                      unsigned(n),
+                                      unsigned( capacity() ),
+                                      int(max_size) );
+            }
+            else {
+                sock_reg_.reserve( n );
+            }
+            
+            
+        }
+        
+        void socket_set:: remove( socket &s ) throw() 
+        {
+            (void)sock_reg_.remove( &s );
+        }
+        
+        void socket_set:: free() throw()
+        {
+            sock_reg_.free();
+        }
+        
+        
+        void socket_set:: release() throw()
+        {
+            sock_reg_.release();
+        }
+        
+        
+    }
+
+}
+
--- src/net/yocto/net/socket/set.hpp
+++ src/net/yocto/net/socket/set.hpp
@@ -3,7 +3,8 @@
 
 #include "yocto/net/socket.hpp"
 #include "yocto/net/delay.hpp"
-
+#include "yocto/ordered/sorted-vector.hpp"
+#include "yocto/memory/pooled.hpp"
 
 #if defined(__MACH__) || defined(__FreeBSD__)
 #include <sys/select.h>
@@ -15,16 +16,28 @@ namespace yocto
     namespace network
     {
 
-        class socket_set
+        class socket_set : public container
         {
         public:
             static const size_t max_size; //!< system FD_SET max sizes
+            virtual ~socket_set() throw();
+            explicit socket_set();
+            explicit socket_set(size_t);
             
-
+            virtual const char *name() const throw();      //!< socket_set
+            virtual size_t      size() const throw();      //!< sockets in set
+            virtual size_t      capacity() const throw();  //!< current capacity
+            virtual void        free() throw();            //!< reset the register
+            virtual void        release() throw();         //!< free and release memory
+            virtual void        reserve( size_t n );       //!< if possible
+            
+            void insert( socket & s );          //!< insert a socket into the set
+            void remove( socket & s ) throw();  //!< remove a socket from the set
+            
+            
         private:
             YOCTO_DISABLE_COPY_AND_ASSIGN(socket_set);
             typedef socket *socket_ptr;
-
             //! order socket by DECREASING file descriptor value
             class    sock_cmp
             {
@@ -35,7 +48,10 @@ namespace yocto
             private:
                 YOCTO_DISABLE_COPY_AND_ASSIGN(sock_cmp);
             };
-
+            typedef sorted_vector<socket_ptr,sock_cmp,memory::pooled::allocator> fd_reg;
+            fd_reg sock_reg_; //!< user's registered sockets
+            fd_set recv_set_; //!< ready to recv
+            fd_set send_set_; //!< ready to send
         };
 
     }
