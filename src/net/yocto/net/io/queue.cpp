8fc4870a309df457092a67366e9dcfa3e0880535
ybouret <yann.bouret@gmail.com>
2015-05-27 14:54:08 +0200

    need to test ioQ

--- src/net/yocto/net/io/queue.cpp
+++ src/net/yocto/net/io/queue.cpp
@@ -7,7 +7,8 @@ namespace yocto
 
         io_queue:: io_queue( io_cache &shared ) throw() :
         shblk(shared),
-        blocks()
+        blocks(),
+        active(0)
         {
         }
 
@@ -17,8 +18,49 @@ namespace yocto
             {
                 shblk.store( blocks.pop_front() );
             }
+            active = 0;
         }
 
+
+        size_t io_queue:: load(io_socket &iosock)
+        {
+            // find some place
+            if(0==blocks.tail || 0 == blocks.tail->space())
+            {
+                blocks.push_back( shblk.query() );
+            }
+            // write the content at the end
+            assert(NULL!=blocks.tail);
+            assert(blocks.tail->space()>0);
+            const size_t nw = blocks.tail->load(iosock);
+            active += nw;
+            return nw;
+        }
+
+        size_t io_queue:: emit(io_socket &iosock)
+        {
+            if(blocks.head)
+            {
+                const size_t nr = blocks.head->emit(iosock);
+                if(blocks.head->bytes()<=0)
+                {
+                    shblk.store( blocks.pop_front() );
+                }
+                assert(nr<=active);
+                active -= nr;
+                return nr;
+            }
+            else
+            {
+                assert(active<=0);
+                return 0;
+            }
+        }
+
+        size_t io_queue:: bytes() const throw()
+        {
+            return active;
+        }
     }
 
 }
--- src/net/yocto/net/io/queue.hpp
+++ src/net/yocto/net/io/queue.hpp
@@ -17,9 +17,16 @@ namespace yocto
             io_queue( io_cache &shared ) throw();
             ~io_queue() throw();
 
+            size_t load(io_socket &iosock); //!< load data
+            size_t emit(io_socket &iosock); //!< emit data
+            size_t bytes() const throw();
+
+
         private:
             YOCTO_DISABLE_COPY_AND_ASSIGN(io_queue);
-            core::list_of_cpp<io_block> blocks;
+            core::list_of_cpp<io_block> blocks; //!< dynamic blocks
+            size_t                      active; //!< total bytes, book keeping
+
         };
 
     }
