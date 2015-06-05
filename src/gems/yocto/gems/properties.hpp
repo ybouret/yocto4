aeda1520f0393c790034d5bbd9b905dee66c14f9
ybouret <yann.bouret@gmail.com>
2015-06-05 16:59:08 +0200

    ...

--- src/gems/yocto/gems/properties.hpp
+++ src/gems/yocto/gems/properties.hpp
@@ -8,10 +8,13 @@ namespace yocto
 {
     namespace gems
     {
+
         
         class properties
         {
         public:
+
+            
             const string name; //!< given
             const word_t uuid; //!< for communication
 
@@ -24,6 +27,7 @@ namespace yocto
             static word_t compute_uuid(const string &id) throw();
 
 
+            
 
         private:
             YOCTO_DISABLE_COPY_AND_ASSIGN(properties);
