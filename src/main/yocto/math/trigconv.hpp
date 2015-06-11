8301e628d30967d62746be601f860290f5709155
ybouret <yann.bouret@gmail.com>
2015-06-11 10:19:33 +0200

    updated trigconv

--- src/main/yocto/math/trigconv.hpp
+++ src/main/yocto/math/trigconv.hpp
@@ -10,6 +10,8 @@ namespace yocto
 
         inline float  Deg2Rad( const float  angle_deg ) throw() { return numeric<float>::pi  * angle_deg / 180.0f; }
         inline double Deg2Rad( const double angle_deg ) throw() { return numeric<double>::pi * angle_deg / 180.0;  }
+        inline float  Rad2Deg( const float  angle_rad ) throw() { return 180.0f * angle_rad / numeric<float>::pi;  }
+        inline double Rad2Deg( const double angle_rad ) throw() { return 180.0  * angle_rad / numeric<double>::pi; }
     }
 
 }
