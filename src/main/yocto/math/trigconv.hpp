27b8b515fe9c770168cb26c2ff657c96c77aef77
ybouret <yann.bouret@gmail.com>
2015-07-03 14:06:05 +0200

    some utils...

--- src/main/yocto/math/trigconv.hpp
+++ src/main/yocto/math/trigconv.hpp
@@ -12,6 +12,11 @@ namespace yocto
         inline double Deg2Rad( const double angle_deg ) throw() { return numeric<double>::pi * angle_deg / 180.0;  }
         inline float  Rad2Deg( const float  angle_rad ) throw() { return 180.0f * angle_rad / numeric<float>::pi;  }
         inline double Rad2Deg( const double angle_rad ) throw() { return 180.0  * angle_rad / numeric<double>::pi; }
+
+        inline double CosDeg( const double angle_deg ) throw() { return Cos( Deg2Rad(angle_deg) ); }
+        inline double SinDeg( const double angle_deg ) throw() { return Sin( Deg2Rad(angle_deg) ); }
+        inline float  CosDeg( const float  angle_deg ) throw() { return Cos( Deg2Rad(angle_deg) ); }
+        inline float  SinDeg( const float  angle_deg ) throw() { return Sin( Deg2Rad(angle_deg) ); }
     }
 
 }
