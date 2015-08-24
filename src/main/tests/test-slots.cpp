d0dfc97a23ff8bbf7c39922a66dd1879cd886676
ybouret <yann.bouret@gmail.com>
2015-08-24 10:53:41 +0200

    WIP slots test

--- src/main/tests/test-slots.cpp
+++ src/main/tests/test-slots.cpp
@@ -1,25 +1,45 @@
 #include "yocto/utest/run.hpp"
 #include "yocto/container/slots.hpp"
 #include "yocto/code/rand.hpp"
+#include "yocto/string.hpp"
+#include "support.hpp"
+
 using namespace yocto;
 
 YOCTO_UNIT_TEST_IMPL(slots)
 {
-    slots_of<int> s(10);
-    for(size_t i=1;i<=s.capacity/2;++i)
     {
-        s.push_back(i);
+        slots_of<int> s(10);
+        for(size_t i=1;i<=s.capacity/2;++i)
+        {
+            s.push_back(i);
+        }
+
+        for(size_t i=s.size+1;i<=s.capacity;++i)
+        {
+            s.append<size_t>(alea_lt(100));
+        }
+
+        for(size_t i=1;i<=s.size;++i)
+        {
+            std::cerr << "s[" << i << "]=" << s[i] << std::endl;
+        }
     }
 
-    for(size_t i=s.size+1;i<=s.capacity;++i)
+
     {
-        s.append<size_t>(alea_lt(100));
+        slots_of<string> s(19);
+        for(size_t i=1;i<=s.capacity;++i)
+        {
+            const string tmp = gen<string>::get();
+            s.push_back(tmp);
+        }
+        for(size_t i=1;i<=s.size;++i)
+        {
+            std::cerr << "s[" << i << "]=" << s[i] << std::endl;
+        }
     }
 
-    for(size_t i=1;i<=s.size;++i)
-    {
-        std::cerr << "s[" << i << "]=" << s[i] << std::endl;
-    }
 
 }
 YOCTO_UNIT_TEST_DONE()
