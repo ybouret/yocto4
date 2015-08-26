27a702420d4b4970635b9101a7b4073e2db091bf
ybouret <yann.bouret@gmail.com>
2015-08-26 14:19:10 +0200

    cleanup server

--- src/main/yocto/threading/server.hpp
+++ src/main/yocto/threading/server.hpp
@@ -69,62 +69,6 @@ namespace yocto
             static  void thread_run(void*) throw();
         };
 
-
-#if 0
-        //! a task server
-        class server : public layout
-        {
-        public:
-            explicit server();
-            virtual ~server() throw();
-            
-            //! a job a a full fledge functor
-            typedef functor<void,null_type> job;
-        
-            //! dynamic task, containing the job to do
-            class task
-            {
-            public:
-                task   *next;
-                task   *prev;
-                job     work;
-                
-            private:
-                task( const job & );
-                ~task() throw();
-                YOCTO_DISABLE_COPY_AND_ASSIGN(task);
-                friend class threading::server;
-            };
-            
-            void enqueue( const job &J );
-            void flush() throw();
-            
-        private:
-            threads   workers;
-            condition process;
-            condition synchro;
-            
-        public:
-            mutex  &access;
-            
-        private:
-            YOCTO_DISABLE_COPY_AND_ASSIGN(server);
-            core::list_of<task> tasks;  //!< tasks to process
-            core::list_of<task> activ;  //!< tasks being processed
-            core::pool_of<task> tpool;  //!< memory
-            size_t              ready;  //!< for first sync
-            bool                dying;  //!< terminating
-            
-            //! called with 'this', call the run() function
-            static void thread_entry(void *) throw();
-            
-            void run() throw();
-            
-            void initialize();
-            void terminate() throw();
-            
-        };
-#endif
         
     }
 }
