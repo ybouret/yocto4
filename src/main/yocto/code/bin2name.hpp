928c38538c69ac7ef156cfb6f6deb590c03a6c35
ybouret <yann.bouret@gmail.com>
2015-09-04 16:40:27 +0200

    simpler naming

--- src/main/yocto/code/bin2name.hpp
+++ src/main/yocto/code/bin2name.hpp
@@ -2,6 +2,7 @@
 #define YOCTO_BIN2NAME_INCLUDED 1
 
 #include "yocto/code/round.hpp"
+#include "yocto/code/utils.hpp"
 #include <cstring>
 
 namespace yocto
@@ -30,13 +31,16 @@ namespace yocto
     class bin2id
     {
     public:
-        const char value[bin2name::mem_size];
+        static const size_t out_size = 2*sizeof(T) + 1;
+        static const size_t mem_size = YOCTO_ROUND4(out_size);
+
+        const char value[mem_size];
 
         inline bin2id(const T *addr) throw() :
         value()
         {
             assert(addr);
-            bin2name::fill((char*)value,addr,sizeof(T));
+            fill_with(addr);
         }
 
         inline bin2id(const bin2id &other) throw() :
@@ -55,10 +59,26 @@ namespace yocto
 
         inline bin2id & operator=(const T *addr) throw()
         {
-            bin2name::fill((char*)value,addr,sizeof(T));
+            fill_with(addr);
             return *this;
         }
 
+    private:
+        inline void fill_with(const T *addr) throw()
+        {
+            const uint8_t *p = (const uint8_t *)addr;
+            char          *q = (char          *)value;
+
+            for(size_t i=0;i<sizeof(T);++i)
+            {
+                const char    *h = hexa_text[p[i]];
+                const size_t   j = (i<<1);
+                q[j+0] = h[0];
+                q[j+1] = h[1];
+            }
+            
+            for(size_t i=2*sizeof(T);i<mem_size;++i) q[i] = 0;
+        }
     };
 }
 
