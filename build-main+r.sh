742b2ba3bb897f361d6fd096b823b25eab46d612
ybouret <yann.bouret@gmail.com>
2015-09-14 15:01:36 +0200

    corrected

--- build-main+r.sh
+++ build-main+r.sh
@@ -1,1 +1,6 @@
-#build main+R interface...
+if [ $# != 2 ]; then
+	cmake -E cmake_echo_color --red "build.sh Tools BUILD_TYPE=(Debug|Release)";
+	exit 1;	
+fi
+cmake -E cmake_echo_color --blue "Building SDK for $1, BUILD_TYPE=$2..."
+YOCTO_EXCLUDE="lang script kr net mpa mk gems lua++ z++ bz2++ mpi ocl gfx json chem visit fltk fame python tiff++ lua lzo z bz2 png jpeg sqlite" TARGETS=install make $1 BUILD_TYPE=$2
