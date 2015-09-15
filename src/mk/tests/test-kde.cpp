5aa1ce5e99141faac00dc3d11ec33ef10ee97aa2
ybouret <yann.bouret@gmail.com>
2015-09-15 09:14:50 +0200

    start KDE

---
+++ src/mk/tests/test-kde.cpp
@@ -0,0 +1,11 @@
+#include "yocto/math/stat/kde.hpp"
+#include "yocto/utest/run.hpp"
+
+using namespace yocto;
+using namespace math;
+
+YOCTO_UNIT_TEST_IMPL(kde)
+{
+    
+}
+YOCTO_UNIT_TEST_DONE()
---
+++ src/mk/yocto/math/stat/kde.hpp
@@ -0,0 +1,26 @@
+#ifndef YOCTO_MATH_KDE_INCLUDED
+#define YOCTO_MATH_KDE_INCLUDED 1
+
+#include "yocto/math/types.hpp"
+#include "yocto/functor.hpp"
+#include "yocto/sequence/array.hpp"
+
+namespace yocto
+{
+    namespace math
+    {
+
+        template <typename T>
+        class KDE
+        {
+        public:
+            
+
+        private:
+            YOCTO_DISABLE_COPY_AND_ASSIGN(KDE);
+        };
+    }
+}
+
+
+#endif
