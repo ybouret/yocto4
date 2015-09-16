b8e22b3b8ded7892551213852b759b20b995a862
ybouret <yann.bouret@gmail.com>
2015-09-16 15:42:07 +0200

    cleanup for R

--- build-main+r.sh
+++ build-main+r.sh
@@ -3,4 +3,4 @@ if [ $# != 2 ]; then
 	exit 1;	
 fi
 cmake -E cmake_echo_color --blue "Building SDK for $1, BUILD_TYPE=$2..."
-YOCTO_EXCLUDE="lang script kr net mpa gems lua++ z++ bz2++ mpi ocl gfx json chem visit fltk fame python tiff++ lua lzo z bz2 png jpeg sqlite" TARGETS=install make $1 BUILD_TYPE=$2
+YOCTO_EXCLUDE="lang mk script kr net mpa gems lua++ z++ bz2++ mpi ocl gfx json chem visit fltk fame python tiff++ lua lzo z bz2 png jpeg sqlite" TARGETS=install make $1 BUILD_TYPE=$2
