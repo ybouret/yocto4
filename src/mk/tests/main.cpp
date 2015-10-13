9afa0a862101291dc91a831e088a3fd2eadaa32d
ybouret <yann.bouret@gmail.com>
2015-10-13 15:26:29 +0200

    moved cholesky

--- src/mk/tests/main.cpp
+++ src/mk/tests/main.cpp
@@ -5,44 +5,41 @@ using namespace yocto;
 
 YOCTO_UNIT_TEST_INIT(64)
 {
-	YOCTO_UNIT_TEST_DECL(z);
+    YOCTO_UNIT_TEST_DECL(z);
     YOCTO_UNIT_TEST_DECL(mtx);
     YOCTO_UNIT_TEST_DECL(lu);
     YOCTO_UNIT_TEST_DECL(adjoint);
 
-	YOCTO_UNIT_TEST_DECL(matrix);
+    YOCTO_UNIT_TEST_DECL(matrix);
     YOCTO_UNIT_TEST_DECL(symker);
-    
-	YOCTO_UNIT_TEST_DECL(intg);
-	YOCTO_UNIT_TEST_DECL(drvs);
-	YOCTO_UNIT_TEST_DECL(zero);
-	YOCTO_UNIT_TEST_DECL(opt);
 
-    
-	YOCTO_UNIT_TEST_DECL(cholesky);
-    
+    YOCTO_UNIT_TEST_DECL(intg);
+    YOCTO_UNIT_TEST_DECL(drvs);
+    YOCTO_UNIT_TEST_DECL(zero);
+    YOCTO_UNIT_TEST_DECL(opt);
+
     YOCTO_UNIT_TEST_DECL(stiff);
     YOCTO_UNIT_TEST_DECL(stat);
-    
+
     YOCTO_UNIT_TEST_DECL(linear);
     YOCTO_UNIT_TEST_DECL(extend);
     YOCTO_UNIT_TEST_DECL(extend2);
 
     YOCTO_UNIT_TEST_DECL(trigo);
-    
+
     YOCTO_UNIT_TEST_DECL(svd);
     YOCTO_UNIT_TEST_DECL(ortho);
-    
-	YOCTO_UNIT_TEST_DECL(bracket);
+
+    YOCTO_UNIT_TEST_DECL(bracket);
     YOCTO_UNIT_TEST_DECL(cgrad);
     YOCTO_UNIT_TEST_DECL(cgrad2);
-    
+
     YOCTO_UNIT_TEST_DECL(newton);
     YOCTO_UNIT_TEST_DECL(newton2);
     YOCTO_UNIT_TEST_DECL(svdgs);
-    
+
     YOCTO_UNIT_TEST_DECL(tr2d);
-    
+
     YOCTO_UNIT_TEST_DECL(fft1);
     YOCTO_UNIT_TEST_DECL(psd);
     YOCTO_UNIT_TEST_DECL(tridiag);
@@ -50,28 +47,28 @@ YOCTO_UNIT_TEST_INIT(64)
     YOCTO_UNIT_TEST_DECL(spline2d);
 
     YOCTO_UNIT_TEST_DECL(diag);
-    
+
     YOCTO_UNIT_TEST_DECL(fit_circle);
     YOCTO_UNIT_TEST_DECL(fit_ellipse);
 
     YOCTO_UNIT_TEST_DECL(eigv);
     YOCTO_UNIT_TEST_DECL(balance);
-    
+
     YOCTO_UNIT_TEST_DECL(michaelis);
-    
+
     YOCTO_UNIT_TEST_DECL(ode_expl);
 
     //YOCTO_UNIT_TEST_DECL(mprove);
-    
+
 
     YOCTO_UNIT_TEST_DECL(sparse);
     YOCTO_UNIT_TEST_DECL(spcj);
-    
+
     YOCTO_UNIT_TEST_DECL(tao);
 
-	YOCTO_UNIT_TEST_DECL(gnl);
-	YOCTO_UNIT_TEST_DECL(fit_poly);
-	YOCTO_UNIT_TEST_DECL(fit_gauss);
+    YOCTO_UNIT_TEST_DECL(gnl);
+    YOCTO_UNIT_TEST_DECL(fit_poly);
+    YOCTO_UNIT_TEST_DECL(fit_gauss);
     YOCTO_UNIT_TEST_DECL(fit_pade);
     YOCTO_UNIT_TEST_DECL(fit_vol);
 
@@ -84,10 +81,10 @@ YOCTO_UNIT_TEST_INIT(64)
 
     YOCTO_UNIT_TEST_DECL(cwt);
     YOCTO_UNIT_TEST_DECL(spike2d);
-	YOCTO_UNIT_TEST_DECL(spike);
-
-	YOCTO_UNIT_TEST_DECL(kde);
-
+    YOCTO_UNIT_TEST_DECL(spike);
+    
+    YOCTO_UNIT_TEST_DECL(kde);
+    
 }
 YOCTO_UNIT_TEST_EXEC()
 
