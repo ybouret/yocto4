9bef78b83bb828e21168114781defe0e33ae999c
ybouret <yann.bouret@gmail.com>
2015-10-15 08:58:20 +0200

    ok for simple tridiag and ctridiag

--- src/mk/tests/test-tri-diag.cpp
+++ src/mk/tests/test-tri-diag.cpp
@@ -1,17 +1,81 @@
+#include "yocto/mpa/rational.hpp"
 #include "yocto/math/core/tridiag.hpp"
 #include "yocto/utest/run.hpp"
-
-#include "yocto/mpa/rational.hpp"
+#include "yocto/math/core/tao.hpp"
 
 using namespace yocto;
 using namespace math;
 
+template <typename T>
+static inline void test_td()
+{
+    for(size_t n=1;n<=8;++n)
+    {
+        std::cerr << std::endl;
+        std::cerr << "size=" << n << std::endl;
+        TriDiag<T>  p(n);
+        cTriDiag<T> q(n);
+        vector<T>   r(n);
+        for(size_t i=1;i<=n;++i)
+        {
+            p.a[i] = T(-1);
+            p.c[i] = T(1);
+            p.b[i] = T(3);
+
+            q.a[i] = p.a[i];
+            q.b[i] = p.b[i];
+            q.c[i] = p.c[i];
+
+            r[i] =  T(i);
+
+        }
+        std::cerr << "p=" << p << std::endl;
+        if(n>=2) std::cerr << "q=" << q << std::endl;
+        std::cerr << "r=" << r << std::endl;
+
+        vector<T> xp(n);
+        vector<T> xq(n);
+        vector<T> y(n);
+
+        if(!p.solve(xp,r))
+        {
+            std::cerr << "TriDiag failure!" << std::endl;
+        }
+        else
+        {
+            std::cerr << "xp=" << xp << std::endl;
+            p.apply(y,xp);
+            std::cerr << "yp=" << y << std::endl;
+            tao::sub(y,r);
+            std::cerr << "dp=" << y << std::endl;
+        }
+
+        if(n>=2)
+        {
+            if(!q.solve(xq,r) )
+            {
+                std::cerr << "cTriDiag failure!" << std::endl;
+            }
+            else
+            {
+                std::cerr << "xq=" << xq << std::endl;
+                q.apply(y,xq);
+                std::cerr << "yq=" << y << std::endl;
+                tao::sub(y,r);
+                std::cerr << "dp=" << y << std::endl;
+            }
+        }
+    }
+}
+
 YOCTO_UNIT_TEST_IMPL(triDiag)
 {
-
-    tri_diag<mpq> q(1);
+    test_td<float>();
+    test_td<double>();
+    test_td< complex<float>  >();
+    test_td< complex<double> >();
+    test_td<mpq>();
     
-
 }
 YOCTO_UNIT_TEST_DONE()
 
--- src/mk/yocto/math/core/tridiag.hpp
+++ src/mk/yocto/math/core/tridiag.hpp
@@ -3,23 +3,97 @@
 
 #include "yocto/sequence/many-arrays.hpp"
 #include "yocto/xnumeric.hpp"
+#include "yocto/math/matrix.hpp"
 
 namespace yocto
 {
     namespace math
     {
 
+        //! tridiagonal base class, for simple or cyclic
         template <typename T>
-        class tri_diag_base
+        class _TriDiag
         {
         public:
             typedef array<T>       array_type;
             typedef memory::global memory_type;
-            virtual ~tri_diag_base() throw() {}
+            virtual ~_TriDiag() throw() {}
 
+            //__________________________________________________________________
+            //
+            // Virtual Interface
+            //__________________________________________________________________
             virtual T operator()(const size_t i,const size_t j) const = 0;
+            virtual bool __solve( array_type &x, const array_type &r) = 0;
+
+
+            //__________________________________________________________________
+            //
+            // Non-Virtual Interface
+            //__________________________________________________________________
+            inline bool solve(array_type &x, const array_type &r)
+            {
+                return __solve(x,r);
+            }
+
 
             inline size_t size() const throw() { return arrays.size; }
+
+            inline void output( std::ostream &os ) const
+            {
+                const _TriDiag<T> &self = *this;
+                const size_t       n    = size();
+                os << "[";
+                for( size_t i=1; i <= n; ++i )
+                {
+                    for( size_t j=1; j <= n; ++j )
+                    {
+                        os << " " << self(i,j);
+                    }
+                    if( i < n )
+                        os << " ;";
+                }
+                os << "]";
+            }
+
+            inline friend std::ostream & operator<<( std::ostream &os, const _TriDiag<T> &M )
+            {
+                M.output(os);
+                return os;
+            }
+
+            inline void set( matrix<T> &M ) const
+            {
+                assert(M.cols==M.rows);
+                assert(M.rows==size());
+                const _TriDiag<T> &self = *this;
+                const size_t       n    = size();
+                for(size_t i=1;i<=n;++i)
+                {
+                    for(size_t j=1;j<=n;++j)
+                    {
+                        M[i][j] = self(i,j);
+                    }
+                }
+            }
+
+            //! u = this * v
+            inline void apply( array_type &u, const array_type &v) const
+            {
+                assert(u.size()>=size());
+                assert(v.size()>=size());
+                const _TriDiag<T> &self = *this;
+                const size_t       n    = size();
+                for(size_t i=1;i<=n;++i)
+                {
+                    T sum = xnumeric<T>::zero();
+                    for(size_t j=1;j<=n;++j)
+                    {
+                        sum += self(i,j) * v[j];
+                    }
+                    u[i] = sum;
+                }
+            }
 
         protected:
             many_arrays<T,memory_type> arrays;
@@ -30,73 +104,255 @@ namespace yocto
             array_type &c;
 
         protected:
-            explicit inline tri_diag_base(size_t na) :
-            arrays(na),
+            explicit inline _TriDiag(size_t na) :
+            arrays(4+na),
             a( arrays.next_array() ),
             b( arrays.next_array() ),
-            c( arrays.next_array() )
+            c( arrays.next_array() ),
+            g( arrays.next_array() )
             {
             }
 
+            array_type &g;
 
         private:
-            YOCTO_DISABLE_COPY_AND_ASSIGN(tri_diag_base);
+            YOCTO_DISABLE_COPY_AND_ASSIGN(_TriDiag);
         };
 
+        namespace kernel
+        {
+            template <typename T>
+            struct __tridiag
+            {
+                typedef typename real_of<T>::type scalar_type;
+
+                // solve [a,b,c]*u = r, using auxiliary u
+                static inline
+                bool solve_simple(const array<T>  &a,
+                                  const array<T>  &b,
+                                  const array<T>  &c,
+                                  array<T>        &g,
+                                  array<T>        &u,
+                                  const array<T>  &r
+                                  )
+                {
+                    const size_t n = a.size();
+                    assert(b.size()==n);
+                    assert(c.size()==n);
+                    assert(g.size()==n);
+                    assert(u.size()>=n);
+                    assert(r.size()>=n);
+                    const scalar_type pmin = xnumeric<scalar_type>::minimum();
+                    T piv = b[1];
+                    if( Fabs( piv ) <= pmin )
+                        return false;
+
+                    u[1] = r[1] / piv;
+
+                    //__________________________________________________________
+                    //
+                    // first pass
+                    //__________________________________________________________
+
+                    for( size_t j=2, jm=1; j <= n; ++j,++jm )
+                    {
+                        g[j] = c[jm] / piv;
+                        piv  = b[j] - a[j] * g[j];
+                        if( Fabs( piv ) <= pmin )
+                            return false;
+                        u[j] = (r[j] - a[j] * u[jm])/piv;
+                    }
+
+                    //__________________________________________________________
+                    //
+                    // second pass
+                    //__________________________________________________________
+                    for( size_t j=n-1, jp=n;j>0;--j,--jp)
+                    {
+                        assert(j+1==jp);
+                        u[j] -= g[jp] * u[jp];
+                    }
+
+                    return true;
+                }
+
+                static inline
+                bool solve_cyclic(const array<T>  &a,
+                                  const array<T>  &b,
+                                  const array<T>  &c,
+                                  array<T>        &g,
+                                  array<T>        &u,
+                                  array<T>        &z,
+                                  array<T>        &bb,
+                                  array<T>        &x,
+                                  const array<T>  &r
+                                  )
+                {
+                    const size_t n      =  a.size(); assert(n>=2);
+                    const T      alpha  =  a[1];
+                    const T      beta   =  c[n];
+                    const T      gamma  = -b[1];
+
+                    //__________________________________________________________
+                    //
+                    // solve A.x = r
+                    //__________________________________________________________
+                    bb[1]=b[1]-gamma;
+                    bb[n]=b[n]-(alpha*beta)/gamma;
+                    for(size_t i=2;i<n;i++)
+                        bb[i]=b[i];
+                    if( ! solve_simple(a,bb,c,g,x,r) )
+                        return false;
+
+                    //__________________________________________________________
+                    //
+                    // compute the vector u
+                    //__________________________________________________________
+                    const T __zero = xnumeric<T>::zero();
+                    const T __one  = xnumeric<T>::one();
+                    u[1]=gamma;
+                    u[n]=alpha;
+                    for(size_t i=2;i<n;i++)
+                        u[i]=__zero;
+
+                    //__________________________________________________________
+                    //
+                    // solve A.z = u
+                    //__________________________________________________________
+                    if(! solve_simple(a,bb,c,g,z,u) )
+                        return false;
+
+                    //__________________________________________________________
+                    //
+                    // compute the factor
+                    //__________________________________________________________
+                    const T fact=
+                    (x[1]+beta*x[n]/gamma)/
+                    (__one+z[1]+beta*z[n]/gamma);
+                    for(size_t i=n;i>0;--i)
+                        x[i] -= fact*z[i];
+                    return true;
+                }
+
+
+            };
+        }
+
+
+        //! simple tridiagonal matrix
         template <typename T>
-        class tri_diag : public tri_diag_base<T>
+        class TriDiag : public _TriDiag<T>
         {
         public:
-            explicit tri_diag(size_t n) : tri_diag_base<T>(6)
+            typedef typename _TriDiag<T>::array_type array_type;
+
+            explicit TriDiag(size_t n) : _TriDiag<T>(0)
             {
                 this->arrays.allocate(n);
             }
 
-            virtual ~tri_diag() throw()
+            virtual ~TriDiag() throw()
             {
             }
 
             virtual T operator()(const size_t i,const size_t j) const
             {
-                assert(i<=this->size());
-                assert(j<=this->size());
-                if(i<j)
+                assert(i>0);assert(i<=this->size());
+                assert(j>0);assert(j<=this->size());
+
+                const ptrdiff_t k = ptrdiff_t(i) - ptrdiff_t(j);
+                switch( k )
                 {
-                    if(1==j-i)
-                    {
-                        return this->c[i];
-                    }
-                    else
-                    {
-                        return xnumeric<T>::zero();
-                    }
+                    case -1: return this->c[i];
+                    case  0: return this->b[i];
+                    case  1: return this->a[i];
+
+                    default:
+                        break;
                 }
-                else
-                {
-                     if(j<i)
-                     {
-                         if(1==i-j)
-                         {
-                             return this->a[i];
-                         }
-                         else
-                         {
-                             return xnumeric<T>::zero();
-                         }
-                     }
-                    else
-                    {
-                        return this->b[i];
-                    }
-                }
+                return xnumeric<T>::zero();
+            }
+
+            virtual bool __solve( array_type &x, const array_type &r) throw()
+            {
+                return kernel::__tridiag<T>::solve_simple(this->a,this->b,this->c,this->g,x,r);
             }
 
         private:
-            YOCTO_DISABLE_COPY_AND_ASSIGN(tri_diag);
+            YOCTO_DISABLE_COPY_AND_ASSIGN(TriDiag);
         };
 
-    }
 
+        template <typename T>
+        class cTriDiag : public _TriDiag<T>
+        {
+        public:
+            typedef typename _TriDiag<T>::array_type array_type;
+
+            explicit cTriDiag(size_t n) :
+            _TriDiag<T>(3),
+            u(  this->arrays.next_array() ),
+            z(  this->arrays.next_array() ),
+            bb( this->arrays.next_array() )
+            {
+                this->arrays.allocate(n);
+            }
+
+            virtual ~cTriDiag() throw()
+            {
+            }
+
+
+            //! cyclic access
+            /**
+             M[1][n] = c[n]
+             M[n][1] = a[1]
+             */
+            virtual T operator()(size_t i, size_t j) const throw()
+            {
+                assert(i>0);assert(i<=this->size());
+                assert(j>0);assert(j<=this->size());
+                const size_t n = this->size();
+
+                if(i==1&&j==n) return this->c[n];
+                if(i==n&&j==1) return this->a[1];
+
+                const ptrdiff_t k = ptrdiff_t(i) - ptrdiff_t(j);
+                switch( k )
+                {
+                    case -1: return this->c[i];
+                    case  0: return this->b[i];
+                    case  1: return this->a[i];
+                        
+                    default:
+                        break;
+                }
+                return xnumeric<T>::zero();
+            }
+
+            virtual bool __solve( array_type &x, const array_type &r) throw()
+            {
+                return kernel::__tridiag<T>::solve_cyclic(this->a,
+                                                          this->b,
+                                                          this->c,
+                                                          this->g,
+                                                          this->u,
+                                                          this->z,
+                                                          this->bb,
+                                                          x,
+                                                          r);
+            }
+
+            
+        private:
+            YOCTO_DISABLE_COPY_AND_ASSIGN(cTriDiag);
+            array_type &u;
+            array_type &z;
+            array_type &bb;
+        };
+        
+    }
+    
 }
 
 #endif
