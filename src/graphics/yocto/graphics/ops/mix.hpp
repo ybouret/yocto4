bf3ac1cb08bec29c67b4fa750b83fbd62c93ae86
ybouret <yann.bouret@gmail.com>
2015-11-17 15:27:25 +0100

    start mix, will move to blend

---
+++ src/graphics/yocto/graphics/ops/mix.hpp
@@ -0,0 +1,19 @@
+#ifndef YOCTO_GRAPHICS_OPS_MIX_INCLUDED
+#define YOCTO_GRAPHICS_OPS_MIX_INCLUDED 1
+
+#include "yocto/graphics/rgb.hpp"
+
+namespace yocto
+{
+    namespace graphics
+    {
+
+        struct mix
+        {
+            
+        }
+
+    }
+}
+
+#endif
