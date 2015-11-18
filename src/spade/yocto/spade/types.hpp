29b5683581e0c4e2e2187475c89c08db6fb1b486
ybouret <yann.bouret@gmail.com>
2015-11-18 10:04:57 +0100

    rotation in 2D

--- src/spade/yocto/spade/types.hpp
+++ src/spade/yocto/spade/types.hpp
@@ -11,6 +11,21 @@ namespace yocto
     namespace spade
     {
 
+        struct geo
+        {
+            //! rotate in 2D
+            template <typename VTX>
+            static inline
+            VTX rotate( const VTX &v, const typename VTX::type theta ) throw()
+            {
+                const typename VTX::type ct = math::Cos(theta);
+                const typename VTX::type st = math::Sin(theta);
+                VTX p(v);
+                p.x = ct * v.x - st*v.y;
+                p.y = st * v.x + ct*v.y;
+                return p;
+            }
+        };
     }
 }
 
