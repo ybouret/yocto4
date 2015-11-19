8367ca67609b4ad48c31e31c994bc3cbc2134f0c
ybouret <yann.bouret@gmail.com>
2015-11-19 09:15:31 +0100

    updated for spade

--- build-main+r.sh
+++ build-main+r.sh
@@ -3,4 +3,4 @@ if [ $# != 2 ]; then
 	exit 1;	
 fi
 cmake -E cmake_echo_color --blue "Building SDK for $1, BUILD_TYPE=$2..."
-YOCTO_EXCLUDE="chemical lang script kr net gems lua++ z++ bz2++ ocl graphics json chem visit fltk fame python tiff++ lua lzo z bz2 png jpeg sqlite" TARGETS=install make $1 BUILD_TYPE=$2
+YOCTO_EXCLUDE="spade chemical lang script kr net gems lua++ z++ bz2++ ocl graphics json chem visit fltk fame python tiff++ lua lzo z bz2 png jpeg sqlite" TARGETS=install make $1 BUILD_TYPE=$2
