dcecd64c2770fbcb5d84c490cd7934ca5faf7cb7
ybouret <yann.bouret@gmail.com>
2015-11-24 16:39:19 +0100

    will make level1 and level2 grammar

--- src/chemical/tests/test-gram.cpp
+++ src/chemical/tests/test-gram.cpp
@@ -7,58 +7,14 @@
 using namespace yocto;
 
 
-static const char *ATOM[] =
+static const char level1_grammar[] =
 {
-    "H", "He",
-    "Na", "Mg", "Al", "Si", "P", "S", "Cl"
-};
-
-class CHEMparser : public lang::parser
-{
-public:
-    explicit CHEMparser() : lang::parser("CHEM","main")
-    {
-
-
-        Alt &atom = alt();
-        for(size_t i=0;i<sizeof(ATOM)/sizeof(ATOM[0]);++i)
-        {
-            atom.append(&text(ATOM[i],lang::syntax::univocal));
-        }
-
-        //__________________________________________________________________
-        //
-        // lexical
-        //__________________________________________________________________
-        scanner.drop("WS", "[:blank:]");
-        scanner.endl("ENDL");
-
-        top_level( zero_or_more(atom) );
-
-        gramviz("chem.dot");
-        ios::graphviz_render("chem.dot");
-
-    }
-
-    virtual ~CHEMparser() throw()
-    {
-    }
-    
-
-private:
-    YOCTO_DISABLE_COPY_AND_ASSIGN(CHEMparser);
+#include "../yocto/chemical/level1.inc"
 };
 
 YOCTO_UNIT_TEST_IMPL(gram)
 {
-    CHEMparser prs;
-    ios::icstream fp(ios::cstdin);
-    auto_ptr<lang::syntax::xnode> tree( prs.run(fp) );
-    if(tree.is_valid())
-    {
-        tree->graphviz("xnode.dot");
-        ios::graphviz_render("xnode.dot");
-    }
-
+    auto_ptr<lang::parser> prs( lang::parser::generate("level1",level1_grammar,sizeof(level1_grammar)));
+    
 }
 YOCTO_UNIT_TEST_DONE()
