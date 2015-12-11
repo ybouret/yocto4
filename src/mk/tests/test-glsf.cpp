7906c26f34385c958ffea9e3da6bd8a9dd9505fe
ybouret <yann.bouret@gmail.com>
2015-12-11 14:52:12 +0100

    start GLSF

---
+++ src/mk/tests/test-glsf.cpp
@@ -0,0 +1,11 @@
+#include "yocto/utest/run.hpp"
+#include "yocto/math/fit/glsf.hpp"
+
+using namespace yocto;
+using namespace math;
+
+YOCTO_UNIT_TEST_IMPL(glsf)
+{
+
+}
+YOCTO_UNIT_TEST_DONE()
---
+++ src/mk/yocto/math/fit/glsf.hpp
@@ -0,0 +1,15 @@
+#ifndef YOCTO_MATH_FIT_GLSF_INCLUDED
+#define YOCTO_MATH_FIT_GLSF_INCLUDED 1
+
+#include "yocto/math/types.hpp"
+#include "yocto/sequence/many-arrays.hpp"
+
+namespace yocto
+{
+    namespace math
+    {
+        
+    }
+}
+
+#endif
