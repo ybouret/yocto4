6aa98db2638053cb6d678419d11648587c67b87b
ybouret <yann.bouret@gmail.com>
2015-12-11 16:52:10 +0100

    new glsf

--- src/mk/tests/test-glsf.cpp
+++ src/mk/tests/test-glsf.cpp
@@ -1,11 +1,90 @@
 #include "yocto/utest/run.hpp"
 #include "yocto/math/fit/glsf.hpp"
+#include "yocto/sequence/lw-array.hpp"
+#include "yocto/ios/ocstream.hpp"
 
 using namespace yocto;
 using namespace math;
 
+static inline void save(const string &fn, const array<double> &x, const array<double> &y)
+{
+    ios::ocstream fp(fn,false);
+    for(size_t i=1;i<=x.size();++i)
+    {
+        fp("%g %g\n", x[i], y[i]);
+    }
+}
+
+namespace
+{
+    struct diffusion
+    {
+
+        double compute( double t, const array<double> &a )
+        {
+            const double t0    = a[1];
+            const double slope = a[2];
+            return sqrt( slope*(t-t0) );
+        }
+        
+    };
+}
+
+
+
 YOCTO_UNIT_TEST_IMPL(glsf)
 {
+    const double _t1[] = { 30,80,140,200,270,320,430,550,640,720,830,890 };
+    const double _x1[] = { 4.414520,5.011710,5.632319,6.194379,6.721311,7.330211,8.009368,8.735363,9.297424,9.707260,10.339578,10.878220};
+
+    const double _t2[] = { 60,120,200,270,360,460,580,670,790,920,1040 };
+    const double _x2[] = { 5.199063,5.854801,6.662763,7.365340,8.067916,8.782201,9.578454,10.175644,10.878220,11.651054,12.213115};
+
+    const lw_array<double> t1( (double*)_t1, sizeof(_t1)/sizeof(_t1[0]) );
+    const lw_array<double> x1( (double*)_x1, sizeof(_x1)/sizeof(_x1[0]) );
+    const lw_array<double> t2( (double*)_t2, sizeof(_t2)/sizeof(_t2[0]) );
+    const lw_array<double> x2( (double*)_x2, sizeof(_x2)/sizeof(_x2[0]) );
+
+    save("d1.dat",t1,x1);
+    save("d2.dat",t2,x2);
+
+
+    vector<double> z1(t1.size(),0);
+    vector<double> z2(t2.size(),0);
+
+    GLS<double>::Samples samples(2);
+    GLS<double>::Sample &S1 = samples.append(t1, x1, z1);
+    GLS<double>::Sample &S2 = samples.append(t2, x2, z2);
+
+    samples.prepare(3,2);
+
+    S1.link(1,2);
+    S1.link(3,1);
+    std::cerr << "S1.Gamma=" << S1.Gamma << std::endl;
+
+    S2.link(2,2);
+    S2.link(3,1);
+    std::cerr << "S2.Gamma=" << S2.Gamma << std::endl;
+
+
+    diffusion diff;
+    GLS<double>::Function F( &diff, & diffusion::compute );
+
+    vector<double> aorg(3);
+    vector<bool>   used(3,true);
+    double &slope1  = aorg[1];
+    double &slope2  = aorg[2];
+    double &t0      = aorg[3];
+    t0     = -20;
+    slope1 = 0.02;
+    slope2 = 0.01;
+
+    samples.computeD2(F,aorg,used);
+
+
+    samples.computeD2_(F,aorg);
+    save("f1.dat",t1,z1);
+    save("f2.dat",t2,z2);
 
 }
 YOCTO_UNIT_TEST_DONE()
--- src/mk/yocto/math/fit/glsf.hpp
+++ src/mk/yocto/math/fit/glsf.hpp
@@ -1,14 +1,347 @@
 #ifndef YOCTO_MATH_FIT_GLSF_INCLUDED
 #define YOCTO_MATH_FIT_GLSF_INCLUDED 1
 
-#include "yocto/math/types.hpp"
-#include "yocto/sequence/many-arrays.hpp"
+#include "yocto/sequence/vector.hpp"
+#include "yocto/counted-object.hpp"
+#include "yocto/math/core/tao.hpp"
+#include "yocto/container/matrix.hpp"
+#include "yocto/math/fcn/drvs.hpp"
+#include "yocto/ptr/arc.hpp"
 
 namespace yocto
 {
     namespace math
     {
-        
+
+        ////////////////////////////////////////////////////////////////////////
+        //
+        // Generalized Least Squares
+        //
+        ////////////////////////////////////////////////////////////////////////
+        template <typename T>
+        class GLS
+        {
+        public:
+            typedef array<T>                           Array;
+            typedef vector<T>                          Vector;
+            typedef functor<T,TL2(T,const Array &)>    Function;
+            typedef typename numeric<double>::function Function1;
+
+            //__________________________________________________________________
+            //
+            // a sample: holds reference to data+fit
+            //__________________________________________________________________
+            class Sample : public counted_object
+            {
+            public:
+                typedef arc_ptr<Sample> Pointer;
+
+                explicit Sample( const Array &XX, const Array &YY, Array &ZZ ) :
+                X(XX),
+                Y(YY),
+                Z(ZZ),
+                N(0),
+                L(0),
+                M(0),
+                Gamma(),
+                u(),
+                dFdu(),
+                dFda(),
+                beta(),
+                F1( this, & Sample::Eval ),
+                ivar(0),
+                xval(0),
+                hook(0)
+                {
+                }
+
+                virtual ~Sample() throw()
+                {
+                }
+
+
+                const Array &X;
+                const Array &Y;
+                Array       &Z;
+
+                const size_t N;     //!< current max number of points
+                const size_t L;     //!< local  #variables
+                const size_t M;     //!< global #variables
+                matrix<int>  Gamma; //!< L*M matrix
+                Vector       u;     //!< #L local variables = Gam*a
+                Vector       dFdu;  //!< #L local gradient
+                Vector       dFda;  //!< #M global gradient
+                Vector       beta;  //!< #M descent direction
+                matrix<T>    curv;  //!< M*M local curvature
+
+                inline void link( size_t iGlobal, size_t iLocal )
+                {
+                    assert(iGlobal>0);
+                    assert(iGlobal<=Gamma.cols);
+                    assert(iLocal>0);
+                    assert(iLocal<=Gamma.rows);
+                    Gamma[iLocal][iGlobal] = 1;
+                }
+
+                inline T computeD2(Function    &F,
+                                   const Array &a)
+                {
+                    assert(X.size()==Y.size());
+                    assert(Y.size()==Z.size());
+                    size_t &count = (size_t &)N;
+                    count = 0;
+                    assert(a.size()==M);
+                    tao::mul(u,Gamma,a);
+                    T D2 = 0;
+                    for(size_t i=X.size();i>0;--i)
+                    {
+                        const T d = Y[i] - (Z[i]=F(X[i],u));
+                        D2 += d*d;
+                        ++count;
+                    }
+                    return D2;
+                }
+
+                inline T computeD2(Function      &F,
+                                   const Array   &a,
+                                   derivative<T> &drvs,
+                                   const T        du)
+                {
+                    assert(X.size()==Y.size());
+                    assert(Y.size()==Z.size());
+                    assert(a.size()==M);
+
+                    hook = &F;
+
+                    size_t &count = (size_t &)N;
+                    count = 0;
+                    tao::mul(u,Gamma,a);
+                    tao::ld(beta,0);
+                    curv.ldz();
+
+                    T D2 = 0;
+                    for(size_t i=X.size();i>0;--i)
+                    {
+                        const T Xi = X[i];
+
+                        // compute gradient along u
+                        for(size_t j=L;j>0;--j)
+                        {
+                            ivar    = j;
+                            xval    = Xi;
+                            dFdu[j] = drvs(F1,u[j],du);
+                        }
+
+                        // deduce gradient along a
+                        tao::mul_trn(dFda, Gamma, dFdu);
+
+
+                        // compute curent D2
+                        const T d  = Y[i] - F(Xi,u);
+                        D2 += d*d;
+
+                        // update local descent direction
+                        for(size_t j=M;j>0;--j)
+                        {
+                            const T dFda_j = dFda[j];
+                            beta[j] += d * dFda_j;
+                            for(size_t k=M;k>=j;--k)
+                            {
+                                curv[j][k] += dFda_j * dFda[k];
+                            }
+                        }
+
+                        ++count;
+                    }
+                    return D2;
+                }
+
+                inline void prepare( size_t global_nvar, size_t local_nvar )
+                {
+                    assert(global_nvar>0);
+                    assert(local_nvar>0);
+                    assert(local_nvar<=global_nvar);
+                    (size_t &)L = local_nvar;
+                    (size_t &)M = global_nvar;
+                    Gamma.make(L,M);
+                    u.make(L);
+                    dFdu.make(L);
+                    dFda.make(M);
+                    beta.make(M);
+                    curv.make(M,M);
+
+                    Gamma.ldz();
+                }
+
+                inline void prepare( size_t global_nvar )
+                {
+                    prepare(global_nvar,global_nvar);
+                    Gamma.ld1();
+                }
+
+            private:
+                Function1    F1;
+                size_t       ivar;  //!< for gadient
+                T            xval;  //!< for gradient
+                Function    *hook;  //!< for gradient
+                YOCTO_DISABLE_COPY_AND_ASSIGN(Sample);
+
+                inline T Eval( T U )
+                {
+                    assert(ivar>0);
+                    assert(ivar<=L);
+                    assert(hook);
+                    const T save = u[ivar];
+                    u[ivar] = U;
+                    const T ans  = (*hook)(xval,u);
+                    u[ivar] = save;
+                    return ans;
+                }
+            };
+
+
+            //__________________________________________________________________
+            //
+            // Samples: hold multiple samples
+            //__________________________________________________________________
+            typedef vector<typename Sample::Pointer> _Samples;
+
+            class Samples : public _Samples
+            {
+            public:
+                const size_t   M;
+                Vector         beta;
+                matrix<T>      curv;
+                derivative<T>  drvs;
+                T              scale;
+
+                explicit Samples(size_t n) :
+                _Samples(n,as_capacity),
+                M(0),
+                beta(),
+                curv(),
+                drvs(),
+                scale(1e-4)
+                {
+                }
+
+                virtual ~Samples() throw()
+                {
+                }
+
+                inline
+                Sample &append( const Array &X, const Array &Y, Array &Z)
+                {
+                    typename Sample::Pointer p( new Sample(X,Y,Z) );
+                    this->push_back(p);
+                    return *p;
+                }
+
+                inline void prepare(size_t global_nvar, size_t local_nvar)
+                {
+                    _Samples &self = *this;
+                    for(size_t i=self.size();i>0;--i)
+                    {
+                        self[i]->prepare(global_nvar,local_nvar);
+                    }
+                    setup(global_nvar);
+                }
+
+
+                inline void prepare(size_t global_nvar)
+                {
+                    _Samples &self = *this;
+                    for(size_t i=self.size();i>0;--i)
+                    {
+                        self[i]->prepare(global_nvar);
+                    }
+                    setup(global_nvar);
+                }
+
+                T computeD2_(Function &F, const Array &a )
+                {
+                    T D2 = 0;
+                    _Samples &self = *this;
+                    for(size_t i=self.size();i>0;--i)
+                    {
+                        D2 += self[i]->computeD2(F,a);
+                    }
+                    return D2;
+                }
+
+
+                inline T computeD2(Function          &F,
+                                   const Array       &aorg,
+                                   const array<bool> &used)
+
+                {
+                    assert(M==aorg.size());
+                    assert(M==used.size());
+
+                    // initialize
+                    tao::ld(beta,0);
+                    curv.ldz();
+                    T D2   = 0;
+
+                    // full computation
+                    _Samples &self = *this;
+                    for(size_t i=self.size();i>0;--i)
+                    {
+                        Sample &S = *self[i];
+                        D2 += S.computeD2(F,aorg,drvs,scale);
+                        std::cerr << "beta" << i << "=" << S.beta << std::endl;
+                        std::cerr << "curv" << i << "=" << S.curv << std::endl;
+                        for(size_t j=M;j>0;--j)
+                        {
+                            beta[j] += S.beta[j];
+                            for(size_t k=M;k>=j;--k)
+                            {
+                                curv[j][k] += S.curv[j][k];
+                            }
+                        }
+                    }
+
+                    // legalizing
+                    for(size_t j=M;j>0;--j)
+                    {
+                        if(used[j])
+                        {
+                            for(size_t r=1;r<=M;++r)
+                            {
+                                for(size_t c=1;c<r;++c)
+                                {
+                                    curv[r][c] = curv[c][r];
+                                }
+                            }
+                        }
+                        else
+                        {
+                            beta[j] = 0;
+                            for(size_t i=M;i>0;--i)
+                            {
+                                curv[i][j] = curv[j][i] = 0;
+                            }
+                            curv[j][j] = 1;
+                        }
+                    }
+
+                    std::cerr << "beta=" << beta << std::endl;
+                    std::cerr << "curv=" << curv << std::endl;
+                    return D2;
+                }
+
+            private:
+                YOCTO_DISABLE_COPY_AND_ASSIGN(Samples);
+                void setup(size_t nvar)
+                {
+                    assert(nvar>0);
+                    (size_t &)M = nvar;
+                    beta.make(nvar);
+                    curv.make(nvar);
+                }
+            };
+        };
+
     }
 }
 
