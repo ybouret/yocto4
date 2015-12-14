b225c61462e82006405683363496145727ee20d9
ybouret <yann.bouret@gmail.com>
2015-12-14 10:30:07 +0100

    sample is coded

--- src/mk/yocto/math/fit/glsf.cxx
+++ src/mk/yocto/math/fit/glsf.cxx
@@ -1,9 +1,180 @@
 #include "yocto/math/fit/glsf.hpp"
+#include "yocto/math/ztype.hpp"
+
 
 namespace yocto
 {
     namespace math
     {
-        
+
+        template <>
+        int GLS<real_t>::GET_P10_MAX() throw()
+        {
+            return -int( floor(Log10(numeric<real_t>::epsilon)) );
+        }
+
+        template <>
+        real_t GLS<real_t>:: Sample:: Eval( const real_t U )
+        {
+            assert(ivar>0);
+            assert(ivar<=L);
+            assert(hook);
+            const real_t save = u[ivar];
+            u[ivar] = U;
+            const real_t ans  = (*hook)(xval,u);
+            u[ivar] = save;
+            return ans;
+        }
+
+
+        template <>
+        GLS<real_t>:: Sample:: Sample( const Array &XX, const Array &YY, Array &ZZ ) :
+        X(XX),
+        Y(YY),
+        Z(ZZ),
+        N(0),
+        L(0),
+        M(0),
+        Gamma(),
+        u(),
+        dFdu(),
+        dFda(),
+        beta(),
+        F1( this, & Sample::Eval ),
+        ivar(0),
+        xval(0),
+        hook(0)
+        {
+        }
+
+        template <>
+        GLS<real_t>:: Sample:: ~Sample() throw()
+        {
+        }
+
+        template <>
+        void GLS<real_t>:: Sample:: link( size_t iGlobal, size_t iLocal )
+        {
+            assert(iGlobal>0);
+            assert(iGlobal<=Gamma.cols);
+            assert(iLocal>0);
+            assert(iLocal<=Gamma.rows);
+            Gamma[iLocal][iGlobal] = 1;
+        }
+
+        template <>
+        real_t GLS<real_t>:: Sample:: computeD2(Function    &F,
+                                                const Array &a)
+        {
+            assert(X.size()==Y.size());
+            assert(Y.size()==Z.size());
+            size_t &count = (size_t &)N;
+            count = 0;
+            assert(a.size()==M);
+            tao::mul(u,Gamma,a);
+            real_t D2 = 0;
+            for(size_t i=X.size();i>0;--i)
+            {
+                const real_t d = Y[i] - (Z[i]=F(X[i],u));
+                D2 += d*d;
+                ++count;
+            }
+            return D2;
+        }
+
+        template <>
+        real_t GLS<real_t>:: Sample:: computeD2(Function           &F,
+                                                const Array        &a,
+                                                derivative<real_t> &drvs,
+                                                const real_t        du)
+        {
+            assert(X.size()==Y.size());
+            assert(Y.size()==Z.size());
+            assert(a.size()==M);
+
+            hook = &F;
+
+            size_t &count = (size_t &)N;
+            count = 0;
+            tao::mul(u,Gamma,a);
+            tao::ld(beta,0);
+            curv.ldz();
+
+            real_t D2 = 0;
+            for(size_t i=X.size();i>0;--i)
+            {
+                const real_t Xi = X[i];
+
+                //______________________________________________________
+                //
+                // compute gradient along u
+                //______________________________________________________
+                for(size_t j=L;j>0;--j)
+                {
+                    ivar    = j;
+                    xval    = Xi;
+                    dFdu[j] = drvs(F1,u[j],du);
+                }
+
+                //______________________________________________________
+                //
+                // deduce gradient along a
+                //______________________________________________________
+                tao::mul_trn(dFda, Gamma, dFdu);
+
+
+                //______________________________________________________
+                //
+                // compute curent D2
+                //______________________________________________________
+                const real_t d  = Y[i] - F(Xi,u);
+                D2 += d*d;
+
+                //______________________________________________________
+                //
+                // update local descent direction and curvature
+                //______________________________________________________
+                for(size_t j=M;j>0;--j)
+                {
+                    const real_t dFda_j = dFda[j];
+                    beta[j] += d * dFda_j;
+                    for(size_t k=M;k>=j;--k)
+                    {
+                        curv[j][k] += dFda_j * dFda[k];
+                    }
+                }
+
+                ++count;
+            }
+            return D2;
+        }
+
+        template <>
+        void GLS<real_t>:: Sample:: prepare( size_t global_nvar, size_t local_nvar )
+        {
+            assert(global_nvar>0);
+            assert(local_nvar>0);
+            assert(local_nvar<=global_nvar);
+            (size_t &)L = local_nvar;
+            (size_t &)M = global_nvar;
+            Gamma.make(L,M);
+            u.make(L);
+            dFdu.make(L);
+            dFda.make(M);
+            beta.make(M);
+            curv.make(M,M);
+            
+            Gamma.ldz();
+        }
+
+        template <>
+        void GLS<real_t>:: Sample:: prepare( size_t global_nvar )
+        {
+            prepare(global_nvar,global_nvar);
+            Gamma.ld1();
+        }
+
+
+
     }
 }
--- src/mk/yocto/math/fit/glsf.hpp
+++ src/mk/yocto/math/fit/glsf.hpp
@@ -28,15 +28,12 @@ namespace yocto
         class GLS
         {
         public:
-            static int GET_P10_MAX()
-            {
-                return -int( floor(Log10(numeric<T>::epsilon)) );
-            }
+            static int GET_P10_MAX() throw();
 
             typedef array<T>                           Array;
             typedef vector<T>                          Vector;
             typedef functor<T,TL2(T,const Array &)>    Function;
-            typedef typename numeric<double>::function Function1;
+            typedef typename numeric<T>::function      Function1;
 
             //__________________________________________________________________
             //
@@ -49,29 +46,8 @@ namespace yocto
             public:
                 typedef arc_ptr<Sample> Pointer;
 
-                explicit Sample( const Array &XX, const Array &YY, Array &ZZ ) :
-                X(XX),
-                Y(YY),
-                Z(ZZ),
-                N(0),
-                L(0),
-                M(0),
-                Gamma(),
-                u(),
-                dFdu(),
-                dFda(),
-                beta(),
-                F1( this, & Sample::Eval ),
-                ivar(0),
-                xval(0),
-                hook(0)
-                {
-                }
-
-                virtual ~Sample() throw()
-                {
-                }
-
+                explicit Sample( const Array &XX, const Array &YY, Array &ZZ );
+                virtual ~Sample() throw();
 
                 const Array &X;
                 const Array &Y;
@@ -88,143 +64,32 @@ namespace yocto
                 matrix<T>    curv;  //!< M*M local curvature
 
                 //! link global variable index to local variable index
-                inline void link( size_t iGlobal, size_t iLocal )
-                {
-                    assert(iGlobal>0);
-                    assert(iGlobal<=Gamma.cols);
-                    assert(iLocal>0);
-                    assert(iLocal<=Gamma.rows);
-                    Gamma[iLocal][iGlobal] = 1;
-                }
+                void link( size_t iGlobal, size_t iLocal );
 
                 //!compute D2 and update the Z term
-                inline T computeD2(Function    &F,
-                                   const Array &a)
-                {
-                    assert(X.size()==Y.size());
-                    assert(Y.size()==Z.size());
-                    size_t &count = (size_t &)N;
-                    count = 0;
-                    assert(a.size()==M);
-                    tao::mul(u,Gamma,a);
-                    T D2 = 0;
-                    for(size_t i=X.size();i>0;--i)
-                    {
-                        const T d = Y[i] - (Z[i]=F(X[i],u));
-                        D2 += d*d;
-                        ++count;
-                    }
-                    return D2;
-                }
+                T computeD2(Function    &F,
+                            const Array &a);
 
                 //! compute D2 and curvature
-                inline T computeD2(Function      &F,
-                                   const Array   &a,
-                                   derivative<T> &drvs,
-                                   const T        du)
-                {
-                    assert(X.size()==Y.size());
-                    assert(Y.size()==Z.size());
-                    assert(a.size()==M);
+                T computeD2(Function      &F,
+                            const Array   &a,
+                            derivative<T> &drvs,
+                            const T        du);
 
-                    hook = &F;
+                //! prepare memory with Gamma=0
+                void prepare( size_t global_nvar, size_t local_nvar );
 
-                    size_t &count = (size_t &)N;
-                    count = 0;
-                    tao::mul(u,Gamma,a);
-                    tao::ld(beta,0);
-                    curv.ldz();
-
-                    T D2 = 0;
-                    for(size_t i=X.size();i>0;--i)
-                    {
-                        const T Xi = X[i];
-
-                        //______________________________________________________
-                        //
-                        // compute gradient along u
-                        //______________________________________________________
-                        for(size_t j=L;j>0;--j)
-                        {
-                            ivar    = j;
-                            xval    = Xi;
-                            dFdu[j] = drvs(F1,u[j],du);
-                        }
-
-                        //______________________________________________________
-                        //
-                        // deduce gradient along a
-                        //______________________________________________________
-                        tao::mul_trn(dFda, Gamma, dFdu);
-
-
-                        //______________________________________________________
-                        //
-                        // compute curent D2
-                        //______________________________________________________
-                        const T d  = Y[i] - F(Xi,u);
-                        D2 += d*d;
-
-                        //______________________________________________________
-                        //
-                        // update local descent direction and curvature
-                        //______________________________________________________
-                        for(size_t j=M;j>0;--j)
-                        {
-                            const T dFda_j = dFda[j];
-                            beta[j] += d * dFda_j;
-                            for(size_t k=M;k>=j;--k)
-                            {
-                                curv[j][k] += dFda_j * dFda[k];
-                            }
-                        }
-
-                        ++count;
-                    }
-                    return D2;
-                }
-
-                inline void prepare( size_t global_nvar, size_t local_nvar )
-                {
-                    assert(global_nvar>0);
-                    assert(local_nvar>0);
-                    assert(local_nvar<=global_nvar);
-                    (size_t &)L = local_nvar;
-                    (size_t &)M = global_nvar;
-                    Gamma.make(L,M);
-                    u.make(L);
-                    dFdu.make(L);
-                    dFda.make(M);
-                    beta.make(M);
-                    curv.make(M,M);
-
-                    Gamma.ldz();
-                }
-
-                inline void prepare( size_t global_nvar )
-                {
-                    prepare(global_nvar,global_nvar);
-                    Gamma.ld1();
-                }
+                //! prepare memory with Gamma=Id
+                void prepare( size_t global_nvar );
 
             private:
-                Function1    F1;
+                Function1    F1;    //!< call Eval for gradient
                 size_t       ivar;  //!< for gadient
                 T            xval;  //!< for gradient
                 Function    *hook;  //!< for gradient
                 YOCTO_DISABLE_COPY_AND_ASSIGN(Sample);
 
-                inline T Eval( T U )
-                {
-                    assert(ivar>0);
-                    assert(ivar<=L);
-                    assert(hook);
-                    const T save = u[ivar];
-                    u[ivar] = U;
-                    const T ans  = (*hook)(xval,u);
-                    u[ivar] = save;
-                    return ans;
-                }
+                T Eval( const T U );
             };
 
 
@@ -588,10 +453,10 @@ namespace yocto
             private:
                 YOCTO_DISABLE_COPY_AND_ASSIGN(Samples);
                 Function1 scan;
-
+                
                 Function *hook;
                 Array    *pvar;
-
+                
                 inline void setup(size_t nvar)
                 {
                     assert(nvar>0);
@@ -603,7 +468,7 @@ namespace yocto
                     curv.make(M,M);
                     cinv.make(M);    // large memory model for LU
                 }
-
+                
                 inline T probe(const T u)
                 {
                     assert(hook);
@@ -611,7 +476,7 @@ namespace yocto
                     tao::setprobe(atry,*pvar,u,step);
                     return computeD2_(*hook,atry);
                 }
-
+                
             };
         };
         
