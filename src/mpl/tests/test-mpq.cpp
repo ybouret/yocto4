30955dc7b03bfa51b39e7d170f0486d85eda34f7
ybouret <yann.bouret@gmail.com>
2016-02-21 21:40:23 +0100

    init rational

--- src/mpl/tests/test-mpq.cpp
+++ src/mpl/tests/test-mpq.cpp
@@ -1,1 +1,10 @@
+#include "yocto/mpl/rational.hpp"
+#include "yocto/utest/run.hpp"
 
+using namespace yocto;
+
+YOCTO_UNIT_TEST_IMPL(mpq)
+{
+
+}
+YOCTO_UNIT_TEST_DONE()
--- src/mpl/yocto/mpl/rational.hpp
+++ src/mpl/yocto/mpl/rational.hpp
@@ -1,1 +1,40 @@
+#ifndef YOCTO_MPL_RATIONAL_INCLUDED
+#define YOCTO_MPL_RATIONAL_INCLUDED 1
+
+#include "yocto/mpl/integer.hpp"
+
+namespace yocto
+{
+    namespace mpl
+    {
+        
+        class rational : public object
+        {
+        public:
+            const integer num;
+            const integer den;
+            
+            virtual ~rational() throw() {}
+            inline   rational() : num(0), den(1) {}
+            inline   rational(const rational &r) : num(r.num), den(r.den) {}
+            
+            inline void xch(rational &r) throw()
+            {
+                ( (integer &)num ).xch( (integer &)(r.num) );
+                ( (integer &)den ).xch( (integer &)(r.den) );
+            }
+            
+            inline rational & operator=(const rational &r)
+            {
+                rational q(r);
+                xch(q);
+                return *this;
+            }
+            
+            
+        };
+    }
+}
+
+#endif
 
