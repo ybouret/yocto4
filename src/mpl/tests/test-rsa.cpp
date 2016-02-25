e8e3dac99e28a8e8f227e70b0b646e3ac94c4d5e
ybouret <yann.bouret@gmail.com>
2016-02-25 15:11:23 +0100

    need RSA tests...

--- src/mpl/tests/test-rsa.cpp
+++ src/mpl/tests/test-rsa.cpp
@@ -13,6 +13,11 @@ YOCTO_UNIT_TEST_IMPL(rsa)
     q = mpn::__next_prime(q);
     mpn e = 3+alea_leq(100);
     RSA::PrivateKey prv = RSA::PrivateKey::GenerateFrom(p, q, e);
-    
+    RSA::PublicKey  pub(prv);
+
+    std::cerr << "prv=[" << prv.modulus << "," << prv.privateExponent << "]" << std::endl;
+    std::cerr << "pub=[" << pub.modulus << "," << prv.publicExponent  << "]" << std::endl;
+
+
 }
 YOCTO_UNIT_TEST_DONE()
