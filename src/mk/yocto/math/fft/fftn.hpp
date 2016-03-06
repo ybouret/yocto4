3d224539b7470e6a8bffb181d1e6e8e4b7394021
ybouret <yann.bouret@gmail.com>
2016-03-06 13:32:56 +0100

    WIP FFTN

--- src/mk/yocto/math/fft/fftn.hpp
+++ src/mk/yocto/math/fft/fftn.hpp
@@ -2,6 +2,7 @@
 #define YOCTO_MATH_FFTN_INCLUDED 1
 
 #include "yocto/math/complex.hpp"
+#include "yocto/container/matrix.hpp"
 
 
 namespace yocto
@@ -19,7 +20,19 @@ namespace yocto
              */
             static void Compute(T *data, const size_t nn[], const size_t ndim, int isign) throw();
             
+            static inline void Forward(const T *data, const size_t nn[], const size_t ndim) throw()
+            {
+                Compute(data, nn, ndim, 1);
+            }
+            
+            static inline void Reverse(const T *data, const size_t nn[], const size_t ndim) throw()
+            {
+                Compute(data, nn, ndim, -1);
+            }
+            
         };
+        
+        void FFT(matrix< complex<float> > &);
         
     }
 
