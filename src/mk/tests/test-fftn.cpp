155ae9cfcd379750026f506eadb031bcf0554f9e
ybouret <yann.bouret@gmail.com>
2016-03-08 14:47:00 +0100

    code remade

--- src/mk/tests/test-fftn.cpp
+++ src/mk/tests/test-fftn.cpp
@@ -11,7 +11,7 @@ static inline void perform_fftn()
 {
     for(size_t nr=1;nr<=1024;nr*=2)
     {
-        
+
         for(size_t nc=1;nc<=1024;nc*=2)
         {
             matrix< complex<T> > M(nr,nc);
@@ -19,7 +19,7 @@ static inline void perform_fftn()
             {
                 for(size_t j=1;j<=nc;++j)
                 {
-                    M[i][j].re = alea<double>();
+                    M[i][j].re = alea<T>();
                 }
             }
             matrix< complex<T> > M0 = M;
@@ -30,10 +30,11 @@ static inline void perform_fftn()
             {
                 for(size_t j=1;j<=nc;++j)
                 {
-                    delta += Fabs(M[i][j]-M0[i][j]);
+                    const T d = Fabs(M[i][j]-M0[i][j]);
+                    delta += d*d;
                 }
             }
-            delta /= (nr*nc);
+            delta = Sqrt(delta/(nr*nc));
             std::cerr << " " << delta;
 
         }
@@ -42,12 +43,49 @@ static inline void perform_fftn()
     }
 }
 
+#include <cstring>
+#include "yocto/sys/timings.hpp"
+#include "yocto/sequence/vector.hpp"
+
 YOCTO_UNIT_TEST_IMPL(fftn)
 {
-    std::cerr << "-- float" << std::endl;
-    perform_fftn<float>();
-    
-    std::cerr << "-- double" << std::endl;
-    perform_fftn<double>();
+    const bool perf = (argc>1) && 0==strcmp(argv[1],"perf");
+
+    if(!perf)
+    {
+        std::cerr << "-- float" << std::endl;
+        perform_fftn<float>();
+
+        std::cerr << "-- double" << std::endl;
+        perform_fftn<double>();
+    }
+    else
+    {
+        vector<double> speed;
+        timings tmx;
+        size_t count=0;
+        std::cerr.flush();
+        for(size_t nr=4;nr<=256;nr*=2)
+        {
+            for(size_t nc=4;nc<=256;nc*=2)
+            {
+                ++count;
+                matrix< complex<double> > M(nr,nc);
+                for(size_t i=1;i<=nr;++i)
+                {
+                    for(size_t j=1;j<=nc;++j)
+                    {
+                        M[i][j].re = alea<double>();
+                    }
+                }
+                YOCTO_TIMINGS(tmx,0.5, FFT(M); iFFT(M) );
+                const double spd = tmx.speed/1000;
+                speed.push_back(spd);
+                fprintf(stderr, "%4d x %4d : %12.5lf kops\n", unsigned(nr), unsigned(nc), spd);
+                fflush(stderr);
+            }
+        }
+        std::cerr << "count=" << count << std::endl;
+    }
 }
 YOCTO_UNIT_TEST_DONE()
--- src/mk/yocto/math/fft/fftn.cxx
+++ src/mk/yocto/math/fft/fftn.cxx
@@ -10,59 +10,60 @@ namespace yocto
 {
     namespace math
     {
-        
+
         template <>
         void FourierN<real_t>:: Compute(real_t *data, const size_t nn[], const size_t ndim, int isign) throw()
         {
             /*
-            std::cerr << "fft" << ndim << "(";
-            for(size_t i=1;i<=ndim;++i)
-            {
-                std::cerr << " " << nn[i];
-            }
-            std::cerr << " , " << isign;
-            std::cerr << " )" << std::endl;
+             std::cerr << "fft" << ndim << "(";
+             for(size_t i=1;i<=ndim;++i)
+             {
+             std::cerr << " " << nn[i];
+             }
+             std::cerr << " , " << isign;
+             std::cerr << " )" << std::endl;
              */
             assert(data);
             assert(nn);
             assert(ndim>0);
             assert(1==isign||-1==isign);
-            
-            size_t i1,i2,i3,i2rev,i3rev,ip1,ip2,ip3,ifp1,ifp2;
-            size_t ibit,k1,k2,n,nprev,nrem,ntot;
-            real_t  tempi,tempr;
-            double  theta,wi,wpi,wpr,wr,wtemp;
-            
-            ntot=1;
+
+            size_t  i1,i2,i3,i2rev,i3rev,ip1,ip2,ip3,ifp1,ifp2;
+            size_t  k1,k2;
+
+            size_t ntot=1;
             for(size_t idim=1;idim<=ndim;++idim)
             {
                 assert(is_a_power_of_two(nn[idim]));
                 ntot *= nn[idim];
             }
-            nprev=1;
+
+            size_t nprev=1;
             for(size_t idim=ndim;idim>=1;--idim)
             {
-                n     = nn[idim];
-                nrem  = ntot/(n*nprev);
+                const size_t n     = nn[idim];
+                const size_t nrem  = ntot/(n*nprev);
                 ip1   = nprev << 1;
                 ip2   = ip1*n;
                 ip3   = ip2*nrem;
                 i2rev = 1;
-                for (i2=1;i2<=ip2;i2+=ip1)
+                for(i2=1;i2<=ip2;i2+=ip1)
                 {
-                    if (i2 < i2rev)
+                    if(i2<i2rev)
                     {
-                        for (i1=i2;i1<=i2+ip1-2;i1+=2)
+                        const size_t itop =i2+ip1-2;
+                        for(i1=i2;i1<=itop;i1+=2)
                         {
-                            for (i3=i1;i3<=ip3;i3+=ip2)
+                            for(i3=i1;i3<=ip3;i3+=ip2)
                             {
                                 i3rev=i2rev+i3-i2;
-                                cswap(data[i3],data[i3rev]);
-                                cswap(data[i3+1],data[i3rev+1]);
+                                core::bswap<sizeof(complex<real_t>)>(&data[i3],&data[i3rev]);
+                                //cswap(data[i3],data[i3rev]);
+                                //cswap(data[i3+1],data[i3rev+1]);
                             }
                         }
                     }
-                    ibit=ip2 >> 1;
+                    size_t ibit=ip2 >> 1;
                     while (ibit >= ip1 && i2rev > ibit)
                     {
                         i2rev -= ibit;
@@ -70,32 +71,35 @@ namespace yocto
                     }
                     i2rev += ibit;
                 }
-                
+
                 ifp1=ip1;
                 const double sgn_two_pi = numeric<double>::two_pi * isign;
                 while (ifp1 < ip2)
                 {
                     ifp2  = ifp1 << 1;
-                    theta = sgn_two_pi/(ifp2/ip1);
-                    wtemp = sin(0.5*theta);
-                    wpr   = -2.0*wtemp*wtemp;
-                    wpi   = sin(theta);
-                    wr    = 1;
-                    wi    = 0;
-                    for (i3=1;i3<=ifp1;i3+=ip1)
+                    const double theta = sgn_two_pi/(ifp2/ip1);
+                    double wtemp = sin(0.5*theta);
+                    double wpr   = -2.0*wtemp*wtemp;
+                    double wpi   = sin(theta);
+                    double wr    = 1;
+                    double wi    = 0;
+                    for(i3=1;i3<=ifp1;i3+=ip1)
                     {
-                        for (i1=i3;i1<=i3+ip1-2;i1+=2)
+                        const size_t itop=i3+ip1-2;
+                        for(i1=i3;i1<=itop;i1+=2)
                         {
-                            for (i2=i1;i2<=ip3;i2+=ifp2)
+                            for(i2=i1;i2<=ip3;i2+=ifp2)
                             {
                                 k1=i2;
                                 k2=k1+ifp1;
-                                tempr=real_t(wr)*data[k2]  -real_t(wi)*data[k2+1];
-                                tempi=real_t(wr)*data[k2+1]+real_t(wi)*data[k2];
+                                const size_t k1p1 = k1+1;
+                                const size_t k2p1 = k2+1;
+                                const real_t tempr=real_t(wr)*data[k2]  -real_t(wi)*data[k2p1];
+                                const real_t tempi=real_t(wr)*data[k2p1]+real_t(wi)*data[k2];
                                 data[k2]    = data[k1]-tempr;
-                                data[k2+1]  = data[k1+1]-tempi;
+                                data[k2p1]  = data[k1p1]-tempi;
                                 data[k1]   += tempr;
-                                data[k1+1] += tempi;
+                                data[k1p1] += tempi;
                             }
                         }
                         wr=(wtemp=wr)*wpr-wi*wpi+wr;
@@ -106,12 +110,12 @@ namespace yocto
                 nprev *= n;
             }
 
-            
+
             const real_t coef = real_t(1.0/sqrt( double(ntot) ));
 #define _YOCTO_FFT_SCALE(INDEX) data[INDEX] *= coef
             YOCTO_LOOP_FUNC_(ntot<<1,_YOCTO_FFT_SCALE,1);
 
-
+            
         }
         
         
