ce49aeefafac8ae1f37049e306e66a808d3917ef
ybouret <yann.bouret@gmail.com>
2016-03-14 11:47:21 +0100

    need to change field place...

---
+++ src/main/tests/test-field.cpp
@@ -0,0 +1,9 @@
+#include "yocto/container/field.hpp"
+#include "yocto/utest/run.hpp"
+
+
+YOCTO_UNIT_TEST_IMPL(field)
+{
+
+}
+YOCTO_UNIT_TEST_DONE()
--- src/main/yocto/container/field.hpp
+++ src/main/yocto/container/field.hpp
@@ -1,6 +1,47 @@
 #ifndef YOCTO_CONTAINER_FIELD_INCLUDED
 #define YOCTO_CONTAINER_FIELD_INCLUDED 1
 
+#include "yocto/parallel/patch.hpp"
+
+namespace yocto
+{
+
+    template <typename T, typename COORD>
+    class field_of : public parallel::patch_of<COORD>
+    {
+    public:
+        typedef parallel::patch_of<COORD> patch_type;
+        static const size_t DIM = patch_type::DIM;
+
+        inline virtual ~field_of() throw()
+        {
+        }
+
+    protected:
+        T *entry;
+        explicit field_of( const patch_type &p ) : patch_type(p), entry(0) {}
+
+
+
+    private:
+        YOCTO_DISABLE_COPY_AND_ASSIGN(field_of);
+    };
+
+    
+    template <typename T>
+    class field1D : public field_of<T,parallel::coord1D>
+    {
+    public:
+
+
+
+    private:
+        YOCTO_DISABLE_COPY_AND_ASSIGN(field1D);
+    };
+
+
+}
+
 
 #endif
 
