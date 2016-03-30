4811fe8ce7478519b13c9ad7e295f164f16b3ed3
ybouret <yann.bouret@gmail.com>
2016-03-30 11:09:39 +0200

    bug...

--- src/lang/yocto/lang/syntax/emit.cpp
+++ src/lang/yocto/lang/syntax/emit.cpp
@@ -22,109 +22,6 @@ namespace yocto
         namespace syntax
         {
 
-#if 0
-            static const char xnode_name  [] = "XNODE";
-            static const char leaves_name [] = "LEAVES";
-            static const char walker_args [] = "const XNODE *node";
-
-
-
-            namespace
-            {
-                static inline string label2method(const string &label)
-                {
-                    string       ans = "on_";
-                    const size_t n   = label.size();
-                    for(size_t i=0;i<n;++i)
-                    {
-                        const char C = label[i];
-                        if( isalnum(C) || '_' == C)
-                        {
-                            ans += C;
-                        }
-                        else
-                        {
-                            const uint8_t B(C);
-                            ans += '_';
-                            ans += hexa_text[B];
-                        }
-                    }
-                    return ans;
-                }
-
-                static inline bool __is_internal(const string &label) throw()
-                {
-                    return (0 != strchr( label.c_str(), grammar::internal_char ) );
-                }
-
-                class walker_rule
-                {
-                public:
-                    const string   label;
-                    const string   method;
-
-                    inline walker_rule(const string &l) :
-                    label(l),
-                    method(label2method(label))                    {
-                    }
-
-                    inline  ~walker_rule() throw()
-                    {}
-
-
-                    const string &key() const throw() { return label; }
-
-                    inline walker_rule(const walker_rule &other) :
-                    label( other.label ),
-                    method(other.method)
-                    {
-                    }
-
-
-                private:
-                    YOCTO_DISABLE_ASSIGN(walker_rule);
-                };
-
-                typedef set<string,walker_rule> _walker_db;
-
-                class walker_db : public _walker_db
-                {
-                public:
-                    hashing::mperf mph;
-
-                    explicit walker_db(const size_t n) : _walker_db(n,as_capacity) {}
-                    virtual ~walker_db() throw() {}
-
-                    inline void add( const rule *r, ios::ostream &nsfp)
-                    {
-                        assert(r);
-                        //-- make local perfect hash
-                        mph.insert(r->label,size());
-
-
-                        //-- local database of method
-                        const walker_rule wr(r->label);
-                        if( !insert(wr) )
-                        {
-                            throw exception("unexpected walker failure for '%s'", r->label.c_str());
-                        }
-
-                        //-- store info for binary code
-                        ios::net_string::format(wr.label,nsfp);
-
-                    }
-
-                private:
-                    YOCTO_DISABLE_COPY_AND_ASSIGN(walker_db);
-                };
-
-
-                static inline void __assert_terminal_is(const bool flag, ios::ostream &fp)
-                {
-                    fp << "\t\tassert(" << (flag?"true":"false") << "==node->terminal);\n";
-                }
-            }
-#endif
 
 
             namespace
@@ -191,14 +88,16 @@ namespace yocto
                 class wmaker : public _wmaker
                 {
                 public:
+                    ios::ostream  &fp;   //!< the output
                     string         nsdb; //!< netstring database
                     ios::osstream  nsfp; //!< wrapper to write into nsdb
                     hashing::mperf mph;
                     const string   class_name;
                     const string   class_name_base;
 
-                    explicit wmaker(const size_t n,const string &user_class_name) :
+                    explicit wmaker(const size_t n,const string &user_class_name, ios::ostream &user_fp) :
                     _wmaker(n,as_capacity),
+                    fp(user_fp),
                     nsdb(),
                     nsfp(nsdb),
                     mph(),
@@ -211,8 +110,11 @@ namespace yocto
                     {
                     }
 
-
-                    inline void add( const rule *r )
+                    //__________________________________________________________
+                    //
+                    // prepare databases of rules
+                    //__________________________________________________________
+                    inline void add_one( const rule *r )
                     {
                         bool do_register = true;
 
@@ -236,7 +138,7 @@ namespace yocto
                             // register for class_name hashing
                             ios::net_string::format(r->label, nsfp);
                             nsfp.flush();
-                            
+
                             // register for local hashing
                             mph.insert(r->label, size());
 
@@ -247,21 +149,93 @@ namespace yocto
                                 throw exception("unexpected multiple rule label '%s'", r->label.c_str());
                             }
                         }
-
                     }
 
-                    inline void export_base( ios::ostream &fp ) const
+                    inline void add( const core::list_of<rule> &rr )
+                    {
+                        for(const rule *r = rr.head;r;r=r->next)
+                        {
+                            add_one(r);
+                        }
+                    }
+
+                    //__________________________________________________________
+                    //
+                    // export base class
+                    //__________________________________________________________
+                    inline void export_base() const
                     {
                         fp << "class " << class_name_base << " : public yocto::lang::syntax::walker {\n";
                         fp << "public:\n";
 
-                        fp << "\tinline  " << class_name_base << "() : yocto::lang::syntax::walker(\"" << nsdb << "\")\n\t{}\n";
-                        fp << "\tinline ~" << class_name_base << "() throw() {}\n";
+                        fp << "\tinline explicit " << class_name_base << "() : yocto::lang::syntax::walker(\"" << nsdb << "\")\n\t{}\n";
+                        fp << "\tinline virtual ~" << class_name_base << "() throw() {}\n";
                         fp << "private:\n";
                         fp << "\tYOCTO_DISABLE_COPY_AND_ASSIGN(" << class_name_base << ");\n";
                         fp << "};\n\n";
                     }
 
+                    inline ios::ostream & com() const
+                    {
+                        fp << "\t\t//";
+                        return fp;
+                    }
+
+                    inline void hline() const
+                    {
+                        com(); for(size_t i=0;i<64;++i) fp << '_'; fp << '\n';
+                    }
+
+                    inline void export_prolog( ) const
+                    {
+                        fp << "class " << class_name << " : public " << class_name_base << " {\n";
+                        fp << "public:\n";
+                        fp << "\texplicit " << class_name << "();          //!< implemented by user\n";
+                        fp << "\tvirtual ~" << class_name << "() throw() ; //!< implemented by user\n";
+                    }
+
+
+#define YWK_LABEL(NAME) case NAME ::UUID: fp << #NAME " : '" << r->label << "'"; break
+
+                    inline void export_rules() const
+                    {
+                        fp << "private:\n";
+                        for(const_iterator i=begin();i!=end();++i)
+                        {
+                            const wrule &wr = *i;
+                            const rule  *r  = wr.pRule;
+                            hline();
+                            com();
+                            com();
+                            switch(r->uuid)
+                            {
+                                    YWK_LABEL(terminal);
+                                    YWK_LABEL(optional);
+                                    YWK_LABEL(at_least);
+                                //case alternate::UUID: break;
+                                    YMK_LABEL(alternate);
+                                    //YMK_LABEL(aggregate);
+
+
+
+                                default:
+                                    throw exception("unrecognized rule uuid!");
+                            }
+                            hline();
+
+                        }
+                    }
+
+
+
+                    inline void prepare_public_api() const
+                    {
+                        fp << "public:\n";
+                        hline();
+                        com() << "\n";
+                        com() << "user's data\n";
+                        hline();
+                    }
 
                 private:
                     YOCTO_DISABLE_COPY_AND_ASSIGN(wmaker);
@@ -301,21 +275,27 @@ namespace yocto
             void grammar:: walker_prolog(ios::ostream &fp,
                                          const string &class_name) const
             {
-
-                wmaker wmk( rules.size, class_name);
-                for(const rule *r = rules.head;r;r=r->next)
-                {
-                    wmk.add(r);
-                }
-
-                wmk.export_base(fp);
+                
+                wmaker wmk( rules.size, class_name, fp);
+                wmk.add(rules);
+                wmk.export_base();
+                wmk.export_prolog();
+                wmk.export_rules();
+                wmk.prepare_public_api();
+                
             }
-
+            
             
             void grammar:: walker_epilog(ios::ostream &fp,
                                          const string &class_name) const
             {
-
+                //______________________________________________________________
+                //
+                // epilog
+                //______________________________________________________________
+                fp << "private:\n";
+                fp << "\tYOCTO_DISABLE_COPY_AND_ASSIGN(" << class_name << ");\n";
+                fp << "};\n\n";
             }
             
         }
