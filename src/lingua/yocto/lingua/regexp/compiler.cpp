c1fa780cac6fe04170604cd982d36cf2a707cd4e
ybouret <yann.bouret@gmail.com>
2016-04-05 16:17:06 +0200

    WIP lingua regexp compiler

---
+++ src/lingua/yocto/lingua/regexp/compiler.cpp
@@ -0,0 +1,13 @@
+#include "yocto/lingua/regexp/compiler.hpp"
+
+namespace yocto
+{
+    namespace lingua
+    {
+        pattern * regexp::compile(const string &expr,const p_dict *dict)
+        {
+            return 0;
+        }
+    }
+
+}
---
+++ src/lingua/yocto/lingua/regexp/compiler.hpp
@@ -0,0 +1,17 @@
+#ifndef YOCTO_LINGUA_REGEXP_COMPILER_INCLUDED
+#define YOCTO_LINGUA_REGEXP_COMPILER_INCLUDED 1
+
+#include "yocto/lingua/pattern/dict.hpp"
+
+namespace yocto
+{
+    namespace lingua
+    {
+        struct regexp
+        {
+            static pattern *compile(const string &expr,const p_dict *dict=0);
+        };
+    }
+}
+
+#endif
