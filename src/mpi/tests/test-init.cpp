b99c5af149e4cb128432429bc985f57c2074cc37
ybouret <yann.bouret@gmail.com>
2016-04-25 16:05:14 +0200

    need to test mpi data types with type glossary...

--- src/mpi/tests/test-init.cpp
+++ src/mpi/tests/test-init.cpp
@@ -1,6 +1,7 @@
 #include "yocto/utest/run.hpp"
 
 #include "yocto/mpi/mpi.hpp"
+#include "yocto/associative/type-glossary.hpp"
 
 using namespace yocto;
 
@@ -11,7 +12,12 @@ YOCTO_UNIT_TEST_IMPL(init)
     MPI.CloseStdIO();
     MPI.Printf(stderr, "'%s' is ready\n",MPI.ProcessorName);
 	MPI.Printf0(stderr, "ThreadLevel= %s\n", MPI.ThreadLevelName());
+
+    type_glossary<MPI_Datatype> tg;
+    tg.enroll<char>(MPI_CHAR);
+    tg.enroll<int8_t>(MPI_INT8_T);
     
+
 }
 
 YOCTO_UNIT_TEST_DONE()
