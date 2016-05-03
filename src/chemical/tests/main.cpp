ff31dbf8315827d9b3198adaddeb9d58b7abd6f2
ybouret <yann.bouret@gmail.com>
2016-05-03 09:59:23 +0200

    WIP clean

--- src/chemical/tests/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "yocto/utest/driver.hpp"
-
-YOCTO_UNIT_TEST_INIT(16)
-{
-    YOCTO_UNIT_TEST_DECL(species);
-    YOCTO_UNIT_TEST_DECL(library);
-    YOCTO_UNIT_TEST_DECL(gram);
-}
-YOCTO_UNIT_TEST_EXEC()
