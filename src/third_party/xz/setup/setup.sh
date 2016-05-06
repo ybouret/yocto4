c613ebd8ab39d6b949c749f73187aac1e229ede1
ybouret <yann.bouret@gmail.com>
2016-05-06 11:23:17 +0200

    xz setup.sh

--- src/third_party/xz/setup/setup.sh
+++ src/third_party/xz/setup/setup.sh
@@ -1,13 +1,24 @@
 VERSION=-5.2.2
 ARCHIVE=xz$VERSION.tar.gz
 WORKDIR=xz$VERSION
+OUTDIR=sandbox
+OUTSRC=$OUTDIR/src
+LIBXZA=$OUTDIR/lib/liblzma.a
 
 echo "-- Fetching $ARCHIVE"
 test -f $ARCHIVE || (  wget http://tukaani.org/xz/$ARCHIVE -O $ARCHIVE ) 
 echo "-- Extracting $ARCHIVE"
 test -d $WORKDIR || ( tar xfvz $ARCHIVE)
-echo "-- Configuring..."
-cd $WORKDIR && ./configure --prefix=`pwd`/../sandbox --disable-threads --disable-xz --disable-xzdec --disable-lzmadec --disable-lzmainfo --disable-lzma-links --disable-scripts --disable-doc --disable-shared --disable-nls --disable-rpath && make install
-
-
+echo "-- Configure and Build"
+test -f $LIBXZA || (cd $WORKDIR && ./configure --prefix=`pwd`/../$OUTDIR --disable-threads --disable-xz --disable-xzdec --disable-lzmadec --disable-lzmainfo --disable-lzma-links --disable-scripts --disable-doc --disable-shared --disable-nls --disable-rpath && make -s install)
+SOURCE_FILES=`ar -t $LIBXZA | grep liblzma | sed -e 's/liblzma_la-//' | sed -e 's/\.o/\.c/'`
+mkdir -p $OUTSRC
+for s in $SOURCE_FILES;
+do
+	src=`find $WORKDIR -name $s`
+	cp -f $src $OUTSRC	
+	srcdir=`dirname $src`
+	cp -f $srcdir/*.h $OUTSRC
+done
+cp -f $WORKDIR/config.h $OUTSRC
 
