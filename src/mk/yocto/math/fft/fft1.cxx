ac4ba3a877f27b2597c7b828df607f982a748f7a
ybouret <yann.bouret@gmail.com>
2016-05-06 09:19:24 +0200

    some fft...

--- src/mk/yocto/math/fft/fft1.cxx
+++ src/mk/yocto/math/fft/fft1.cxx
@@ -7,96 +7,131 @@
 
 namespace yocto
 {
-	namespace math
-	{
-		
-		
-		template <>
-		void Fourier<real_t>:: Transform(real_t      *data,
-										 const size_t size, 
-										 const int    isign
-										 ) throw()
-		{
-			assert( data != NULL );
-			assert( is_a_power_of_two(size) );
-			assert( isign==1 || isign==-1);
-			--data;
-			
-			//==================================================================
-			// bit reversal algorithm
-			//==================================================================
-			const size_t n    = size << 1; 
-			const real_t coef = REAL(1.0)/ Sqrt(  real_t(size) ); 
+    namespace math
+    {
 
-#if 0
-            {
-				size_t j=1;
-				for (size_t i=1;i<n;i+=2) 
-				{ 
-					if(j>i) 
-					{ 						
-                        core::bswap<2*sizeof(real_t)>( data+i, data+j);
-					}
-					size_t m = size; // m=  n / 2;
-					while( m>=2 && j>m )
-					{
-						j -= m;
-						m >>= 1;
-					}
-					j += m;
-				}
-			}
-#else
+
+        template <>
+        void Fourier<real_t>:: Transform(real_t      *data,
+                                         const size_t size,
+                                         const int    isign
+                                         ) throw()
+        {
+            assert( data != NULL );
+            assert( is_a_power_of_two(size) );
+            assert( isign==1 || isign==-1);
+            --data;
+
+            //==================================================================
+            // bit reversal algorithm
+            //==================================================================
+            const size_t n    = size << 1;
+            const real_t coef = REAL(1.0)/ Sqrt(  real_t(size) );
             xbitrev::run(data,size);
-#endif
-			
-			//==================================================================
-			// Lanczos-Algorithm
-			//==================================================================
-			{
-				const real_t sgn_two_pi = isign * numeric<real_t>::two_pi;
-				size_t       mmax       = 2;
-				while (n > mmax) 
-				{ 
-					const size_t istep = mmax << 1;
-					const real_t theta = sgn_two_pi/mmax; 
-					real_t wtemp       = Sin(REAL(0.5)*theta);
-					real_t wpr         = -REAL(2.0)*wtemp*wtemp;
-					real_t wpi         = Sin(theta);
-					real_t wr          = REAL(1.0);
-					real_t wi          = REAL(0.0);
-					
-					for (size_t m=1;m<mmax;m+=2) 
-					{ 
-						for (size_t i=m;i<=n;i+=istep) 
-						{
-							real_t      *d_i   = data+i;
-							size_t       j     = i+mmax;
-							real_t      *d_j   = data+j;
-							const real_t tempr = wr*d_j[0]-wi*d_j[1];
-							const real_t tempi = wr*d_j[1]+wi*d_j[0];
-							
-							d_j[0]  = d_i[0]-tempr;
-							d_j[1]  = d_i[1]-tempi;
-							d_i[0] += tempr;
-							d_i[1] += tempi;
-						}
-						wr=(wtemp=wr)*wpr-wi*wpi+wr; 
-						wi=wi*wpr+wtemp*wpi+wi;
-					}
-					mmax=istep;
-				}
-			}
-			
-			//==================================================================
-			// Normalizing
-			//==================================================================
+            
+            //==================================================================
+            // Lanczos-Algorithm
+            //==================================================================
+            {
+                const real_t sgn_two_pi = isign * numeric<real_t>::two_pi;
+                size_t       mmax       = 2;
+                while (n > mmax)
+                {
+                    const size_t istep = mmax << 1;
+                    const real_t theta = sgn_two_pi/mmax;
+                    real_t wtemp       = Sin(REAL(0.5)*theta);
+                    real_t wpr         = -REAL(2.0)*wtemp*wtemp;
+                    real_t wpi         = Sin(theta);
+                    real_t wr          = REAL(1.0);
+                    real_t wi          = REAL(0.0);
+
+                    for (size_t m=1;m<mmax;m+=2)
+                    {
+                        for (size_t i=m;i<=n;i+=istep)
+                        {
+                            real_t      *d_i   = data+i;
+                            size_t       j     = i+mmax;
+                            real_t      *d_j   = data+j;
+                            const real_t tempr = wr*d_j[0]-wi*d_j[1];
+                            const real_t tempi = wr*d_j[1]+wi*d_j[0];
+
+                            d_j[0]  = d_i[0]-tempr;
+                            d_j[1]  = d_i[1]-tempi;
+                            d_i[0] += tempr;
+                            d_i[1] += tempi;
+                        }
+                        wr=(wtemp=wr)*wpr-wi*wpi+wr;
+                        wi=wi*wpr+wtemp*wpi+wi;
+                    }
+                    mmax=istep;
+                }
+            }
+            
+            //==================================================================
+            // Normalizing
+            //==================================================================
 #define _YOCTO_FFT_SCALE(INDEX) data[INDEX] *= coef
-			YOCTO_LOOP_FUNC_(n,_YOCTO_FFT_SCALE,1);
-		}
-		
-		
-		
-		
-	}
+            YOCTO_LOOP_FUNC_(n,_YOCTO_FFT_SCALE,1);
+        }
+        
+        
+        template <>
+        void Fourier<real_t>::  Transform( const real_t *data1, const real_t *data2, real_t *fft1, real_t *fft2, const size_t n) throw()
+        {
+            assert( data1 != NULL );
+            assert( data2 != NULL );
+            assert( fft1  != NULL );
+            assert( fft2  != NULL );
+            
+            assert( is_a_power_of_two(n) );
+            //unsigned long nn3,nn2;
+            //nn3=1+(nn2=2+n+n);
+
+            //__________________________________________________________________
+            //
+            // packing real data in fft1
+            //__________________________________________________________________
+            for(size_t j=0,jj=1;j<n;j++,jj+=2)
+            {
+                fft1[jj-1]=data1[j];
+                fft1[jj]  =data2[j];
+            }
+
+            //__________________________________________________________________
+            //
+            // forward transform complex fft1
+            //__________________________________________________________________
+            Transform(fft1,n,1);
+
+
+            //__________________________________________________________________
+            //
+            // decompose fourier transforms
+            //__________________________________________________________________
+            const size_t np1 = n+1;
+            const size_t nn2 = np1<<1;
+            const size_t nn3 = nn2+1;
+            --fft1;
+            --fft2;
+            fft2[1]=fft1[2];
+            fft1[2]=fft2[2]=REAL(0.0);
+            for(size_t j=3;j<=np1;j+=2)
+            {
+                const size_t jp1= j+1;
+                const real_t rep=REAL(0.5)*(fft1[j]   + fft1[nn2-j]);
+                const real_t rem=REAL(0.5)*(fft1[j]   - fft1[nn2-j]);
+                const real_t aip=REAL(0.5)*(fft1[jp1] + fft1[nn3-j]);
+                const real_t aim=REAL(0.5)*(fft1[jp1] - fft1[nn3-j]);
+                fft1[j]     =  rep;
+                fft1[jp1]   =  aim;
+                fft1[nn2-j] =  rep;
+                fft1[nn3-j] = -aim;
+                fft2[j]     =  aip;
+                fft2[jp1]   = -rem;
+                fft2[nn2-j] =  aip;
+                fft2[nn3-j] =  rem;
+            }
+        }
+        
+    }
 }
--- src/mk/yocto/math/fft/fft1.hpp
+++ src/mk/yocto/math/fft/fft1.hpp
@@ -32,15 +32,17 @@ namespace yocto
 				Transform(data,size,-1);
 			}
 			
-			
+
+            //! FFT of two real data simultaneously: data1[0..n-1], data2[0..n-1], fft1[0..2n-1], fft2[0..2n-2]
+            static void Transform( const T *data1, const T *data2, T *fft1, T *fft2, const size_t n) throw();
 		};
 		
 		
 		void FFT(  complex<float>  *data, size_t size ) throw(); //!< Fourier Transform of data[0..size-1]
 		void iFFT( complex<float>  *data, size_t size ) throw(); //!< Inverse Fourier Transform of data[0..size-1]
 		void FFT(  complex<double> *data, size_t size ) throw(); //!< Fourier Transform of data[0..size-1]
-		void iFFT( complex<double> *data, size_t size ) throw(); //!< Inverse Fourier Transform of data[0..size-1]
-			
+        void iFFT( complex<double> *data, size_t size ) throw(); //!< Inverse Fourier Transform of data[0..size-1]
+        
 	}
 	
 }
