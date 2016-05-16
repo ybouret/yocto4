3969b8bb3b8ba2ae230eeb64adc65d7e7dc30fa4
ybouret <yann.bouret@gmail.com>
2016-05-16 21:12:55 +0200

    upgraded samples, need to understand...

--- samples/tiff/main.cpp
+++ samples/tiff/main.cpp
@@ -3,7 +3,8 @@
 
 int main(int argc, char *argv[])
 {
-    TIFF* tif = TIFFOpen("foo.tif", "r");
+    TIFF* tif = TIFFOpen("foo.tif", "w");
+    TIFFSetField(tif, TIFFTAG_COMPRESSION, COMPRESSION_JPEG);
     TIFFClose(tif);
 }
 
