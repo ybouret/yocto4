247dc371b46c12865c1e5e8e8793e7badc37c3ee
ybouret <yann.bouret@gmail.com>
2016-05-17 08:13:26 +0200

    default compression to LZW...

--- src/graphics/yocto/graphics/image/tiff++.cpp
+++ src/graphics/yocto/graphics/image/tiff++.cpp
@@ -9,7 +9,7 @@ namespace yocto
     namespace graphics
     {
         _TIFF:: _TIFF() throw() : handle(0) {}
-        
+
         _TIFF:: ~ _TIFF() throw()
         {
             assert(handle);
@@ -24,23 +24,23 @@ namespace yocto
 {
     namespace graphics
     {
-        
+
         _TIFF:: Raster:: ~Raster() throw()
         {
             cleanup();
         }
-        
+
         _TIFF:: Raster:: Raster() throw() :
         data(0),
         size(0)
         {
         }
-        
+
         void _TIFF:: Raster:: cleanup() throw()
         {
             memory::kind<memory::global>::release_as<uint32_t>(data,(size_t&)size);
         }
-        
+
         void _TIFF:: Raster:: startup(const size_t n)
         {
             if(n>size)
@@ -52,7 +52,7 @@ namespace yocto
         }
 
     }
-    
+
 }
 
 namespace yocto
@@ -66,18 +66,18 @@ namespace yocto
             handle = TIFFOpen(filename.c_str(),"r");
             if(!handle) throw imported::exception("TIFFOpen","couldn't open '%s'", filename.c_str());
         }
-        
+
         I_TIFF:: ~I_TIFF() throw()
         {
         }
-        
-             
+
+
         bool I_TIFF:: ReadDirectory()
         {
             assert(handle);
             return (1 == TIFFReadDirectory( (TIFF *)handle ));
         }
-        
+
         int  I_TIFF:: GetWidth()
         {
             uint32_t w = 0;
@@ -87,7 +87,7 @@ namespace yocto
             }
             return int(w);
         }
-        
+
         int  I_TIFF:: GetHeight()
         {
             uint32_t h = 0;
@@ -97,7 +97,7 @@ namespace yocto
             }
             return int(h);
         }
-        
+
         void I_TIFF:: SetDirectory(const size_t n)
         {
             if( 1 != TIFFSetDirectory((TIFF *)handle, n) )
@@ -105,27 +105,27 @@ namespace yocto
                 throw imported::exception("TIFFSetDirectory", "couldn't set %u", unsigned(n));
             }
         }
-        
+
         size_t I_TIFF::CountDirectories()
         {
             size_t count = 0;
             do { ++count; } while( ReadDirectory() );
             return count;
         }
-        
+
         void I_TIFF:: ReadRBGAImage(Raster &raster)
         {
             const int    w   = GetWidth();
             const int    h   = GetHeight();
             raster.startup(w*h);
-            
+
             if( 1 != TIFFReadRGBAImage((TIFF*)handle, w, h, raster.data) )
             {
                 throw imported::exception("TIFFReadRGBAImage","failure...");
             }
         }
-        
-        
+
+
     }
 }
 
@@ -137,11 +137,11 @@ namespace yocto
 {
     namespace graphics
     {
-        
+
         O_TIFF :: ~O_TIFF() throw()
         {
         }
-        
+
         O_TIFF:: O_TIFF(const string &filename) :
         _TIFF()
         {
@@ -150,20 +150,20 @@ namespace yocto
             TIFF *out = (TIFF *)handle;
             //TIFFSetField (out, TIFFTAG_IMAGEWIDTH, width);  // set the width of the image
             //TIFFSetField(out, TIFFTAG_IMAGELENGTH, height);    // set the height of the image
-            
+
             TIFFSetField(out, TIFFTAG_SAMPLESPERPIXEL, samples_per_pixel);   // set number of channels per pixel
             TIFFSetField(out, TIFFTAG_BITSPERSAMPLE,   8);    // set the size of the channels
             TIFFSetField(out, TIFFTAG_ORIENTATION, ORIENTATION_TOPLEFT);    // set the origin of the image.
-            
+
             //   Some other essential fields to set
             TIFFSetField(out, TIFFTAG_PLANARCONFIG, PLANARCONFIG_CONTIG);
             TIFFSetField(out, TIFFTAG_PHOTOMETRIC,  PHOTOMETRIC_RGB);
-            
+
             // Initially: no compression
-            TIFFSetField(out, TIFFTAG_COMPRESSION, COMPRESSION_NONE);
-            
+            TIFFSetField(out, TIFFTAG_COMPRESSION, COMPRESSION_LZW);
+
         }
-        
+
 #define O_TIFF_DEFINE(NAME) { #NAME, COMPRESSION_##NAME }
         const O_TIFF::CompressionType
         O_TIFF::NamedCompression[] =
@@ -177,7 +177,7 @@ namespace yocto
             O_TIFF_DEFINE(LZMA),
             {NULL,0}
         };
-        
+
         void O_TIFF:: SetCompression(const char *cmp)
         {
             unsigned flag = COMPRESSION_NONE;
@@ -196,18 +196,20 @@ namespace yocto
             }
             TIFF *out = (TIFF *)handle;
             TIFFSetField(out, TIFFTAG_COMPRESSION, flag);
+#if 0
             switch(flag)
             {
                 case COMPRESSION_LZW:
                     //TIFFSetField(out, TIFFTAG_PREDICTOR, PREDICTOR_HORIZONTAL);
                     break;
-                    
-            default:
-                TIFFSetField(out, TIFFTAG_PREDICTOR, PREDICTOR_NONE);
+
+                default:
+                    ;//TIFFSetField(out, TIFFTAG_PREDICTOR, PREDICTOR_NONE);
             }
-            
+#endif
+
         }
-        
+
         void O_TIFF:: WriteRGBAImage(const Raster &raster, const int w, const int h)
         {
             assert(w*h<=raster.size);
@@ -221,10 +223,10 @@ namespace yocto
             const size_t out_scanline = TIFFScanlineSize(out);
             memory::buffer_of<char,memory::global> tmpbuf( max_of(usr_scanline,out_scanline) );
             unsigned char *buf = static_cast<unsigned char *>(tmpbuf.rw());
-            
+
             // We set the strip size of the file to be size of one row of pixels
             TIFFSetField(out, TIFFTAG_ROWSPERSTRIP, TIFFDefaultStripSize(out,usr_scanline) );
-            
+
             //Now writing image to the file one strip at a time
             for(int row=0,top=h; row<h; ++row)
             {
@@ -233,8 +235,11 @@ namespace yocto
                 if (1 != TIFFWriteScanline(out, buf, row, 0) )
                     break;
             }
-        }
 
+            TIFFCheckpointDirectory(out);
+            TIFFWriteDirectory(out);
+        }
+        
         
     }
     
