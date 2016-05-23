5ef171809eafbbcb94c23c66142703ac2fdb5fa2
ybouret <yann.bouret@gmail.com>
2016-05-23 11:51:49 +0200

    WIP dynstack...

--- src/main/tests/test-dynstack.cpp
+++ src/main/tests/test-dynstack.cpp
@@ -1,9 +1,11 @@
 #include "yocto/stock/dynstack.hpp"
 #include "yocto/utest/run.hpp"
 
+using namespace yocto;
 
 YOCTO_UNIT_TEST_IMPL(dynstack)
 {
-    
+    dynstack_of<double> ds;
+
 }
 YOCTO_UNIT_TEST_DONE()
--- src/main/yocto/stock/dynstack.hpp
+++ src/main/yocto/stock/dynstack.hpp
@@ -3,65 +3,142 @@
 
 #include "yocto/ptr/shared.hpp"
 #include "yocto/memory/global.hpp"
+#include "yocto/core/pool.hpp"
+#include "yocto/core/list.hpp"
+#include "yocto/object.hpp"
+
 namespace yocto
 {
-    template <typename T>
+
     class dynstack
     {
     public:
-        inline virtual ~dynstack() throw()
+        struct frame_t
+        {
+            frame_t  *next;
+            frame_t  *prev;
+            ptrdiff_t bp;
+            ptrdiff_t tp;
+        };
+
+        virtual ~dynstack() throw()
+        {
+            while(hist.size) { object::release1(hist.pop_back()); }
+            while(pool.size) { object::release1(pool.query()); }
+        }
+
+
+    protected:
+        explicit dynstack() throw() : hist(), pool() {}
+        core::list_of<frame_t> hist;
+        core::pool_of<frame_t> pool;
+
+        void clean_history() throw()
+        {
+            while(hist.size) pool.store( hist.pop_back() );
+        }
+
+        dynstack(const dynstack &other) :
+        hist(),
+        pool()
+        {
+            for(const frame_t *f = other.hist.head;f;f=f->next)
+            {
+                frame_t *fc = object::acquire1<frame_t>();
+                fc->bp = f->bp;
+                fc->tp = f->tp;
+                hist.push_back(fc);
+            }
+        }
+
+
+
+
+    private:
+        YOCTO_DISABLE_ASSIGN(dynstack);
+    };
+
+    template <typename T>
+    class dynstack_of : public dynstack
+    {
+    public:
+
+        inline virtual ~dynstack_of() throw()
         {
             __release();
         }
 
-        inline dynstack() throw() :
-        size_(0),
+        inline dynstack_of() throw() :
+        dynstack(),
+        items(0),
         maxi_(0),
-        addr_(0)
+        addr_(0),
+        base_(0),
+        last_(base_+1)
         {}
 
-        inline dynstack(size_t n, const as_capacity_t &) :
-        size_(0),
+        inline dynstack_of(size_t n, const as_capacity_t &) :
+        dynstack(),
+        items(0),
         maxi_(0),
-        addr_( memory::kind<memory::global>::acquire_as<slot_type>(maxi_) )
+        addr_( memory::kind<memory::global>::acquire_as<slot_type>(maxi_) ),
+        base_(addr_),
+        last_(base_+1)
         {
         }
 
-        inline dynstack(const dynstack &other) :
-        size_(0),
+        inline dynstack_of(const dynstack_of &other) :
+        dynstack(other),
+        items(0),
         maxi_(other.size_),
-        addr_( memory::kind<memory::global>::acquire_as<slot_type>(maxi_) )
+        addr_( memory::kind<memory::global>::acquire_as<slot_type>(maxi_) ),
+        base_(addr_),
+        last_(base_+1)
         {
             __copy(other);
         }
 
-        inline dynstack(const dynstack &other, const size_t n) :
-        size_(0),
-        maxi_(other.size_+n),
-        addr_(memory::kind<memory::global>::acquire_as<slot_type>(maxi_) )
+        inline dynstack_of(const dynstack_of &other, const size_t n) :
+        items(0),
+        maxi_(other.items+n),
+        addr_(memory::kind<memory::global>::acquire_as<slot_type>(maxi_) ),
+        base_(addr_),
+        last_(base_+1)
         {
             __copy(other);
         }
 
-        
+        //! #objects in current frame
+        inline size_t size() const throw()
+        {
+            assert(last_>base_);
+            assert(base_>=addr_);
+            return size_t(static_cast<ptrdiff_t>(last_-base_)-1);
+        }
+
+
 
 
 
     private:
         typedef shared_ptr<T> slot_type;
-        size_t     size_;
+        size_t     items;
         size_t     maxi_;
         slot_type *addr_;
+        slot_type *base_;
+        slot_type *last_;
 
-        YOCTO_DISABLE_ASSIGN(dynstack);
+        YOCTO_DISABLE_ASSIGN(dynstack_of);
 
         // free all slots
         inline void __free() throw()
         {
-            while(size_>0)
+            while(items>0)
             {
-                destruct<slot_type>(&addr_[--size_]);
+                destruct<slot_type>(&addr_[--items]);
             }
+            base_ = last_ = addr_;
+            clean_history();
         }
 
         // free slots and release memory
@@ -69,18 +146,22 @@ namespace yocto
         {
             __free();
             memory::kind<memory::global>::release_as<slot_type>(addr_, maxi_);
+            base_ = last_ = 0;
         }
 
         // copy slots => reference
-        inline void __copy(const dynstack &other) throw()
+        inline void __copy(const dynstack_of &other) throw()
         {
             assert(this!=&other);
-            assert(0==size_);
-            assert(maxi_>=other.size_);
-            while(size_<other.size_)
+            assert(0==items);
+            assert(maxi_>=other.items);
+            while(items<other.items)
             {
-                new ( &addr_[size_] ) slot_type(other.addr_[size_]);
+                new ( &addr_[items] ) slot_type(other.addr_[items]);
+                ++items;
             }
+            base_ = addr_+static_cast<ptrdiff_t>(other.base_-other.addr_);
+            last_ = addr_+static_cast<ptrdiff_t>(other.last_-other.addr_);
         }
 
     };
