8a2f732e79fa204277c2f7eeb68d52341c93b365
ybouret <yann.bouret@gmail.com>
2016-06-23 10:24:07 +0200

    basic color mix

---
+++ src/graphics/yocto/graphics/ops/mix.hpp
@@ -0,0 +1,37 @@
+#ifndef YOCTO_OPS_MIX_INCLUDED
+#define YOCTO_OPS_MIX_INCLUDED 1
+
+#include "yocto/graphics/rgb.hpp"
+
+namespace yocto
+{
+    namespace graphics
+    {
+
+        struct mix
+        {
+            //! approx (X*(255-alpha)+Y*(alpha))/255
+            static inline
+            uint8_t mix(const uint8_t X, const uint8_t Y, const uint8_t alpha) throw()
+            {
+                return (unsigned(X)*unsigned(255-alpha)+unsigned(Y)*unsigned(alpha))/255;
+            }
+
+            static inline
+            RGB mix(const RGB &X, const RGB &Y, const uint8_t alpha) throw()
+            {
+                const unsigned WY(alpha);
+                const unsigned WX(255-alpha);
+                return RGB(
+                           (unsigned(X.r)*WX + unsigned(Y.r)*WY)/255,
+                           (unsigned(X.g)*WX + unsigned(Y.g)*WY)/255,
+                           (unsigned(X.b)*WX + unsigned(Y.b)*WY)/255,
+                );
+            }
+
+        };
+
+    }
+}
+
+#endif
