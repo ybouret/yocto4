f0372221d1fba1da9fdb8870dd8fb9ecf53566a1
ybouret <yann.bouret@gmail.com>
2016-06-23 10:26:14 +0200

    bug corrected

--- src/graphics/yocto/graphics/ops/mix.hpp
+++ src/graphics/yocto/graphics/ops/mix.hpp
@@ -12,20 +12,20 @@ namespace yocto
         {
             //! approx (X*(255-alpha)+Y*(alpha))/255
             static inline
-            uint8_t mix(const uint8_t X, const uint8_t Y, const uint8_t alpha) throw()
+            uint8_t blend(const uint8_t X, const uint8_t Y, const uint8_t alpha) throw()
             {
                 return (unsigned(X)*unsigned(255-alpha)+unsigned(Y)*unsigned(alpha))/255;
             }
 
             static inline
-            RGB mix(const RGB &X, const RGB &Y, const uint8_t alpha) throw()
+            RGB blend(const RGB &X, const RGB &Y, const uint8_t alpha) throw()
             {
                 const unsigned WY(alpha);
                 const unsigned WX(255-alpha);
                 return RGB(
                            (unsigned(X.r)*WX + unsigned(Y.r)*WY)/255,
                            (unsigned(X.g)*WX + unsigned(Y.g)*WY)/255,
-                           (unsigned(X.b)*WX + unsigned(Y.b)*WY)/255,
+                           (unsigned(X.b)*WX + unsigned(Y.b)*WY)/255
                 );
             }
 
