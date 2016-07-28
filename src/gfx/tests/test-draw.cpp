91410f719d92e59659abfdb55a3491769de3b9e2
ybouret <yann.bouret@gmail.com>
2016-07-28 10:50:49 +0200

    basic draw line...

---
+++ src/gfx/tests/test-draw.cpp
@@ -0,0 +1,46 @@
+#include "yocto/gfx/image.hpp"
+#include "yocto/gfx/image/jpeg.hpp"
+#include "yocto/gfx/image/png.hpp"
+#include "yocto/gfx/image/tiff.hpp"
+
+#include "yocto/gfx/ops/geometry.hpp"
+#include "yocto/gfx/draw/line.hpp"
+#include "yocto/utest/run.hpp"
+#include "yocto/math/trigconv.hpp"
+#include "yocto/gfx/color/named-colors.hpp"
+#include "yocto/code/rand.hpp"
+
+using namespace yocto;
+using namespace gfx;
+
+YOCTO_UNIT_TEST_IMPL(draw)
+{
+    YOCTO_GFX_DECL_FORMAT(jpeg);
+    YOCTO_GFX_DECL_FORMAT(png);
+    YOCTO_GFX_DECL_FORMAT(tiff);
+
+    imageIO          &IMG = image::instance();
+
+
+    pixmap<RGB> img(800,600);
+
+
+    const unit_t w  = img.w;
+    const size_t dw = w+w;
+    const unit_t h  = img.h;
+    const size_t dh = h+h;
+
+    img.ldz();
+    for(size_t k=0;k<100;++k)
+    {
+        unit_t x0 = -w + unit_t( alea_leq(dw) );
+        unit_t x1 = -w + unit_t( alea_leq(dw) );
+        unit_t y0 = -h + unit_t( alea_leq(dh) );
+        unit_t y1 = -h + unit_t( alea_leq(dh) );
+        draw_line(img,x0,y0,x1,y1, named_color::fetch(alea_leq(named_color::count)),128+alea_leq(127));
+    }
+
+    IMG.save("draw.png",img,0);
+
+}
+YOCTO_UNIT_TEST_DONE()
---
+++ src/gfx/yocto/gfx/draw/line.hpp
@@ -0,0 +1,45 @@
+#ifndef YOCTO_GFX_DRAW_LINE_INCLUDED
+#define YOCTO_GFX_DRAW_LINE_INCLUDED 1
+
+#include "yocto/gfx/pixmap.hpp"
+#include "yocto/gfx/pixel.hpp"
+
+namespace yocto
+{
+    namespace gfx
+    {
+        template <typename T>
+        inline void draw_line(pixmap<T>    &img,
+                              unit_t        x0,
+                              unit_t        y0,
+                              unit_t        x1,
+                              unit_t        y1,
+                              const  T      C,
+                              const uint8_t alpha)
+        {
+            unit_t dx =  std::abs(x1-x0), sx = (x0<x1) ? 1 : -1;
+            unit_t dy = -std::abs(y1-y0), sy = (y0<y1) ? 1 : -1;
+            unit_t err = dx+dy, e2;                                   /* error value e_xy */
+
+            for (;;){                                                          /* loop */
+                //setPixel(x0,y0);
+                const vertex p(x0,y0);
+                if(img.has(p))
+                {
+                    img[p] = pixel<T>::blend(img[p],C,alpha);
+                }
+                e2 = 2*err;
+                if (e2 >= dy) {                                         /* e_xy+e_x > 0 */
+                    if (x0 == x1) break;
+                    err += dy; x0 += sx;
+                }
+                if (e2 <= dx) {                                         /* e_xy+e_y < 0 */
+                    if (y0 == y1) break;
+                    err += dx; y0 += sy;
+                }
+            }
+        }
+    }
+}
+
+#endif
